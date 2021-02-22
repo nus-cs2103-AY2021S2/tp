0e0fad50c4a1fe2cb9e9df77428f75924b5ec039
Tan Jin <cjtanjin@gmail.com>
2021-02-22 22:01:49 +0800

    Chore: add checks for failed branch switch and merge

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -5,10 +5,10 @@
 
 if [[ $1 == "" ]];
 then
-	echo "Branch to sync defaulted to master."
-	BRANCH_TO_SYNC="master"
+    echo "Branch to sync defaulted to master."
+    BRANCH_TO_SYNC="master"
 else
-	BRANCH_TO_SYNC=$1
+    BRANCH_TO_SYNC=$1
 fi
 
 BRANCH_EXIST=$(git branch --list $BRANCH_TO_SYNC)
@@ -21,16 +21,20 @@ fi
 
 CURRENT_BRANCH=`git branch`
 
-if [[ $CURRENT_BRANCH != *"*	$BRANCH_TO_SYNC"* ]];
+if [[ $CURRENT_BRANCH != *"*    $BRANCH_TO_SYNC"* ]];
 then
-	echo "Switching to branch $BRANCH_TO_SYNC..."
-	git checkout $BRANCH_TO_SYNC
+    echo "Switching to branch $BRANCH_TO_SYNC..."
+    git checkout $BRANCH_TO_SYNC
+    if [ ! $? -eq 0 ]; then
+        echo "Unable to switch branch."
+        exit
+    fi
 fi
 
 REMOTE=`git remote -v`
 
-if [[ $REMOTE == *"upstream	https://github.com/AY2021S2-CS2103T-T12-4/tp.git (fetch)"*
-    && $REMOTE == *"upstream	https://github.com/AY2021S2-CS2103T-T12-4/tp.git (push)"* ]];
+if [[ $REMOTE == *"upstream https://github.com/AY2021S2-CS2103T-T12-4/tp.git (fetch)"*
+    && $REMOTE == *"upstream    https://github.com/AY2021S2-CS2103T-T12-4/tp.git (push)"* ]];
 then
     echo "Upstream found..."
 else
@@ -41,7 +45,11 @@ fi
 
 echo "Fetching upstream..."
 git fetch upstream
-echo "Merging upstream with master..."
+echo "Merging upstream master with local branch $BRANCH_TO_SYNC..."
 git merge upstream/master
+if [ ! $? -eq 0 ]; then
+    echo "Unable to merge branch."
+    exit
+fi
 echo "Pushing local master to remote repository..."
 git push
