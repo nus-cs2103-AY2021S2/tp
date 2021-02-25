934c2632274362fdd51e03e3453d8fd3b6206eaa
Nguyen Son Linh <linhns59@gmail.com>
2021-02-25 21:48:15 +0800

    Attempt to pass CI. Change checkstyle.xml to allow wildcard import

--- src/main/java/seedu/address/model/person/Remark.java
+++ src/main/java/seedu/address/model/person/Remark.java
@@ -1,11 +1,18 @@
 package seedu.address.model.person;
 
 import static java.util.Objects.requireNonNull;
-import static seedu.address.commons.util.AppUtil.checkArgument;
 
+/**
+ * Represents a Person's address in the address book.
+ */
 public class Remark {
     public final String value;
 
+    /**
+     * Constructs an {@code Remark}.
+     *
+     * @param remark A valid remark.
+     */
     public Remark(String remark) {
         requireNonNull(remark);
         value = remark;
