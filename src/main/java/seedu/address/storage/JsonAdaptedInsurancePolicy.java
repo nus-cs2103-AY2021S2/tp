052b68e2b8e2101a83662cf08e8ca338887b2760
Swa Yong Shen <yongshenswa@gmail.com>
2021-02-26 10:28:57 +0800

    Change storage to include InsurancePolicy

---
+++ src/main/java/seedu/address/storage/JsonAdaptedInsurancePolicy.java
@@ -0,0 +1,42 @@
+package seedu.address.storage;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
+import seedu.address.model.insurancepolicy.InsurancePolicy;
+
+/**
+ * Jackson-friendly version of {@link InsurancePolicy}.
+ */
+class JsonAdaptedInsurancePolicy {
+
+    private final String policyId;
+
+    /**
+     * Constructs a {@code JsonAdaptedInsurancePolicy} with the given {@code policyId}.
+     */
+    @JsonCreator
+    public JsonAdaptedInsurancePolicy(String policyId) {
+        this.policyId = policyId;
+    }
+
+    /**
+     * Converts a given {@code Tag} into this class for Jackson use.
+     */
+    public JsonAdaptedInsurancePolicy(InsurancePolicy source) {
+        policyId = source.policyId;
+    }
+
+    @JsonValue
+    public String getPolicyId() {
+        return policyId;
+    }
+
+    /**
+     * Converts this Jackson-friendly adapted InsurancePolicy object into the model's {@code InsurancePolicy} object.
+     */
+    public InsurancePolicy toModelType() {
+        return new InsurancePolicy(policyId);
+    }
+
+}
--- src/main/java/seedu/address/storage/JsonAdaptedPerson.java
+++ src/main/java/seedu/address/storage/JsonAdaptedPerson.java
@@ -9,7 +9,9 @@ import java.util.stream.Collectors;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import seedu.address.commons.core.index.Index;
 import seedu.address.commons.exceptions.IllegalValueException;
+import seedu.address.model.insurancepolicy.InsurancePolicy;
 import seedu.address.model.person.Address;
 import seedu.address.model.person.Email;
 import seedu.address.model.person.Name;
@@ -29,6 +31,7 @@ class JsonAdaptedPerson {
     private final String email;
     private final String address;
     private final List<JsonAdaptedTag> tagged = new ArrayList<>();
+    private final List<JsonAdaptedInsurancePolicy> policies = new ArrayList<>();
 
     /**
      * Constructs a {@code JsonAdaptedPerson} with the given person details.
@@ -36,13 +39,17 @@ class JsonAdaptedPerson {
     @JsonCreator
     public JsonAdaptedPerson(@JsonProperty("name") String name, @JsonProperty("phone") String phone,
             @JsonProperty("email") String email, @JsonProperty("address") String address,
-            @JsonProperty("tagged") List<JsonAdaptedTag> tagged) {
+            @JsonProperty("tagged") List<JsonAdaptedTag> tagged,
+            @JsonProperty("policies") List<JsonAdaptedInsurancePolicy> policies) {
         this.name = name;
         this.phone = phone;
         this.email = email;
         this.address = address;
         if (tagged != null) {
             this.tagged.addAll(tagged);
+        }
+        if (policies != null) {
+            this.policies.addAll(policies);
         }
     }
 
@@ -57,6 +64,9 @@ class JsonAdaptedPerson {
         tagged.addAll(source.getTags().stream()
                 .map(JsonAdaptedTag::new)
                 .collect(Collectors.toList()));
+        policies.addAll(source.getPolicies().stream()
+                .map(JsonAdaptedInsurancePolicy::new)
+                .collect(Collectors.toList()));
     }
 
     /**
@@ -68,6 +78,11 @@ class JsonAdaptedPerson {
         final List<Tag> personTags = new ArrayList<>();
         for (JsonAdaptedTag tag : tagged) {
             personTags.add(tag.toModelType());
+        }
+
+        final List<InsurancePolicy> personPolicies = new ArrayList<>();
+        for (JsonAdaptedInsurancePolicy policy : policies) {
+            personPolicies.add(policy.toModelType());
         }
 
         if (name == null) {
@@ -103,7 +118,10 @@ class JsonAdaptedPerson {
         final Address modelAddress = new Address(address);
 
         final Set<Tag> modelTags = new HashSet<>(personTags);
-        return new Person(modelName, modelPhone, modelEmail, modelAddress, modelTags);
+
+        final List<InsurancePolicy> modelPolicies = new ArrayList<>(personPolicies);
+
+        return new Person(modelName, modelPhone, modelEmail, modelAddress, modelTags, modelPolicies);
     }
 
 }
