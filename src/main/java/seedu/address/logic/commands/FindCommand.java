c5d9abb2ac3509995147f8434c4ceb16ab2879df
Jellybeano <lanyuxuan98@gmail.com>
2021-02-28 03:22:15 +0800

    Stage files part 2

--- src/main/java/seedu/address/logic/commands/FindCommand.java
+++ src/main/java/seedu/address/logic/commands/FindCommand.java
@@ -7,14 +7,14 @@ import seedu.address.model.Model;
 import seedu.address.model.person.QuestionContainsKeywordsPredicate;
 
 /**
- * Finds and lists all persons in address book whose questions contains any of the argument keywords.
+ * Finds and lists all flashcards in address book whose questions contains any of the argument keywords.
  * Keyword matching is case insensitive.
  */
 public class FindCommand extends Command {
 
     public static final String COMMAND_WORD = "find";
 
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds all persons whose questions contain any of "
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds all flashcards whose questions contain any of "
             + "the specified keywords (case-insensitive) and displays them as a list with index numbers.\n"
             + "Parameters: KEYWORD [MORE_KEYWORDS]...\n"
             + "Example: " + COMMAND_WORD + " alice bob charlie";
@@ -28,9 +28,9 @@ public class FindCommand extends Command {
     @Override
     public CommandResult execute(Model model) {
         requireNonNull(model);
-        model.updateFilteredPersonList(predicate);
+        model.updateFilteredFlashcardList(predicate);
         return new CommandResult(
-                String.format(Messages.MESSAGE_PERSONS_LISTED_OVERVIEW, model.getFilteredPersonList().size()));
+                String.format(Messages.MESSAGE_FLASHCARDS_LISTED_OVERVIEW, model.getFilteredFlashcardList().size()));
     }
 
     @Override
