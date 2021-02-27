272bc355abdec787a2df79d83ca1693feaeb1de2
Tan Jin <cjtanjin@gmail.com>
2021-02-27 18:51:45 +0800

    Refactor: tidy up ImposterParser imports

--- src/main/java/seedu/us/among/logic/parser/ImposterParser.java
+++ src/main/java/seedu/us/among/logic/parser/ImposterParser.java
@@ -1,9 +1,11 @@
 package seedu.us.among.logic.parser;
 
+import static seedu.us.among.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.us.among.commons.core.Messages.MESSAGE_UNKNOWN_COMMAND;
+
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import seedu.us.among.commons.core.Messages;
 import seedu.us.among.logic.commands.AddCommand;
 import seedu.us.among.logic.commands.ClearCommand;
 import seedu.us.among.logic.commands.Command;
@@ -36,7 +38,7 @@ public class ImposterParser {
     public Command parseCommand(String userInput) throws ParseException {
         final Matcher matcher = BASIC_COMMAND_FORMAT.matcher(userInput.trim());
         if (!matcher.matches()) {
-            throw new ParseException(String.format(Messages.MESSAGE_INVALID_COMMAND_FORMAT, HelpCommand.MESSAGE_USAGE));
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, HelpCommand.MESSAGE_USAGE));
         }
 
         final String commandWord = matcher.group("commandWord");
@@ -71,7 +73,7 @@ public class ImposterParser {
             return new SendCommandParser().parse(arguments);
 
         default:
-            throw new ParseException(Messages.MESSAGE_UNKNOWN_COMMAND);
+            throw new ParseException(MESSAGE_UNKNOWN_COMMAND);
         }
     }
 
