2f863ecdeaecc1c25ff6c8fa6720956f636842ae
XindiTian <xinditian@gmail.com>
2021-02-27 14:31:43 +0800

    Refactor package names and data files to Dictionote

---
+++ src/test/java/seedu/dictionote/logic/commands/ClearCommandTest.java
@@ -0,0 +1,32 @@
+package seedu.dictionote.logic.commands;
+
+import static seedu.dictionote.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.dictionote.testutil.TypicalPersons.getTypicalAddressBook;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.dictionote.model.AddressBook;
+import seedu.dictionote.model.Model;
+import seedu.dictionote.model.ModelManager;
+import seedu.dictionote.model.UserPrefs;
+
+public class ClearCommandTest {
+
+    @Test
+    public void execute_emptyAddressBook_success() {
+        Model model = new ModelManager();
+        Model expectedModel = new ModelManager();
+
+        assertCommandSuccess(new ClearCommand(), model, ClearCommand.MESSAGE_SUCCESS, expectedModel);
+    }
+
+    @Test
+    public void execute_nonEmptyAddressBook_success() {
+        Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+        Model expectedModel = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+        expectedModel.setAddressBook(new AddressBook());
+
+        assertCommandSuccess(new ClearCommand(), model, ClearCommand.MESSAGE_SUCCESS, expectedModel);
+    }
+
+}
