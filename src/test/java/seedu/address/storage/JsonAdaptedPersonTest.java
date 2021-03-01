6efd3769642e163676d43c33a210f4991316b865
CSjiade <jdlian505@gmail.com>
2021-03-02 01:16:30 +0800

    update tutorial-adding-command

--- src/test/java/seedu/address/storage/JsonAdaptedPersonTest.java
+++ src/test/java/seedu/address/storage/JsonAdaptedPersonTest.java
@@ -90,12 +90,11 @@ public class JsonAdaptedPersonTest {
 
     @Test
     public void toModelType_invalidAddress_throwsIllegalValueException() {
-        JsonAdaptedPerson person =
-                new JsonAdaptedPerson(VALID_NAME, VALID_PHONE, VALID_EMAIL,
-                        INVALID_ADDRESS, VALID_REMARK, VALID_TAGS);
+        JsonAdaptedPerson person = new JsonAdaptedPerson(VALID_NAME, VALID_PHONE, VALID_EMAIL,
+                INVALID_ADDRESS, VALID_REMARK, VALID_TAGS);
         String expectedMessage = Address.MESSAGE_CONSTRAINTS;
         assertThrows(IllegalValueException.class,
-        expectedMessage, person::toModelType);
+                expectedMessage, person::toModelType);
     }
 
     @Test
