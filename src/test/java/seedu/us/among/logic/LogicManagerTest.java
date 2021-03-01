fdd22fd15a6dc6585f4dc179edcbf8e08418d2f5
Juliet <teohqianying@gmail.com>
2021-03-01 21:07:17 +0800

    Fix test bug where delete was not changed to remove

--- src/test/java/seedu/us/among/logic/LogicManagerTest.java
+++ src/test/java/seedu/us/among/logic/LogicManagerTest.java
@@ -58,7 +58,7 @@ public class LogicManagerTest {
 
     @Test
     public void execute_commandExecutionError_throwsCommandException() {
-        String deleteCommand = "delete 9";
+        String deleteCommand = "remove 9";
         assertCommandException(deleteCommand, MESSAGE_INVALID_ENDPOINT_DISPLAYED_INDEX);
     }
 
