13448714efef4259ac9e6f21d848ae0639130221
Hzxin <E0424941@u.nus.edu>
2021-03-03 02:05:53 +0800

    Build Skeleton-Calendar

--- src/main/java/seedu/address/ui/CalendarWindow.java
+++ src/main/java/seedu/address/ui/CalendarWindow.java
@@ -1,16 +1,22 @@
 package seedu.address.ui;
 
+import java.time.LocalDate;
 import java.util.logging.Logger;
 
 import javafx.fxml.FXML;
-import javafx.scene.input.Clipboard;
-import javafx.scene.input.ClipboardContent;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 import seedu.address.commons.core.LogsCenter;
 
 public class CalendarWindow extends UiPart<Stage> {
     private static final Logger logger = LogsCenter.getLogger(CalendarWindow.class);
     private static final String FXML = "CalendarWindow.fxml";
+
+    @FXML
+    private Label currentMonth;
+    @FXML
+    private GridPane calendar;
 
     /**
      * Creates a new CalendarWindow.
@@ -21,12 +27,37 @@ public class CalendarWindow extends UiPart<Stage> {
         super(FXML, root);
     }
 
+
     /**
      * Creates a new CalendarWindow.
      */
     public CalendarWindow() {
-        this(new Stage());
+        super(FXML);
+        loadCalendar();
     }
+
+
+    private CalendarBox loadInfo(LocalDate startDateTime) {
+        return new CalendarBox(startDateTime);
+    }
+
+    private void loadCalendar() {
+        LocalDate today = LocalDate.now();
+        LocalDate currentDate = LocalDate.of(today.getYear(), today.getMonth(), 1);
+        for (int i = 2; i < 7; i++) {
+            for (int j = 0; j < 7; j++) {
+                CalendarBox calendarBox = loadInfo(currentDate);
+                calendar.add(calendarBox.getRoot(), j, i);
+
+                if (currentDate.getMonthValue() != LocalDate.now().getMonthValue()) {
+                    calendarBox.getRoot().setDisable(true);
+                    calendarBox.getRoot().setStyle("-fx-background-color: grey");
+                }
+                currentDate = currentDate.plusDays(1);
+            }
+        }
+    }
+
 
     /**
      * Shows the calendar window.
@@ -51,6 +82,7 @@ public class CalendarWindow extends UiPart<Stage> {
         getRoot().show();
         getRoot().centerOnScreen();
     }
+
 
     /**
      * Returns true if the calendar window is currently being shown.
