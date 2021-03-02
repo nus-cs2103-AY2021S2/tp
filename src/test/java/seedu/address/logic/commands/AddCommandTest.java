e36aa206cc6e080d655d65f0cdb82d58cf99bba4
Icelenaugust <dingyoujia0823@gmail.com>
2021-03-03 00:05:39 +0800

    Fix checkstyle

--- src/test/java/seedu/address/logic/commands/AddCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddCommandTest.java
@@ -1,25 +1,11 @@
 package seedu.address.logic.commands;
 
-import static java.util.Objects.requireNonNull;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.testutil.Assert.assertThrows;
 
-import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.function.Predicate;
-
 import org.junit.jupiter.api.Test;
 
-import javafx.collections.ObservableList;
-import seedu.address.commons.core.GuiSettings;
-import seedu.address.logic.commands.exceptions.CommandException;
-import seedu.address.model.AddressBook;
-import seedu.address.model.Model;
-import seedu.address.model.ReadOnlyAddressBook;
-import seedu.address.model.ReadOnlyUserPrefs;
 import seedu.address.model.person.Person;
 import seedu.address.testutil.PersonBuilder;
 
@@ -79,8 +65,8 @@ public class AddCommandTest {
 
 
     ///**
-     //* A default model stub that have all of the methods failing.
-     //*/
+    //* A default model stub that have all of the methods failing.
+    //*/
     /*
     private class ModelStub implements Model {
         @Override
@@ -157,8 +143,8 @@ public class AddCommandTest {
      */
 
     ///**
-     //* A Model stub that contains a single person.
-     //*/
+    //* A Model stub that contains a single person.
+    //*/
 
     /*
     private class ModelStubWithPerson extends ModelStub {
@@ -179,8 +165,8 @@ public class AddCommandTest {
      */
 
     ///**
-     //* A Model stub that always accept the person being added.
-     //*/
+    //* A Model stub that always accept the person being added.
+    //*/
 
     /*
     private class ModelStubAcceptingPersonAdded extends ModelStub {
