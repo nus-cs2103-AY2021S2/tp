00fc13dc090849f4ee28de2b97557eb093266d2d
w2vgd <junwei.green@gmail.com>
2021-03-03 12:02:25 +0800

    Add javadocs for property Name class

--- src/main/java/seedu/address/model/property/Name.java
+++ src/main/java/seedu/address/model/property/Name.java
@@ -3,10 +3,18 @@ package seedu.address.model.property;
 import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.util.AppUtil.checkArgument;
 
+/**
+ * Represents a Property's name.
+ * Guarantees: immutable; is valid as declared in {@link #isValidName(String)}
+ */
 public class Name {
     private static final String MESSAGE_CONSTRAINTS =
             "Property names should only contain alphanumeric characters and spaces, and it should not be blank";
 
+    /*
+     * The first character of the property name must not be a whitespace,
+     * otherwise " " (a blank string) becomes a valid input.
+     */
     private static final String VALIDATION_REGEX = "[\\p{Alnum}][\\p{Alnum} ]*";
 
     public final String propertyName;
@@ -17,6 +25,11 @@ public class Name {
         this.propertyName = propertyName;
     }
 
+    /**
+     * Returns true if a given string is a valid property name.
+     * @param test the string to test.
+     * @return true if the given string is a valid property name, otherwise false.
+     */
     public static boolean isValidName(String test) {
         return test.matches(VALIDATION_REGEX);
     }
