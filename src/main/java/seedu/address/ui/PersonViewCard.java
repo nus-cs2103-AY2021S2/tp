2865280047b4127e3502e81874726486f310b099
vuminhhieu <minhhieu7dams@gmail.com>
2021-03-05 00:52:03 +0800

    Add a new region in the UI

---
+++ src/main/java/seedu/address/ui/PersonViewCard.java
@@ -0,0 +1,11 @@
+package seedu.address.ui;
+
+import javafx.scene.layout.Region;
+
+public class PersonViewCard extends UiPart<Region> {
+    private static final String FXML = "PersonViewCard.fxml";
+
+    public PersonViewCard() {
+        super(FXML);
+    }
+}
