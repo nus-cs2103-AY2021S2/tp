0b92f7f8e3c18cf36d980982c918847c587bb80c
Colin <colintan97@gmail.com>
2021-03-05 20:46:48 +0800

    Add test for year

--- src/test/java/seedu/address/model/resident/YearTest.java
+++ src/test/java/seedu/address/model/resident/YearTest.java
@@ -6,35 +6,34 @@ import static seedu.address.testutil.Assert.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
-public class PhoneTest {
+public class YearTest {
 
     @Test
     public void constructor_null_throwsNullPointerException() {
-        assertThrows(NullPointerException.class, () -> new Phone(null));
+        assertThrows(NullPointerException.class, () -> new Year(null));
     }
 
     @Test
-    public void constructor_invalidPhone_throwsIllegalArgumentException() {
-        String invalidPhone = "";
-        assertThrows(IllegalArgumentException.class, () -> new Phone(invalidPhone));
+    public void constructor_invalidYear_throwsIllegalArgumentException() {
+        String invalidYear = "";
+        assertThrows(IllegalArgumentException.class, () -> new Year(invalidYear));
     }
 
     @Test
-    public void isValidPhone() {
-        // null phone number
-        assertThrows(NullPointerException.class, () -> Phone.isValidPhone(null));
+    public void isValidYear() {
+        // null year
+        assertThrows(NullPointerException.class, () -> Year.isValidYear(null));
 
-        // invalid phone numbers
-        assertFalse(Phone.isValidPhone("")); // empty string
-        assertFalse(Phone.isValidPhone(" ")); // spaces only
-        assertFalse(Phone.isValidPhone("91")); // less than 3 numbers
-        assertFalse(Phone.isValidPhone("phone")); // non-numeric
-        assertFalse(Phone.isValidPhone("9011p041")); // alphabets within digits
-        assertFalse(Phone.isValidPhone("9312 1534")); // spaces within digits
+        // invalid years
+        assertFalse(Year.isValidYear("")); // empty string
+        assertFalse(Year.isValidYear(" ")); // spaces only
+        assertFalse(Year.isValidYear("6")); // number out of range
+        assertFalse(Year.isValidYear("year")); // non-numeric
+        assertFalse(Year.isValidYear("Year3")); // alphabets with number
+        assertFalse(Year.isValidYear(" 3 ")); // alphabets with number
 
-        // valid phone numbers
-        assertTrue(Phone.isValidPhone("911")); // exactly 3 numbers
-        assertTrue(Phone.isValidPhone("93121534"));
-        assertTrue(Phone.isValidPhone("124293842033123")); // long phone numbers
+
+        // valid years
+        assertTrue(Year.isValidYear("4"));
     }
 }
