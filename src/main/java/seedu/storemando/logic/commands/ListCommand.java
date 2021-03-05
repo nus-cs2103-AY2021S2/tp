1ab3d75e31f5d4abeea45da6adeec7c67706a8c1
chewwh09 <weishawtyhao9@gmail.com>
2021-03-06 02:13:47 +0800

    Add predicate condition into list command

--- src/main/java/seedu/storemando/logic/commands/ListCommand.java
+++ src/main/java/seedu/storemando/logic/commands/ListCommand.java
@@ -1,9 +1,11 @@
 package seedu.storemando.logic.commands;
 
 import static java.util.Objects.requireNonNull;
-import static seedu.storemando.model.Model.PREDICATE_SHOW_ALL_ITEMS;
+
+import java.util.ArrayList;
 
 import seedu.storemando.model.Model;
+import seedu.storemando.model.item.LocationContainsKeywordsPredicate;
 
 /**
  * Lists all items in the storemando to the user.
@@ -14,11 +16,20 @@ public class ListCommand extends Command {
 
     public static final String MESSAGE_SUCCESS = "Listed all items";
 
+    private final LocationContainsKeywordsPredicate predicate;
+
+    public ListCommand(LocationContainsKeywordsPredicate predicate) {
+        this.predicate = predicate;
+    }
+
+    public ListCommand() {
+        this.predicate = new LocationContainsKeywordsPredicate(new ArrayList<>());
+    }
 
     @Override
     public CommandResult execute(Model model) {
         requireNonNull(model);
-        model.updateFilteredItemList(PREDICATE_SHOW_ALL_ITEMS);
-        return new CommandResult(MESSAGE_SUCCESS);
+        model.updateFilteredItemList(predicate);
+        return new CommandResult(MESSAGE_SUCCESS); // might need change
     }
 }
