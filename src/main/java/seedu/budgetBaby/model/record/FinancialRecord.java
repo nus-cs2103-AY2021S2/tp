0518154813493ca7419682c082e7a3c80edaa230
eksinyue <xinyuehehxy@gmail.com>
2021-03-06 11:22:40 +0800

    Initialize FinancialRecord class

---
+++ src/main/java/seedu/budgetBaby/model/record/FinancialRecord.java
@@ -0,0 +1,23 @@
+package seedu.budgetBaby.model.record;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.budgetBaby.commons.util.AppUtil.checkArgument;
+
+public class FinancialRecord {
+
+    public final String description;
+    public final Double amount;
+
+    /**
+     * Constructs a {@code FinancialRecord}.
+     *
+     * @param description A valid description.
+     * @param amount      A valid amount.
+     */
+    public FinancialRecord(String description, Double amount) {
+//        requireNonNull(tagName);
+//        checkArgument(isValidTagName(tagName), MESSAGE_CONSTRAINTS);
+        this.description = description;
+        this.amount = amount;
+    }
+}
