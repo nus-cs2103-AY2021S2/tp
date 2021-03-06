266a7e33234a8664639b8494280728874596a009
Samuel Fang <samuelfangjw@gmail.com>
2021-03-06 21:38:37 +0800

    Set default screen dimensions to 1440x900

--- src/main/java/seedu/address/commons/core/GuiSettings.java
+++ src/main/java/seedu/address/commons/core/GuiSettings.java
@@ -10,8 +10,8 @@ import java.util.Objects;
  */
 public class GuiSettings implements Serializable {
 
-    private static final double DEFAULT_HEIGHT = 1920;
-    private static final double DEFAULT_WIDTH = 1080;
+    private static final double DEFAULT_HEIGHT = 900;
+    private static final double DEFAULT_WIDTH = 1440;
 
     private final double windowWidth;
     private final double windowHeight;
