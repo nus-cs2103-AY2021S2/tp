36050e84c71dc26782d54f0cb63a05b63dad825a
kumsssss <>
2021-03-07 23:40:27 +0800

    add test case

--- src/main/java/seedu/storemando/model/StoreMando.java
+++ src/main/java/seedu/storemando/model/StoreMando.java
@@ -22,7 +22,8 @@ public class StoreMando implements ReadOnlyStoreMando {
      *
      * Note that non-static init blocks are not recommended to use. There are other ways to avoid duplication
      *   among constructors.
-     */ {
+     */
+    {
         items = new UniqueItemList();
     }
 
