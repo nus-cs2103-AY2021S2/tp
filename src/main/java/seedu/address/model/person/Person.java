4c4b2885d39fc26b9072f55309f48e497d1fca3d
ppris <49683462+pPris@users.noreply.github.com>
2021-03-09 01:21:41 +0800

    Add order description to person constructor and update constructor calls

--- src/main/java/seedu/address/model/person/Person.java
+++ src/main/java/seedu/address/model/person/Person.java
@@ -28,13 +28,14 @@ public class Person {
     /**
      * Every field must be present and not null.
      */
-    public Person(Name name, Phone phone, Email email, Address address, Set<Tag> tags) {
+    public Person(Name name, Phone phone, Email email, Address address, Set<Tag> tags, Set<OrderDescription> orderDescriptions) {
         requireAllNonNull(name, phone, email, address, tags);
         this.name = name;
         this.phone = phone;
         this.email = email;
         this.address = address;
         this.tags.addAll(tags);
+        this.orderDescriptions.addAll(orderDescriptions);
     }
 
     public Name getName() {
