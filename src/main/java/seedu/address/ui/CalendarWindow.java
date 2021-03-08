947cf257854893daa42de9a8f4aab3f4ca16ab0f
Jonathan <banchiang98@gmail.com>
2021-03-08 11:44:17 +0800

    Remove unused import in CalendarWindow.java

--- src/main/java/seedu/address/ui/CalendarWindow.java
+++ src/main/java/seedu/address/ui/CalendarWindow.java
@@ -5,7 +5,6 @@ import java.util.logging.Logger;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
-import javafx.scene.image.Image;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 import seedu.address.commons.core.LogsCenter;
