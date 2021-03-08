37b255b14d74ad4b94ba320cb40de9d544dcea5d
SiTingST <33442908+SiTingST@users.noreply.github.com>
2021-03-08 23:10:34 +0800

    Update FindCommand class

--- src/main/java/seedu/address/logic/parser/FindCommandParser.java
+++ src/main/java/seedu/address/logic/parser/FindCommandParser.java
@@ -6,7 +6,7 @@ import java.util.Arrays;
 
 import seedu.address.logic.commands.FindCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
-import seedu.address.model.person.NameContainsKeywordsPredicate;
+import seedu.address.model.person.MatriculationNumberContainsKeywordsPredicate;
 
 /**
  * Parses input arguments and creates a new FindCommand object
@@ -19,15 +19,51 @@ public class FindCommandParser implements Parser<FindCommand> {
      * @throws ParseException if the user input does not conform the expected format
      */
     public FindCommand parse(String args) throws ParseException {
+
         String trimmedArgs = args.trim();
-        if (trimmedArgs.isEmpty()) {
+        String prefix = String.valueOf(trimmedArgs.charAt(0));
+
+        if(trimmedArgs.length() == 9 && prefix.equalsIgnoreCase("A")){
+            check_valid_matriculation_number(args);
+        }
+        else{
             throw new ParseException(
                     String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
         }
 
         String[] nameKeywords = trimmedArgs.split("\\s+");
 
-        return new FindCommand(new NameContainsKeywordsPredicate(Arrays.asList(nameKeywords)));
+        return new FindCommand(new MatriculationNumberContainsKeywordsPredicate(Arrays.asList(nameKeywords)));
     }
 
+    /**
+     * Check if the parsed input is a valid matriculation number with 9 characters, starting from letter "A"
+     * and ending with another alphabet.
+     * @param matriculationNumber
+     * @throws ParseException
+     */
+    public void check_valid_matriculation_number(String matriculationNumber ) throws ParseException {
+
+        if(!isLetter(matriculationNumber.substring(9))){
+            throw new ParseException(
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
+        }
+
+        try{
+            Integer.parseInt(matriculationNumber.substring(2,9));
+        } catch(NumberFormatException | IndexOutOfBoundsException outOfBounds) {
+            throw new ParseException(
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
+        }
+    }
+
+    /**
+     * Check to see if the last character of the matriculation number is a letter.
+     * @param input last character of the matriculation number
+     * @throws ParseException
+     */
+
+    public boolean isLetter(String input) {
+        return input.matches("[a-zA-Z]+");
+    }
 }
