8e3d19ead3a68ad986e6b97c88895b99485dab02
chewwh09 <weishawtyhao9@gmail.com>
2021-03-09 03:01:04 +0800

    Remove parseException

--- src/main/java/seedu/storemando/logic/parser/ListCommandParser.java
+++ src/main/java/seedu/storemando/logic/parser/ListCommandParser.java
@@ -3,7 +3,6 @@ package seedu.storemando.logic.parser;
 import java.util.Arrays;
 
 import seedu.storemando.logic.commands.ListCommand;
-import seedu.storemando.logic.parser.exceptions.ParseException;
 import seedu.storemando.model.item.LocationContainsKeywordsPredicate;
 
 /**
@@ -14,8 +13,6 @@ public class ListCommandParser implements Parser<ListCommand> {
     /**
      * Parses the given {@code String} of arguments in the context of the ListCommand
      * and returns a ListCommand object for execution.
-     *
-     * @throws ParseException if the user input does not conform the expected format
      */
     public ListCommand parse(String args) {
         String trimmedArgs = args.trim();
