96d6fa4faed17c0489195a02baf6e40430ca81ec
JasonLim <jasaaanlim@users.noreply.github.com>
2021-03-08 17:26:25 +0800

    Add JavaDoc for FoodIntake

--- src/main/java/seedu/address/model/food/FoodIntake.java
+++ src/main/java/seedu/address/model/food/FoodIntake.java
@@ -2,10 +2,19 @@ package seedu.address.model.food;
 
 import java.time.LocalDateTime;
 
+/**
+ * Represents a FoodIntake record.
+ */
 public class FoodIntake {
     private LocalDateTime date;
     private Food food;
-    
+
+    /**
+     * Creates a FoodIntake object representing the Food consumed at a particular date and time.
+     *
+     * @param date LocalDateTime of when the food was eaten
+     * @param food The related Food object that was consumed
+     */
     public FoodIntake(LocalDateTime date, Food food) {
         this.date = date;
         this.food = food;
