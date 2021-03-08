36e7d5678a3ba2a4a2da6f7f99a2eb4b672ceb0e
kieron560 <kieron560@gmail.com>
2021-03-08 22:41:41 +0800

    Add EventBook and ReadOnlyEventBook

---
+++ src/main/java/seedu/address/model/ReadOnlyEventBook.java
@@ -0,0 +1,15 @@
+package seedu.address.model;
+
+import javafx.collections.ObservableList;
+import seedu.address.model.event.Event;
+
+/**
+ * Unmodifiable view of an event book
+ */
+public interface ReadOnlyEventBook {
+    /**
+     * Returns an unmodifiable view of the persons list.
+     * This list will not contain any duplicate persons.
+     */
+    ObservableList<Event> getEventList();
+}
