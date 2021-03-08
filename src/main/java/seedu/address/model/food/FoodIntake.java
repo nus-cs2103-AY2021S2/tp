0d2fec6b4957453823c7a0014c4cf917ec5b73f9
JasonLim <jasaaanlim@users.noreply.github.com>
2021-03-08 17:57:33 +0800

    Update FoodIntake toString()

--- src/main/java/seedu/address/model/food/FoodIntake.java
+++ src/main/java/seedu/address/model/food/FoodIntake.java
@@ -1,11 +1,13 @@
 package seedu.address.model.food;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 /**
  * Represents a FoodIntake record.
  */
 public class FoodIntake {
+    private static final String DATETIME_FORMAT = "d MMM yyyy h:mma";
     private LocalDateTime date;
     private Food food;
 
@@ -26,5 +28,11 @@ public class FoodIntake {
 
     public LocalDateTime getDate() {
         return this.date;
+    }
+
+    @Override
+    public String toString() {
+        return this.date.format(DateTimeFormatter.ofPattern(DATETIME_FORMAT)) + ": "
+                + this.food.toString();
     }
 }
