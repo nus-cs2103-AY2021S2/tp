71a90309a3199844209e95b03f10bf731e17b552
JasonLim <jasaaanlim@users.noreply.github.com>
2021-03-08 18:11:03 +0800

    Add JavaDocs for FoodIntakeList

--- src/main/java/seedu/address/model/food/FoodIntakeList.java
+++ src/main/java/seedu/address/model/food/FoodIntakeList.java
@@ -4,14 +4,25 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Represents a list of FoodIntakes starting from the specified date.
+ */
 public class FoodIntakeList {
     private LocalDateTime startDate;
     private List<FoodIntake> foodIntakeList = new ArrayList<>();
 
+    /**
+     * Constructs a FoodIntakeList.
+     * @param startDate the date that the FoodIntakeList begins collecting from
+     */
     public FoodIntakeList(LocalDateTime startDate) {
         this.startDate = startDate;
     }
 
+    /**
+     * Adds a FoodIntake object to the FoodIntakeList.
+     * @param foodIntake FoodIntake object to add to list
+     */
     public void addFoodIntake(FoodIntake foodIntake) {
         this.foodIntakeList.add(foodIntake);
     }
