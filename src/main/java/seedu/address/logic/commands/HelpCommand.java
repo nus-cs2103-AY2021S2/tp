68e438d8014d38a3b35cfdbc51058969ed2564df
nicoleang09 <nicoleang09@gmail.com>
2021-03-09 21:32:01 +0800

    Edit HelpCommand logic to display summary of commands

--- src/main/java/seedu/address/logic/commands/HelpCommand.java
+++ src/main/java/seedu/address/logic/commands/HelpCommand.java
@@ -1,11 +1,21 @@
 package seedu.address.logic.commands;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+import seedu.address.commons.core.LogsCenter;
+import seedu.address.logic.LogicManager;
 import seedu.address.model.Model;
+
+import java.util.Arrays;
+import java.util.logging.Logger;
 
 /**
  * Format full help instructions for every command for display.
  */
 public class HelpCommand extends Command {
+    private final Logger logger = LogsCenter.getLogger(HelpCommand.class);
 
     public static final String COMMAND_WORD = "help";
 
@@ -14,8 +24,55 @@ public class HelpCommand extends Command {
 
     public static final String SHOWING_HELP_MESSAGE = "Opened help window.";
 
+    public String HELP_MESSAGE = "Commands available";
+
     @Override
     public CommandResult execute(Model model) {
-        return new CommandResult(SHOWING_HELP_MESSAGE, true, false);
+        try {
+            String projectDir = System.getProperty("user.dir");
+//            BufferedReader reader = new BufferedReader(new FileReader("../../../../../../../../docs/UserGuide.md"));
+            BufferedReader reader = new BufferedReader(new FileReader(projectDir + "/docs/UserGuide.md"));
+
+
+            String currLine = reader.readLine();
+            while (currLine != null && !currLine.equals("--------|------------------")) {
+                currLine = reader.readLine();
+            }
+
+            currLine = reader.readLine();
+            while (currLine != null) {
+                logger.info("currLine: " + currLine);
+
+                HELP_MESSAGE += commandSummaryParser(currLine);
+//                logger.info("help message now: " + HELP_MESSAGE);
+
+                currLine = reader.readLine();
+            }
+
+            logger.info("help message now: \n" + HELP_MESSAGE);
+
+
+            reader.close();
+
+        } catch (IOException e) {
+            System.out.println("Error reading file: " + e);
+        }
+
+//        return new CommandResult(SHOWING_HELP_MESSAGE, true, false);
+        return new CommandResult(HELP_MESSAGE, true, false);
+    }
+
+    private String commandSummaryParser(String info) {
+        String[] separatedInfo = info.split("\\|");
+//        logger.info("separatedInfo: " + Arrays.toString(separatedInfo));
+        String[] commandName = separatedInfo[0].split("\\*");
+//        logger.info("commandName[]: " + Arrays.toString(commandName));
+
+        String[] commandDesc = separatedInfo[1].split("`");
+//        logger.info("commandDesc[]: " + Arrays.toString(commandDesc));
+
+//        logger.info("\n" + commandName[2] + ": " + commandDesc[1]);
+
+        return "\n" + commandName[2] + ": " + commandDesc[1];
     }
 }
