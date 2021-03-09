dec15420dd0f1fc2b36a3bf1547668378d21d20b
Kingsley Kuan <kingsley@kingsleykuan.com>
2021-03-09 21:56:54 +0800

    Add subject classes

---
+++ src/main/java/seedu/address/model/subject/SubjectQualification.java
@@ -0,0 +1,56 @@
+package seedu.address.model.subject;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.AppUtil.checkArgument;
+
+/**
+ * Represents a Tutor's qualifications in a subject in Tutor Tracker.
+ * Guarantees: immutable; is valid as declared in {@link #isValidQualification(String)}
+ */
+public class SubjectQualification {
+    public static final String MESSAGE_CONSTRAINTS =
+            "Qualifications should only contain alphanumeric characters and spaces, and it should not be blank";
+
+    /*
+     * The first character of the subject qualification must not be a whitespace,
+     * otherwise " " (a blank string) becomes a valid input.
+     */
+    public static final String VALIDATION_REGEX = "[\\p{Alnum}][\\p{Alnum} ]*";
+
+    public final String qualification;
+
+    /**
+     * Constructs a {@code SubjectQualification}.
+     *
+     * @param qualification A valid subject qualification.
+     */
+    public SubjectQualification(String qualification) {
+        requireNonNull(qualification);
+        checkArgument(isValidQualification(qualification), MESSAGE_CONSTRAINTS);
+        this.qualification = qualification;
+    }
+
+    /**
+     * Returns true if a given string is a valid subject qualification.
+     */
+    public static boolean isValidQualification(String test) {
+        return test.matches(VALIDATION_REGEX);
+    }
+
+    @Override
+    public String toString() {
+        return qualification;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof SubjectQualification // instanceof handles nulls
+                && qualification.equals(((SubjectQualification) other).qualification)); // state check
+    }
+
+    @Override
+    public int hashCode() {
+        return qualification.hashCode();
+    }
+}
---
+++ src/main/java/seedu/address/model/subject/TutorSubject.java
@@ -0,0 +1,93 @@
+package seedu.address.model.subject;
+
+import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
+
+import java.util.Objects;
+
+/**
+ * Represents a subject that a tutor offers.
+ * Guarantees: details are present and not null, field values are validated, immutable.
+ */
+public class TutorSubject {
+    private final SubjectName name;
+    private final SubjectLevel level;
+    private final SubjectRate rate;
+    private final SubjectExperience experience;
+    private final SubjectQualification qualification;
+
+    /**
+     * Every field must be present and not null.
+     */
+    public TutorSubject(SubjectName name, SubjectLevel level, SubjectRate rate,
+            SubjectExperience experience, SubjectQualification qualification) {
+        requireAllNonNull(name, level, rate, experience, qualification);
+        this.name = name;
+        this.level = level;
+        this.rate = rate;
+        this.experience = experience;
+        this.qualification = qualification;
+    }
+
+    public SubjectName getName() {
+        return name;
+    }
+
+    public SubjectLevel getLevel() {
+        return level;
+    }
+
+    public SubjectRate getRate() {
+        return rate;
+    }
+
+    public SubjectExperience getExperience() {
+        return experience;
+    }
+
+    public SubjectQualification getQualification() {
+        return qualification;
+    }
+
+    /**
+     * Returns true if both tutor subjects have the same identity and data fields.
+     */
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        if (!(other instanceof TutorSubject)) {
+            return false;
+        }
+
+        TutorSubject otherTutorSubject = (TutorSubject) other;
+        return otherTutorSubject.getName().equals(getName())
+                && otherTutorSubject.getLevel().equals(getLevel())
+                && otherTutorSubject.getRate().equals(getRate())
+                && otherTutorSubject.getExperience().equals(getExperience())
+                && otherTutorSubject.getQualification().equals(getQualification());
+    }
+
+    @Override
+    public int hashCode() {
+        // use this method for custom fields hashing instead of implementing your own
+        return Objects.hash(name, level, rate, experience, qualification);
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder builder = new StringBuilder();
+        builder.append(getName())
+                .append("; Level: ")
+                .append(getLevel())
+                .append("; Rate: ")
+                .append(getRate())
+                .append("; Years of Experience: ")
+                .append(getExperience())
+                .append("; Qualification: ")
+                .append(getQualification());
+
+        return builder.toString();
+    }
+}
