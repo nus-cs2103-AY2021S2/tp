cb9c5a0399eb7eb4e4867bb4dda9e2af8ddc66c2
Shizheng001 <e0309721@u.nus.edu>
2021-03-09 15:17:41 +0800

    added delete user command

--- src/main/java/seedu/address/commons/core/Messages.java
+++ src/main/java/seedu/address/commons/core/Messages.java
@@ -7,7 +7,8 @@ public class Messages {
 
     public static final String MESSAGE_UNKNOWN_COMMAND = "Unknown command";
     public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Invalid command format! \n%1$s";
-    public static final String MESSAGE_INVALID_PERSON_DISPLAYED_INDEX = "The person index provided is invalid";
+    public static final String MESSAGE_NO_SUCH_NAME_IN_BOOK = "There is no such person in Car@Tinder";
+    public static final String MESSAGE_INVALID_PERSON_DISPLAYED_INDEX = "Index provided is out of range";
     public static final String MESSAGE_PERSONS_LISTED_OVERVIEW = "%1$d persons listed!";
 
 }
