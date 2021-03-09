96d69b534821b56450d1b890925aa6ba859feb72
jellymias <51073704+jellymias@users.noreply.github.com>
2021-03-09 16:33:55 +0800

    Add Add Assignment function

---
+++ src/main/java/seedu/address/logic/parser/AddAssignmentCommandParser.java
@@ -0,0 +1,44 @@
+package seedu.address.logic.parser;
+
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_ASSIGNMENT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_DEADLINE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_MODULE;
+
+import java.time.LocalDateTime;
+
+import seedu.address.logic.commands.AddAssignmentCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.module.Assignment;
+import seedu.address.model.module.Description;
+
+/**
+ * Parses input arguments and creates a new AddAssignmentCommand object
+ */
+public class AddAssignmentCommandParser extends AddCommandParser implements Parser<AddAssignmentCommand> {
+
+    /**
+     * Parses the given {@code String} of arguments in the context of the AddAssignmentCommand
+     * and returns an AddAssignmentCommand object for execution.
+     * @throws ParseException if the user input does not conform the expected format
+     */
+    public AddAssignmentCommand parse(String args) throws ParseException {
+        ArgumentMultimap argMultimap =
+                ArgumentTokenizer.tokenize(args, PREFIX_MODULE, PREFIX_ASSIGNMENT, PREFIX_DEADLINE);
+
+        if (!arePrefixesPresent(argMultimap, PREFIX_ASSIGNMENT, PREFIX_DEADLINE)
+                || !argMultimap.getPreamble().isEmpty()) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddAssignmentCommand.MESSAGE_USAGE));
+        }
+        //todo add support for module
+
+        Description description = ParserUtil.parseAssignmentDescription(argMultimap.getValue(PREFIX_ASSIGNMENT).get());
+        LocalDateTime deadline = ParserUtil.parseAssignmentDeadline(argMultimap.getValue(PREFIX_DEADLINE).get());
+
+        Assignment assignment = new Assignment(description, deadline);
+
+        return new AddAssignmentCommand(assignment);
+    }
+
+}
+
