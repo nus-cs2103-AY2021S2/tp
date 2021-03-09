9932b70e71a8521502bf84824f9ec54c0e18db58
yhtMinceraft1010X <tayyihsuen@gmail.com>
2021-03-09 15:22:21 +0800

    Fix typos in Javadoc

--- src/test/java/seedu/module/logic/parser/NotDoneCommandParserTest.java
+++ src/test/java/seedu/module/logic/parser/NotDoneCommandParserTest.java
@@ -11,8 +11,8 @@ import seedu.module.logic.commands.NotDoneCommand;
 
 /**
  * As we are only doing white-box testing, our test cases do not cover path variations
- * outside of the DoneCommand code. For example, inputs "1" and "1 abc" take the
- * same path through the DoneCommand, and therefore we test only one of them.
+ * outside of the NotDoneCommand code. For example, inputs "1" and "1 abc" take the
+ * same path through the NotDoneCommand, and therefore we test only one of them.
  * The path variation for those two cases occur inside the ParserUtil, and
  * therefore should be covered by the ParserUtilTest.
  */
