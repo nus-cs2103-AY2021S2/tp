3080a3a8ae2dba9839f99acef0eda0644a80ef93
Glenn Lee <glenn@Glenns-MacBook-Pro.local>
2021-03-09 19:28:22 +0800

    Update FindCommand Usage

--- src/main/java/seedu/address/logic/commands/FindCommand.java
+++ src/main/java/seedu/address/logic/commands/FindCommand.java
@@ -19,8 +19,8 @@ public class FindCommand extends Command {
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds all persons whose names contain any of "
             + "the specified keywords (case-insensitive) and displays them as a list with index numbers.\n"
-            + "Parameters: KEYWORD [MORE_KEYWORDS]...\n"
-            + "Example: " + COMMAND_WORD + " alice bob charlie";
+            + "Parameters: [-n NAME] [-t TAGS]...\n"
+            + "Example: " + COMMAND_WORD + "-n Bob -t CS2030";
 
     private final List<Predicate<Person>> predicates;
 
