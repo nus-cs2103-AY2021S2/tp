dac2453f01453f85803828831b8d34ae19896e1a
xiinweii98 <xiinweii98@gmail.com>
2021-03-09 17:55:06 +0800

    Updated according to reviews from PR.
    -Edited TypicalPersons and typicalPersonsAddressBook to include variety of date formats
    -Edited DeliveryDateTest to test the .equals method
    -Edited DeliveryDateTest to test for invalid delimiters and formats
    
    Noteworthy problems that may face in the future:
    DeliveryDates of persons/typical persons created in Test Cases are mostly hard-coded, as such, the test cases are not future proof since "Today's" date will keep moving forward.
    This is because isValidDeliveryDate checks and makes sure deliveryDate is at least 3 days after "Today's" date.
    While it is possible to code .java test files to make use of LocalDate.now() to input DeliveryDates for the test cases, I am currently uncertain if I can do the same for .json test files.
    This is important as there are some .json test files that are hard-coded and used for Test Cases and so have a depency.
    For now, all valid DeliveryDates used for testing is set for 2022.

--- src/test/java/seedu/address/model/person/DeliveryDateTest.java
+++ src/test/java/seedu/address/model/person/DeliveryDateTest.java
@@ -1,7 +1,6 @@
 package seedu.address.model.person;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 import static seedu.address.testutil.Assert.assertThrows;
 
 import java.time.LocalDate;
@@ -25,6 +24,21 @@ public class DeliveryDateTest {
     }
 
     @Test
+    public void equals() {
+        LocalDate validDeliveryDate = LocalDate.now().plusDays(5L);
+        DateTimeFormatter format1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        DateTimeFormatter format2 = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        DateTimeFormatter format3 = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        DateTimeFormatter format4 = DateTimeFormatter.ofPattern("dd MMM yyyy");
+        assertEquals(new DeliveryDate(format1.format(validDeliveryDate)),
+                new DeliveryDate(format2.format(validDeliveryDate)));
+        assertEquals(new DeliveryDate(format2.format(validDeliveryDate)),
+                new DeliveryDate(format3.format(validDeliveryDate)));
+        assertEquals(new DeliveryDate(format3.format(validDeliveryDate)),
+                new DeliveryDate(format4.format(validDeliveryDate)));
+    }
+
+    @Test
     public void isValidDeliveryDate() {
         // null delivery date
         assertThrows(NullPointerException.class, () -> DeliveryDate.isValidDeliveryDate(null));
@@ -33,6 +47,10 @@ public class DeliveryDateTest {
         assertFalse(DeliveryDate.isValidDeliveryDate("01/13/2022")); // invalid month
         assertFalse(DeliveryDate.isValidDeliveryDate("32/01/2022")); // invalid day
         assertFalse(DeliveryDate.isValidDeliveryDate("01/01/202020")); // invalid year
+        assertFalse(DeliveryDate.isValidDeliveryDate("01,01,2022")); // invalid delimiter
+        assertFalse(DeliveryDate.isValidDeliveryDate("01:01:2022")); // invalid delimiter
+        assertFalse(DeliveryDate.isValidDeliveryDate("01/01-2022")); // inconsistent delimiter
+        assertFalse(DeliveryDate.isValidDeliveryDate("2022/12/31")); // invalid format
         // less than 3 working days
         assertFalse(DeliveryDate.isValidDeliveryDate(LocalDate.now().plusDays(2L).toString()));
 
--- src/test/java/seedu/address/testutil/TypicalPersons.java
+++ src/test/java/seedu/address/testutil/TypicalPersons.java
@@ -34,18 +34,18 @@ public class TypicalPersons {
             .withAddress("311, Clementi Ave 2, #02-25")
             .withEmail("johnd@example.com").withPhone("98765432")
             .withTags("owesMoney", "friends")
-            .withDeliveryDate("01/01/2022").build();
+            .withDeliveryDate("01-01-2022").build();
     public static final Person CARL = new PersonBuilder().withName("Carl Kurz").withPhone("95352563")
-            .withEmail("heinz@example.com").withAddress("wall street").withDeliveryDate("01/01/2022").build();
+            .withEmail("heinz@example.com").withAddress("wall street").withDeliveryDate("01.01.2022").build();
     public static final Person DANIEL = new PersonBuilder().withName("Daniel Meier").withPhone("87652533")
             .withEmail("cornelia@example.com").withAddress("10th street").withTags("friends")
-            .withDeliveryDate("01/01/2022").build();
+            .withDeliveryDate("01 Jan 2022").build();
     public static final Person ELLE = new PersonBuilder().withName("Elle Meyer").withPhone("9482224")
-            .withEmail("werner@example.com").withAddress("michegan ave").withDeliveryDate("01/01/2022").build();
+            .withEmail("werner@example.com").withAddress("michegan ave").withDeliveryDate("31/12/2022").build();
     public static final Person FIONA = new PersonBuilder().withName("Fiona Kunz").withPhone("9482427")
-            .withEmail("lydia@example.com").withAddress("little tokyo").withDeliveryDate("01/01/2022").build();
+            .withEmail("lydia@example.com").withAddress("little tokyo").withDeliveryDate("28-02-2022").build();
     public static final Person GEORGE = new PersonBuilder().withName("George Best").withPhone("9482442")
-            .withEmail("anna@example.com").withAddress("4th street").withDeliveryDate("01/01/2022").build();
+            .withEmail("anna@example.com").withAddress("4th street").withDeliveryDate("06.03.2022").build();
 
     // Manually added
     public static final Person HOON = new PersonBuilder().withName("Hoon Meier").withPhone("8482424")
