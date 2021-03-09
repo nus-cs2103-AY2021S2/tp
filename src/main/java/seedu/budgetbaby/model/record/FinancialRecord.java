a7e227db1fe7cdeda6d2bd449348892273cd630d
eksinyue <xinyuehehxy@gmail.com>
2021-03-09 11:18:03 +0800

    Add FinancialRecord and Description class

--- src/main/java/seedu/budgetbaby/model/record/FinancialRecord.java
+++ src/main/java/seedu/budgetbaby/model/record/FinancialRecord.java
@@ -1,12 +1,20 @@
 package seedu.budgetbaby.model.record;
 
-import static java.util.Objects.requireNonNull;
-import static seedu.budgetBaby.commons.util.AppUtil.checkArgument;
+import java.util.*;
 
+/**
+ * Represents a Financial Record in the budget tracker.
+ * Guarantees: details are present and not null, field values are validated, immutable.
+ */
 public class FinancialRecord {
 
-    public final String description;
-    public final Double amount;
+    private final static String FINANCIAL_RECORD_DETAILS_DELIMITER = " | ";
+
+    // Data fields
+    private final Description description;
+    private final Double amount;
+    private final Date timestamp;
+//    private final Set<Tag> tags = new HashSet<>();
 
     /**
      * Constructs a {@code FinancialRecord}.
@@ -14,10 +22,55 @@ public class FinancialRecord {
      * @param description A valid description.
      * @param amount      A valid amount.
      */
-    public FinancialRecord(String description, Double amount) {
+    public FinancialRecord(Description description, Double amount) {
 //        requireNonNull(tagName);
 //        checkArgument(isValidTagName(tagName), MESSAGE_CONSTRAINTS);
         this.description = description;
         this.amount = amount;
+        this.timestamp = new Date();
+//        this.tags.addAll(tags);
     }
+
+    public Description getDescription() {
+        return description;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public Date getTimestamp() {
+        return timestamp;
+    }
+
+    /**
+     * Returns an immutable tag set, which throws {@code UnsupportedOperationException}
+     * if modification is attempted.
+     */
+//    public Set<Tag> getTags() {
+//        return Collections.unmodifiableSet(tags);
+//    }
+    @Override
+    public int hashCode() {
+        // use this method for custom fields hashing instead of implementing your own
+        return Objects.hash(description, amount, timestamp);
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder builder = new StringBuilder();
+        builder.append(getTimestamp())
+            .append(FINANCIAL_RECORD_DETAILS_DELIMITER)
+            .append(getDescription())
+            .append(FINANCIAL_RECORD_DETAILS_DELIMITER)
+            .append(getAmount());
+
+//        Set<Tag> tags = getTags();
+//        if (!tags.isEmpty()) {
+//            builder.append("; Tags: ");
+//            tags.forEach(builder::append);
+//        }
+        return builder.toString();
+    }
+
 }
