b364172ca356cc495f2a1da0dbba1c7acd7a4ca1
Shizheng001 <e0309721@u.nus.edu>
2021-03-09 15:53:19 +0800

    updated delete user command and deletecommand tests

--- src/main/java/seedu/address/logic/parser/DeleteCommandParser.java
+++ src/main/java/seedu/address/logic/parser/DeleteCommandParser.java
@@ -1,7 +1,6 @@
 package seedu.address.logic.parser;
 
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
-import seedu.address.model.person.Name;
 import seedu.address.logic.commands.DeleteCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 
