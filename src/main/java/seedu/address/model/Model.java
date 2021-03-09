aafc269612cf2f0f3cab4dbc6c3aa3bd66c0c6dc
Benedict Khoo <benedictkhoo.mw@u.nus.edu>
2021-03-09 23:01:19 +0800

    Command History: Fix failing tests and checkstyle

--- src/main/java/seedu/address/model/Model.java
+++ src/main/java/seedu/address/model/Model.java
@@ -5,7 +5,6 @@ import java.util.function.Predicate;
 
 import javafx.collections.ObservableList;
 import seedu.address.commons.core.GuiSettings;
-import seedu.address.model.commandhistory.CommandHistoryEntry;
 import seedu.address.model.commandhistory.ReadOnlyCommandHistory;
 import seedu.address.model.person.Person;
 
