6904e1b1aace5f04e62a23c1dea8af0c1f8f5b7f
Shizheng001 <e0309721@u.nus.edu>
2021-03-09 17:03:03 +0800

    updated Delete command

--- src/test/java/seedu/address/logic/parser/DeleteCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/DeleteCommandParserTest.java
@@ -1,8 +1,8 @@
 package seedu.address.logic.parser;
 
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
-import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
 
 import org.junit.jupiter.api.Test;
 
@@ -22,5 +22,10 @@ public class DeleteCommandParserTest {
     @Test
     public void parse_validArgs_returnsDeleteCommand() {
         assertParseSuccess(parser, "name", new DeleteCommand("name"));
+    }
+
+    @Test
+    public void parse_invalidArgs_throwsParseException() {
+        assertParseFailure(parser, "", String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteCommand.MESSAGE_USAGE));
     }
 }
