e77cb7157cf382e9ede62370f11c95f0945fa279
LimJunxue <junxue1@hotmail.com>
2021-03-09 22:51:06 +0800

    Fix checkstyle errors

--- src/test/java/seedu/address/logic/commands/ClearCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ClearCommandTest.java
@@ -5,9 +5,9 @@ import static seedu.address.testutil.TypicalTasks.getTypicalPlanner;
 
 import org.junit.jupiter.api.Test;
 
-import seedu.address.model.Planner;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
+import seedu.address.model.Planner;
 import seedu.address.model.UserPrefs;
 
 public class ClearCommandTest {
