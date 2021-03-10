c107b33f5d6c063e59b8bef22ea648b1f7ffb024
Vevek Selvam <vevekselvam@gmail.com>
2021-03-10 20:01:49 +0800

    Add AddTtoCommand & AddTtoCommandParser. Methods still need to be worked on.

--- src/main/java/seedu/address/logic/parser/CliSyntax.java
+++ src/main/java/seedu/address/logic/parser/CliSyntax.java
@@ -11,5 +11,7 @@ public class CliSyntax {
     public static final Prefix PREFIX_EMAIL = new Prefix("e/");
     public static final Prefix PREFIX_ADDRESS = new Prefix("a/");
     public static final Prefix PREFIX_TAG = new Prefix("t/");
-
+    public static final Prefix PREFIX_DESCRIPTION = new Prefix("d/");
+    public static final Prefix PREFIX_START_DATE = new Prefix("s/");
+    public static final Prefix PREFIX_EVENT_INTERVAL = new Prefix("r/");
 }
