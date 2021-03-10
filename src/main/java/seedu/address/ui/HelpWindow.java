2b123a1f33b305e19efc3e73a7c29266fe938b76
chewterence <chewterence@hotmail.com>
2021-03-10 22:16:33 +0800

    Update help windows to updated user guide

--- src/main/java/seedu/address/ui/HelpWindow.java
+++ src/main/java/seedu/address/ui/HelpWindow.java
@@ -15,8 +15,9 @@ import seedu.address.commons.core.LogsCenter;
  */
 public class HelpWindow extends UiPart<Stage> {
 
-    public static final String USERGUIDE_URL = "https://se-education.org/addressbook-level3/UserGuide.html";
-    public static final String HELP_MESSAGE = "Refer to the user guide: " + USERGUIDE_URL;
+    public static final String USERGUIDE_URL = "https://ay2021s2-cs2103t-w10-1.github.io/tp/UserGuide.html";
+    public static final String HELP_MESSAGE = "For more information on how to use the GreenMileageEfforts terminal,"
+            + " please refer to the user guide: " + USERGUIDE_URL;
 
     private static final Logger logger = LogsCenter.getLogger(HelpWindow.class);
     private static final String FXML = "HelpWindow.fxml";
