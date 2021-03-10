3caea60223da4ed18dabd5529ca24fb740f45d96
Glenn Lee <glenn@Glenns-MacBook-Pro.local>
2021-03-10 17:35:37 +0800

    Fix CheckStyle violations

--- src/main/java/seedu/hippocampus/logic/commands/TagsCommand.java
+++ src/main/java/seedu/hippocampus/logic/commands/TagsCommand.java
@@ -16,7 +16,8 @@ public class TagsCommand extends Command {
 
     public static final String MESSAGE_SUCCESS = "Listed all tags";
 
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Lists all tags and the count of person tagged in Hippocampus."
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Lists all tags and the count of"
+            + "person tagged in Hippocampus."
             + "If keyword is given, list all tags containing given keyword.\n"
             + "Parameters: [-f TAG]\n"
             + "Example: find -f class";
