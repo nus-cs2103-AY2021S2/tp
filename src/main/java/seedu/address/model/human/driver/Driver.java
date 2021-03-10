e066736f684a5eb0bc2d036fa3a971fcf1948693
Zeche <zx3110@gmail.com>
2021-03-10 18:33:05 +0800

    Add Driver as child of Human class

---
+++ src/main/java/seedu/address/model/human/driver/Driver.java
@@ -0,0 +1,73 @@
+package seedu.address.model.human.driver;
+
+import seedu.address.model.human.Human;
+import seedu.address.model.human.Name;
+import seedu.address.model.human.person.Person;
+import seedu.address.model.human.person.Phone;
+
+
+import java.util.Objects;
+
+public class Driver extends Human {
+
+    public Driver(Name name, Phone phone) {
+        super(name, phone);
+    }
+
+    public Name getName() {
+        return super.name;
+    }
+
+    public Phone getPhone() {
+        return super.phone;
+    }
+
+    /**
+     * Returns true if both persons have the same name.
+     * This defines a weaker notion of equality between two persons.
+     */
+    public boolean isSameDriver(Driver otherDriver) {
+        if (otherDriver == this) {
+            return true;
+        }
+
+        return otherDriver != null
+                && otherDriver.getName().equals(getName());
+    }
+
+    /**
+     * Returns true if both persons have the same identity and data fields.
+     * This defines a stronger notion of equality between two persons.
+     */
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        if (!(other instanceof Person)) {
+            return false;
+        }
+
+        Person otherPerson = (Person) other;
+        return otherPerson.getName().equals(getName())
+                && otherPerson.getPhone().equals(getPhone());
+    }
+
+    @Override
+    public int hashCode() {
+        // use this method for custom fields hashing instead of implementing your own
+        return Objects.hash(name, phone);
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder builder = new StringBuilder();
+        builder.append(getName())
+                .append("; Phone: ")
+                .append(getPhone())
+                .append("; Email: ");
+
+        return builder.toString();
+    }
+}
