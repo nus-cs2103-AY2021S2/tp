3b5cc8693e117df8edd3efaa4063c3227c4026af
yaowei-soc <e0407296@u.nus.edu>
2021-03-11 01:25:58 +0800

    test: Improve code coverage
    - Remove unnecessary ModelStub in FilterCommandParserTest
    - Add FilterCommand in AddressBookParserTest.

--- src/test/java/seedu/address/logic/commands/FilterCommandTest.java
+++ src/test/java/seedu/address/logic/commands/FilterCommandTest.java
@@ -3,22 +3,19 @@ package seedu.address.logic.commands;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_ADDRESS;
-
-import java.nio.file.Path;
-import java.util.function.Predicate;
+import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
 import org.junit.jupiter.api.Test;
 
-import javafx.collections.ObservableList;
-import seedu.address.commons.core.GuiSettings;
 import seedu.address.logic.parser.ArgumentMultimap;
 import seedu.address.model.DisplayFilterPredicate;
 import seedu.address.model.Model;
-import seedu.address.model.ReadOnlyAddressBook;
-import seedu.address.model.ReadOnlyUserPrefs;
-import seedu.address.model.person.Person;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
 
 class FilterCommandTest {
+
+    private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
 
     @Test
     public void execute_modelUpdated_success() throws Exception {
@@ -29,127 +26,13 @@ class FilterCommandTest {
                 argumentMultimap);
 
         FilterCommand filterCommand = new FilterCommand(displayFilterPredicate);
-        ModelStub modelStub = new ModelStubWithFilter();
 
         // before executing
-        assertNotEquals(modelStub.getDisplayFilter(), displayFilterPredicate);
+        assertNotEquals(model.getDisplayFilter(), displayFilterPredicate);
 
-        filterCommand.execute(modelStub);
+        filterCommand.execute(model);
 
         // after executing
-        assertEquals(modelStub.getDisplayFilter(), displayFilterPredicate);
+        assertEquals(model.getDisplayFilter(), displayFilterPredicate);
     }
-
-    /**
-     * A default model stub that have all of the methods failing.
-     */
-    private class ModelStub implements Model {
-
-        @Override
-        public void setUserPrefs(ReadOnlyUserPrefs userPrefs) {
-            throw new AssertionError("This method should not be called.");
-        }
-
-        @Override
-        public ReadOnlyUserPrefs getUserPrefs() {
-            throw new AssertionError("This method should not be called.");
-        }
-
-        @Override
-        public GuiSettings getGuiSettings() {
-            throw new AssertionError("This method should not be called.");
-        }
-
-        @Override
-        public void setGuiSettings(GuiSettings guiSettings) {
-            throw new AssertionError("This method should not be called.");
-        }
-
-        @Override
-        public Path getAddressBookFilePath() {
-            throw new AssertionError("This method should not be called.");
-        }
-
-        @Override
-        public void setAddressBookFilePath(Path addressBookFilePath) {
-            throw new AssertionError("This method should not be called.");
-        }
-
-        @Override
-        public void addPerson(Person person) {
-            throw new AssertionError("This method should not be called.");
-        }
-
-        @Override
-        public void setAddressBook(ReadOnlyAddressBook newData) {
-            throw new AssertionError("This method should not be called.");
-        }
-
-        @Override
-        public ReadOnlyAddressBook getAddressBook() {
-            throw new AssertionError("This method should not be called.");
-        }
-
-        @Override
-        public boolean hasPerson(Person person) {
-            throw new AssertionError("This method should not be called.");
-        }
-
-        @Override
-        public void deletePerson(Person target) {
-            throw new AssertionError("This method should not be called.");
-        }
-
-        @Override
-        public void setPerson(Person target, Person editedPerson) {
-            throw new AssertionError("This method should not be called.");
-        }
-
-        @Override
-        public ObservableList<Person> getFilteredPersonList() {
-            throw new AssertionError("This method should not be called.");
-        }
-
-        @Override
-        public void updateFilteredPersonList(Predicate<Person> predicate) {
-            throw new AssertionError("This method should not be called.");
-        }
-
-        @Override
-        public void updateDisplayFilter(DisplayFilterPredicate displayFilterPredicate) {
-            throw new AssertionError("This method should not be called.");
-        }
-
-        @Override
-        public DisplayFilterPredicate getDisplayFilter() {
-            throw new AssertionError("This method should not be called.");
-        }
-    }
-
-    /**
-     * A Model stub that implements {@code DisplayFilterPredicate}.
-     */
-    private class ModelStubWithFilter extends FilterCommandTest.ModelStub {
-
-        private DisplayFilterPredicate displayFilterPredicate;
-
-        public ModelStubWithFilter() {
-            displayFilterPredicate = new DisplayFilterPredicate();
-        }
-
-        public ModelStubWithFilter(DisplayFilterPredicate displayFilterPredicate) {
-            this.displayFilterPredicate = displayFilterPredicate;
-        }
-
-        @Override
-        public void updateDisplayFilter(DisplayFilterPredicate displayFilterPredicate) {
-            this.displayFilterPredicate = displayFilterPredicate;
-        }
-
-        @Override
-        public DisplayFilterPredicate getDisplayFilter() {
-            return displayFilterPredicate;
-        }
-    }
-
 }
