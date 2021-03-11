822f0d7e641e389875069e01cc1db2b239f06ac9
nicoleang09 <nicoleang09@gmail.com>
2021-03-11 22:37:26 +0800

    Run checkstyle for HelpCommand class

--- src/main/java/seedu/address/logic/commands/HelpCommand.java
+++ src/main/java/seedu/address/logic/commands/HelpCommand.java
@@ -3,18 +3,15 @@ package seedu.address.logic.commands;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.logging.Logger;
 
 import seedu.address.commons.core.LogsCenter;
 import seedu.address.model.Model;
-
-import java.util.logging.Logger;
 
 /**
  * Format full help instructions for every command for display.
  */
 public class HelpCommand extends Command {
-    private final Logger logger = LogsCenter.getLogger(HelpCommand.class);
-
     public static final String COMMAND_WORD = "help";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Shows program usage instructions.\n"
@@ -22,13 +19,13 @@ public class HelpCommand extends Command {
 
     public static final String SHOWING_HELP_MESSAGE = "Opened help window.";
 
-    public String helpMessage = "Commands available";
+    private String helpMessage = "Commands available";
+    private final Logger logger = LogsCenter.getLogger(HelpCommand.class);
 
     @Override
     public CommandResult execute(Model model) {
         try {
             String projectDir = System.getProperty("user.dir");
-//            BufferedReader reader = new BufferedReader(new FileReader("../../../../../../../../docs/UserGuide.md"));
             BufferedReader reader = new BufferedReader(new FileReader(projectDir + "/docs/UserGuide.md"));
 
 
@@ -42,7 +39,6 @@ public class HelpCommand extends Command {
                 logger.info("currLine: " + currLine);
 
                 helpMessage += commandSummaryParser(currLine);
-//                logger.info("help message now: " + HELP_MESSAGE);
 
                 currLine = reader.readLine();
             }
@@ -56,20 +52,20 @@ public class HelpCommand extends Command {
             System.out.println("Error reading file: " + e);
         }
 
-//        return new CommandResult(SHOWING_HELP_MESSAGE, true, false);
+        // return new CommandResult(SHOWING_HELP_MESSAGE, true, false);
         return new CommandResult(helpMessage, true, false);
     }
 
     private String commandSummaryParser(String info) {
         String[] separatedInfo = info.split("\\|");
-//        logger.info("separatedInfo: " + Arrays.toString(separatedInfo));
+        // logger.info("separatedInfo: " + Arrays.toString(separatedInfo));
         String[] commandName = separatedInfo[0].split("\\*");
-//        logger.info("commandName[]: " + Arrays.toString(commandName));
+        // logger.info("commandName[]: " + Arrays.toString(commandName));
 
         String[] commandDesc = separatedInfo[1].split("`");
-//        logger.info("commandDesc[]: " + Arrays.toString(commandDesc));
+        // logger.info("commandDesc[]: " + Arrays.toString(commandDesc));
 
-//        logger.info("\n" + commandName[2] + ": " + commandDesc[1]);
+        // logger.info("\n" + commandName[2] + ": " + commandDesc[1]);
 
         return "\n" + commandName[2] + ": " + commandDesc[1];
     }
