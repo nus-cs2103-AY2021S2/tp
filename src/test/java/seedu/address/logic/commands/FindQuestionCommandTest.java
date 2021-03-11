df8ef853f5dd6a6d52efe5947cb0b59ffff68bc4
yeoutzer <loo_yeou_tzer@hotmail.com>
2021-03-12 04:06:20 +0800

    Edit FindQuestionCommandTest.java to follow checkstyle

--- src/test/java/seedu/address/logic/commands/FindQuestionCommandTest.java
+++ src/test/java/seedu/address/logic/commands/FindQuestionCommandTest.java
@@ -5,8 +5,8 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.commons.core.Messages.MESSAGE_FLASHCARDS_LISTED_OVERVIEW;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
-import static seedu.address.testutil.TypicalFlashcards.NEWTON;
 import static seedu.address.testutil.TypicalFlashcards.MERGE;
+import static seedu.address.testutil.TypicalFlashcards.NEWTON;
 import static seedu.address.testutil.TypicalFlashcards.RECURSION;
 import static seedu.address.testutil.TypicalFlashcards.getTypicalFlashBack;
 
