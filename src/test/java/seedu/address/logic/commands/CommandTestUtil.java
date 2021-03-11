f6d88ffdd05ff3114aadf1ca4993c5d57129fb22
Prabhakaran-Gokul <prabhakaran.gokul@gmail.com>
2021-03-12 04:03:08 +0800

    Change Review Validation logic
    Reviews can take any values, and it should not be blank

--- src/test/java/seedu/address/logic/commands/CommandTestUtil.java
+++ src/test/java/seedu/address/logic/commands/CommandTestUtil.java
@@ -50,7 +50,7 @@ public class CommandTestUtil {
 
     public static final String INVALID_NAME_DESC = " " + PREFIX_NAME + "James&"; // '&' not allowed in names
     public static final String INVALID_PHONE_DESC = " " + PREFIX_PHONE + "911a"; // 'a' not allowed in phones
-    public static final String INVALID_REVIEW_DESC = " " + PREFIX_REVIEW + "bob!yahoo"; // missing '@' symbol
+    public static final String INVALID_REVIEW_DESC = " " + PREFIX_REVIEW; // empty string not allowed for review
     public static final String INVALID_ADDRESS_DESC = " " + PREFIX_ADDRESS; // empty string not allowed for addresses
     public static final String INVALID_TAG_DESC = " " + PREFIX_TAG + "hubby*"; // '*' not allowed in tags
 
--- src/test/java/seedu/address/model/person/ReviewTest.java
+++ src/test/java/seedu/address/model/person/ReviewTest.java
@@ -28,34 +28,12 @@ public class ReviewTest {
         assertFalse(Review.isValidReview("")); // empty string
         assertFalse(Review.isValidReview(" ")); // spaces only
 
-        // missing parts
-        assertFalse(Review.isValidReview("@example.com")); // missing local part
-        assertFalse(Review.isValidReview("peterjackexample.com")); // missing '@' symbol
-        assertFalse(Review.isValidReview("peterjack@")); // missing domain name
-
-        // invalid parts
-        assertFalse(Review.isValidReview("peterjack@-")); // invalid domain name
-        assertFalse(Review.isValidReview("peterjack@exam_ple.com")); // underscore in domain name
-        assertFalse(Review.isValidReview("peter jack@example.com")); // spaces in local part
-        assertFalse(Review.isValidReview("peterjack@exam ple.com")); // spaces in domain name
-        assertFalse(Review.isValidReview(" peterjack@example.com")); // leading space
-        assertFalse(Review.isValidReview("peterjack@example.com ")); // trailing space
-        assertFalse(Review.isValidReview("peterjack@@example.com")); // double '@' symbol
-        assertFalse(Review.isValidReview("peter@jack@example.com")); // '@' symbol in local part
-        assertFalse(Review.isValidReview("peterjack@example@com")); // '@' symbol in domain name
-        assertFalse(Review.isValidReview("peterjack@.example.com")); // domain name starts with a period
-        assertFalse(Review.isValidReview("peterjack@example.com.")); // domain name ends with a period
-        assertFalse(Review.isValidReview("peterjack@-example.com")); // domain name starts with a hyphen
-        assertFalse(Review.isValidReview("peterjack@example.com-")); // domain name ends with a hyphen
-
         // valid Review
         assertTrue(Review.isValidReview("PeterJack_1190@example.com"));
-        assertTrue(Review.isValidReview("a@bc")); // minimal
+        assertTrue(Review.isValidReview("good")); // minimal
         assertTrue(Review.isValidReview("test@localhost")); // alphabets only
-        assertTrue(Review.isValidReview("!#$%&'*+/=?`{|}~^.-@example.org")); // special characters local part
-        assertTrue(Review.isValidReview("123@145")); // numeric local part and domain name
-        assertTrue(Review.isValidReview("a1+be!@example1.com")); // mixture of alphanumeric and special characters
-        assertTrue(Review.isValidReview("peter_jack@very-very-very-long-example.com")); // long domain name
-        assertTrue(Review.isValidReview("if.you.dream.it_you.can.do.it@example.com")); // long local part
+        assertTrue(Review.isValidReview("!#$%&'*+/=?`{|}~^.-@.")); // special characters
+        assertTrue(Review.isValidReview("123145")); // numeric
+        assertTrue(Review.isValidReview("a1+be!@")); // mixture of alphanumeric and special characters
     }
 }
