5d801e04db8dbb8a905030b187df5a7953e4d006
yutingzou <zouyuting0801@gmail.com>
2021-03-11 22:16:15 +0800

    Fix checkstyle error

--- src/main/java/seedu/address/model/subject/SubjectList.java
+++ src/main/java/seedu/address/model/subject/SubjectList.java
@@ -1,15 +1,15 @@
 package seedu.address.model.subject;
 
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import seedu.address.model.subject.exceptions.DuplicateSubjectException;
-import seedu.address.model.subject.exceptions.SubjectNotFoundException;
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
 
 import java.util.Iterator;
 import java.util.List;
 
-import static java.util.Objects.requireNonNull;
-import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import seedu.address.model.subject.exceptions.DuplicateSubjectException;
+import seedu.address.model.subject.exceptions.SubjectNotFoundException;
 
 public class SubjectList implements Iterable<TutorSubject> {
 
