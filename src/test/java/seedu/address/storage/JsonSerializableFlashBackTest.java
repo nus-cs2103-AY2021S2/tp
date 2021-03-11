851c5c0a6c5f99b4a1b9d0b0459ee13e40111cc4
yeoutzer <loo_yeou_tzer@hotmail.com>
2021-03-12 04:22:21 +0800

    Edit JsonSerializableFlashBackTest.java due to refactor

--- src/test/java/seedu/address/storage/JsonSerializableFlashBackTest.java
+++ src/test/java/seedu/address/storage/JsonSerializableFlashBackTest.java
@@ -11,36 +11,37 @@ import org.junit.jupiter.api.Test;
 import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.commons.util.JsonUtil;
 import seedu.address.model.FlashBack;
-import seedu.address.testutil.TypicalPersons;
+import seedu.address.testutil.TypicalFlashcards;
 
 public class JsonSerializableFlashBackTest {
 
     private static final Path TEST_DATA_FOLDER = Paths.get("src", "test", "data", "JsonSerializableAddressBookTest");
-    private static final Path TYPICAL_PERSONS_FILE = TEST_DATA_FOLDER.resolve("typicalPersonsAddressBook.json");
-    private static final Path INVALID_PERSON_FILE = TEST_DATA_FOLDER.resolve("invalidPersonAddressBook.json");
-    private static final Path DUPLICATE_PERSON_FILE = TEST_DATA_FOLDER.resolve("duplicatePersonAddressBook.json");
+    private static final Path TYPICAL_PERSONS_FILE = TEST_DATA_FOLDER.resolve("typicalFlashcardFlashBack.json");
+    private static final Path INVALID_PERSON_FILE = TEST_DATA_FOLDER.resolve("invalidFlashcardFlashBack.json");
+    private static final Path DUPLICATE_PERSON_FILE = TEST_DATA_FOLDER.resolve("duplicateFlashcardFlashBack.json");
 
     @Test
-    public void toModelType_typicalPersonsFile_success() throws Exception {
-        JsonSerializableAddressBook dataFromFile = JsonUtil.readJsonFile(TYPICAL_PERSONS_FILE,
-                JsonSerializableAddressBook.class).get();
+    public void toModelType_typicalFlashcardsFile_success() throws Exception {
+        JsonSerializableFlashBack dataFromFile = JsonUtil.readJsonFile(TYPICAL_PERSONS_FILE,
+                JsonSerializableFlashBack.class).get();
         FlashBack flashBackFromFile = dataFromFile.toModelType();
-        FlashBack typicalPersonsFlashBack = TypicalPersons.getTypicalAddressBook();
-        assertEquals(flashBackFromFile, typicalPersonsFlashBack);
+        FlashBack typicalFlashBack = TypicalFlashcards.getTypicalFlashBack();
+        assertEquals(flashBackFromFile, typicalFlashBack);
     }
 
+    //TODO: Might need to change invalidFlashcardFlashBack.java question field
     @Test
-    public void toModelType_invalidPersonFile_throwsIllegalValueException() throws Exception {
-        JsonSerializableAddressBook dataFromFile = JsonUtil.readJsonFile(INVALID_PERSON_FILE,
-                JsonSerializableAddressBook.class).get();
+    public void toModelType_invalidFlashcardFile_throwsIllegalValueException() throws Exception {
+        JsonSerializableFlashBack dataFromFile = JsonUtil.readJsonFile(INVALID_PERSON_FILE,
+                JsonSerializableFlashBack.class).get();
         assertThrows(IllegalValueException.class, dataFromFile::toModelType);
     }
 
     @Test
-    public void toModelType_duplicatePersons_throwsIllegalValueException() throws Exception {
-        JsonSerializableAddressBook dataFromFile = JsonUtil.readJsonFile(DUPLICATE_PERSON_FILE,
-                JsonSerializableAddressBook.class).get();
-        assertThrows(IllegalValueException.class, JsonSerializableAddressBook.MESSAGE_DUPLICATE_PERSON,
+    public void toModelType_duplicateFlashcards_throwsIllegalValueException() throws Exception {
+        JsonSerializableFlashBack dataFromFile = JsonUtil.readJsonFile(DUPLICATE_PERSON_FILE,
+                JsonSerializableFlashBack.class).get();
+        assertThrows(IllegalValueException.class, JsonSerializableFlashBack.MESSAGE_DUPLICATE_PERSON,
                 dataFromFile::toModelType);
     }
 
