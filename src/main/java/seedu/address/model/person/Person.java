e3156a891e747d3fa561aadb1ce1584efe96767d
Keith Low <keithlow98@gmail.com>
2021-03-11 22:07:02 +0800

    Change isSamePerson method in Person.java.
    This changes the notion of equality between students.
    Since students are uniquely identified by their matriculation number,
    the isSamePerson() method has been updated to reflect this.

--- src/main/java/seedu/address/model/person/Person.java
+++ src/main/java/seedu/address/model/person/Person.java
@@ -88,7 +88,7 @@ public class Person {
         }
 
         return otherPerson != null
-                && otherPerson.getName().equals(getName());
+                && otherPerson.getMatriculationNumber().equals(getMatriculationNumber());
     }
 
     /**
