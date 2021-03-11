46e9b3042c5b07e16e8cbb88b80c8b4879356af0
Samuel Fang <samuelfangjw@gmail.com>
2021-03-12 01:01:00 +0800

    Fix checkstyle violations

--- src/main/java/seedu/address/model/task/Completable.java
+++ src/main/java/seedu/address/model/task/Completable.java
@@ -1,6 +1,5 @@
 package seedu.address.model.task;
 
-import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.util.AppUtil.checkArgument;
 import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
 
