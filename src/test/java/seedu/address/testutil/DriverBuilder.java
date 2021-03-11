c6631224c12c4c8b89228a1fd0711a1d5e5cbc15
Zeche <zx3110@gmail.com>
2021-03-11 19:13:14 +0800

    Add testcases for Driver
    * Add testcases for Driver class
    * Add DriverBuilder and TypicalDrivers to mirror Person Testing flow

---
+++ src/test/java/seedu/address/testutil/DriverBuilder.java
@@ -0,0 +1,59 @@
+package seedu.address.testutil;
+
+import seedu.address.model.human.Name;
+import seedu.address.model.human.Phone;
+import seedu.address.model.human.driver.Driver;
+import seedu.address.model.human.person.Address;
+import seedu.address.model.util.SampleDataUtil;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * A utility class to help with building Person objects.
+ */
+public class DriverBuilder {
+
+    public static final String DEFAULT_NAME = "Amy Bee";
+    public static final String DEFAULT_PHONE = "85355255";
+
+    private Name name;
+    private Phone phone;
+
+    /**
+     * Creates a {@code PersonBuilder} with the default details.
+     */
+    public DriverBuilder() {
+        name = new Name(DEFAULT_NAME);
+        phone = new Phone(DEFAULT_PHONE);
+    }
+
+    /**
+     * Initializes the PersonBuilder with the data of {@code personToCopy}.
+     */
+    public DriverBuilder(Driver personToCopy) {
+        name = personToCopy.getName();
+        phone = personToCopy.getPhone();
+    }
+
+    /**
+     * Sets the {@code Name} of the {@code Person} that we are building.
+     */
+    public DriverBuilder withName(String name) {
+        this.name = new Name(name);
+        return this;
+    }
+
+    /**
+     * Sets the {@code Phone} of the {@code Person} that we are building.
+     */
+    public DriverBuilder withPhone(String phone) {
+        this.phone = new Phone(phone);
+        return this;
+    }
+
+    public Driver build() {
+        return new Driver(name, phone);
+    }
+
+}
---
+++ src/test/java/seedu/address/testutil/TypicalDrivers.java
@@ -0,0 +1,32 @@
+package seedu.address.testutil;
+
+import seedu.address.model.human.driver.Driver;
+
+import static seedu.address.logic.commands.CommandTestUtil.VALID_NAME_AMY;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_NAME_BOB;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_PHONE_AMY;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_PHONE_BOB;
+
+/**
+ * A utility class containing a list of {@code Person} objects to be used in tests.
+ */
+public class TypicalDrivers {
+
+    public static final Driver ALICE = new DriverBuilder().withName("Alice Pauline").withPhone("94351253").build();
+    public static final Driver BENSON = new DriverBuilder().withName("Benson Meier").withPhone("98765432").build();
+    public static final Driver CARL = new DriverBuilder().withName("Carl Kurz").withPhone("95352563").build();
+    public static final Driver DANIEL = new DriverBuilder().withName("Daniel Meier").withPhone("87652533").build();
+    public static final Driver ELLE = new DriverBuilder().withName("Elle Meyer").withPhone("9482224").build();
+    public static final Driver FIONA = new DriverBuilder().withName("Fiona Kunz").withPhone("9482427").build();
+    public static final Driver GEORGE = new DriverBuilder().withName("George Best").withPhone("9482442").build();
+
+    // Manually added
+    public static final Driver HOON = new DriverBuilder().withName("Hoon Meier").withPhone("8482424").build();
+    public static final Driver IDA = new DriverBuilder().withName("Ida Mueller").withPhone("8482131").build();
+
+    // Manually added - Person's details found in {@code CommandTestUtil}
+    public static final Driver AMY = new DriverBuilder().withName(VALID_NAME_AMY).withPhone(VALID_PHONE_AMY).build();
+    public static final Driver BOB = new DriverBuilder().withName(VALID_NAME_BOB).withPhone(VALID_PHONE_BOB).build();
+
+    private TypicalDrivers() {} // prevents instantiation
+}
