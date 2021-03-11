5ff87d8d55e5577524beb3b615d27dbcbf92fe94
yeoutzer <loo_yeou_tzer@hotmail.com>
2021-03-12 04:23:14 +0800

    Edit TestUtil.java to change javadocs

--- src/test/java/seedu/address/testutil/TestUtil.java
+++ src/test/java/seedu/address/testutil/TestUtil.java
@@ -33,21 +33,21 @@ public class TestUtil {
     }
 
     /**
-     * Returns the middle index of the person in the {@code model}'s person list.
+     * Returns the middle index of the flashcard in the {@code model}'s flashcard list.
      */
     public static Index getMidIndex(Model model) {
         return Index.fromOneBased(model.getFilteredFlashcardList().size() / 2);
     }
 
     /**
-     * Returns the last index of the person in the {@code model}'s person list.
+     * Returns the last index of the flashcard in the {@code model}'s flashcard list.
      */
     public static Index getLastIndex(Model model) {
         return Index.fromOneBased(model.getFilteredFlashcardList().size());
     }
 
     /**
-     * Returns the person in the {@code model}'s person list at {@code index}.
+     * Returns the flashcard in the {@code model}'s flashcard list at {@code index}.
      */
     public static Flashcard getPerson(Model model, Index index) {
         return model.getFilteredFlashcardList().get(index.getZeroBased());
