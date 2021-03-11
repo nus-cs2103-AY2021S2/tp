e3507589e26e88cc63a296e6159d81e169777018
kieron560 <kieron560@gmail.com>
2021-03-11 21:43:28 +0800

    Fix Test Checkstyle

--- src/test/java/seedu/address/logic/commands/AddCommandIntegrationTest.java
+++ src/test/java/seedu/address/logic/commands/AddCommandIntegrationTest.java
@@ -3,7 +3,6 @@ package seedu.address.logic.commands;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.testutil.TypicalEvents.getTypicalEventBook;
-import static seedu.address.testutil.TypicalEvents.getTypicalEvents;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
 import org.junit.jupiter.api.BeforeEach;
--- src/test/java/seedu/address/testutil/TypicalEvents.java
+++ src/test/java/seedu/address/testutil/TypicalEvents.java
@@ -4,11 +4,9 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import seedu.address.model.AddressBook;
 import seedu.address.model.EventBook;
 import seedu.address.model.event.Event;
 import seedu.address.model.event.EventStatus;
-import seedu.address.model.person.Person;
 
 /**
  * A utility class containing a list of {@code Event} objects to be used in tests.
