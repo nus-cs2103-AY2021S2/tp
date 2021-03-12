6b91d7d2a6d29547aa260c28df659aad1099f503
chewterence <chewterence@hotmail.com>
2021-03-12 15:55:22 +0800

    Update logic to complete JUnit tests
    * remove redundant prefixes in CliSyntax
    * update parser classes to match updated CliSyntax

--- src/main/java/seedu/address/logic/parser/EditCommandParser.java
+++ src/main/java/seedu/address/logic/parser/EditCommandParser.java
@@ -22,9 +22,6 @@ import seedu.address.model.tag.Tag;
  */
 public class EditCommandParser implements Parser<EditCommand> {
 
-    public static final TripDay STUB_TRIPDAY = new TripDay("monday");
-    public static final TripTime STUB_TRIPTIME = new TripTime("2300");
-
     /**
      * Parses the given {@code String} of arguments in the context of the EditCommand
      * and returns an EditCommand object for execution.
@@ -33,8 +30,8 @@ public class EditCommandParser implements Parser<EditCommand> {
     public EditCommand parse(String args) throws ParseException {
         requireNonNull(args);
         ArgumentMultimap argMultimap =
-                ArgumentTokenizer.tokenize(args, PREFIX_NAME, PREFIX_PHONE, PREFIX_ADDRESS, PREFIX_TRIPDAYS,
-                        PREFIX_TRIPTIMES, PREFIX_TAG);
+                ArgumentTokenizer.tokenize(args, PREFIX_NAME, PREFIX_PHONE, PREFIX_ADDRESS, PREFIX_TRIPDAY,
+                        PREFIX_TRIPTIME, PREFIX_TAG);
 
         Index index;
 
@@ -54,10 +51,10 @@ public class EditCommandParser implements Parser<EditCommand> {
         if (argMultimap.getValue(PREFIX_ADDRESS).isPresent()) {
             editPersonDescriptor.setAddress(ParserUtil.parseAddress(argMultimap.getValue(PREFIX_ADDRESS).get()));
         }
-        if (argMultimap.getValue(PREFIX_TRIPDAYS).isPresent()) {
+        if (argMultimap.getValue(PREFIX_TRIPDAY).isPresent()) {
             editPersonDescriptor.setTripDay(ParserUtil.parseTripDay(argMultimap.getValue(PREFIX_TRIPDAY).get()));
         }
-        if (argMultimap.getValue(PREFIX_TRIPTIMES).isPresent()) {
+        if (argMultimap.getValue(PREFIX_TRIPTIME).isPresent()) {
             editPersonDescriptor.setTripTime(ParserUtil.parseTripTime(argMultimap.getValue(PREFIX_TRIPTIME).get()));
         }
 
