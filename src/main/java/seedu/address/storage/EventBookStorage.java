ee82068079499eaccf8adf4653eee405c5b399af
JerardSoh <jerard.soh@gmail.com>
2021-03-12 15:48:45 +0800

    Fix typo

--- src/main/java/seedu/address/storage/EventBookStorage.java
+++ src/main/java/seedu/address/storage/EventBookStorage.java
@@ -18,7 +18,7 @@ public interface EventBookStorage {
     Path getEventBookFilePath();
 
     /**
-     * Returns AddressBook data as a {@link ReadOnlyEventBook}.
+     * Returns EventBook data as a {@link ReadOnlyEventBook}.
      *   Returns {@code Optional.empty()} if storage file is not found.
      * @throws DataConversionException if the data in storage is not in the expected format.
      * @throws IOException if there was any problem when reading from the storage.
