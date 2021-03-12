fe073fa740c3a15c2947b7dd7f4ff3e46df987c0
umer <umergta@gmail.com>
2021-03-13 00:46:39 +0800

    change duplicate tasks to check for name and module instead of name only

--- src/main/java/seedu/module/model/task/Task.java
+++ src/main/java/seedu/module/model/task/Task.java
@@ -68,7 +68,7 @@ public class Task {
     }
 
     /**
-     * Returns true if both tasks have the same name.
+     * Returns true if both tasks have the same name and same module code.
      * This defines a weaker notion of equality between two tasks.
      */
     public boolean isSameTask(Task otherTask) {
@@ -77,7 +77,8 @@ public class Task {
         }
 
         return otherTask != null
-                && otherTask.getName().equals(getName());
+                && otherTask.getName().equals(getName())
+                && otherTask.getModule().equals(getModule());
     }
 
     /**
