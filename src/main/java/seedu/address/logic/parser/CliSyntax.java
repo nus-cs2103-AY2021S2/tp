1f12765146fa5468758a6f4e19134430e1039c2e
Prabhakaran-Gokul <prabhakaran.gokul@gmail.com>
2021-03-12 11:55:35 +0800

    Change Prefix of Review from r/ to re/

--- src/main/java/seedu/address/logic/parser/CliSyntax.java
+++ src/main/java/seedu/address/logic/parser/CliSyntax.java
@@ -8,7 +8,7 @@ public class CliSyntax {
     /* Prefix definitions */
     public static final Prefix PREFIX_NAME = new Prefix("n/");
     public static final Prefix PREFIX_PHONE = new Prefix("p/");
-    public static final Prefix PREFIX_REVIEW = new Prefix("r/");
+    public static final Prefix PREFIX_REVIEW = new Prefix("re/");
     public static final Prefix PREFIX_ADDRESS = new Prefix("a/");
     public static final Prefix PREFIX_TAG = new Prefix("t/");
 
