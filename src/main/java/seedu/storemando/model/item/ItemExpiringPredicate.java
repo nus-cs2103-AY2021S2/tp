a9470fb94fd85d23ad3e9987b64c03288e775943
chewwh09 <weishawtyhao9@gmail.com>
2021-03-12 17:40:44 +0800

    Add ItemExpiringPredicate class

---
+++ src/main/java/seedu/storemando/model/item/ItemExpiringPredicate.java
@@ -0,0 +1,29 @@
+package seedu.storemando.model.item;
+
+import static java.time.temporal.ChronoUnit.DAYS;
+
+import java.util.function.Predicate;
+import java.time.LocalDate;
+
+public class ItemExpiringPredicate implements Predicate<Item> {
+    private final Long index;
+
+    public ItemExpiringPredicate(Long index) {
+        this.index = index;
+    }
+
+    @Override
+    public boolean test(Item item) {
+        LocalDate itemExpiryDate = item.getExpiryDate().expiryDate;
+        LocalDate today = LocalDate.now();
+        Long daysDifference = DAYS.between(today, itemExpiryDate);
+        return daysDifference <= index;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+            || (other instanceof ItemExpiringPredicate // instanceof handles nulls
+            && index == ((ItemExpiringPredicate)other).index); // state check
+    }
+}
