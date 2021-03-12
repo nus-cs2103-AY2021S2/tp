6c69d38b33357dd768ec1e528513b83646e628ed
chewwh09 <weishawtyhao9@gmail.com>
2021-03-13 03:04:59 +0800

    Add ReminderCommandParser Test

---
+++ src/test/java/seedu/storemando/logic/parser/ReminderCommandParserTest.java
@@ -0,0 +1,28 @@
+package seedu.storemando.logic.parser;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.storemando.logic.commands.ReminderCommand;
+import seedu.storemando.model.item.ItemExpiringPredicate;
+
+import static seedu.storemando.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.storemando.logic.parser.CommandParserTestUtil.assertParseFailure;
+import static seedu.storemando.logic.parser.CommandParserTestUtil.assertParseSuccess;
+
+public class ReminderCommandParserTest {
+
+    private final ReminderCommandParser parser = new ReminderCommandParser();
+
+    @Test
+    public void parse_emptyArg_throwsParseException() {
+        assertParseFailure(parser, "  ",
+            String.format(MESSAGE_INVALID_COMMAND_FORMAT, ReminderCommand.MESSAGE_USAGE));
+    }
+
+    @Test
+    public void parse_validArgs_returnsReminderCommand() {
+        long numOfDays = 3;
+        ReminderCommand expectedReminderCommand =  new ReminderCommand(new ItemExpiringPredicate(numOfDays));
+        assertParseSuccess(parser, "3", expectedReminderCommand);
+    }
+}
