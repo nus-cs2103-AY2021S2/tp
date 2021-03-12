ba4aa65ee80600ccaf3af01b4e9f4af55c07bf68
LLG <37901425+whatthelump@users.noreply.github.com>
2021-03-12 12:03:09 +0800

    Update SampleDataUtil.java
    Changed the ordering of import statements to match lexicographical order.

--- src/main/java/seedu/address/model/util/SampleDataUtil.java
+++ src/main/java/seedu/address/model/util/SampleDataUtil.java
@@ -4,8 +4,8 @@ import java.util.Arrays;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-import seedu.address.model.ResidenceTracker;
 import seedu.address.model.ReadOnlyResidenceTracker;
+import seedu.address.model.ResidenceTracker;
 import seedu.address.model.person.Address;
 import seedu.address.model.person.Email;
 import seedu.address.model.person.Name;
