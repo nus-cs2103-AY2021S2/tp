d91726bf1d7d77b37633a169fa53e382a1b5efab
chewterence <chewterence@hotmail.com>
2021-03-12 16:39:48 +0800

    Fix TypicalPersons class and json file
    * update persons with valid TripDay

--- src/test/java/seedu/address/testutil/TypicalPersons.java
+++ src/test/java/seedu/address/testutil/TypicalPersons.java
@@ -16,42 +16,42 @@ public class TypicalPersons {
 
     public static final Person ALICE = new PersonBuilder().withName("Alice Pauline")
             .withAddress("123, Jurong West Ave 6, #08-111")
-            .withPhone("94351253").withTripDay(TRIPDAY_DESC_FRIDAY).withTripTime(TRIPTIME_DESC_EVENING)
+            .withPhone("94351253").withTripDay("FRIDAY").withTripTime("1800")
             .withTags("friends").build();
     public static final Person BENSON = new PersonBuilder().withName("Benson Meier")
             .withAddress("311, Clementi Ave 2, #02-25")
-            .withPhone("98765432").withTripDay(TRIPDAY_DESC_FRIDAY).withTripTime(TRIPTIME_DESC_MORNING)
+            .withPhone("98765432").withTripDay("FRIDAY").withTripTime("1800")
             .withTags("owesMoney", "friends").build();
     public static final Person CARL = new PersonBuilder().withName("Carl Kurz").withPhone("95352563")
-            .withTripDay(TRIPDAY_DESC_FRIDAY).withTripTime(TRIPTIME_DESC_EVENING)
+            .withTripDay("FRIDAY").withTripTime("1800")
             .withAddress("wall street").build();
     public static final Person DANIEL = new PersonBuilder().withName("Daniel Meier").withPhone("87652533")
-            .withTripDay(TRIPDAY_DESC_MONDAY).withTripTime(TRIPTIME_DESC_MORNING)
+            .withTripDay("FRIDAY").withTripTime("1800")
             .withAddress("10th street").withTags("friends").build();
     public static final Person ELLE = new PersonBuilder().withName("Elle Meyer").withPhone("9482224")
-            .withTripDay(TRIPDAY_DESC_FRIDAY).withTripTime(TRIPTIME_DESC_EVENING)
+            .withTripDay("FRIDAY").withTripTime("2100")
             .withAddress("michegan ave").build();
     public static final Person FIONA = new PersonBuilder().withName("Fiona Kunz").withPhone("9482427")
-            .withTripDay(TRIPDAY_DESC_MONDAY).withTripTime(TRIPTIME_DESC_MORNING)
+            .withTripDay("MONDAY").withTripTime("0830")
             .withAddress("little tokyo").build();
     public static final Person GEORGE = new PersonBuilder().withName("George Best").withPhone("9482442")
-            .withTripDay(TRIPDAY_DESC_MONDAY).withTripTime(TRIPTIME_DESC_MORNING)
+            .withTripDay("THURSDAY").withTripTime("0715")
             .withAddress("4th street").build();
 
     // Manually added
     public static final Person HOON = new PersonBuilder().withName("Hoon Meier").withPhone("8482424")
-            .withTripDay(TRIPDAY_DESC_FRIDAY).withTripTime(TRIPTIME_DESC_EVENING)
+            .withTripDay("FRIDAY").withTripTime("1800")
             .withAddress("little india").build();
     public static final Person IDA = new PersonBuilder().withName("Ida Mueller").withPhone("8482131")
-            .withTripDay(TRIPDAY_DESC_FRIDAY).withTripTime(TRIPTIME_DESC_EVENING)
+            .withTripDay("FRIDAY").withTripTime("1800")
             .withAddress("chicago ave").build();
 
     // Manually added - Person's details found in {@code CommandTestUtil}
     public static final Person AMY = new PersonBuilder().withName(VALID_NAME_AMY).withPhone(VALID_PHONE_AMY)
-            .withTripDay(TRIPDAY_DESC_FRIDAY).withTripTime(TRIPTIME_DESC_EVENING)
+            .withTripDay("FRIDAY").withTripTime("1800")
             .withAddress(VALID_ADDRESS_AMY).withTags(VALID_TAG_FRIEND).build();
     public static final Person BOB = new PersonBuilder().withName(VALID_NAME_BOB).withPhone(VALID_PHONE_BOB)
-            .withTripDay(TRIPDAY_DESC_FRIDAY).withTripTime(TRIPTIME_DESC_EVENING)
+            .withTripDay("FRIDAY").withTripTime("1800")
             .withAddress(VALID_ADDRESS_BOB).withTags(VALID_TAG_HUSBAND, VALID_TAG_FRIEND)
             .build();
 
