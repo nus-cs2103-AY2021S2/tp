832b8f38ac985e4480e6f78c415d1ab30ff5a2ad
SiTingST <33442908+SiTingST@users.noreply.github.com>
2021-03-14 00:25:42 +0800

    Add FilterCommand

---
+++ src/main/java/seedu/address/logic/parser/FilterCommandParser.java
@@ -0,0 +1,47 @@
+package seedu.address.logic.parser;
+
+import seedu.address.logic.commands.FilterCommand;
+import seedu.address.logic.commands.FindCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.person.*;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+
+/**
+ * Parses input arguments and creates a new FilterCommand object
+ */
+public class FilterCommandParser implements Parser<FilterCommand> {
+
+    private static final List<String> VaccinatedStatus = Arrays.asList("vaccinated", "unvaccinated");
+    private static final List<String> FACULTY = Arrays.asList("FASS", "BIZ", "COM", "SCALE", "DEN", "SDE", "DNUS",
+            "ENG", "ISEP", "LAW", "MED", "MUSIC", "SPH", "SPP", "SCI", "USP", "YNC");
+    ;
+    private static final List<String> SCHOOL_RESIDENCE = Arrays.asList("PGPH", "PGPR", "KE7H", "SH", "KRH", "TH", "EH",
+            "RH", "RVRC", "YNC", "TC", "CAPT", "RC4", "USP", "UTR", "DOES NOT LIVE ON CAMPUS");
+
+
+    /**
+     * Parses the given {@code String} of arguments in the context of the FilterCommand
+     * and returns a FilterCommand object for execution.
+     *
+     * @throws ParseException if the user input does not conform the expected format
+     */
+    public FilterCommand parse(String args) throws ParseException {
+
+        String filter_condition = args.trim();
+
+        if (VaccinatedStatus.contains(filter_condition)) {
+            return new FilterCommand(new VaccinationStatusContainsKeywords(filter_condition));
+        } else if (FACULTY.contains(filter_condition)) {
+            return new FilterCommand(new FacultyContainsKeywords(filter_condition));
+        } else if (SCHOOL_RESIDENCE.contains(filter_condition)) {
+            return new FilterCommand(new SchoolResidenceContainsKeywords(filter_condition));
+        } else {
+            throw new ParseException(
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, FilterCommand.MESSAGE_USAGE));
+        }
+    }
+}
