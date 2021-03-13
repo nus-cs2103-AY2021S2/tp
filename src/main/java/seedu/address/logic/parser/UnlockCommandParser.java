4e0aec52291fb552e9239556b278896f5c4f9808
Swa Yong Shen <yongshenswa@gmail.com>
2021-03-13 20:49:07 +0800

    Fix UnlockCommandParser bug where empty string was detected as valid argument

--- src/main/java/seedu/address/logic/parser/UnlockCommandParser.java
+++ src/main/java/seedu/address/logic/parser/UnlockCommandParser.java
@@ -12,7 +12,7 @@ public class UnlockCommandParser implements Parser<UnlockCommand> {
     public UnlockCommand parse(String args) throws ParseException {
         String trimmedArgs = args.trim();
         String[] splitArgs = trimmedArgs.split("\\s+");
-        if (splitArgs.length != 1) {
+        if (splitArgs.length != 1 || trimmedArgs.length() == 0) {
             throw new ParseException(String.format(Messages.MESSAGE_INVALID_COMMAND_FORMAT, LockCommand.MESSAGE_USAGE));
         }
         return new UnlockCommand(splitArgs[0]);
