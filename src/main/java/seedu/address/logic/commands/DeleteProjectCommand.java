4789bb56a4edcc2ae0b593214533eb7fdba506d7
Eriksen2411 <ducdanh24112001@gmail.com>
2021-03-14 06:28:28 +0800

    add Javadoc for deleteProjectCommand class

--- src/main/java/seedu/address/logic/commands/DeleteProjectCommand.java
+++ src/main/java/seedu/address/logic/commands/DeleteProjectCommand.java
@@ -10,6 +10,9 @@ import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.project.Project;
 
+/**
+ * Deletes a project identified using it's displayed index from the project list.
+ */
 public class DeleteProjectCommand extends Command {
 
     public static final String COMMAND_WORD = "deleteP";
