cab43b7529f177ce91e7eb91ac12a89d449fb8fc
arsatis <zhengyong@u.nus.edu>
2021-03-13 23:16:05 +0800

    Fix checkstyle error

--- src/test/java/seedu/smartlib/logic/parser/FindReaderCommandParserTest.java
+++ src/test/java/seedu/smartlib/logic/parser/FindReaderCommandParserTest.java
@@ -17,7 +17,8 @@ public class FindReaderCommandParserTest {
 
     @Test
     public void parse_emptyArg_throwsParseException() {
-        assertParseFailure(parser, "     ", String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindReaderCommand.MESSAGE_USAGE));
+        assertParseFailure(parser, "     ", String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                FindReaderCommand.MESSAGE_USAGE));
     }
 
     @Test
