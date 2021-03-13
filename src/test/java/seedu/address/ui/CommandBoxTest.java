2d32c0a367c6e0983582062338b870f7813885a0
Samuel Fang <samuelfangjw@gmail.com>
2021-03-13 21:07:26 +0800

    Fix broken test

--- src/test/java/seedu/address/ui/CommandBoxTest.java
+++ src/test/java/seedu/address/ui/CommandBoxTest.java
@@ -10,7 +10,7 @@ import org.junit.jupiter.api.Test;
 import guitests.guihandles.CommandBoxHandle;
 import javafx.scene.input.KeyCode;
 import seedu.address.logic.commands.CommandResult;
-import seedu.address.logic.commands.ListCommand;
+import seedu.address.logic.commands.ListContactsCommand;
 import seedu.address.logic.commands.exceptions.CommandException;
 
 /**
@@ -20,7 +20,7 @@ import seedu.address.logic.commands.exceptions.CommandException;
  */
 public class CommandBoxTest extends GuiUnitTest {
 
-    private static final String COMMAND_THAT_SUCCEEDS = ListCommand.COMMAND_WORD;
+    private static final String COMMAND_THAT_SUCCEEDS = ListContactsCommand.COMMAND_WORD;
     private static final String COMMAND_THAT_FAILS = "invalid command";
 
     private ArrayList<String> defaultStyleOfCommandBox;
