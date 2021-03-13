65d7d6e272650509d2614783c0e1b8cbdc4424ea
Ivan Tan <ivantjh97@gmail.com>
2021-03-14 00:35:37 +0800

    Add delete meeting cmd

--- src/main/java/seedu/address/logic/parser/CliSyntax.java
+++ src/main/java/seedu/address/logic/parser/CliSyntax.java
@@ -18,4 +18,7 @@ public class CliSyntax {
     public static final Prefix PREFIX_DATE = new Prefix("d/");
     public static final Prefix PREFIX_TIME = new Prefix("t/");
     public static final Prefix PREFIX_DESCRIPTION = new Prefix("desc/");
+
+    // For commands that need a secondary index
+    public static final Prefix PREFIX_INDEX = new Prefix("i/");
 }
