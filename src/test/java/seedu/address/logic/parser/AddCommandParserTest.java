47c275ac539c927812dffbac6f4368f94af297f6
arihantjain97 <suhailarihant@gmail.com>
2021-03-14 07:05:04 +0800

    checkstyle

--- src/test/java/seedu/address/logic/parser/AddCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/AddCommandParserTest.java
@@ -30,10 +30,13 @@ import static seedu.address.testutil.TypicalPersons.AMY;
 import static seedu.address.testutil.TypicalPersons.BOB;
 
 import org.junit.jupiter.api.Test;
-
 import seedu.address.logic.commands.AddCommand;
 import seedu.address.model.description.Description;
-import seedu.address.model.person.*;
+import seedu.address.model.person.Colour;
+import seedu.address.model.person.DressCode;
+import seedu.address.model.person.Name;
+import seedu.address.model.person.Person;
+import seedu.address.model.person.Size;
 import seedu.address.testutil.PersonBuilder;
 
 public class AddCommandParserTest {
