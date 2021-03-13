1ef1b2bb4e59b5edc2ec04a01180f9f9fe02091e
Joel Ho <joelhoengkiat@gmail.com>
2021-03-13 19:22:02 +0800

    Add interaction with model for DriveCommand
    Add capability to assign Drivers to DriveCommand

--- src/main/java/seedu/address/model/human/person/Person.java
+++ src/main/java/seedu/address/model/human/person/Person.java
@@ -41,6 +41,27 @@ public class Person extends Human {
         this.tags.addAll(tags);
     }
 
+    /**
+     * Creates a new {@code Person} with a driver.
+     * @param name the {@code Name} of the {@code Person}
+     * @param phone the {@code Phone} of the {@code Person}
+     * @param address the {@code Address} of the {@code Person}
+     * @param tripDay the {@code TripDay} of the {@code Person}
+     * @param tripTime the {@code TripTime} of the {@code Person}
+     * @param driver the {@code Driver} assigned to {@code Person}
+     * @param tags the {@code Tag}s of the {@code Person}
+     */
+    public Person(Name name, Phone phone, Address address, TripDay tripDay, TripTime tripTime, Driver driver,
+                  Set<Tag> tags) {
+        super(name, phone);
+        requireAllNonNull(address, tripDay, tripTime, tags);
+        this.address = address;
+        this.tripDay = tripDay;
+        this.tripTime = tripTime;
+        this.driver = Optional.of(driver);
+        this.tags.addAll(tags);
+    }
+
     public Address getAddress() {
         return address;
     }
