f194cb0408f96c528909fa54d87ce9bc0722884c
Donavan <donavanlim28@gmail.com>
2021-03-13 18:39:08 +0800

    Changed name to restaurant name

--- src/main/java/seedu/address/model/person/Name.java
+++ src/main/java/seedu/address/model/person/Name.java
@@ -4,7 +4,7 @@ import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.util.AppUtil.checkArgument;
 
 /**
- * Represents a Person's name in the address book.
+ * Represents a restaurant's name in The Food Diary
  * Guarantees: immutable; is valid as declared in {@link #isValidName(String)}
  */
 public class Name {
@@ -13,7 +13,7 @@ public class Name {
             "Names should only contain alphanumeric characters and spaces, and it should not be blank";
 
     /*
-     * The first character of the address must not be a whitespace,
+     * The first character of the name must not be a whitespace,
      * otherwise " " (a blank string) becomes a valid input.
      */
     public static final String VALIDATION_REGEX = "[\\p{Alnum}][\\p{Alnum} ]*";
@@ -21,9 +21,9 @@ public class Name {
     public final String fullName;
 
     /**
-     * Constructs a {@code Name}.
+     * Constructs a {@code Restaurant Name}.
      *
-     * @param name A valid name.
+     * @param name A valid restaurant name.
      */
     public Name(String name) {
         requireNonNull(name);
@@ -32,7 +32,7 @@ public class Name {
     }
 
     /**
-     * Returns true if a given string is a valid name.
+     * Returns true if a given string is a valid restaurant name.
      */
     public static boolean isValidName(String test) {
         return test.matches(VALIDATION_REGEX);
