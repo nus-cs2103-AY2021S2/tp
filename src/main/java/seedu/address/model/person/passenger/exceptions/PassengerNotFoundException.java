2ec27ddc7b8926ec2a8f7d1054487073590046a4
Joel Ho <joelhoengkiat@gmail.com>
2021-03-14 03:10:33 +0800

    Refactor Person->Passenger

--- src/main/java/seedu/address/model/person/passenger/exceptions/PassengerNotFoundException.java
+++ src/main/java/seedu/address/model/person/passenger/exceptions/PassengerNotFoundException.java
@@ -1,6 +1,6 @@
 package seedu.address.model.person.passenger.exceptions;
 
 /**
- * Signals that the operation is unable to find the specified person.
+ * Signals that the operation is unable to find the specified passenger.
  */
-public class PersonNotFoundException extends RuntimeException {}
+public class PassengerNotFoundException extends RuntimeException {}
