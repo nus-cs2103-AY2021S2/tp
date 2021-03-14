be714743d509077ae356aea742e5abbf349de405
Assyarul <muhdassyarul@gmail.com>
2021-03-14 15:36:55 +0800

    Add Friend Group List Ui
    No Ui in place to display the list of groups available in the app.
    
    Let's implement the Ui in.
    
    No testing has been done in this commit.

---
+++ src/main/java/seedu/address/ui/GroupListPanel.java
@@ -0,0 +1,60 @@
+package seedu.address.ui;
+
+import java.util.logging.Logger;
+
+import javafx.application.Platform;
+import javafx.collections.MapChangeListener;
+import javafx.collections.ObservableMap;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.Region;
+import javafx.util.Duration;
+import seedu.address.commons.core.LogsCenter;
+import seedu.address.model.group.Group;
+import seedu.address.model.person.Name;
+
+public class GroupListPanel extends UiPart<Region> {
+    private static final String FXML = "GroupListPanel.fxml";
+    private final Logger logger = LogsCenter.getLogger(GroupListPanel.class);
+
+    @FXML
+    ListView<Name> groupListView;
+
+    /**
+     * Creates a {@code GroupListPanel} with the given {@code ObservableMap}.
+     */
+    public GroupListPanel(ObservableMap<Name, Group> groupMap) {
+        super(FXML);
+        groupListView.getItems().addAll(groupMap.keySet());
+        groupMap.addListener((MapChangeListener<Name, Group>) change -> {
+            if (change.wasAdded()) {
+                groupListView.getItems().add(change.getKey());
+            }
+            Platform.runLater(() ->
+            {
+                groupListView.getSelectionModel().select(change.getKey());
+                groupListView.scrollTo(change.getKey());
+            });
+        });
+        groupListView.setCellFactory(listView -> new GroupListPanel.GroupListViewCell());
+    }
+
+    /**
+     * Custom {@code ListCell} that displays the graphics of a Group {@code Name}.
+     */
+    class GroupListViewCell extends ListCell<Name> {
+        @Override
+        protected void updateItem(Name groupName, boolean empty) {
+            super.updateItem(groupName, empty);
+
+            if (empty || groupName == null) {
+                setGraphic(null);
+                setText(null);
+            } else {
+                setGraphic(new Label(groupName.fullName));
+            }
+        }
+    }
+}
--- src/main/java/seedu/address/ui/MainWindow.java
+++ src/main/java/seedu/address/ui/MainWindow.java
@@ -32,6 +32,7 @@ public class MainWindow extends UiPart<Stage> {
 
     // Independent Ui parts residing in this Ui container
     private PersonListPanel personListPanel;
+    private GroupListPanel groupListPanel;
     private ResultDisplay resultDisplay;
     private HelpWindow helpWindow;
 
@@ -43,6 +44,9 @@ public class MainWindow extends UiPart<Stage> {
 
     @FXML
     private StackPane personListPanelPlaceholder;
+
+    @FXML
+    private StackPane groupListPanelPlaceholder;
 
     @FXML
     private StackPane resultDisplayPlaceholder;
@@ -110,6 +114,9 @@ public class MainWindow extends UiPart<Stage> {
      * Fills up all the placeholders of this window.
      */
     void fillInnerParts() {
+        groupListPanel = new GroupListPanel(logic.getAddressBook().getGroupMap());
+        groupListPanelPlaceholder.getChildren().add(groupListPanel.getRoot());
+
         personListPanel = new PersonListPanel(logic.getFilteredPersonList());
         personListPanelPlaceholder.getChildren().add(personListPanel.getRoot());
 
