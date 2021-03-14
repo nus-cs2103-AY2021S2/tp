356942cc3c54a58973a58ac619eb0db1b560931a
TeoHoeKeat <TeoHoeKeat@gmail.com>
2021-03-14 16:24:21 +0800

    Update UI : Add error style for error type  feedback

--- src/main/java/seedu/dictionote/ui/MainWindow.java
+++ src/main/java/seedu/dictionote/ui/MainWindow.java
@@ -410,7 +410,7 @@ public class MainWindow extends UiPart<Stage> {
             return commandResult;
         } catch (CommandException | ParseException e) {
             logger.info("Invalid command: " + commandText);
-            resultDisplay.setFeedbackToUser(e.getMessage());
+            resultDisplay.setErrorFeedbackToUser(e.getMessage());
             throw e;
         }
     }
--- src/main/java/seedu/dictionote/ui/ResultDisplay.java
+++ src/main/java/seedu/dictionote/ui/ResultDisplay.java
@@ -2,6 +2,7 @@ package seedu.dictionote.ui;
 
 import static java.util.Objects.requireNonNull;
 
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.layout.Region;
@@ -11,6 +12,7 @@ import javafx.scene.layout.Region;
  */
 public class ResultDisplay extends UiPart<Region> {
 
+    public static final String ERROR_STYLE_CLASS = "error";
     private static final String FXML = "ResultDisplay.fxml";
 
     @FXML
@@ -20,9 +22,42 @@ public class ResultDisplay extends UiPart<Region> {
         super(FXML);
     }
 
+    /**
+     * Sets feed back text with default style
+     */
     public void setFeedbackToUser(String feedbackToUser) {
         requireNonNull(feedbackToUser);
         resultDisplay.setText(feedbackToUser);
+        setStyleToDefault();
+    }
+
+    /**
+     * Sets feed back text with error style
+     */
+    public void setErrorFeedbackToUser(String feedbackToUser) {
+        requireNonNull(feedbackToUser);
+        resultDisplay.setText(feedbackToUser);
+        setStyleToIndicateFailure();
+    }
+
+    /**
+     * Sets the result box style to indicate a failed command.
+     */
+    private void setStyleToIndicateFailure() {
+        ObservableList<String> styleClass = resultDisplay.getStyleClass();
+
+        if (styleClass.contains(ERROR_STYLE_CLASS)) {
+            return;
+        }
+
+        styleClass.add(ERROR_STYLE_CLASS);
+    }
+
+    /**
+     * Sets the result box style to use the default style.
+     */
+    private void setStyleToDefault() {
+        resultDisplay.getStyleClass().remove(ERROR_STYLE_CLASS);
     }
 
 }
