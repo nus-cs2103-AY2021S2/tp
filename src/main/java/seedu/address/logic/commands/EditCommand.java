ee33c55b4d27fb7f18452ebcfd9c1889c1dbe160
Joel Ho <joelhoengkiat@gmail.com>
2021-03-14 15:10:04 +0800

    Fix EditCommand wrong merge resolution
    Fix import PREDICATE_SHOW_ALL_PERSON -> PREDICATE_SHOW_ALL_PASSENGERS

--- src/main/java/seedu/address/logic/commands/EditCommand.java
+++ src/main/java/seedu/address/logic/commands/EditCommand.java
@@ -7,7 +7,7 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TRIPDAY;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TRIPTIME;
-import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
+import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PASSENGERS;
 
 import java.util.Collections;
 import java.util.HashSet;
