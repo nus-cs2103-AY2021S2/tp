2f2b830c03e2ddf14802fbdce6e0cc08f3faf82f
Cp-John <e0376984@u.nus.edu>
2021-03-14 19:50:37 +0800

    Change javadoc comment in delete appointment command parser

--- src/main/java/seedu/address/logic/parser/DeleteAppointmentCommandParser.java
+++ src/main/java/seedu/address/logic/parser/DeleteAppointmentCommandParser.java
@@ -8,7 +8,7 @@ import seedu.address.logic.commands.DeleteCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 
 /**
- * Parses input arguments and creates a new DeleteCommand object
+ * Parses input arguments and creates a new DeleteAppointmentCommand object
  */
 public class DeleteAppointmentCommandParser implements Parser<DeleteAppointmentCommand> {
 
