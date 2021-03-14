2a9281406807c627bdbf1d3cc3314d7a88bab46f
pavitra <pavirajan02@gmail.com>
2021-03-14 13:52:14 +0800

    The same edits as the previous editing-help-command branch.

--- src/main/java/seedu/address/logic/commands/ClearCommand.java
+++ src/main/java/seedu/address/logic/commands/ClearCommand.java
@@ -12,6 +12,7 @@ public class ClearCommand extends Command {
 
     public static final String COMMAND_WORD = "clear";
     public static final String MESSAGE_SUCCESS = "CakeCollate has been cleared!";
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Clears all entries from CakeCollate.";
 
 
     @Override
