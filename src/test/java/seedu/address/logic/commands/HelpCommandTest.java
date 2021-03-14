95567172e1d1dce7d30a0e271a8a7155f06c1838
Jellybeano <lanyuxuan98@gmail.com>
2021-03-15 02:04:03 +0800

    Fix checkstyle erros

--- src/test/java/seedu/address/logic/commands/HelpCommandTest.java
+++ src/test/java/seedu/address/logic/commands/HelpCommandTest.java
@@ -12,10 +12,10 @@ public class HelpCommandTest {
     private Model model = new ModelManager();
     private Model expectedModel = new ModelManager();
 
-        @Test
-        public void execute_help_success() {
-            CommandResult expectedCommandResult = new CommandResult(
-                    SHOWING_HELP_MESSAGE, true, false, false);
-            assertCommandSuccess(new HelpCommand(), model, expectedCommandResult, expectedModel);
-        }
+    @Test
+    public void execute_help_success() {
+        CommandResult expectedCommandResult = new CommandResult(
+                SHOWING_HELP_MESSAGE, true, false, false);
+        assertCommandSuccess(new HelpCommand(), model, expectedCommandResult, expectedModel);
+    }
 }
