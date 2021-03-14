1b0d63ee56591c425fd8e08c895992d4a0712076
pngsebastian <pngsebastian@gmail.com>
2021-03-15 03:49:07 +0800

    fix: reorder import statements

--- src/main/java/seedu/address/logic/commands/DeleteAppointmentCommand.java
+++ src/main/java/seedu/address/logic/commands/DeleteAppointmentCommand.java
@@ -1,13 +1,14 @@
 package seedu.address.logic.commands;
 
+import static java.util.Objects.requireNonNull;
+
 import javafx.collections.ObservableList;
+
 import seedu.address.commons.core.Messages;
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.appointment.Appointment;
-
-import static java.util.Objects.requireNonNull;
 
 /**
  * Deletes an appointment from the appointment schedule
