bd1209647d1f5cda70d5f4ba78857d80e574dae2
Hzxin <E0424941@u.nus.edu>
2021-03-15 01:05:08 +0800

    Add LocalDateTimeUtil

---
+++ src/main/java/seedu/address/commons/util/LocalDateTimeUtil.java
@@ -0,0 +1,38 @@
+package seedu.address.commons.util;
+
+import static java.util.Objects.requireNonNull;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.time.format.ResolverStyle;
+
+public class LocalDateTimeUtil {
+
+
+    public static final String DATE_TIME_CONSTRAINTS =
+            "DateTime should be a valid date and time in the format of dd-MM-yyyy HH:mm. e.g. 29-11-2020 23:59";
+    public static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("dd-MM-uuuu")
+            .withResolverStyle(ResolverStyle.STRICT);
+    public static final DateTimeFormatter DATETIME_FORMATTER = DateTimeFormatter.ofPattern("dd-MM-uuuu HHmm")
+            .withResolverStyle(ResolverStyle.STRICT);
+
+
+    /**
+     * Returns true if a given string is a valid date number.
+     *
+     * @param test the string value to be put to test.
+     * @return true if the test string is valid and false otherwise
+     */
+    public static boolean isValidDateTime(String test) {
+        requireNonNull(test);
+        try {
+            LocalDateTime.parse(test, DATETIME_FORMATTER);
+        } catch (DateTimeParseException e) {
+            return false;
+        }
+        return true;
+    }
+
+    
+}
