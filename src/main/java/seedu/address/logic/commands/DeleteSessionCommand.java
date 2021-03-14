9f9d24403ddd005777aaa540528454cdc1767292
chanellNg <ngchanell287@gmail.com>
2021-03-15 03:18:42 +0800

    Updated checkstyle for delete session

--- src/main/java/seedu/address/logic/commands/DeleteSessionCommand.java
+++ src/main/java/seedu/address/logic/commands/DeleteSessionCommand.java
@@ -1,9 +1,8 @@
 package seedu.address.logic.commands;
 
+import static java.util.Objects.requireNonNull;
 import java.util.List;
 import java.util.Optional;
-
-import static java.util.Objects.requireNonNull;
 
 import seedu.address.commons.core.Messages;
 import seedu.address.logic.commands.exceptions.CommandException;
