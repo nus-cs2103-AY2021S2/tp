c23d2e061741818dc16185c495e6a3ce87ac5209
Jared Lim <jaredlim1998@hotmail.com>
2021-03-14 19:11:14 +0800

    Fixed integration errors

--- src/main/java/seedu/address/logic/commands/appointmentcommands/DeleteAppointmentCommand.java
+++ src/main/java/seedu/address/logic/commands/appointmentcommands/DeleteAppointmentCommand.java
@@ -6,7 +6,6 @@ import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.Command;
 import seedu.address.logic.commands.CommandResult;
 import seedu.address.logic.commands.exceptions.CommandException;
-import seedu.address.model.Appointment;
 import seedu.address.model.Model;
 import seedu.address.model.appointment.Appointment;
 
