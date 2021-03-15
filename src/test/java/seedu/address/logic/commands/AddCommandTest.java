203c066e0f07a7a504fa31ab45ea4e440ea2b29c
KevinLohJunYong <e0439232@u.nus.edu>
2021-03-16 01:37:45 +0800

    done

--- src/test/java/seedu/address/logic/commands/AddCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddCommandTest.java
@@ -130,11 +130,6 @@ public class AddCommandTest {
         }
 
         @Override
-        public boolean hasSchedule(Schedule schedule) {
-            throw new AssertionError("This method should not be called.");
-        }
-
-        @Override
         public void deletePerson(Person target) {
             throw new AssertionError("This method should not be called.");
         }
