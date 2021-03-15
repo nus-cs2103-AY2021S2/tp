f9d67316252e184cd108c1d8411af738507f5be3
Onn Wei Sin <onnwards@gmail.com>
2021-03-15 23:20:24 +0800

    modify list and clear patient command words

--- src/main/java/seedu/address/logic/commands/ListPatientCommand.java
+++ src/main/java/seedu/address/logic/commands/ListPatientCommand.java
@@ -10,9 +10,8 @@ import seedu.address.model.Model;
  */
 public class ListPatientCommand extends Command {
 
-    public static final String COMMAND_WORD = "list";
-
-    public static final String MESSAGE_SUCCESS = "Listed all persons";
+    public static final String COMMAND_WORD = "list-patient";
+    public static final String MESSAGE_SUCCESS = "Listed all patients";
 
 
     @Override
