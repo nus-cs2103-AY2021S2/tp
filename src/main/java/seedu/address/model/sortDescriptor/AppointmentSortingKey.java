6aae05d05c38c1ffb6b7ad0bd8f25566f0a8364b
Cp-John <e0376984@u.nus.edu>
2021-03-15 21:19:30 +0800

    Change field name for AppointmentSortingKey

--- src/main/java/seedu/address/model/sortDescriptor/AppointmentSortingKey.java
+++ src/main/java/seedu/address/model/sortDescriptor/AppointmentSortingKey.java
@@ -1,2 +1,60 @@
-package seedu.address.model.sortDescriptor;public class AppointmentSortingKey {
+package seedu.address.model.sortDescriptor;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.AppUtil.checkArgument;
+
+/**
+ * Represents a value.
+ * Guarantees: immutable; is valid as declared in {@link #isValidAppointmentSortingKey(String)}.
+ */
+public class AppointmentSortingKey {
+    public static final String MESSAGE_CONSTRAINTS = "AppointmentSortingKey can take value of either datetime or name" +
+            ", and it should not be any other values";
+
+    private static final String VALIDATION_REGEX = "datetime|name";
+
+    public final String value;
+
+    /**
+     * Constructs a {@code AppointmentSortingKey}.
+     *
+     * @param value A valid value representing an AppointmentSortingKey.
+     */
+    public AppointmentSortingKey(String value) {
+        requireNonNull(value);
+        checkArgument(isValidAppointmentSortingKey(value), MESSAGE_CONSTRAINTS);
+        this.value = value;
+    }
+
+    /**
+     * Returns true if a given string is a valid value.
+     *
+     * @param test The string to test.
+     * @return True if the given string is a valid value, otherwise false.
+     */
+    public static boolean isValidAppointmentSortingKey(String test) {
+        return test.matches(VALIDATION_REGEX);
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (this == other) {
+            return true;
+        }
+        if (!(other instanceof AppointmentSortingKey)) {
+            return false;
+        }
+        AppointmentSortingKey otherAppointmentSortingKey = (AppointmentSortingKey) other;
+        return value.equals(otherAppointmentSortingKey.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
 }
