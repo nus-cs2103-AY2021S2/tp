151a4336ec1bc637f30c49744accc7de3410bc09
tsh22 <sihuitoh22@gmail.com>
2021-03-16 00:40:21 +0800

    Update comments

--- src/main/java/seedu/address/model/ReadOnlyAddressBook.java
+++ src/main/java/seedu/address/model/ReadOnlyAddressBook.java
@@ -16,8 +16,8 @@ public interface ReadOnlyAddressBook {
     ObservableList<Person> getPersonList();
 
     /**
-     * Returns an unmodifiable view of the persons list.
-     * This list will not contain any duplicate persons.
+     * Returns an unmodifiable view of the tasks list.
+     * This list will not contain any duplicate tasks.
      */
     ObservableList<Task> getTaskList();
 }
