7e2e0c41e15468fcb5ed107d154f9a1f00c5a4f9
Cp-John <e0376984@u.nus.edu>
2021-03-15 16:22:19 +0800

    Delete unused import

--- src/test/java/seedu/address/logic/commands/AddAppointmentCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddAppointmentCommandTest.java
@@ -17,7 +17,6 @@ import javafx.collections.ObservableList;
 import seedu.address.commons.core.GuiSettings;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
-import seedu.address.model.PropertyBook;
 import seedu.address.model.ReadOnlyAddressBook;
 import seedu.address.model.ReadOnlyAppointmentBook;
 import seedu.address.model.ReadOnlyPropertyBook;
