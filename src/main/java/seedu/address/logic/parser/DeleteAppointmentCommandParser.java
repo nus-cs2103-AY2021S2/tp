73353c31f0953fb05ac64df09130ae1990b407e5
pngsebastian <pngsebastian@gmail.com>
2021-03-15 20:06:44 +0800

    fix: add implementation of Parser<DeleteAppointmentCommand>

--- src/main/java/seedu/address/logic/parser/DeleteAppointmentCommandParser.java
+++ src/main/java/seedu/address/logic/parser/DeleteAppointmentCommandParser.java
@@ -9,7 +9,7 @@ import seedu.address.logic.parser.exceptions.ParseException;
 /**
  * Parses input arguments and creates a new DeleteAppointmentCommand object
  */
-public class DeleteAppointmentCommandParser {
+public class DeleteAppointmentCommandParser implements Parser<DeleteAppointmentCommand> {
 
     /**
      * Parses the given {@code String} of arguments in the context of the DeleteAppointmentCommand
