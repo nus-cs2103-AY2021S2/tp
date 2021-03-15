ae80a3476e761d6334d7b808b55664991d83ca71
yuchen <yuchenwang97@gmail.com>
2021-03-15 10:05:45 +0800

    Remove unused imports.

--- src/test/java/seedu/address/logic/commands/FindAppointmentCommandTest.java
+++ src/test/java/seedu/address/logic/commands/FindAppointmentCommandTest.java
@@ -3,11 +3,8 @@ package seedu.address.logic.commands;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static seedu.address.commons.core.Messages.MESSAGE_APPOINTMENT_LISTED_OVERVIEW;
 import static seedu.address.commons.core.Messages.MESSAGE_APPOINTMENT_LISTED_OVERVIEW_SINGULAR;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
-import static seedu.address.testutil.TypicalAppointments.MEET_ALEX;
-import static seedu.address.testutil.TypicalAppointments.MEET_BOB;
 import static seedu.address.testutil.TypicalAppointments.getTypicalAppointmentBook;
 
 import java.util.Arrays;
