e0e6750cb4a3ccade722d34b7c86fe577bb81934
tsh22 <sihuitoh22@gmail.com>
2021-03-16 00:36:35 +0800

    Update comments

--- src/main/java/seedu/address/logic/parser/FindTaskCommandParser.java
+++ src/main/java/seedu/address/logic/parser/FindTaskCommandParser.java
@@ -12,7 +12,7 @@ public class FindTaskCommandParser implements Parser<FindTaskCommand> {
 
     /**
      * Parses the given {@code String} of arguments in the context of the FindCommand
-     * and returns a FindCommand object for execution.
+     * and returns a FindTaskCommand object for execution.
      * @throws ParseException if the user input does not conform the expected format
      */
     public FindTaskCommand parse(String args) throws ParseException {
