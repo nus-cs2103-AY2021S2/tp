845b0e56f021b56f0b733074409342fd49377e3c
Cp-John <e0376984@u.nus.edu>
2021-03-15 19:30:15 +0800

    Remove unused import statement

--- src/test/java/seedu/address/logic/commands/ClearAllCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ClearAllCommandTest.java
@@ -8,7 +8,6 @@ import org.junit.jupiter.api.Test;
 
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
-import seedu.address.model.PropertyBook;
 import seedu.address.model.UserPrefs;
 
 public class ClearAllCommandTest {
