3e2dfaafa41b8a992bf1f65d86496c5f4d844fb1
Samuel Fang <samuelfangjw@gmail.com>
2021-03-16 17:40:53 +0800

    Add tests for FileUtil

--- src/test/java/seedu/address/commons/util/FileUtilTest.java
+++ src/test/java/seedu/address/commons/util/FileUtilTest.java
@@ -1,12 +1,21 @@
 package seedu.address.commons.util;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.testutil.Assert.assertThrows;
 
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
 
 public class FileUtilTest {
+
+    @TempDir
+    public Path testFolder;
 
     @Test
     public void isValidPath() {
@@ -20,4 +29,15 @@ public class FileUtilTest {
         assertThrows(NullPointerException.class, () -> FileUtil.isValidPath(null));
     }
 
+    @Test
+    public void createFile_fileExists_success() throws IOException {
+        // valid path
+        Path validFilePath = testFolder.resolve("valid/file/path");
+
+        // create file at path
+        FileUtil.createFile(validFilePath);
+
+        // method does not throw error
+        assertDoesNotThrow(() -> FileUtil.createFile(validFilePath));
+    }
 }
