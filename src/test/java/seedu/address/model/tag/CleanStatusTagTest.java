3bd38e1d0d0ec0f589714d2e8bd8c68a187708d6
wangtao0717 <e0688546@u.nus.edu>
2021-03-16 20:07:29 +0800

    add a CleanStatusTagTest

---
+++ src/test/java/seedu/address/model/tag/CleanStatusTagTest.java
@@ -0,0 +1,41 @@
+package seedu.address.model.residence;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.testutil.Assert.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.model.tag.CleanStatusTag;
+
+public class CleanStatusTagTest {
+
+    @Test
+    public void constructor_null_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> new CleanStatusTag(null));
+    }
+
+    @Test
+    public void constructor_invalidName_throwsIllegalArgumentException() {
+        String invalidName = "";
+        assertThrows(IllegalArgumentException.class, () -> new CleanStatusTag(invalidName));
+    }
+
+    @Test
+    public void isValidName() {
+        // null name
+        assertThrows(NullPointerException.class, () -> CleanStatusTag.isValidCleanStatusTag(null));
+
+        // invalid name
+        assertFalse(CleanStatusTag.isValidCleanStatusTag("")); // empty string
+        assertFalse(CleanStatusTag.isValidCleanStatusTag(" ")); // spaces only
+        assertFalse(CleanStatusTag.isValidCleanStatusTag("^")); // only non-alphanumeric characters
+        assertFalse(CleanStatusTag.isValidCleanStatusTag("peter*")); // contains non-alphanumeric characters
+
+        // valid name
+        assertTrue(CleanStatusTag.isValidCleanStatusTag("y")); // lowercase y
+        assertTrue(CleanStatusTag.isValidCleanStatusTag("Y")); // uppercase Y
+        assertTrue(CleanStatusTag.isValidCleanStatusTag("n")); // lowercase n
+        assertTrue(CleanStatusTag.isValidCleanStatusTag("N")); // uppercase N
+    }
+}
