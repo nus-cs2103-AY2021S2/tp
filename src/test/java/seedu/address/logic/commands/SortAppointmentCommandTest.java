0936d84de3dba59bb3e222c86a08442a2c772010
Cp-John <e0376984@u.nus.edu>
2021-03-16 15:17:50 +0800

    Finish updating SortAppointmentCommandTest

--- src/test/java/seedu/address/logic/commands/SortAppointmentCommandTest.java
+++ src/test/java/seedu/address/logic/commands/SortAppointmentCommandTest.java
@@ -2,57 +2,145 @@ package seedu.address.logic.commands;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static seedu.address.logic.commands.CommandTestUtil.DESC_AMY;
-import static seedu.address.logic.commands.CommandTestUtil.DESC_BOB;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_ADDRESS_BOB;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_EMAIL_BOB;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_NAME_BOB;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_PHONE_BOB;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_TAG_HUSBAND;
+import static seedu.address.logic.commands.CommandTestUtil.ASC_DATETIME;
+import static seedu.address.logic.commands.CommandTestUtil.DESC_NAME;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_SORTING_KEY_APPOINTMENT_DATETIME;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_SORTING_KEY_APPOINTMENT_NAME;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_SORTING_ORDER_ASC;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_SORTING_ORDER_DESC;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.testutil.TypicalAppointments.getTypicalAppointmentBook;
+
+import java.util.Comparator;
 
 import org.junit.jupiter.api.Test;
 
-import seedu.address.logic.commands.EditCommand.EditPersonDescriptor;
-import seedu.address.testutil.EditPersonDescriptorBuilder;
+import seedu.address.logic.commands.SortAppointmentCommand.SortAppointmentDescriptor;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+import seedu.address.model.appointment.Appointment;
+import seedu.address.testutil.SortAppointmentDescriptorBuilder;
+
 
 public class SortAppointmentCommandTest {
+
+    private Model model = new ModelManager(getTypicalAppointmentBook(), new UserPrefs());
+
+    @Test
+    public void execute_filterByDatetimeInAscendingOrder_success() {
+        SortAppointmentDescriptor descriptor = new SortAppointmentDescriptorBuilder()
+                .withSortingOrder(VALID_SORTING_ORDER_ASC)
+                .withAppointmentSortingKey(VALID_SORTING_KEY_APPOINTMENT_DATETIME)
+                .build();
+        SortAppointmentCommand sortAppointmentCommand = new SortAppointmentCommand(descriptor);
+
+        String expectedMessage = String.format(SortAppointmentCommand.MESSAGE_SUCCESS, descriptor);
+
+        Model expectedModel = new ModelManager(getTypicalAppointmentBook(), new UserPrefs());
+
+        Comparator<Appointment> comparator = (o1, o2) -> {
+            if (o1.getDate().compareTo(o2.getDate()) == 0) {
+                return o1.getTime().compareTo(o2.getTime());
+            } else {
+                return o1.getDate().compareTo(o2.getDate());
+            }
+        };
+
+        expectedModel.sortAppointmentList(comparator);
+
+        assertCommandSuccess(sortAppointmentCommand, model, expectedMessage, expectedModel);
+    }
+
+    @Test
+    public void execute_filterByDatetimeInDescendingOrder_success() {
+        SortAppointmentDescriptor descriptor = new SortAppointmentDescriptorBuilder()
+                .withSortingOrder(VALID_SORTING_ORDER_DESC)
+                .withAppointmentSortingKey(VALID_SORTING_KEY_APPOINTMENT_DATETIME)
+                .build();
+        SortAppointmentCommand sortAppointmentCommand = new SortAppointmentCommand(descriptor);
+
+        String expectedMessage = String.format(SortAppointmentCommand.MESSAGE_SUCCESS, descriptor);
+
+        Model expectedModel = new ModelManager(getTypicalAppointmentBook(), new UserPrefs());
+
+        Comparator<Appointment> comparator = (o1, o2) -> {
+            if (o1.getDate().compareTo(o2.getDate()) == 0) {
+                return -1 * o1.getTime().compareTo(o2.getTime());
+            } else {
+                return -1 * o1.getDate().compareTo(o2.getDate());
+            }
+        };
+
+        expectedModel.sortAppointmentList(comparator);
+
+        assertCommandSuccess(sortAppointmentCommand, model, expectedMessage, expectedModel);
+    }
+
+    @Test
+    public void execute_filterByNameInAscendingOrder_success() {
+        SortAppointmentDescriptor descriptor = new SortAppointmentDescriptorBuilder()
+                .withSortingOrder(VALID_SORTING_ORDER_ASC)
+                .withAppointmentSortingKey(VALID_SORTING_KEY_APPOINTMENT_NAME)
+                .build();
+        SortAppointmentCommand sortAppointmentCommand = new SortAppointmentCommand(descriptor);
+
+        String expectedMessage = String.format(SortAppointmentCommand.MESSAGE_SUCCESS, descriptor);
+
+        Model expectedModel = new ModelManager(getTypicalAppointmentBook(), new UserPrefs());
+
+        Comparator<Appointment> comparator = (o1, o2) -> o1.getName().compareTo(o2.getName());
+
+        expectedModel.sortAppointmentList(comparator);
+
+        assertCommandSuccess(sortAppointmentCommand, model, expectedMessage, expectedModel);
+    }
+
+    @Test
+    public void execute_filterByNameInDescendingOrder_success() {
+        SortAppointmentDescriptor descriptor = new SortAppointmentDescriptorBuilder()
+                .withSortingOrder(VALID_SORTING_ORDER_DESC)
+                .withAppointmentSortingKey(VALID_SORTING_KEY_APPOINTMENT_NAME)
+                .build();
+        SortAppointmentCommand sortAppointmentCommand = new SortAppointmentCommand(descriptor);
+
+        String expectedMessage = String.format(SortAppointmentCommand.MESSAGE_SUCCESS, descriptor);
+
+        Model expectedModel = new ModelManager(getTypicalAppointmentBook(), new UserPrefs());
+
+        Comparator<Appointment> comparator = (o1, o2) -> -1 * o1.getName().compareTo(o2.getName());
+
+        expectedModel.sortAppointmentList(comparator);
+
+        assertCommandSuccess(sortAppointmentCommand, model, expectedMessage, expectedModel);
+    }
 
     @Test
     public void equals() {
         // same values -> returns true
-        EditPersonDescriptor descriptorWithSameValues = new EditPersonDescriptor(DESC_AMY);
-        assertTrue(DESC_AMY.equals(descriptorWithSameValues));
+        SortAppointmentDescriptor descriptorWithSameValues = new SortAppointmentDescriptor(ASC_DATETIME);
+        assertTrue(ASC_DATETIME.equals(descriptorWithSameValues));
 
         // same object -> returns true
-        assertTrue(DESC_AMY.equals(DESC_AMY));
+        assertTrue(ASC_DATETIME.equals(ASC_DATETIME));
 
         // null -> returns false
-        assertFalse(DESC_AMY.equals(null));
+        assertFalse(ASC_DATETIME.equals(null));
 
         // different types -> returns false
-        assertFalse(DESC_AMY.equals(5));
+        assertFalse(ASC_DATETIME.equals(5));
 
         // different values -> returns false
-        assertFalse(DESC_AMY.equals(DESC_BOB));
+        assertFalse(ASC_DATETIME.equals(DESC_NAME));
 
-        // different name -> returns false
-        EditPersonDescriptor editedAmy = new EditPersonDescriptorBuilder(DESC_AMY).withName(VALID_NAME_BOB).build();
-        assertFalse(DESC_AMY.equals(editedAmy));
+        // different sorting orders -> returns false
+        SortAppointmentDescriptor editedDescriptor = new SortAppointmentDescriptorBuilder(ASC_DATETIME)
+                .withSortingOrder(VALID_SORTING_ORDER_DESC).build();
+        assertFalse(ASC_DATETIME.equals(editedDescriptor));
 
-        // different phone -> returns false
-        editedAmy = new EditPersonDescriptorBuilder(DESC_AMY).withPhone(VALID_PHONE_BOB).build();
-        assertFalse(DESC_AMY.equals(editedAmy));
-
-        // different email -> returns false
-        editedAmy = new EditPersonDescriptorBuilder(DESC_AMY).withEmail(VALID_EMAIL_BOB).build();
-        assertFalse(DESC_AMY.equals(editedAmy));
-
-        // different address -> returns false
-        editedAmy = new EditPersonDescriptorBuilder(DESC_AMY).withAddress(VALID_ADDRESS_BOB).build();
-        assertFalse(DESC_AMY.equals(editedAmy));
-
-        // different tags -> returns false
-        editedAmy = new EditPersonDescriptorBuilder(DESC_AMY).withTags(VALID_TAG_HUSBAND).build();
-        assertFalse(DESC_AMY.equals(editedAmy));
+        // different sorting keys -> returns false
+        editedDescriptor = new SortAppointmentDescriptorBuilder(ASC_DATETIME)
+                .withAppointmentSortingKey(VALID_SORTING_KEY_APPOINTMENT_NAME).build();
+        assertFalse(ASC_DATETIME.equals(editedDescriptor));
     }
 }
