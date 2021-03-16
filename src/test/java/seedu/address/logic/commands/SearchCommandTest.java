41ef8ddad11061937837839b73082c72c5aeacad
mabel-kang <e0425446@u.nus.edu>
2021-03-17 01:11:46 +0800

    Update codes according to checkstyle rules

--- src/test/java/seedu/address/logic/commands/SearchCommandTest.java
+++ src/test/java/seedu/address/logic/commands/SearchCommandTest.java
@@ -8,7 +8,11 @@ import static seedu.address.commons.core.Messages.MESSAGE_PERSONS_LISTED_OVERVIE
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_SCHOOL;
-import static seedu.address.testutil.TypicalPersons.*;
+import static seedu.address.testutil.TypicalPersons.ALICE;
+import static seedu.address.testutil.TypicalPersons.CARL;
+import static seedu.address.testutil.TypicalPersons.ELLE;
+import static seedu.address.testutil.TypicalPersons.FIONA;
+import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
 import java.util.Arrays;
 import java.util.Collections;
