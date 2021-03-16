e9b2be82ebd9391f2b6f78e2f97d9b050568c4b7
Azeem Vasanwala <azeem.iron@gmail.com>
2021-03-16 20:58:52 +0800

    fix(javadocs): Remove person references in Javadocs for RoomNumber class

--- src/main/java/seedu/address/model/room/RoomNumber.java
+++ src/main/java/seedu/address/model/room/RoomNumber.java
@@ -4,7 +4,7 @@ import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.util.AppUtil.checkArgument;
 
 /**
- * Represents a Person's name in the address book.
+ * Represents a Room's number in the address book.
  * Guarantees: immutable; is valid as declared in {@link #isValidRoomNumber(String)}
  */
 public class RoomNumber {
