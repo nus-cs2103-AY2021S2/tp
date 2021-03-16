bb43a99a6822db3b00f9555a311aa74f52adad86
tsh22 <sihuitoh22@gmail.com>
2021-03-16 15:34:27 +0800

    Remove unused import statement

--- src/main/java/seedu/address/ui/ScheduleListPanel.java
+++ src/main/java/seedu/address/ui/ScheduleListPanel.java
@@ -9,7 +9,6 @@ import javafx.scene.control.ListView;
 import javafx.scene.layout.Region;
 import seedu.address.commons.core.LogsCenter;
 import seedu.address.model.schedule.Schedule;
-import seedu.address.model.task.Task;
 
 /**
  * Panel containing the list of schedules.
