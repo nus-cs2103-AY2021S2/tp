8bb252591245d65083c009638a70a1366a2ebab2
Icelenaugust <dingyoujia0823@gmail.com>
2021-03-16 10:47:02 +0800

    Remove unused import

--- src/test/java/seedu/address/model/task/CompletionStatusTest.java
+++ src/test/java/seedu/address/model/task/CompletionStatusTest.java
@@ -2,7 +2,6 @@ package seedu.address.model.task;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static seedu.address.testutil.Assert.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
