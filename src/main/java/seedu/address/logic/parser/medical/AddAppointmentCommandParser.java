989f55ed90ae20a5a5470bb82e298018d62c3e71
vivegank <77456578+vivegank@users.noreply.github.com>
2021-03-16 18:38:37 +0800

    Test AddAppointmentCommandParser and AddAppointmentCommand

--- src/main/java/seedu/address/logic/parser/medical/AddAppointmentCommandParser.java
+++ src/main/java/seedu/address/logic/parser/medical/AddAppointmentCommandParser.java
@@ -5,7 +5,6 @@ import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT
 import static seedu.address.logic.parser.CliSyntax.PREFIX_DATE;
 
 import java.time.LocalDateTime;
-import java.util.stream.Stream;
 
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.medical.AddAppointmentCommand;
@@ -13,7 +12,6 @@ import seedu.address.logic.parser.ArgumentMultimap;
 import seedu.address.logic.parser.ArgumentTokenizer;
 import seedu.address.logic.parser.Parser;
 import seedu.address.logic.parser.ParserUtil;
-import seedu.address.logic.parser.Prefix;
 import seedu.address.logic.parser.exceptions.ParseException;
 
 public class AddAppointmentCommandParser implements Parser<AddAppointmentCommand> {
@@ -50,11 +48,12 @@ public class AddAppointmentCommandParser implements Parser<AddAppointmentCommand
         return new AddAppointmentCommand(index, date);
     }
 
-    /**
-     * Returns true if none of the prefixes contains empty {@code Optional} values in the given
-     * {@code ArgumentMultimap}.
-     */
-    private static boolean arePrefixesPresent(ArgumentMultimap argumentMultimap, Prefix... prefixes) {
-        return Stream.of(prefixes).allMatch(prefix -> argumentMultimap.getValue(prefix).isPresent());
-    }
+    //Is this needed in AddAppointmentCommandParser? Seems unused.
+    ///**
+    // * Returns true if none of the prefixes contains empty {@code Optional} values in the given
+    // * {@code ArgumentMultimap}.
+    // */
+    //private static boolean arePrefixesPresent(ArgumentMultimap argumentMultimap, Prefix... prefixes) {
+    //    return Stream.of(prefixes).allMatch(prefix -> argumentMultimap.getValue(prefix).isPresent());
+    //}
 }
