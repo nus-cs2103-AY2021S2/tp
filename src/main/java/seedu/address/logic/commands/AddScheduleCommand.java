8551961fc461e203edb3b85d55b16a2d0deb66a4
Nicholas <me@cheeze2000.net>
2021-03-16 14:06:47 +0800

    Fix wildcard imports

--- src/main/java/seedu/address/logic/commands/AddScheduleCommand.java
+++ src/main/java/seedu/address/logic/commands/AddScheduleCommand.java
@@ -1,7 +1,10 @@
 package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
-import static seedu.address.logic.parser.CliSyntax.*;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_END_DATE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_START_DATE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
