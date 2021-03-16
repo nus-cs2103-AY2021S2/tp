55b2d84dc665d4f39941f1d568d9373eb54c0884
Hzxin <E0424941@u.nus.edu>
2021-03-16 22:07:56 +0800

    Synk reminder pop-up with storage

--- src/main/java/seedu/address/ui/ReminderWindow.java
+++ src/main/java/seedu/address/ui/ReminderWindow.java
@@ -1,11 +1,17 @@
 package seedu.address.ui;
 
+import java.time.LocalDate;
+import java.time.YearMonth;
 import java.util.logging.Logger;
 
 import javafx.fxml.FXML;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import seedu.address.commons.core.GuiSettings;
 import seedu.address.commons.core.LogsCenter;
+import seedu.address.model.Event;
+import seedu.address.model.EventList;
+import seedu.address.storage.CalendarStorage;
 
 /**
  * The Reminder Window. Provides the user schedule within a week.
@@ -13,15 +19,38 @@ import seedu.address.commons.core.LogsCenter;
 public class ReminderWindow extends UiPart<Stage> {
 
     private static final String FXML = "ReminderWindow.fxml";
-    private final Logger logger = LogsCenter.getLogger(ReminderWindow.class);
+    private LocalDate todayDate;
+    private CalendarStorage calendarStorage;
 
+    @FXML
+    private Text content;
 
     /**
      * Creates a new ReminderWindow.
      */
-    public ReminderWindow() {
+    public ReminderWindow(CalendarStorage calendarStorage) {
         super(FXML);
+        this.todayDate = LocalDate.now();
+        this.calendarStorage = calendarStorage;
+        calendarStorage.refreshStorage();
         setWindowDefaultSize(new GuiSettings());
+        content.setText(generateReminderContent());
+
+    }
+
+    private String generateReminderContent() {
+        String eventContent = "";
+        for (int i = 0 ; i < 3; i++) {
+            EventList eventList = calendarStorage.getDateEvents(todayDate.plusDays(i));
+            if (eventList.isEmpty()) {
+                continue;
+            }
+            eventContent += eventList.toString() + "\n";
+        }
+        if (eventContent.length() == 0) {
+            eventContent = "You have no events!";
+        }
+        return eventContent;
     }
 
     void show() {
