4e6ee0fc6ddc3713d2e83c26bfb43d64c4fdf520
Joel Ho <joelhoengkiat@gmail.com>
2021-03-16 14:20:13 +0800

    Remove unused import from DriveCommand

--- src/main/java/seedu/address/logic/commands/DriveCommand.java
+++ src/main/java/seedu/address/logic/commands/DriveCommand.java
@@ -6,7 +6,6 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
 import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PASSENGERS;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 import java.util.StringJoiner;
