8e054f6a85a202da97995636cfe0595348c14f42
Isaiah Koh <zaiah0505@hotmail.com>
2021-03-16 18:23:53 +0800

    Modified MainWindw.java to display ClientDetailFragment on startup

--- src/main/java/seedu/iscam/ui/ClientDetailFragment.java
+++ src/main/java/seedu/iscam/ui/ClientDetailFragment.java
@@ -1,19 +1,60 @@
 package seedu.iscam.ui;
 
-import java.awt.TextArea;
+import java.util.Comparator;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.Region;
+import seedu.iscam.model.client.Client;
 
 
 public class ClientDetailFragment extends UiPart<Region>{
 
-    private static final String FXML = "ResultDisplay.fxml";
+    private static final String FXML = "ClientDetailFragment.fxml";
+
+    private Client client;
 
     @FXML
-    private TextArea clientDetailName;
+    private Label name;
+    @FXML
+    private Label phone;
+    @FXML
+    private Label clientLocation;
+    @FXML
+    private Label email;
+    @FXML
+    private FlowPane tags;
 
     public ClientDetailFragment(){
         super(FXML);
+    }
+
+    public void setClientDetials(Client client) {
+        this.client = client;
+        name.setText(client.getName().fullName);
+        phone.setText(client.getPhone().value);
+        clientLocation.setText(client.getLocation().value);
+        email.setText(client.getEmail().value);
+        client.getTags().stream()
+                .sorted(Comparator.comparing(tag -> tag.tagName))
+                .forEach(tag -> tags.getChildren().add(new Label(tag.tagName)));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        // short circuit if same object
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof ClientDetailFragment)) {
+            return false;
+        }
+
+        // state check
+        ClientDetailFragment fragment = (ClientDetailFragment) other;
+        return client.equals(fragment.client);
     }
 }
--- src/main/java/seedu/iscam/ui/MainWindow.java
+++ src/main/java/seedu/iscam/ui/MainWindow.java
@@ -34,6 +34,7 @@ public class MainWindow extends UiPart<Stage> {
     private ClientListPanel clientListPanel;
     private ResultDisplay resultDisplay;
     private HelpWindow helpWindow;
+    private ClientDetailFragment clientDetailFragment;
 
     @FXML
     private StackPane commandBoxPlaceholder;
@@ -49,6 +50,9 @@ public class MainWindow extends UiPart<Stage> {
 
     @FXML
     private StackPane statusbarPlaceholder;
+
+    @FXML
+    private StackPane clientDetailFragmentPlaceholder;
 
     /**
      * Creates a {@code MainWindow} with the given {@code Stage} and {@code Logic}.
@@ -122,6 +126,9 @@ public class MainWindow extends UiPart<Stage> {
 
         CommandBox commandBox = new CommandBox(this::executeCommand);
         commandBoxPlaceholder.getChildren().add(commandBox.getRoot());
+
+        ClientDetailFragment clientDetailFragment = new ClientDetailFragment();
+        clientDetailFragmentPlaceholder.getChildren().add(clientDetailFragment.getRoot());
     }
 
     /**
