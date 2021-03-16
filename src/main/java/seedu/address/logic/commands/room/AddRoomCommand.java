e28a9b26f8b94027a8dc8a14ac589bdaf19defbf
Azeem Vasanwala <azeem.iron@gmail.com>
2021-03-16 20:24:00 +0800

    fix(add room): update usage message to reflect accurate occupancy status

--- src/main/java/seedu/address/logic/commands/room/AddRoomCommand.java
+++ src/main/java/seedu/address/logic/commands/room/AddRoomCommand.java
@@ -24,7 +24,7 @@ public class AddRoomCommand extends Command {
             + "Example: " + COMMAND_WORD + " "
             + PREFIX_ROOM_NUMBER + "12-123 "
             + PREFIX_ROOM_TYPE + "CORRIDOR_AC "
-            + PREFIX_ROOM_OCCUPANCY_STATUS + "YES "
+            + PREFIX_ROOM_OCCUPANCY_STATUS + "Y "
             + PREFIX_ROOM_TAG + "SHN";
 
     public static final String MESSAGE_SUCCESS = "New room added: %1$s";
