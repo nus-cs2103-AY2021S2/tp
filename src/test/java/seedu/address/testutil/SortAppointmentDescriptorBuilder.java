aabc1d024a3d85fabc7151f7f5cc5fcea8083121
Cp-John <e0376984@u.nus.edu>
2021-03-16 11:52:30 +0800

    Add builder for sort appointment descriptor testing

---
+++ src/test/java/seedu/address/testutil/SortAppointmentDescriptorBuilder.java
@@ -0,0 +1,41 @@
+package seedu.address.testutil;
+
+import seedu.address.logic.commands.SortAppointmentCommand.SortAppointmentDescriptor;
+import seedu.address.model.sort.descriptor.AppointmentSortingKey;
+import seedu.address.model.sort.descriptor.SortingOrder;
+
+/**
+ * A utility class to help with building SortAppointmentDescriptor objects.
+ */
+public class SortAppointmentDescriptorBuilder {
+
+    private SortAppointmentDescriptor descriptor;
+
+    public SortAppointmentDescriptorBuilder() {
+        descriptor = new SortAppointmentDescriptor();
+    }
+
+    public SortAppointmentDescriptorBuilder(SortAppointmentDescriptor descriptor) {
+        this.descriptor = new SortAppointmentDescriptor(descriptor);
+    }
+    
+    /**
+     * Sets the {@code SortingOrder} of the {@code SortAppointmentDescriptor} that we are building.
+     */
+    public SortAppointmentDescriptorBuilder withSortingOrder(String sortingOrder) {
+        descriptor.setSortingOrder(new SortingOrder(sortingOrder));
+        return this;
+    }
+
+    /**
+     * Sets the {@code AppointmentSortingKey} of the {@code SortAppointmentDescriptor} that we are building.
+     */
+    public SortAppointmentDescriptorBuilder withAppointmentSortingKey(String appointmentSortingKey) {
+        descriptor.setAppointmentSortingKey(new AppointmentSortingKey(appointmentSortingKey));
+        return this;
+    }
+
+    public SortAppointmentDescriptor build() {
+        return descriptor;
+    }
+}
