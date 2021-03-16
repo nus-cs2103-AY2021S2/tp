98e86c1e1cb8467aa0b4973f173bcd4199827188
Vevek Selvam <vevekselvam@gmail.com>
2021-03-16 16:53:55 +0800

    Update Command Parser

---
+++ src/main/java/seedu/address/logic/parser/DeleteDeadlineCommandParser.java
@@ -0,0 +1,53 @@
+package seedu.address.logic.parser;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_REMOVE_TASK_INDEX;
+
+import java.util.stream.Stream;
+
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.commands.DeleteDeadlineCommand;
+import seedu.address.logic.commands.DeleteTodoCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+
+/**
+ * Parses input arguments and creates a new DeleteDeadlineCommand object
+ */
+public class DeleteDeadlineCommandParser implements Parser<DeleteDeadlineCommand> {
+
+    /**
+     * Parses the given {@code String} of arguments in the context of the DeleteDeadlineCommand
+     * and returns a DeleteDeadlineCommand object for execution.
+     * @throws ParseException if the user input does not conform the expected format.
+     */
+    public DeleteDeadlineCommand parse(String args) throws ParseException {
+        requireNonNull(args);
+        ArgumentMultimap argMultimap =
+                ArgumentTokenizer.tokenize(args, PREFIX_REMOVE_TASK_INDEX);
+
+        if (!arePrefixesPresent(argMultimap, PREFIX_REMOVE_TASK_INDEX)) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteDeadlineCommand.MESSAGE_USAGE));
+        }
+
+        Index index;
+
+        try {
+            index = ParserUtil.parseIndex(argMultimap.getPreamble());
+            Index targetDeadlineIndex = ParserUtil.parseIndex(argMultimap.getValue(PREFIX_REMOVE_TASK_INDEX).get());
+            return new DeleteDeadlineCommand(index, targetDeadlineIndex);
+        } catch (ParseException pe) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteDeadlineCommand.MESSAGE_USAGE),
+                    pe);
+        }
+    }
+
+    /**
+     * Returns true if none of the prefixes contains empty {@code Optional} values in the given
+     * {@code ArgumentMultimap}.
+     */
+    private static boolean arePrefixesPresent(ArgumentMultimap argumentMultimap, Prefix... prefixes) {
+        return Stream.of(prefixes).allMatch(prefix -> argumentMultimap.getValue(prefix).isPresent());
+    }
+
+}
