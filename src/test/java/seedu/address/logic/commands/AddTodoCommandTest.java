49d6e09d6ad49534a1fce46b8b207244dd3e5385
Vevek Selvam <vevekselvam@gmail.com>
2021-03-17 19:48:26 +0800

    Add tests

--- src/test/java/seedu/address/logic/commands/AddTodoCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddTodoCommandTest.java
@@ -19,9 +19,8 @@ import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
 import seedu.address.model.project.Project;
-import seedu.address.model.task.Interval;
-import seedu.address.model.task.repeatable.Event;
-import seedu.address.testutil.EventBuilder;
+import seedu.address.model.task.todo.Todo;
+import seedu.address.testutil.TodoBuilder;
 
 public class AddTodoCommandTest {
 
@@ -34,30 +33,28 @@ public class AddTodoCommandTest {
 
     @Test
     public void execute_validParameters_success() throws Exception {
-        Event validEvent = new EventBuilder().withDescription("CS2106 Tutorial")
-                .withAtDate(LocalDate.of(2020, 01, 01)).withInterval(Interval.WEEKLY).build();
+        Todo validTodo = new TodoBuilder().withDescription("CS2106 Assignment").build();
 
-        CommandResult commandResult = new AddEventCommand(INDEX_FIRST, validEvent).execute(model);
+        CommandResult commandResult = new AddTodoCommand(INDEX_FIRST, validTodo).execute(model);
 
-        assertEquals(String.format(Messages.MESSAGE_ADD_EVENT_SUCCESS, validEvent), commandResult.getFeedbackToUser());
+        assertEquals(String.format(Messages.MESSAGE_ADD_TODO_SUCCESS, validTodo), commandResult.getFeedbackToUser());
     }
 
     @Test
     public void execute_invalidProjectIndex_throwsCommandException() {
-        Event eventToAdd = new EventBuilder().withDescription("CS2106 Tutorial")
-                .withAtDate(LocalDate.of(2020, 01, 01)).withInterval(Interval.WEEKLY).build();
+        Todo todoToAdd = new TodoBuilder().withDescription("CS2106 Assignment").build();
         // Typical project list contains only 2 projects
-        AddEventCommand addEventCommand = new AddEventCommand(INDEX_THIRD, eventToAdd);
+        AddTodoCommand addTodoCommand = new AddTodoCommand(INDEX_THIRD, todoToAdd);
 
         assertThrows(
                 CommandException.class,
-                Messages.MESSAGE_INVALID_PROJECT_DISPLAYED_INDEX, () -> addEventCommand.execute(model)
+                Messages.MESSAGE_INVALID_PROJECT_DISPLAYED_INDEX, () -> addTodoCommand.execute(model)
         );
     }
 
     @Test
-    public void execute_duplicateEvent_throwsCommandException() {
-        Event eventToAdd = new EventBuilder().withDescription("CS2106 Tutorial")
+    public void execute_duplicateTodo_throwsCommandException() {
+        Todo todoToAdd = new TodoBuilder().withDescription("CS2106 Assignment")
                 .withAtDate(LocalDate.of(2020, 01, 01)).withInterval(Interval.WEEKLY).build();
         Project projectToAddTo = model.getFilteredProjectList().get(INDEX_FIRST.getZeroBased());
         AddEventCommand addEventCommand = new AddEventCommand(INDEX_FIRST, eventToAdd);
@@ -69,7 +66,7 @@ public class AddTodoCommandTest {
 
     @Test
     public void equals() {
-        Event eventToAdd = new EventBuilder().withDescription("CS2106 Tutorial")
+        Event eventToAdd = new EventBuilder().withDescription("CS2106 Assignment")
                 .withAtDate(LocalDate.of(2020, 01, 01)).withInterval(Interval.WEEKLY).build();
         AddEventCommand addEventToOneCommand = new AddEventCommand(INDEX_FIRST, eventToAdd);
         AddEventCommand addEventToTwoCommand = new AddEventCommand(INDEX_SECOND, eventToAdd);
