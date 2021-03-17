493b0b5af34815a7b2df5836e8897f71e86145df
tashawan23 <tas.wan23@gmail.com>
2021-03-17 15:36:58 +0800

    Edit error messages

--- src/main/java/seedu/address/logic/commands/AddTaskCommand.java
+++ src/main/java/seedu/address/logic/commands/AddTaskCommand.java
@@ -2,7 +2,6 @@ package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_DESCRIPTION;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_TITLE;
 
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
