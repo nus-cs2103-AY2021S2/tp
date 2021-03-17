055bbee69a0e313e101f007f00a4e58312572bbd
GuoJun <guojunloh1234@gmail.com>
2021-03-17 17:29:05 +0800

    Update the format of Isbn

--- src/main/java/seedu/smartlib/model/book/Isbn.java
+++ src/main/java/seedu/smartlib/model/book/Isbn.java
@@ -11,17 +11,12 @@ public class Isbn {
 
 
     public static final String MESSAGE_CONSTRAINTS =
-            "ISBN should only contain numbers, and it should be 13 digits long with 4 hyphens.";
-    public static final String PREFIX_REGEX = "\\d{3}";
-    public static final String REGISTRATION_GROUP_REGEX = "\\d{1,5}";
-    public static final String REGISTRANT_REGEX = "\\d{1,7}";
-    public static final String PUBLICATION_REGEX = "\\d{1,6}";
-    public static final String CHECK_DIGIT_REGEX = "\\d{1}";
-    public static final String VALIDATION_REGEX = PREFIX_REGEX + "-" + REGISTRATION_GROUP_REGEX
-            + "-" + REGISTRANT_REGEX + "-" + PUBLICATION_REGEX + "-" + CHECK_DIGIT_REGEX;
+            "ISBN should only contain numbers, and it should only contain 13 digits.";
 
-    //The length of isbn is 13 and includes 4 hyphens.
-    public static final int ISBN_LENGTH = 17;
+    public static final String VALIDATION_REGEX = "\\d{13}";
+
+    //The length of isbn is 13.
+    public static final int ISBN_LENGTH = 13;
 
     public final String value;
 
