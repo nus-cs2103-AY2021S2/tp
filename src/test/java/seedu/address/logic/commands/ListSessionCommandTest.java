5eb1fda21b366717f15ce8d8126e27f20730bcd8
yungweezy <choonweiw@gmail.com>
2021-03-18 00:08:49 +0800

    Fix checkstyle errors.

--- src/test/java/seedu/address/logic/commands/ListSessionCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ListSessionCommandTest.java
@@ -1,15 +1,16 @@
 package seedu.address.logic.commands;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import seedu.address.model.Model;
-import seedu.address.model.ModelManager;
-import seedu.address.model.UserPrefs;
-
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.logic.commands.CommandTestUtil.showStudentAtIndex;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_STUDENT;
 import static seedu.address.testutil.TypicalStudents.getTypicalAddressBook;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
 
 /**
  * Contains integration tests (interaction with the Model) and unit tests for ListStudentCommand.
