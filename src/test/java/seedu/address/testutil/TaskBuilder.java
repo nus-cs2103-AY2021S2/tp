e64639a3943eac7b838d0f9394020877908aa113
kaixiangtay <taykaixiang97@gmail.com>
2021-03-17 18:25:35 +0800

    Remove unnecessary println statement

--- src/test/java/seedu/address/testutil/TaskBuilder.java
+++ src/test/java/seedu/address/testutil/TaskBuilder.java
@@ -111,7 +111,6 @@ public class TaskBuilder {
      * Sets the {@code RecurringSchedule} of the {@code Task} that we are building.
      */
     public TaskBuilder withRecurringSchedule(String recurringSchedule) {
-        System.out.println(recurringSchedule);
         this.recurringSchedule = new RecurringSchedule(recurringSchedule);
         return this;
     }
