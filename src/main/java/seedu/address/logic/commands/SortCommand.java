3164a4ad2241e11134b9ffbe7f144af1c9413931
edelinetenges <edelinetenges@gmail.com>
2021-03-18 23:24:00 +0800

    Edit style

--- src/main/java/seedu/address/logic/commands/SortCommand.java
+++ src/main/java/seedu/address/logic/commands/SortCommand.java
@@ -1,13 +1,13 @@
 package seedu.address.logic.commands;
 
+import static java.util.Objects.requireNonNull;
+import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
+
 import javafx.collections.ObservableList;
 import javafx.collections.transformation.SortedList;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.person.Person;
-
-import static java.util.Objects.requireNonNull;
-import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
 
 /**
  * Sorts the list of persons in the address book in alphabetical order.
@@ -32,5 +32,4 @@ public class SortCommand extends Command {
 
         return new CommandResult(MESSAGE_SORT_SUCCESS);
     }
-
 }
--- src/main/java/seedu/address/model/person/Person.java
+++ src/main/java/seedu/address/model/person/Person.java
@@ -13,7 +13,7 @@ import seedu.address.model.tag.Tag;
  * Represents a Person in the address book.
  * Guarantees: details are present and not null, field values are validated, immutable.
  */
-public class Person implements Comparable<Person>{
+public class Person implements Comparable<Person> {
 
     // Identity fields
     private final Name name;
