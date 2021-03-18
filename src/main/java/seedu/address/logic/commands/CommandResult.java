bf828446a8f4fc07543668214dea7d8e8a80e2ef
yutingzou <zouyuting0801@gmail.com>
2021-03-18 17:58:00 +0800

    Update CommandResult and ListCommand

--- src/main/java/seedu/address/logic/commands/CommandResult.java
+++ src/main/java/seedu/address/logic/commands/CommandResult.java
@@ -4,17 +4,12 @@ import static java.util.Objects.requireNonNull;
 
 import java.util.Objects;
 
-import javafx.collections.ObservableList;
-import seedu.address.model.person.Person;
-
 /**
  * Represents the result of a command execution.
  */
 public class CommandResult {
 
     private final String feedbackToUser;
-
-    private final ObservableList<Person> tutorsToView;
 
     /** Help information should be shown to the user. */
     private final boolean showHelp;
@@ -26,9 +21,8 @@ public class CommandResult {
      * Constructs a {@code CommandResult} with the specified fields.
      */
 
-    public CommandResult(String feedbackToUser, ObservableList<Person> tutorsToView, boolean showHelp, boolean exit) {
+    public CommandResult(String feedbackToUser, boolean showHelp, boolean exit) {
         this.feedbackToUser = requireNonNull(feedbackToUser);
-        this.tutorsToView = tutorsToView;
         this.showHelp = showHelp;
         this.exit = exit;
     }
@@ -38,23 +32,11 @@ public class CommandResult {
      * and other fields set to their default value.
      */
     public CommandResult(String feedbackToUser) {
-        this(feedbackToUser, null, false, false);
-    }
-
-    public CommandResult(String feedbackToUser, ObservableList<Person> tutorsToView) {
-        this(feedbackToUser, tutorsToView, false, false);
-    }
-
-    public CommandResult(String feedbackToUser, boolean showHelp, boolean exit) {
-        this(feedbackToUser, null, showHelp, exit);
+        this(feedbackToUser, false, false);
     }
 
     public String getFeedbackToUser() {
         return feedbackToUser;
-    }
-
-    public ObservableList<Person> getTutorsToView() {
-        return tutorsToView;
     }
 
     public boolean isShowHelp() {
@@ -78,14 +60,13 @@ public class CommandResult {
 
         CommandResult otherCommandResult = (CommandResult) other;
         return feedbackToUser.equals(otherCommandResult.feedbackToUser)
-                && (tutorsToView == null || tutorsToView.equals(otherCommandResult.tutorsToView))
                 && showHelp == otherCommandResult.showHelp
                 && exit == otherCommandResult.exit;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(feedbackToUser, tutorsToView, showHelp, exit);
+        return Objects.hash(feedbackToUser, showHelp, exit);
     }
 
 }
