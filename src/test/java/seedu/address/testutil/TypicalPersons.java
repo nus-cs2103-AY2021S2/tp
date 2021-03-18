c788c0dde8e6046a51996020f31035ea14b36e5b
Kingsley Kuan <kingsley@kingsleykuan.com>
2021-03-18 15:10:21 +0800

    Fix unit tests involving storage and update typical persons to include subjects

--- src/test/java/seedu/address/testutil/TypicalPersons.java
+++ src/test/java/seedu/address/testutil/TypicalPersons.java
@@ -33,21 +33,35 @@ public class TypicalPersons {
     public static final Person ALICE = new PersonBuilder().withName("Alice Pauline").withGender("Female")
             .withAddress("123, Jurong West Ave 6, #08-111").withEmail("alice@example.com")
             .withPhone("94351253")
+            .withSubject("Mathematics", "Secondary 4", "60", "5", "A-Level")
             .withTags("friends").build();
     public static final Person BENSON = new PersonBuilder().withName("Benson Meier").withGender("Male")
             .withAddress("311, Clementi Ave 2, #02-25")
             .withEmail("johnd@example.com").withPhone("98765432")
+            .withSubject("English", "Secondary 4", "60", "4", "A-Level")
+            .withSubject("Geography", "Secondary 4", "60", "4", "A-Level")
             .withTags("owesMoney", "friends").build();
     public static final Person CARL = new PersonBuilder().withName("Carl Kurz").withGender("Male")
-            .withPhone("95352563").withEmail("heinz@example.com").withAddress("wall street").build();
+            .withPhone("95352563").withEmail("heinz@example.com").withAddress("wall street")
+            .withSubject("English", "Secondary 3", "50", "5", "A-Level")
+            .withSubject("Literature", "Secondary 3", "50", "5", "A-Level")
+            .build();
     public static final Person DANIEL = new PersonBuilder().withName("Daniel Meier").withGender("Male")
-            .withPhone("87652533").withEmail("cornelia@example.com").withAddress("10th street").withTags("friends").build();
+            .withPhone("87652533").withEmail("cornelia@example.com").withAddress("10th street")
+            .withSubject("Physics", "Secondary 3", "50", "6", "A-Level")
+            .withTags("friends").build();
     public static final Person ELLE = new PersonBuilder().withName("Elle Meyer").withGender("Female")
-            .withPhone("9482224").withEmail("werner@example.com").withAddress("michegan ave").build();
+            .withPhone("9482224").withEmail("werner@example.com").withAddress("michegan ave")
+            .withSubject("Geography", "Secondary 2", "40", "3", "A-Level")
+            .build();
     public static final Person FIONA = new PersonBuilder().withName("Fiona Kunz").withGender("Female")
-            .withPhone("9482427").withEmail("lydia@example.com").withAddress("little tokyo").build();
+            .withPhone("9482427").withEmail("lydia@example.com").withAddress("little tokyo")
+            .withSubject("History", "Secondary 2", "40", "7", "A-Level")
+            .build();
     public static final Person GEORGE = new PersonBuilder().withName("George Best").withGender("Male")
-            .withPhone("9482442").withEmail("anna@example.com").withAddress("4th street").build();
+            .withPhone("9482442").withEmail("anna@example.com").withAddress("4th street")
+            .withSubject("Chemistry", "Secondary 2", "40", "4", "A-Level")
+            .build();
 
     // Manually added
     public static final Person HOON = new PersonBuilder().withName("Hoon Meier").withGender("Female")
