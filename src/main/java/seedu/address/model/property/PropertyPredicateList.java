41abd5418cc88cd9f7c27be904a380237d37db25
yuchen <yuchenwang97@gmail.com>
2021-03-19 18:09:33 +0800

    Add PropertyPredicateList as a way to encapsulate a list equals method for predicates.

---
+++ src/main/java/seedu/address/model/property/PropertyPredicateList.java
@@ -0,0 +1,32 @@
+package seedu.address.model.property;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+public class PropertyPredicateList {
+    List<Predicate<Property>> predicates;
+
+    public PropertyPredicateList(List<Predicate<Property>> predicates) {
+        this.predicates = predicates;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        if (other instanceof PropertyPredicateList
+                && this.predicates.size() == ((PropertyPredicateList) other).predicates.size()) {
+            for (int i = 0; i < predicates.size(); i++) {
+                if (this.predicates.get(i) != ((PropertyPredicateList) other).predicates.get(i)) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        return false;
+
+    }
+}
