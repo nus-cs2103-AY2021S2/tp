5d655d75864df8aefba9a635a0f2dc311c4c09e5
LI <gabriellee0903@gmail.com>
2021-03-19 21:21:58 +0800

    Add unit tests for ListTaskCommand and ListEventCommand

---
+++ src/test/java/seedu/address/logic/commands/ListEventCommandTest.java
@@ -0,0 +1,39 @@
+package seedu.address.logic.commands;
+
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.logic.commands.CommandTestUtil.showEventAtIndex;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_EVENT;
+import static seedu.address.testutil.TypicalEvents.getTypicalSochedule;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+
+/**
+ * Contains integration tests (interaction with the Model) and unit tests for ListEventCommand.
+ */
+public class ListEventCommandTest {
+
+    private Model model;
+    private Model expectedModel;
+
+    @BeforeEach
+    public void setUp() {
+        model = new ModelManager(getTypicalSochedule(), new UserPrefs());
+        expectedModel = new ModelManager(model.getSochedule(), new UserPrefs());
+    }
+
+    @Test
+    public void execute_listIsNotFiltered_showsSameList() {
+        assertCommandSuccess(new ListTaskCommand(), model, ListEventCommand.MESSAGE_SUCCESS, expectedModel);
+    }
+
+    @Test
+    public void execute_listIsFiltered_showsEverything() {
+        showEventAtIndex(model, INDEX_FIRST_EVENT);
+        assertCommandSuccess(new ListTaskCommand(), model, ListEventCommand.MESSAGE_SUCCESS, expectedModel);
+    }
+}
