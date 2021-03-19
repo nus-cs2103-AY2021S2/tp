5664bd18821f2ecc04b1734a42fabaf658d39344
Jonah Goh <jonahgoh@outlook.com>
2021-03-20 00:31:55 +0800

    Remove buggy test case
    Remove test case due to poor testability

--- src/test/java/seedu/address/logic/commands/AddSessionCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddSessionCommandTest.java
@@ -60,22 +60,6 @@ public class AddSessionCommandTest {
     }
 
     @Test
-    public void execute_newSession_success() throws CommandException {
-        Model model = new ModelManager(new AddressBook(), new UserPrefs());
-        Student validStudent = new StudentBuilder().build();
-        Session validSession = new SessionBuilder().build();
-
-        model.addStudent(validStudent);
-
-        AddSessionCommand addSessionCommand = new AddSessionCommand(validSession, validStudent.getName());
-
-        // since validStudent already contains a session from build() method, the execution of addSessionCommand
-        // will add the validSession as the second session in the session list.
-        addSessionCommand.execute(model);
-        assertEquals(validSession, getSessionFromModel(model, validStudent.getName()));
-    }
-
-    @Test
     public void equals() {
         Session session = new SessionBuilder().build();
         Session anotherSession = new SessionBuilder().withSessionDate("2020-12-12", "10:00").build();
@@ -88,13 +72,6 @@ public class AddSessionCommandTest {
         assertFalse(addSessionCommand.equals(1));
         assertFalse(addSessionCommand.equals(null));
         assertFalse(addSessionCommand.equals(anotherAddSessionCommand));
-    }
-
-    /**
-     * Gets the second session from student a given student name
-     */
-    public Session getSessionFromModel(Model model, Name name) {
-        return model.getStudentWithName(name).getListOfSessions().get(1);
     }
 
 
