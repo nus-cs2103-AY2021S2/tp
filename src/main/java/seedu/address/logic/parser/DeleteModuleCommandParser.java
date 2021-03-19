865fa48d37e23777b04066dc26f23b0ff5db4586
DESKTOP-T6HFRHE\Yihe <wyharry628@gmail.com>
2021-03-19 18:34:16 +0800

    checkstyle god damn it

--- src/main/java/seedu/address/logic/parser/DeleteModuleCommandParser.java
+++ src/main/java/seedu/address/logic/parser/DeleteModuleCommandParser.java
@@ -1,9 +1,9 @@
 package seedu.address.logic.parser;
 
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_MODULE_CODE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_PLAN_NUMBER;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_SEM_NUMBER;
-import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 
 import java.util.stream.Stream;
 
