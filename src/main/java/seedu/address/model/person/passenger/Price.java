e7be2c3f77a83d35adbe8743c23dfce9e3ff2744
Zeche <zx3110@gmail.com>
2021-03-19 23:15:28 +0800

    Add Price object class

---
+++ src/main/java/seedu/address/model/person/passenger/Price.java
@@ -0,0 +1,53 @@
+package seedu.address.model.person.passenger;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.AppUtil.checkArgument;
+
+/**
+ * Represents the highest price a passenger is willing to pay for a ride.
+ * Guarantees: immutable; is valid as declared in {@link #isValidPrice(String)}
+ */
+public class Price {
+
+
+    public static final String MESSAGE_CONSTRAINTS =
+            "The price you are willing to pay should be entered as a positive decimal number.";
+    public static final String VALIDATION_REGEX = "\\d+(.\\d)?";
+    public final String value;
+
+    /**
+     * Constructs a {@code Price}.
+     *
+     * @param price A valid price.
+     */
+    public Price(String price) {
+        requireNonNull(price);
+        checkArgument(isValidPrice(price), MESSAGE_CONSTRAINTS);
+        value = price;
+    }
+
+    /**
+     * Returns true if a given string is a valid price.
+     */
+    public static boolean isValidPrice(String test) {
+        return test.matches(VALIDATION_REGEX);
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof Price // instanceof handles nulls
+                && value.equals(((Price) other).value)); // state check
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+
+}
