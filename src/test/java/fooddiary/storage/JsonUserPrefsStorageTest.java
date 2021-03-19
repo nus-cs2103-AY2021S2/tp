306f413bb7fab3434655875b351a9a9291e157c5
Donavan <donavanlim28@gmail.com>
2021-03-20 00:21:31 +0800

    Fixed JSON and FXML test files to appropriately pass test cases

--- src/test/java/fooddiary/storage/JsonUserPrefsStorageTest.java
+++ src/test/java/fooddiary/storage/JsonUserPrefsStorageTest.java
@@ -73,7 +73,7 @@ public class JsonUserPrefsStorageTest {
     private UserPrefs getTypicalUserPrefs() {
         UserPrefs userPrefs = new UserPrefs();
         userPrefs.setGuiSettings(new GuiSettings(1000, 500, 300, 100));
-        userPrefs.setFoodDiaryFilePath(Paths.get("addressbook.json"));
+        userPrefs.setFoodDiaryFilePath(Paths.get("fooddiary.json"));
         return userPrefs;
     }
 
