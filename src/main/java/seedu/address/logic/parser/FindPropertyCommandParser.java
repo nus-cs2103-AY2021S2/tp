87a26cfdafe9aeea7eac1a23fa3c684dd2e21ae1
yuchen <yuchenwang97@gmail.com>
2021-03-19 18:49:34 +0800

    Add property find prefixes.

--- src/main/java/seedu/address/logic/parser/FindPropertyCommandParser.java
+++ src/main/java/seedu/address/logic/parser/FindPropertyCommandParser.java
@@ -2,10 +2,14 @@ package seedu.address.logic.parser;
 
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
 
 import seedu.address.logic.commands.FindPropertyCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.property.Property;
 import seedu.address.model.property.PropertyContainsKeywordsPredicate;
 
 /**
@@ -13,6 +17,17 @@ import seedu.address.model.property.PropertyContainsKeywordsPredicate;
  */
 public class FindPropertyCommandParser implements Parser<FindPropertyCommand> {
 
+    private Predicate<Property> whichPredicate(String key) throws ParseException {
+        String[] components = key.split("/");
+        if (components.length > 2) {
+            throw new ParseException(
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindPropertyCommand.MESSAGE_USAGE));
+        }
+        switch (components[0]) {
+        case "pm":
+
+        }
+    }
     /**
      * Parses the given {@code String} of arguments in the context of the FindPropertyCommand
      * and returns a FindPropertyCommand object for execution.
@@ -26,7 +41,19 @@ public class FindPropertyCommandParser implements Parser<FindPropertyCommand> {
                     String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindPropertyCommand.MESSAGE_USAGE));
         }
 
+
         String[] nameKeywords = trimmedArgs.split("\\s+");
+
+        List<Predicate<Property>> predicates = new ArrayList<>();
+        List<String> keywords = new ArrayList<>();
+
+        for (String s : nameKeywords) {
+            if (s.contains("\\")) {
+
+            } else {
+                keywords.add(s);
+            }
+        }
 
         return new FindPropertyCommand(new PropertyContainsKeywordsPredicate(Arrays.asList(nameKeywords)));
     }
