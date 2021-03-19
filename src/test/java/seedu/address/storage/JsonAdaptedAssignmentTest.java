1c76e0ba22240cb364dc9b5791bb2be8f2777144
Hzxin <E0424941@u.nus.edu>
2021-03-19 21:03:53 +0800

    Add adaptedAssignmentTest

--- src/test/java/seedu/address/storage/JsonAdaptedAssignmentTest.java
+++ src/test/java/seedu/address/storage/JsonAdaptedAssignmentTest.java
@@ -1,13 +1,15 @@
 package seedu.address.storage;
 
 import org.junit.jupiter.api.Test;
-import seedu.address.model.module.Assignment;
+import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.model.module.Description;
-import seedu.address.model.tag.Tag;
+
+import java.time.LocalDateTime;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static seedu.address.logic.commands.CommandTestUtil.*;
-import static seedu.address.testutil.TypicalRemindMe.BENSON;
+import static seedu.address.storage.JsonAdaptedAssignment.MISSING_FIELD_MESSAGE_FORMAT;
+import static seedu.address.testutil.Assert.assertThrows;
 import static seedu.address.testutil.TypicalRemindMe.VALID_ASSIGNMENT_1;
 
 class JsonAdaptedAssignmentTest {
@@ -23,5 +25,25 @@ class JsonAdaptedAssignmentTest {
                 VALID_EXAM_DATETIME_1, VALID_TITLE_CS2103);
 
         assertEquals(VALID_ASSIGNMENT_1, assignment.toModelType());
+    }
+
+
+
+    @Test
+    public void toModelType_nullDescription_throwsIllegalValueException() {
+        JsonAdaptedAssignment assignment =
+            new JsonAdaptedAssignment(null, VALID_EXAM_DATETIME_1, VALID_TITLE_CS2103);
+        String expectedMessage = String.format(MISSING_FIELD_MESSAGE_FORMAT, Description.class.getSimpleName());
+        assertThrows(IllegalValueException.class, expectedMessage, assignment::toModelType);
+    }
+
+    @Test
+    public void toModelType_invalidLocalDateTime_throwsIllegalValueException() {
+        JsonAdaptedAssignment assignment =
+            new JsonAdaptedAssignment(VALID_ASSIGNMENT_DESCRIPTION_1, INVALID_DEADLINE, VALID_TITLE_CS2103);
+
+        String expectedMessage = String.format(MISSING_FIELD_MESSAGE_FORMAT, LocalDateTime.class.getSimpleName());
+
+        assertThrows(IllegalValueException.class, expectedMessage, assignment::toModelType);
     }
 }
