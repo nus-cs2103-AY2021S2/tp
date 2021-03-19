858ae4a90277abeebc7681eddd9fe0739e2727c5
nickyfoo <nicholasfoocl@gmail.com>
2021-03-19 11:35:04 +0800

    Update Model and ModelManager to include EventBook functionality

--- src/main/java/seedu/partyplanet/model/Model.java
+++ src/main/java/seedu/partyplanet/model/Model.java
@@ -1,6 +1,10 @@
 package seedu.partyplanet.model;
 
+import static java.util.Objects.requireNonNull;
+import static seedu.partyplanet.commons.util.CollectionUtil.requireAllNonNull;
+
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.function.Predicate;
@@ -8,6 +12,7 @@ import java.util.function.Predicate;
 import javafx.collections.ObservableList;
 import seedu.partyplanet.commons.core.GuiSettings;
 import seedu.partyplanet.commons.util.StateHistory;
+import seedu.partyplanet.model.event.Event;
 import seedu.partyplanet.model.person.Person;
 
 /**
@@ -16,6 +21,9 @@ import seedu.partyplanet.model.person.Person;
 public interface Model {
     /** {@code Predicate} that always evaluate to true */
     Predicate<Person> PREDICATE_SHOW_ALL_PERSONS = unused -> true;
+
+    /** {@code Predicate} that always evaluate to true */
+    Predicate<Event> PREDICATE_SHOW_ALL_EVENTS = unused -> true;
 
     /**
      * Replaces user prefs data with the data in {@code userPrefs}.
@@ -79,12 +87,6 @@ public interface Model {
      */
     void setPerson(Person target, Person editedPerson);
 
-    /** Adds the current state to the StateHistory */
-    void addState();
-
-    /** Returns the StateHistory */
-    StateHistory getStateHistory();
-
     /** Returns an unmodifiable view of the filtered person list */
     ObservableList<Person> getFilteredPersonList();
 
@@ -102,4 +104,61 @@ public interface Model {
      * @throws NullPointerException if {@code comparator} is null.
      */
     void sortPersonList(Comparator<Person> comparator);
+
+    /**
+     * Replaces event book data with the data in {@code eventbook}.
+     */
+    void setEventBook(ReadOnlyEventBook eventBook);
+
+    /** Returns the EventBook */
+    ReadOnlyEventBook getEventBook();
+
+    /**
+     * Returns true if a event with the same identity as {@code event} exists in PartyPlanet.
+     */
+    boolean hasEvent(Event event);
+
+    /**
+     * Deletes the given event.
+     * The event must exist in PartyPlanet.
+     */
+    void deleteEvent(Event target);
+
+    /**
+     * Adds the given event.
+     * {@code event} must not already exist in PartyPlanet.
+     */
+    void addEvent(Event event);
+
+    /**
+     * Replaces the given event {@code target} with {@code editedEvent}.
+     * {@code target} must exist in PartyPlanet.
+     * The event name of {@code editedEvent} must not be the same as another existing event in PartyPlanet.
+     */
+    void setEvent(Event target, Event editedEvent);
+
+
+    /** Returns an unmodifiable view of the filtered event list */
+    ObservableList<Event> getFilteredEventList();
+
+    /**
+     * Updates the filter of the filtered event list to filter by the given {@code predicate}.
+     * @throws NullPointerException if {@code predicate} is null.
+     */
+    void updateFilteredEventList(Predicate<Event> predicate);
+
+    /**
+     * Sorts the filtered event list to be sorted by the given {@code comparator}.
+     * @throws NullPointerException if {@code comparator} is null.
+     */
+    void sortEventList(Comparator<Event> comparator);
+
+    /** Returns a copy of the person list */
+    List<Event> getEventListCopy();
+
+    /** Adds the current state to the StateHistory */
+    void addState();
+
+    /** Returns the StateHistory */
+    StateHistory getStateHistory();
 }
