498e9c46621a6f1e0c5f311d262c895bd511bd11
Zen Lee Yijie <iarezenly@gmail.com>
2021-03-21 17:25:58 +0800

    Add javadocs, test cases for Statistics class

--- src/main/java/seedu/address/model/flashcard/Statistics.java
+++ src/main/java/seedu/address/model/flashcard/Statistics.java
@@ -3,23 +3,43 @@ package seedu.address.model.flashcard;
 import java.util.List;
 import java.util.Objects;
 
+/**
+ * Represents a flash card's statistics in FlashBack.
+ */
 public class Statistics {
     public static final String MESSAGE_CONSTRAINT = "Flash card review count and correct count should be "
             + "positive integers, the correct count should be less than or equal to the review count";
     private int reviewCount;
     private int correctCount;
 
-
+    /**
+     * Constructs a {@code Statistics}.
+     *
+     * Review count and correct count are initialized to 0.
+     */
     public Statistics() {
         this.reviewCount = 0;
         this.correctCount = 0;
     }
 
+    /**
+     * Constructs a {@code Statistics} where the review count and correct count are initialized to values specified
+     * in the parameters.
+     *
+     * @param reviewCount The number of times the flash card is reviewed.
+     * @param correctCount The number of times the user got the correct answer in review mode.
+     */
     public Statistics(int reviewCount, int correctCount) {
         this.reviewCount = reviewCount;
         this.correctCount = correctCount;
     }
 
+    /**
+     * Constructs a {@code Statistics} where the review count is initialized to the sum of all review counts in the
+     * list and the correct count is initialized to the sum of all correct counts in the list.
+     *
+     * @param flashcards A list of flashcards
+     */
     public Statistics(List<Flashcard> flashcards) {
         for (Flashcard card : flashcards) {
             reviewCount += card.getStats().getReviewCount();
@@ -27,6 +47,13 @@ public class Statistics {
         }
     }
 
+    /**
+     * Tests whether the statistics if valid or not.
+     *
+     * @param stats The statistics to test for validity.
+     * @return false if the correct/review count is negative, or if the correct count is greater than the review count.
+     *         true otherwise.
+     */
     public static boolean isValidStats(Statistics stats) {
         if (stats.getCorrectCount() < 0 ||  stats.getReviewCount() < 0) {
             return false;
@@ -37,22 +64,41 @@ public class Statistics {
         }
     }
 
+    /**
+     * Increments the review count by 1.
+     */
     public void incrementReviewCount() {
         reviewCount++;
     }
 
+    /**
+     * Increments the correct count by 1.
+     */
     public void incrementCorrectCount() {
         correctCount++;
     }
 
+    /**
+     * Gets the number of times the flash card is reviewed.
+     * @return The review count associated with the flash card.
+     */
     public int getReviewCount() {
         return reviewCount;
     }
 
+    /**
+     * Gets the number of times the user got the answer correct during review mode.
+     * @return The correct count associated with the flash card.
+     */
     public int getCorrectCount() {
         return correctCount;
     }
 
+    /**
+     * Gets the rate where the user got the answer correct during review mode.
+     * @return 0 if the review count is 0.
+     *           otherwise return the percentage of times where user got the answer correct in review mode.
+     */
     public double getCorrectRate() {
         if (reviewCount == 0) {
             return 0.0;
--- src/test/java/seedu/address/model/flashcard/StatisticsTest.java
+++ src/test/java/seedu/address/model/flashcard/StatisticsTest.java
@@ -1,4 +1,71 @@
 package seedu.address.model.flashcard;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import static seedu.address.testutil.Assert.assertThrows;
+
 public class StatisticsTest {
+    @Test
+    public void constructor_null_cardList_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> new Statistics(null));
+    }
+
+    @Test
+    public void constructor_withReviewCorrectCount_validCorrectRate() {
+        // correct count is 2, review count is 4
+        Statistics stats = new Statistics(4, 2);
+        assertTrue(stats.getCorrectRate() == 50.0);
+    }
+
+    @Test
+    public void constructor_noArguments_zeroReviewCorrectCount() {
+        Statistics noArgsStats = new Statistics();
+        assertTrue(noArgsStats.getCorrectCount() == 0 && noArgsStats.getReviewCount() == 0);
+    }
+
+    @Test
+    public void incrementReviewCorrectCount_invalidStats() {
+        Statistics stats = new Statistics(2, 2);
+        stats.incrementReviewCount();
+        stats.incrementCorrectCount();
+        stats.incrementCorrectCount();
+        // correct count is now greater than review count
+        assertFalse(Statistics.isValidStats(stats));
+    }
+
+    @Test
+    public void negativeReviewCorrectCount_invalidStats() {
+        // review count is -1
+        Statistics negativeReviewCountStats = new Statistics(-1, 2);
+        assertFalse(Statistics.isValidStats(negativeReviewCountStats));
+
+        // correct count is -1
+        Statistics negativeCorrectCountStats = new Statistics(5, -1);
+        assertFalse(Statistics.isValidStats(negativeCorrectCountStats));
+    }
+
+    @Test
+    public void equals() {
+        Statistics firstStats = new Statistics(2, 1);
+        Statistics secondStats = new Statistics();
+
+        // same object -> returns true
+        assertTrue(firstStats.equals(firstStats));
+
+        // same values -> returns true
+        Statistics firstStatsCopy = new Statistics(2, 1);
+        assertTrue(firstStats.equals(firstStatsCopy));
+
+        // different types -> returns false
+        assertFalse(firstStats.equals(1));
+
+        // null -> returns false
+        assertFalse(firstStats.equals(null));
+
+        // different correct count or different review count -> returns false
+        assertFalse(firstStats.equals(secondStats));
+    }
 }
