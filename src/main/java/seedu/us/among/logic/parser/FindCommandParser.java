ee5fb785f8c29253b7e346950957009bb9ce4fc4
ong6 <junxiongong2@gmail.com>
2021-03-21 17:14:34 +0800

    Command fixed another small bug

--- src/main/java/seedu/us/among/logic/parser/FindCommandParser.java
+++ src/main/java/seedu/us/among/logic/parser/FindCommandParser.java
@@ -44,8 +44,9 @@ public class FindCommandParser implements Parser<FindCommand> {
         String[] nameKeywords;
 
         //Case 1: If no prefix present (general search)
-        if (argMultimap.arePrefixesPresent()) {
+        if (!argMultimap.arePrefixesPresent(PREFIX_METHOD, PREFIX_ADDRESS, PREFIX_DATA, PREFIX_HEADER, PREFIX_TAG)) {
             nameKeywords = getNameKeywords(args);
+            System.out.println("wiowajdoawkd" + nameKeywords.toString());
             return new FindCommand(new EndPointContainsKeywordsPredicate(Arrays.asList(nameKeywords)));
         }
 
@@ -81,7 +82,7 @@ public class FindCommandParser implements Parser<FindCommand> {
             nameKeywords = getNameKeywords(input);
             predicateList.add(new TagsContainsKeywordsPredicate(Arrays.asList(nameKeywords)));
         }
-
+        System.out.println("what the fuck" + predicateList.toString());
         return new FindCommand(predicateList.stream().reduce(x -> true, Predicate::and));
     }
 
