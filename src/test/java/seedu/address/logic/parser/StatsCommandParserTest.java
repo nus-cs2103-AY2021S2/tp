b7f2d816f68f2278de5b98cda384f73cf406d513
Zen Lee Yijie <iarezenly@gmail.com>
2021-03-21 16:00:32 +0800

    Add tests for StatsCommand and StatsCommandParser

---
+++ src/test/java/seedu/address/logic/parser/StatsCommandParserTest.java
@@ -0,0 +1,30 @@
+package seedu.address.logic.parser;
+
+import org.junit.jupiter.api.Test;
+import seedu.address.logic.commands.StatsCommand;
+
+import java.util.Optional;
+
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_FLASHCARD;
+
+public class StatsCommandParserTest {
+    private StatsCommandParser parser = new StatsCommandParser();
+    @Test
+    public void parse_validArgs_returnsStatsCommand() {
+        assertParseSuccess(parser, "1", new StatsCommand(Optional.of(INDEX_FIRST_FLASHCARD)));
+    }
+
+    @Test
+    public void parse_noArgs_returnsStatsCommand() {
+        assertParseSuccess(parser, "", new StatsCommand(Optional.empty()));
+    }
+
+    @Test
+    public void parse_invalidArgs_throwsParseException() {
+        assertParseFailure(parser, "a", String.format(MESSAGE_INVALID_COMMAND_FORMAT, StatsCommand.MESSAGE_USAGE));
+        assertParseFailure(parser, "-10", String.format(MESSAGE_INVALID_COMMAND_FORMAT, StatsCommand.MESSAGE_USAGE));
+    }
+}
