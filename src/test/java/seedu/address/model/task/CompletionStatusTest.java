a1bb8f5f85b4c88ab6e4f4b1686e524575edeb80
SoulUseless <jkyap99@gmail.com>
2021-03-21 19:54:12 +0800

    Add extra unit tests to modified Task attributes

--- src/test/java/seedu/address/model/task/CompletionStatusTest.java
+++ src/test/java/seedu/address/model/task/CompletionStatusTest.java
@@ -1,9 +1,14 @@
 package seedu.address.model.task;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.testutil.TypicalTasks.ASSIGNMENT;
+import static seedu.address.testutil.TypicalTasks.EXERCISE;
 
 import org.junit.jupiter.api.Test;
+
+import seedu.address.testutil.TaskBuilder;
 
 public class CompletionStatusTest {
 
@@ -16,5 +21,15 @@ public class CompletionStatusTest {
         // valid completionStatus
         assertTrue(CompletionStatus.isValidStatus("INCOMPLETE"));
         assertTrue(CompletionStatus.isValidStatus("COMPLETE"));
+    }
+
+    @Test
+    public void compareTest() {
+        assertEquals(ASSIGNMENT.getCompletionStatus().compareTo(EXERCISE.getCompletionStatus()), 0);
+
+        Task assignment = new TaskBuilder(ASSIGNMENT).build();
+        assignment.markTaskAsDone();
+        assertEquals(assignment.getCompletionStatus().compareTo(EXERCISE.getCompletionStatus()), 1);
+        assertEquals(EXERCISE.getCompletionStatus().compareTo(assignment.getCompletionStatus()), -1);
     }
 }
