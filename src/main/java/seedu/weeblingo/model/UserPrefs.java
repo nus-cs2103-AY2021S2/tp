a6bdc7c659075d764aa7b7f75434b75e36dc1d06
Cheng20010201 <E0493630@u.nus.edu>
2021-03-22 10:37:17 +0800

    Load off old database. And fix preferences.json due to unintended refactoring.

--- src/main/java/seedu/weeblingo/model/UserPrefs.java
+++ src/main/java/seedu/weeblingo/model/UserPrefs.java
@@ -15,7 +15,7 @@ public class UserPrefs implements ReadOnlyUserPrefs {
 
     private GuiSettings guiSettings = new GuiSettings();
 
-    private Path flashcardsFilePath = Paths.get("database" , "flashcards.json");
+    private Path flashcardsFilePath = Paths.get("data" , "flashcards.json");
 
     /**
      * Creates a {@code UserPrefs} with default values.
