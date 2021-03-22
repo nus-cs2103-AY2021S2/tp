07d1419e7d2ba2ca1e763c8d03c3aa14b153aa2d
Ellevy <46534252+Ellevy@users.noreply.github.com>
2021-03-22 21:05:22 +0800

    Elist to search by name

---
+++ src/main/java/seedu/partyplanet/logic/commands/EListCommand.java
@@ -0,0 +1,86 @@
+package seedu.partyplanet.logic.commands;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.partyplanet.model.Model.PREDICATE_SHOW_ALL_EVENTS;
+
+import java.util.Comparator;
+import java.util.function.Predicate;
+
+import seedu.partyplanet.commons.core.Messages;
+import seedu.partyplanet.model.Model;
+import seedu.partyplanet.model.event.Event;
+
+/**
+ * Lists all events in PartyPlanet to the user.
+ */
+public class EListCommand extends Command {
+
+    public static final String COMMAND_WORD = "elist";
+
+    public static final String MESSAGE_SUCCESS = "Listed all events";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Lists event in PartyPlanet "
+            + "according to specified prefix combinations, with optional sort order.\n"
+            + "Parameters: [--exact] [--any] [-n NAME] [-r DETAIL] ... [-s SORT_ORDER]\n"
+            + "Sort fields: 'n' (name, default), 'd' (date)\n"
+            + "Sort orders: 'asc' (ascending, default), 'desc' (descending)\n"
+            + "Example: list --exact -n Jan celebration -r 10 people -s name\n";
+
+    public static final Comparator<Event> SORT_NAME = Comparator.comparing(x -> x.getName().fullName);
+    public static final Comparator<Event> SORT_EVENTDATE = Comparator.comparing(Event::getDate);
+
+    private final Comparator<Event> comparator;
+    private final Predicate<Event> predicate;
+
+    /**
+     * Default empty EListCommand.
+     * Shows the whole list.
+     */
+    public EListCommand() {
+        this(PREDICATE_SHOW_ALL_EVENTS);
+    }
+
+    /**
+     * More general EListCommand accepting a single filtering predicate.
+     * Default in ascending order, and the ANY flag is not applicable.
+     */
+    public EListCommand(Predicate<Event> predicate) {
+        this(predicate, SORT_NAME);
+    }
+
+    /**
+     * Most general EListCommand.
+     *
+     * @param predicate Predicate to filter people by
+     * @param comparator Sorting comparator
+     */
+    public EListCommand(Predicate<Event> predicate, Comparator<Event> comparator) {
+        this.predicate = predicate;
+        this.comparator = comparator;
+    }
+
+    @Override
+    public CommandResult execute(Model model) {
+        requireNonNull(model);
+        model.sortEventList(comparator);
+        model.updateFilteredEventList(predicate);
+        if (model.getEventListCopy().size() == model.getFilteredEventList().size()) {
+            return new CommandResult(EListCommand.MESSAGE_SUCCESS); // No event filtered out
+        }
+        return new CommandResult(
+                String.format(Messages.MESSAGE_EVENTS_LISTED_OVERVIEW, model.getFilteredEventList().size()));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+        if (!(other instanceof EListCommand)) {
+            return false;
+        }
+        EListCommand command = (EListCommand) other;
+        return comparator.equals(command.comparator)
+                && predicate.equals(((EListCommand) other).predicate); // state check
+    }
+}
---
+++ src/main/java/seedu/partyplanet/logic/parser/EListCommandParser.java
@@ -0,0 +1,153 @@
+package seedu.partyplanet.logic.parser;
+
+import static seedu.partyplanet.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.partyplanet.logic.commands.EListCommand.SORT_EVENTDATE;
+import static seedu.partyplanet.logic.commands.EListCommand.SORT_NAME;
+import static seedu.partyplanet.logic.parser.CliSyntax.FLAG_ANY;
+import static seedu.partyplanet.logic.parser.CliSyntax.FLAG_EXACT;
+import static seedu.partyplanet.logic.parser.CliSyntax.PREFIX_NAME;
+import static seedu.partyplanet.logic.parser.CliSyntax.PREFIX_ORDER;
+import static seedu.partyplanet.logic.parser.CliSyntax.PREFIX_SORT;
+import static seedu.partyplanet.model.Model.PREDICATE_SHOW_ALL_EVENTS;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Predicate;
+
+import seedu.partyplanet.logic.commands.EListCommand;
+import seedu.partyplanet.logic.parser.exceptions.ParseException;
+import seedu.partyplanet.model.event.Event;
+import seedu.partyplanet.model.event.predicates.EventNameContainsExactKeywordsPredicate;
+import seedu.partyplanet.model.event.predicates.EventNameContainsKeywordsPredicate;
+
+/**
+ * Parses input arguments and creates a new ListCommand object
+ */
+public class EListCommandParser implements Parser<EListCommand> {
+
+
+    /**
+     * Parses the given {@code String} of arguments in the context of the EListCommand
+     * and returns an EListCommand object for execution.
+     *
+     * @throws ParseException if the user input does not conform the expected format
+     */
+    public EListCommand parse(String args) throws ParseException {
+        ArgumentMultimap argMap = ArgumentTokenizer.tokenize(
+                args, PREFIX_NAME, PREFIX_SORT, PREFIX_ORDER, FLAG_EXACT, FLAG_ANY);
+
+        if (!argMap.getPreamble().isEmpty()) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, EListCommand.MESSAGE_USAGE));
+        }
+
+        Predicate<Event> predicate = getPredicate(argMap);
+        Comparator<Event> comparator = getComparator(argMap);
+        return new EListCommand(predicate, comparator);
+    }
+
+    /**
+     * Returns the overall filtering predicate.
+     */
+    private Predicate<Event> getPredicate(ArgumentMultimap argMap) {
+        List<Predicate<Event>> predicates = getPredicates(argMap);
+        return mergePredicates(predicates, argMap);
+    }
+
+    /**
+     * Returns a list of filtering predicates depending on whether partial search is disabled.
+     */
+    private List<Predicate<Event>> getPredicates(ArgumentMultimap argMap) {
+        boolean isExactSearch = argMap.contains(FLAG_EXACT);
+        List<Predicate<Event>> predicates = new ArrayList<>();
+        if (isExactSearch) {
+            for (String name : argMap.getAllValues(PREFIX_NAME)) {
+                predicates.add(new EventNameContainsExactKeywordsPredicate(name));
+            }
+        } else {
+            for (String name : argMap.getAllValues(PREFIX_NAME)) {
+                predicates.add(new EventNameContainsKeywordsPredicate(name));
+            }
+        }
+        return predicates;
+    }
+
+    /**
+     * Returns combines a list of filtering predicates depending on whether search is performed for any predicate.
+     */
+    private Predicate<Event> mergePredicates(List<Predicate<Event>> predicates, ArgumentMultimap argMap) {
+        boolean isAnySearch = argMap.contains(FLAG_ANY);
+        Predicate<Event> overallPredicate;
+        if (predicates.isEmpty()) {
+            overallPredicate = PREDICATE_SHOW_ALL_EVENTS;
+        } else if (isAnySearch) {
+            overallPredicate = x -> false;
+            for (Predicate<Event> predicate : predicates) {
+                overallPredicate = overallPredicate.or(predicate);
+            }
+        } else {
+            overallPredicate = x -> true;
+            for (Predicate<Event> predicate : predicates) {
+                overallPredicate = overallPredicate.and(predicate);
+            }
+        }
+        return overallPredicate;
+    }
+
+    /**
+     * Returns the comparator used to sort the filtered list.
+     */
+    private Comparator<Event> getComparator(ArgumentMultimap argMap) throws ParseException {
+        Comparator<Event> comparator = getSortOrder(argMap);
+        return applySortDirection(comparator, argMap);
+    }
+
+    /**
+     * Returns the comparator depending on the specified field to be sorted.
+     */
+    private Comparator<Event> getSortOrder(ArgumentMultimap argMap) throws ParseException {
+        Optional<String> sortType = argMap.getValue(PREFIX_SORT);
+        if (sortType.isEmpty()) {
+            return SORT_NAME; // default
+        } else {
+            switch (sortType.get().toLowerCase()) {
+            case "n": // fallthrough
+            case "name":
+                return SORT_NAME;
+            case "d": // fallthrough
+            case "date":
+                return SORT_EVENTDATE;
+            default:
+                throw new ParseException(
+                        String.format(MESSAGE_INVALID_COMMAND_FORMAT, EListCommand.MESSAGE_USAGE));
+            }
+        }
+    }
+
+    /**
+     * Returns a comparator that is reversed if reverse order is specified.
+     */
+    private Comparator<Event> applySortDirection(
+            Comparator<Event> comparator, ArgumentMultimap argMap) throws ParseException {
+        Optional<String> orderType = argMap.getValue(PREFIX_ORDER);
+        if (orderType.isEmpty()) {
+            return comparator; // default
+        } else {
+            switch (orderType.get().toLowerCase()) {
+            case "a": // fallthrough
+            case "asc":
+            case "ascending":
+                return comparator;
+            case "d": // fallthrough
+            case "des":
+            case "desc":
+            case "descending":
+                return comparator.reversed();
+            default:
+                throw new ParseException(
+                        String.format(MESSAGE_INVALID_COMMAND_FORMAT, EListCommand.MESSAGE_USAGE));
+            }
+        }
+    }
+}
