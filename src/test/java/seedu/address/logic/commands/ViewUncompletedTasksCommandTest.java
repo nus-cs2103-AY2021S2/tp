c726d2f6b5960160c291e7c478c22c789c0832f9
rachelljt <rachel.ljt@gmail.com>
2021-03-22 15:26:48 +0800

    fix checkstyle

--- src/test/java/seedu/address/logic/commands/ViewUncompletedTasksCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ViewUncompletedTasksCommandTest.java
@@ -1,13 +1,15 @@
 package seedu.address.logic.commands;
 
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
 import seedu.address.testutil.TypicalTasks;
 
-import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 
 /**
  * Contains integration tests (interaction with the Model) and unit tests for ViewUncompletedTasksCommand.
