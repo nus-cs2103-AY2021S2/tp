ce255716f95a5f42ffa14cc4abd7966d8f2f7970
jellymias <51073704+jellymias@users.noreply.github.com>
2021-03-22 23:07:01 +0800

    Integrate EditExamCommandParser

--- src/main/java/seedu/address/logic/parser/editcommandparser/EditCommandParser.java
+++ src/main/java/seedu/address/logic/parser/editcommandparser/EditCommandParser.java
@@ -4,6 +4,7 @@ import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_ASSIGNMENT;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_DATE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_EXAM;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_GENERAL_EVENT;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_MODULE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
@@ -44,6 +45,8 @@ public class EditCommandParser {
             command = new EditEventCommandParser().parse(args);
         } else if (editAssignmentCondition(argMultimap)) {
             command = new EditAssignmentCommandParser().parse(args);
+        } else if (editExamCondition(argMultimap)) {
+            command = new EditExamCommandParser().parseCommand(args);
         } else {
             throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
                     EditCommand.MESSAGE_USAGE));
@@ -72,6 +75,12 @@ public class EditCommandParser {
     private boolean editAssignmentCondition(ArgumentMultimap argMultimap) {
         return arePrefixesPresent(argMultimap, PREFIX_MODULE)
                 && arePrefixesPresent(argMultimap, PREFIX_ASSIGNMENT)
+                && argMultimap.getPreamble().isEmpty();
+    }
+
+    private boolean editExamCondition(ArgumentMultimap argMultimap) {
+        return arePrefixesPresent(argMultimap, PREFIX_MODULE)
+                && arePrefixesPresent(argMultimap, PREFIX_EXAM)
                 && argMultimap.getPreamble().isEmpty();
     }
 
