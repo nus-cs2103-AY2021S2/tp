8b95dd0b6fc8b779eabb14efbe7d41ae586b1e23
Tan Jin <cjtanjin@gmail.com>
2021-03-23 22:25:51 +0800

    Chore: add test cases for SendCommandParser

--- src/test/java/seedu/us/among/logic/parser/SendCommandParserTest.java
+++ src/test/java/seedu/us/among/logic/parser/SendCommandParserTest.java
@@ -11,6 +11,9 @@ import seedu.us.among.logic.commands.SendCommand;
 
 public class SendCommandParserTest {
 
+    private static final String MESSAGE_INVALID_FORMAT = String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+            SendCommand.MESSAGE_USAGE);
+
     private SendCommandParser parser = new SendCommandParser();
 
     @Test
@@ -19,8 +22,19 @@ public class SendCommandParserTest {
     }
 
     @Test
+    public void parse_emptyArgs_returnsSendCommand() {
+        assertParseFailure(parser, "", MESSAGE_INVALID_FORMAT);
+        assertParseFailure(parser, " ", MESSAGE_INVALID_FORMAT);
+    }
+
+    @Test
     public void parse_invalidArgs_throwsParseException() {
-        assertParseFailure(parser, "a", String.format(MESSAGE_INVALID_COMMAND_FORMAT, SendCommand.MESSAGE_USAGE));
+        assertParseFailure(parser, "a", MESSAGE_INVALID_FORMAT);
+        assertParseFailure(parser, ".", MESSAGE_INVALID_FORMAT);
+        assertParseFailure(parser, "1a.", MESSAGE_INVALID_FORMAT);
+        assertParseFailure(parser, "a1", MESSAGE_INVALID_FORMAT);
+        assertParseFailure(parser, ".1", MESSAGE_INVALID_FORMAT);
+        assertParseFailure(parser, "1.", MESSAGE_INVALID_FORMAT);
     }
 
 }
