d321db7b7f313888e0f81fd389d3547d68f48987
Prabhakaran-Gokul <prabhakaran.gokul@gmail.com>
2021-03-24 02:48:23 +0800

    Fix merge conflicts

--- src/test/java/fooddiary/logic/parser/AddOnCommandParserTest.java
+++ src/test/java/fooddiary/logic/parser/AddOnCommandParserTest.java
@@ -20,7 +20,7 @@ public class AddOnCommandParserTest {
     @Test
     public void parse_missingParts_failure() {
         // no index specified
-        assertParseFailure(parser, CommandTestUtil.VALID_NAME_AMY, MESSAGE_INVALID_FORMAT);
+        assertParseFailure(parser, CommandTestUtil.VALID_NAME_A, MESSAGE_INVALID_FORMAT);
 
         // no field specified
         assertParseFailure(parser, "1", AddOnCommand.MESSAGE_NOT_ADDED_ON);
@@ -32,17 +32,17 @@ public class AddOnCommandParserTest {
     @Test
     public void parse_invalidPreamble_failure() {
         // negative index
-        assertParseFailure(parser, "-5" + CommandTestUtil.REVIEW_DESC_AMY, MESSAGE_INVALID_FORMAT);
+        assertParseFailure(parser, "-5" + CommandTestUtil.REVIEW_DESC_A, MESSAGE_INVALID_FORMAT);
 
         // zero index
-        assertParseFailure(parser, "0" + CommandTestUtil.REVIEW_DESC_AMY, MESSAGE_INVALID_FORMAT);
+        assertParseFailure(parser, "0" + CommandTestUtil.REVIEW_DESC_A, MESSAGE_INVALID_FORMAT);
 
         // invalid arguments being parsed as preamble
         assertParseFailure(parser, "1 some random string", MESSAGE_INVALID_FORMAT);
 
         // invalid arguments being parsed before prefix
         assertParseFailure(parser, "1 some random string"
-                + CommandTestUtil.REVIEW_DESC_AMY, MESSAGE_INVALID_FORMAT);
+                + CommandTestUtil.REVIEW_DESC_A, MESSAGE_INVALID_FORMAT);
 
         // invalid prefix being parsed as preamble
         assertParseFailure(parser, "1 i/ string", MESSAGE_INVALID_FORMAT);
