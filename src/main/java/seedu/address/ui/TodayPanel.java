86898ac9244133644edef5bbd65dd0d0a2e29c57
Samuel Fang <samuelfangjw@gmail.com>
2021-03-23 22:06:40 +0800

    Add current date display to today panel

--- src/main/java/seedu/address/ui/TodayPanel.java
+++ src/main/java/seedu/address/ui/TodayPanel.java
@@ -1,9 +1,13 @@
 package seedu.address.ui;
 
+import java.time.LocalDate;
 import java.util.logging.Logger;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.layout.Region;
 import seedu.address.commons.core.LogsCenter;
+import seedu.address.commons.util.DateUtil;
 
 /**
  * Panel displaying today screen.
@@ -11,12 +15,30 @@ import seedu.address.commons.core.LogsCenter;
 public class TodayPanel extends UiPart<Region> {
     private static final String FXML = "TodayPanel.fxml";
     private final Logger logger = LogsCenter.getLogger(TodayPanel.class);
+    private LocalDate currentDate;
+
+    @FXML
+    Label date;
 
     /**
      * Creates a {@code TodayPanel}.
      */
     public TodayPanel() {
         super(FXML);
+
+        currentDate = LocalDate.now();
+        date.setText(DateUtil.decodeDate(currentDate));
+
+        initEventsSection();
+        initDeadlinesSection();
+    }
+
+    private void initDeadlinesSection() {
+
+    }
+
+    private void initEventsSection() {
+
     }
 
 }
