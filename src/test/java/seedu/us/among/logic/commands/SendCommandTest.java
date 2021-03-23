970f72099dcfcfe37ecdabdae8fe32afdd82a68b
Tan Jin <cjtanjin@gmail.com>
2021-03-23 22:13:22 +0800

    Chore: add test cases for SendCommand

--- src/test/java/seedu/us/among/logic/commands/SendCommandTest.java
+++ src/test/java/seedu/us/among/logic/commands/SendCommandTest.java
@@ -3,15 +3,24 @@ package seedu.us.among.logic.commands;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.us.among.logic.commands.CommandTestUtil.assertCommandFailure;
+import static seedu.us.among.logic.commands.CommandTestUtil.showEndpointAtIndex;
 import static seedu.us.among.testutil.Assert.assertThrows;
 import static seedu.us.among.testutil.TypicalEndpoints.getTypicalEndpointList;
 import static seedu.us.among.testutil.TypicalIndexes.INDEX_EIGHTH_ENDPOINT;
 import static seedu.us.among.testutil.TypicalIndexes.INDEX_FIRST_ENDPOINT;
 import static seedu.us.among.testutil.TypicalIndexes.INDEX_SECOND_ENDPOINT;
 
+import java.io.IOException;
+
 import org.junit.jupiter.api.Test;
 
+import seedu.us.among.commons.core.Messages;
+import seedu.us.among.commons.core.index.Index;
 import seedu.us.among.commons.util.JsonUtil;
+import seedu.us.among.logic.commands.exceptions.CommandException;
+import seedu.us.among.logic.request.exceptions.AbortRequestException;
+import seedu.us.among.logic.request.exceptions.RequestException;
 import seedu.us.among.model.Model;
 import seedu.us.among.model.ModelManager;
 import seedu.us.among.model.UserPrefs;
@@ -35,6 +44,37 @@ public class SendCommandTest {
                 sampleValidResponse);
         assertEquals(expectedResponse,
                 commandResult.getFeedbackToUser());
+    }
+
+    @Test
+    public void constructor_validIndex_success() {
+        new SendCommand(INDEX_FIRST_ENDPOINT);
+    }
+
+    @Test
+    public void execute_outOfBoundEndpointIndexUnfilteredList_failure() {
+        Index outOfBoundIndex = Index.fromOneBased(model.getFilteredEndpointList().size() + 1);
+        SendCommand SendCommand = new SendCommand(outOfBoundIndex);
+        assertCommandFailure(SendCommand, model, Messages.MESSAGE_INVALID_ENDPOINT_DISPLAYED_INDEX);
+    }
+
+    @Test
+    public void execute_outOfBoundEndpointIndexFilteredList_failure() {
+        showEndpointAtIndex(model, INDEX_FIRST_ENDPOINT);
+        Index outOfBoundIndex = INDEX_SECOND_ENDPOINT;
+        // ensures that outOfBoundIndex is still in bounds of API endpoint list
+        assertTrue(outOfBoundIndex.getZeroBased() < model.getEndpointList().getEndpointList().size());
+
+        assertCommandFailure(new SendCommand(outOfBoundIndex), model,
+                Messages.MESSAGE_INVALID_ENDPOINT_DISPLAYED_INDEX);
+    }
+
+    @Test
+    public void execute_validEndpointIndex_success() throws CommandException, AbortRequestException,
+            RequestException, IOException {
+        SendCommand SendCommand = new SendCommand(Index.fromZeroBased(7));
+        assertEquals(JsonUtil.toPrettyPrintJsonString(SampleDataUtil.getSampleValidResponse()),
+                SendCommand.execute(model).getFeedbackToUser());
     }
 
     @Test
