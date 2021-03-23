7c792ae2e2f6b02a08c22bd81d96a34bfa804425
SiTingST <33442908+SiTingST@users.noreply.github.com>
2021-03-23 20:14:27 +0800

    Fix bugs

--- src/main/java/seedu/address/model/person/MatriculationNumberContainsKeywordsPredicate.java
+++ src/main/java/seedu/address/model/person/MatriculationNumberContainsKeywordsPredicate.java
@@ -1,22 +1,15 @@
 package seedu.address.model.person;
 
-import seedu.address.commons.core.LogsCenter;
-import seedu.address.logic.LogicManager;
-
 import java.util.function.Predicate;
-import java.util.logging.Logger;
 
 /**
  * Tests that a {@code Person}'s {@code Matriculation Number} matches any of the keywords given.
  */
 public class MatriculationNumberContainsKeywordsPredicate implements Predicate<Person> {
     private final String keyword;
-    private final Logger logger = LogsCenter.getLogger(LogicManager.class);
 
     public MatriculationNumberContainsKeywordsPredicate(String keyword) {
-        assert keyword.equals(keyword.toUpperCase());
         this.keyword = keyword;
-        logger.info("----------------[MATRIC NUMBER TO BE SEARCHED:][" + keyword + "]");
     }
 
     @Override
