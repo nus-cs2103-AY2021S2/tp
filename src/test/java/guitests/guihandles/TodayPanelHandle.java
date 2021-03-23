bc576c6717a46d8785ba9a59799e053701408151
Samuel Fang <samuelfangjw@gmail.com>
2021-03-24 01:01:59 +0800

    Add test for date in today panel

---
+++ src/test/java/guitests/guihandles/TodayPanelHandle.java
@@ -0,0 +1,24 @@
+package guitests.guihandles;
+
+import javafx.scene.Node;
+import javafx.scene.control.Labeled;
+
+/**
+ * Provides a handle for {@code TodayPanel} containing the list of {@code EventCard} and
+ * {@code CompletableDeadlineCard}.
+ */
+public class TodayPanelHandle extends NodeHandle<Node> {
+    public static final String DATE_ID = "#date";
+    private static final String EVENTS_PLACEHOLDER_ID = "#eventsListViewPlaceholder";
+    private static final String DEADLINES_PLACEHOLDER_ID = "#deadlinesListViewPlaceholder";
+
+
+    public TodayPanelHandle(Node todayPanelNode) {
+        super(todayPanelNode);
+    }
+
+    public String getDisplayedDate() {
+        Labeled date = getChildNode(DATE_ID);
+        return date.getText();
+    }
+}
