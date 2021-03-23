c3051fcbd5c2517022d0e83dc901fe7dc2e66166
Mark Wong <markmcwong@yahoo.com>
2021-03-23 17:45:51 +0800

    fix words that are too long becoming ellipsis

--- src/main/java/seedu/address/ui/LabelWithIcon.java
+++ src/main/java/seedu/address/ui/LabelWithIcon.java
@@ -1,7 +1,8 @@
 package seedu.address.ui;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.Labeled;
+import javafx.scene.control.OverrunStyle;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Region;
@@ -12,7 +13,7 @@ public class LabelWithIcon extends UiPart<Region> {
     @FXML
     private Image icon;
     @FXML
-    private Label text;
+    private Labeled text;
     @FXML
     private ImageView iconHolder;
 
@@ -21,5 +22,6 @@ public class LabelWithIcon extends UiPart<Region> {
         icon = new Image(getClass().getResourceAsStream(iconImgPath));
         iconHolder.setImage(icon);
         text.setText(textToDisplay);
+        text.setTextOverrun(OverrunStyle.CLIP);
     }
 }
