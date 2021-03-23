4ad2592bb1bda85a37db19b1335337e7c38c5c31
Md-Fazil <fuzyll10598@gmail.com>
2021-03-23 17:38:31 +0800

    Change order of assertion in quantity constructor

--- src/main/java/seedu/storemando/model/item/Quantity.java
+++ src/main/java/seedu/storemando/model/item/Quantity.java
@@ -24,8 +24,8 @@ public class Quantity {
      */
     public Quantity(String quantity) {
         requireNonNull(quantity);
-        assert(Long.valueOf(quantity) > 0);
         checkArgument(isValidQuantity(quantity), MESSAGE_CONSTRAINTS);
+        assert(Long.valueOf(quantity) > 0);
         value = quantity;
     }
 
