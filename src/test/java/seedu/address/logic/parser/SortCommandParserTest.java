280f71ab6d0d780195d42cf393dbc779a6022c10
Nguyen Son Linh <linhns59@gmail.com>
2021-03-23 22:00:23 +0800

    add tests for SortCommandParser

---
+++ src/test/java/seedu/address/logic/parser/SortCommandParserTest.java
@@ -0,0 +1,28 @@
+package seedu.address.logic.parser;
+
+import org.junit.jupiter.api.Test;
+import seedu.address.logic.commands.SortCommand;
+import seedu.address.model.flashcard.SortOptions;
+
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
+
+public class SortCommandParserTest {
+    private SortCommandParser parser = new SortCommandParser();
+    @Test
+    public void parse_invalidArgs_throwsParseException() {
+        assertParseFailure(parser, " ", String.format(MESSAGE_INVALID_COMMAND_FORMAT, SortCommand.MESSAGE_USAGE));
+        assertParseFailure(parser, "category -c",
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, SortCommand.MESSAGE_USAGE));
+        assertParseFailure(parser, "question -t",
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, SortCommand.MESSAGE_USAGE));
+    }
+
+    @Test
+    public void parse_validArgs_returnsSortCommand() {
+        SortCommand expectedSortCommand = new SortCommand(SortOptions.QUESTION_DESCENDING);
+        assertParseSuccess(parser, "question -d", expectedSortCommand);
+        assertParseSuccess(parser, " question -d  ", expectedSortCommand); // with leading and trailing spaces
+    }
+}
