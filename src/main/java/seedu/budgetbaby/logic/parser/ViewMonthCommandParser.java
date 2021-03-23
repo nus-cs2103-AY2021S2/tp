3523834d3a4abbce7b3330f685987427ea29d58e
eksinyue <xinyuehehxy@gmail.com>
2021-03-23 12:02:50 +0800

    Clean up ViewMonthCommandParser

--- src/main/java/seedu/budgetbaby/logic/parser/ViewMonthCommandParser.java
+++ src/main/java/seedu/budgetbaby/logic/parser/ViewMonthCommandParser.java
@@ -1,22 +1,12 @@
 package seedu.budgetbaby.logic.parser;
 
-import seedu.budgetbaby.logic.commands.AddFrCommand;
-import seedu.budgetbaby.logic.commands.ViewMonthCommand;
-import seedu.budgetbaby.logic.parser.exceptions.ParseException;
-import seedu.budgetbaby.model.record.Amount;
-import seedu.budgetbaby.model.record.Category;
-import seedu.budgetbaby.model.record.Description;
-import seedu.budgetbaby.model.record.FinancialRecord;
-
 import java.time.YearMonth;
 import java.util.List;
-import java.util.Set;
-import java.util.stream.Stream;
+
+import seedu.budgetbaby.logic.commands.ViewMonthCommand;
+import seedu.budgetbaby.logic.parser.exceptions.ParseException;
 
 import static seedu.budgetbaby.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
-import static seedu.budgetbaby.logic.parser.CliSyntax.PREFIX_AMOUNT;
-import static seedu.budgetbaby.logic.parser.CliSyntax.PREFIX_CATEGORY;
-import static seedu.budgetbaby.logic.parser.CliSyntax.PREFIX_DESCRIPTION;
 
 /**
  * Parses input arguments and creates a new ViewMonthCommand object
@@ -39,13 +29,4 @@ public class ViewMonthCommandParser implements BudgetBabyCommandParser<ViewMonth
 
         return new ViewMonthCommand(month);
     }
-
-    /**
-     * Returns true if none of the prefixes contains empty {@code Optional} values in the given
-     * {@code ArgumentMultimap}.
-     */
-    private static boolean arePrefixesPresent(ArgumentMultimap argumentMultimap, Prefix... prefixes) {
-        return Stream.of(prefixes).allMatch(prefix -> argumentMultimap.getValue(prefix).isPresent());
-    }
-
 }
