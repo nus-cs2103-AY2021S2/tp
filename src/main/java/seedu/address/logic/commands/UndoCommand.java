9ade157d5fe167bbb409fd7ed5f6f6f906868fb6
Cp-John <e0376984@u.nus.edu>
2021-03-23 17:46:28 +0800

    Add a new error message in undo command

--- src/main/java/seedu/address/logic/commands/UndoCommand.java
+++ src/main/java/seedu/address/logic/commands/UndoCommand.java
@@ -22,6 +22,8 @@ public class UndoCommand extends Command {
     public static final String MESSAGE_NO_COMMAND_TO_UNDO = "No add, delete or edit commands in the" +
             " command history to undo";
 
+    public static final String MESSAGE_UNKNOWN_COMMAND = "Unknown command in command history: %s";
+
     private static final Pattern BASIC_COMMAND_FORMAT =
             Pattern.compile("(?<commandWord>\\S+(\\s(appointment|property|all))?)(?<arguments>.*)");
 
@@ -52,23 +54,30 @@ public class UndoCommand extends Command {
         if (commandHistory.empty()) {
             throw new CommandException(MESSAGE_NO_COMMAND_TO_UNDO);
         }
+
         String commandString = commandHistory.pop();
         commandString = commandString.trim();
         final Matcher matcher = BASIC_COMMAND_FORMAT.matcher(commandString);
-        if (matcher.matches()) {
-            final String commandWord = matcher.group("commandWord");
-            switch (commandWord) {
-            case AddAppointmentCommand.COMMAND_WORD:
-            case EditAppointmentCommand.COMMAND_WORD:
-            case DeleteAppointmentCommand.COMMAND_WORD:
-                model.undoAppointmentBook();
-                break;
-            default:
-                model.undoPropertyBook();
-            }
+        if (!matcher.matches()) {
+            throw new CommandException(String.format(MESSAGE_UNKNOWN_COMMAND, commandString));
+        }
+
+        final String commandWord = matcher.group("commandWord");
+        switch (commandWord) {
+        case AddAppointmentCommand.COMMAND_WORD:
+        case EditAppointmentCommand.COMMAND_WORD:
+        case DeleteAppointmentCommand.COMMAND_WORD:
+            model.undoAppointmentBook();
+            break;
+        case AddPropertyCommand.COMMAND_WORD:
+        case EditPropertyCommand.COMMAND_WORD:
+        case DeletePropertyCommand.COMMAND_WORD:
+            model.undoPropertyBook();
+            break;
+        default:
+            throw new CommandException(String.format(MESSAGE_UNKNOWN_COMMAND, commandString));
         }
         return new CommandResult(String.format(String.format(MESSAGE_SUCCESS, commandString)));
     }
-
 
 }
