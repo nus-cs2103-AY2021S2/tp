6c9808bdcd8acd9a55bee5a59d6da175f2dd19e5
Marcus Lee <marcus_lee@u.nus.edu>
2021-03-24 01:37:15 +0800

    add cascading deletion of customers
    Modify ModelManager to also delete all orders pertaining to a
    particular customer when they are deleted.
    
    Modify Order to include method to check if order belongs to a customer

--- src/main/java/seedu/address/model/ModelManager.java
+++ src/main/java/seedu/address/model/ModelManager.java
@@ -4,6 +4,8 @@ import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
 
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.function.Predicate;
 import java.util.logging.Logger;
 
@@ -142,6 +144,18 @@ public class ModelManager implements Model {
     @Override
     public void deletePerson(Person target) {
         personBook.removePerson(target);
+
+        List<Order> deletionList = new ArrayList<>();
+        ObservableList<Order> orders = getOrderBook().getOrderList();
+        for (Order o : orders) {
+            if (o.isFromCustomer(target)) {
+                deletionList.add(o);
+            }
+        }
+
+        for (Order o : deletionList) {
+            deleteOrder(o);
+        }
     }
 
     @Override
