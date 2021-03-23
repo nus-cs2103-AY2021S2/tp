302ddfac32ca7cf875fd0f35222b4c576c9ecf08
cheeerynne <cheeerynne@gmail.com>
2021-03-23 22:23:15 +0800

    Add assertion

--- src/main/java/seedu/address/storage/JsonAdaptedPerson.java
+++ src/main/java/seedu/address/storage/JsonAdaptedPerson.java
@@ -38,6 +38,8 @@ class JsonAdaptedPerson {
      * Converts a given {@code Person} into this class for Jackson use.
      */
     public JsonAdaptedPerson(Person source) {
+        assert source != null : "person source should not be null";
+
         name = source.getName().fullName;
         phone = source.getPhone().value;
         email = source.getEmail().value;
