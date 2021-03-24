231c54e1ed360382f97adffa2ff65bfb102441b5
Jay Chen <jaychenyj@gmail.com>
2021-03-24 18:10:14 +0800

    Update message displayed

--- src/main/java/seedu/storemando/logic/commands/ReminderCommand.java
+++ src/main/java/seedu/storemando/logic/commands/ReminderCommand.java
@@ -2,24 +2,22 @@ package seedu.storemando.logic.commands;
 
 import static java.util.Objects.requireNonNull;
 
-import java.util.List;
-
 import seedu.storemando.logic.commands.exceptions.CommandException;
 import seedu.storemando.model.Model;
 import seedu.storemando.model.expirydate.ItemExpiringPredicate;
-import seedu.storemando.model.item.Item;
 import seedu.storemando.model.item.ItemComparatorByExpiryDate;
 
 /**
- * Finds and lists all items in storemando whose item's expiry date is within a certain days from today.
+ * Finds and lists all items in storemando whose item's expiry date is within a certain days/weeks from today.
  */
 public class ReminderCommand extends Command {
 
     public static final String COMMAND_WORD = "reminder";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Filters all items whose expiry date is within "
-        + "the user-specified number of days from the current date and displays them as a list with index numbers.\n"
-        + "Parameters: numOfDays (must be a positive integer) [timeUnit] (must either be days or weeks)\n"
+        + "the user-specified number of days/weeks from the current date and displays them as a list with index "
+        + "numbers.\n"
+        + "Parameters: Days/Weeks (must be an integer) [timeUnit] (must either be day/week/days/weeks)\n"
         + "Example: \n- " + COMMAND_WORD + " 3 days\n- " + COMMAND_WORD + " 1 week";
 
     public static final String MESSAGE_SUCCESS = "Display all expiring items";
