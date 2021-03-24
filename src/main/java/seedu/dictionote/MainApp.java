0091007a8b529b1295f5b3c2d439702756097287
ALZAHRANI-PC\riyad <riyadh-h@outlook.sa>
2021-03-24 17:43:31 +0800

    Update initiating ContactsList logging statement

--- src/main/java/seedu/dictionote/MainApp.java
+++ src/main/java/seedu/dictionote/MainApp.java
@@ -60,7 +60,7 @@ public class MainApp extends Application {
 
     @Override
     public void init() throws Exception {
-        logger.info("=============================[ Initializing AddressBook ]===========================");
+        logger.info("=============================[ Initializing ContactsList ]===========================");
         logger.info("=============================[ Initializing NoteBook ]==============================");
         logger.info("=============================[ Initializing Dictionary ]============================");
         logger.info("=============================[ Initializing DefinitionBook ]========================");
