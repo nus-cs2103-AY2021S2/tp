ea7e63b2db5abc10fe6adf8d1dfe3fa49e46c55f
Zack Ng <ngzhicai98@gmail.com>
2021-03-24 14:47:33 +0800

    Fix MainApp assertions

--- src/main/java/seedu/address/MainApp.java
+++ src/main/java/seedu/address/MainApp.java
@@ -60,9 +60,8 @@ public class MainApp extends Application {
 
         UserPrefsStorage userPrefsStorage = new JsonUserPrefsStorage(config.getUserPrefsFilePath());
         UserPrefs userPrefs = initPrefs(userPrefsStorage);
-        AddressBookStorage addressBookStorage = null;
+        AddressBookStorage addressBookStorage = new JsonAddressBookStorage(userPrefs.getAddressBookFilePath());
         EventBookStorage eventBookStorage = new JsonEventBookStorage(userPrefs.getEventBookFilePath());
-        assert addressBookStorage != null: "Address book storage is null";
         storage = new StorageManager(addressBookStorage, eventBookStorage, userPrefsStorage);
 
         initLogging(config);
