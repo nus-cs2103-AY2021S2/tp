a70e1d64c0e0760f1060ae62ea616f186aa0ffbe
Jared Lim <jaredlim1998@hotmail.com>
2021-03-24 23:20:57 +0800

    no message

--- src/main/java/seedu/address/model/ModelManager.java
+++ src/main/java/seedu/address/model/ModelManager.java
@@ -4,10 +4,8 @@ import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
 
 import java.nio.file.Path;
-import java.util.List;
 import java.util.function.Predicate;
 import java.util.logging.Logger;
-import java.util.stream.Collectors;
 
 import javafx.collections.ObservableList;
 import javafx.collections.transformation.FilteredList;
@@ -16,9 +14,7 @@ import seedu.address.commons.core.LogsCenter;
 import seedu.address.model.appointment.Appointment;
 import seedu.address.model.appointment.AppointmentDateTime;
 import seedu.address.model.appointment.DateViewPredicate;
-import seedu.address.model.appointment.NamePredicate;
 import seedu.address.model.budget.Budget;
-import seedu.address.model.person.Name;
 import seedu.address.model.person.Person;
 
 /**
@@ -33,7 +29,7 @@ public class ModelManager implements Model {
     private final UserPrefs userPrefs;
     private final FilteredList<Person> filteredPersons;
     private final FilteredList<Appointment> filteredAppointment;
-    //Temporarily re-instantiated to prevent merge conflict
+
     private final BudgetBook budgetBook;
 
     /**
@@ -187,19 +183,6 @@ public class ModelManager implements Model {
     //=========== AppointmentList ============================================================================
 
     /**
-     * Creates an appointment predicate the given person {@code predicate}.
-     *
-     * @throws NullPointerException if {@code predicate} is null.
-     */
-    public Predicate<Appointment> getNamePredicate(Predicate<Person> predicate) {
-        requireNonNull(predicate);
-        ObservableList<Person> list = getFilteredPersonList();
-        List<Name> nameList =
-                list.filtered(predicate).stream().map(Person::getName).collect(Collectors.toList());
-        return new NamePredicate(nameList);
-    }
-
-    /**
      * Updates the filter of the filtered appointment list to filter by the given {@code predicate}.
      *
      * @throws NullPointerException if {@code predicate} is null.
@@ -208,17 +191,6 @@ public class ModelManager implements Model {
     public void updateFilteredAppointmentList(Predicate<Appointment> predicate) {
         requireNonNull(predicate);
         filteredAppointment.setPredicate(predicate);
-    }
-
-    /**
-     * Updates the filter of the filtered appointment list to filter by the given person {@code predicate}.
-     *
-     * @throws NullPointerException if {@code predicate} is null.
-     */
-    @Override
-    public void updateFilteredAppointmentListByName(Predicate<Person> predicate) {
-        requireNonNull(predicate);
-        updateFilteredAppointmentList(getNamePredicate(predicate));
     }
 
     /**
--- src/main/java/seedu/address/model/appointment/NamePredicate.java
+++ src/main/java/seedu/address/model/appointment/NamePredicate.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.function.Predicate;
 
 import seedu.address.commons.util.StringUtil;
+import seedu.address.model.person.Name;
 
 /**
  * Tests that a {@code Person}'s {@code Name} matches any of the keywords given.
@@ -11,16 +12,16 @@ import seedu.address.commons.util.StringUtil;
  * Then, {@code Email} will be extracted out to perform the Predicate Search.
  */
 public class NamePredicate implements Predicate<Appointment> {
-    private final List<String> keywords;
+    private final List<Name> keywords;
 
-    public NamePredicate(List<String> keywords) {
+    public NamePredicate(List<Name> keywords) {
         this.keywords = keywords;
     }
 
     @Override
     public boolean test(Appointment appointment) {
         return keywords.stream()
-                .anyMatch(keyword -> StringUtil.containsWordIgnoreCase(appointment.getName().fullName, keyword));
+                .anyMatch(keyword -> StringUtil.containsWordIgnoreCase(appointment.getName().fullName, keyword.fullName));
     }
 
     @Override
