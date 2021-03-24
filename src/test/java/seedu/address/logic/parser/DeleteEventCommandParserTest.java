18a48022b4a6a3e063db7aee54f53e898f9531f6
Vevek Selvam <vevekselvam@gmail.com>
2021-03-25 02:23:43 +0800

    Update DeleteEventCommandParserTest

--- src/test/java/seedu/address/logic/parser/DeleteEventCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/DeleteEventCommandParserTest.java
@@ -1,5 +1,10 @@
 package seedu.address.logic.parser;
 
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.commands.DeleteTodoCommand;
+
+import org.junit.jupiter.api.Test;
+
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_REMOVE_TASK_INDEX;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
@@ -7,12 +12,7 @@ import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSucces
 import static seedu.address.logic.parser.ParserUtil.MESSAGE_INVALID_INDEX;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST;
 
-import org.junit.jupiter.api.Test;
-
-import seedu.address.commons.core.index.Index;
-import seedu.address.logic.commands.DeleteTodoCommand;
-
-public class DeleteTodoCommandParserTest {
+public class DeleteEventCommandParserTest {
 
     private DeleteTodoCommandParser parser = new DeleteTodoCommandParser();
 
