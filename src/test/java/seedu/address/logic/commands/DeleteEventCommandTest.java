122078833d08826929e009aef679b04959a639bf
Vevek Selvam <vevekselvam@gmail.com>
2021-03-25 02:23:00 +0800

    Add test classes for event

---
+++ src/test/java/seedu/address/logic/commands/DeleteEventCommandTest.java
@@ -0,0 +1,160 @@
+package seedu.address.logic.commands;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static seedu.address.commons.core.Messages.MESSAGE_DELETE_TODO_SUCCESS;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.testutil.Assert.assertThrows;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST;
+import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND;
+import static seedu.address.testutil.TypicalIndexes.INDEX_THIRD;
+import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+import static seedu.address.testutil.TypicalProjects.getTypicalProjectsFolder;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import seedu.address.commons.core.Messages;
+import seedu.address.commons.core.index.Index;
+import seedu.address.commons.exceptions.DateConversionException;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+import seedu.address.model.project.Project;
+import seedu.address.model.task.todo.Todo;
+import seedu.address.testutil.ProjectBuilder;
+import seedu.address.testutil.TodoBuilder;
+
+public class DeleteTodoCommandTest {
+
+    private Model model;
+
+    @BeforeEach
+    public void setUp() throws DateConversionException {
+        model = new ModelManager(getTypicalAddressBook(), getTypicalProjectsFolder(), new UserPrefs());
+    }
+
+    @Test
+    public void execute_validIndexUnfilteredList_success() throws DateConversionException {
+        Todo todoToDelete = new TodoBuilder().build();
+        Project projectToEdit = model.getFilteredProjectList().get(INDEX_FIRST.getZeroBased());
+        Project editedProject = new ProjectBuilder(projectToEdit).build();
+        editedProject.addTodo(todoToDelete);
+
+        model.setProject(
+                projectToEdit,
+                editedProject
+        );
+
+        Index lastTodoIndex = Index.fromOneBased(
+                model.getFilteredProjectList().get(INDEX_FIRST.getZeroBased()).getTodos().getTodos().size());
+
+        DeleteTodoCommand deleteTodoCommand = new DeleteTodoCommand(INDEX_FIRST, lastTodoIndex);
+
+        String expectedMessage = String.format(MESSAGE_DELETE_TODO_SUCCESS, lastTodoIndex.getOneBased());
+
+        ModelManager expectedModel = new ModelManager(
+                getTypicalAddressBook(), getTypicalProjectsFolder(), new UserPrefs()
+        );
+
+        assertCommandSuccess(deleteTodoCommand, model, expectedMessage, expectedModel);
+    }
+
+    @Test
+    public void execute_invalidProjectIndex_throwsCommandException() {
+        Todo todoToDelete = new TodoBuilder().build();
+        Project projectToEdit = model.getFilteredProjectList().get(INDEX_FIRST.getZeroBased());
+        Project editedProject = new ProjectBuilder(projectToEdit).build();
+        editedProject.addTodo(todoToDelete);
+
+        model.setProject(
+                projectToEdit,
+                editedProject
+        );
+
+        Index lastTodoIndex = Index.fromOneBased(
+                model.getFilteredProjectList().get(INDEX_FIRST.getZeroBased()).getTodos().getTodos().size());
+
+        DeleteTodoCommand deleteTodoCommand = new DeleteTodoCommand(INDEX_THIRD, lastTodoIndex);
+
+        assertThrows(
+                CommandException.class,
+                Messages.MESSAGE_INVALID_PROJECT_DISPLAYED_INDEX, () -> deleteTodoCommand.execute(model)
+        );
+    }
+
+    @Test
+    public void execute_invalidTodoIndex_throwsCommandException() {
+        Todo todoToDelete = new TodoBuilder().build();
+        Project projectToEdit = model.getFilteredProjectList().get(INDEX_FIRST.getZeroBased());
+        Project editedProject = new ProjectBuilder(projectToEdit).build();
+        editedProject.addTodo(todoToDelete);
+
+        model.setProject(
+                projectToEdit,
+                editedProject
+        );
+
+        Index invalidTodoIndex = Index.fromOneBased(
+                model.getFilteredProjectList().get(INDEX_FIRST.getZeroBased()).getTodos().getTodos().size() + 1);
+
+        DeleteTodoCommand deleteTodoCommand = new DeleteTodoCommand(INDEX_FIRST, invalidTodoIndex);
+
+        assertThrows(
+                CommandException.class,
+                Messages.MESSAGE_INVALID_TODO_DISPLAYED_INDEX, () -> deleteTodoCommand.execute(model)
+        );
+    }
+
+    @Test
+    public void equals() {
+        Todo todoToDelete = new TodoBuilder().build();
+        Project project1ToEdit = model.getFilteredProjectList().get(INDEX_FIRST.getZeroBased());
+        Project editedProject1 = new ProjectBuilder(project1ToEdit).build();
+        editedProject1.addTodo(todoToDelete);
+
+        model.setProject(
+                project1ToEdit,
+                editedProject1
+        );
+
+        Project project2ToEdit = model.getFilteredProjectList().get(INDEX_SECOND.getZeroBased());
+        Project editedProject2 = new ProjectBuilder(project2ToEdit).build();
+        editedProject2.addTodo(todoToDelete);
+
+
+        model.setProject(
+                project2ToEdit,
+                editedProject2
+        );
+
+        Index lastTodoFromProject1 = Index.fromOneBased(
+                model.getFilteredProjectList().get(INDEX_FIRST.getZeroBased()).getTodos().getTodos().size());
+        Index lastTodoFromProject2 = Index.fromOneBased(
+                model.getFilteredProjectList().get(INDEX_SECOND.getZeroBased()).getTodos().getTodos().size());
+
+        DeleteTodoCommand deleteTodoFromProject1Command = new DeleteTodoCommand(
+                INDEX_FIRST, lastTodoFromProject1);
+        DeleteTodoCommand deleteTodoFromProject2Command = new DeleteTodoCommand(
+                INDEX_SECOND, lastTodoFromProject2);
+
+        // same object -> returns true
+        assertEquals(deleteTodoFromProject1Command, deleteTodoFromProject1Command);
+
+        // same values -> returns true
+        DeleteTodoCommand deleteTodoFromProject1CommandCopy = new DeleteTodoCommand(
+                INDEX_FIRST, lastTodoFromProject1);
+        assertEquals(deleteTodoFromProject1Command, deleteTodoFromProject1CommandCopy);
+
+        // different types -> returns false
+        assertNotEquals(deleteTodoFromProject1Command, 1);
+
+        // null -> returns false
+        assertNotEquals(deleteTodoFromProject1Command, null);
+
+        // different person -> returns false
+        assertNotEquals(deleteTodoFromProject1Command, deleteTodoFromProject2Command);
+    }
+
+}
---
+++ src/test/java/seedu/address/logic/parser/DeleteEventCommandParserTest.java
@@ -0,0 +1,83 @@
+package seedu.address.logic.parser;
+
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_REMOVE_TASK_INDEX;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
+import static seedu.address.logic.parser.ParserUtil.MESSAGE_INVALID_INDEX;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.commands.DeleteTodoCommand;
+
+public class DeleteTodoCommandParserTest {
+
+    private DeleteTodoCommandParser parser = new DeleteTodoCommandParser();
+
+    @Test
+    public void parse_allFieldsPresent_success() {
+        Index expectedProjectIndex = Index.fromOneBased(1);
+        Index expectedTodoIndex = Index.fromOneBased(1);
+
+        String userInput = INDEX_FIRST.getOneBased() + " " + PREFIX_REMOVE_TASK_INDEX + " " + INDEX_FIRST.getOneBased();
+
+        DeleteTodoCommand command = new DeleteTodoCommand(expectedProjectIndex, expectedTodoIndex);
+
+        // all field appear once
+        assertParseSuccess(parser, userInput, command);
+    }
+
+    @Test
+    public void parse_compulsoryFieldMissing_failure() {
+        String expectedMessage = String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteTodoCommand.MESSAGE_USAGE);
+
+        String userInputMissingProject = PREFIX_REMOVE_TASK_INDEX + " " + INDEX_FIRST.getOneBased();
+
+        // missing project index
+        assertParseFailure(parser, userInputMissingProject, expectedMessage);
+
+        String userInputMissingPrefix = INDEX_FIRST.getOneBased() + " " + INDEX_FIRST.getOneBased();
+
+        // missing remove prefix
+        assertParseFailure(parser, userInputMissingPrefix, expectedMessage);
+
+        String userInputMissingTodo = INDEX_FIRST.getOneBased() + " " + PREFIX_REMOVE_TASK_INDEX;
+        String expectedMessageMissingTodo = "Index is not a non-zero unsigned integer.";
+
+        // missing remove prefix
+        assertParseFailure(parser, userInputMissingTodo, expectedMessageMissingTodo);
+    }
+
+    @Test
+    public void parse_invalidValue_failure() {
+        Index validProjectIndex = Index.fromOneBased(1);
+        Index validTodoIndex = Index.fromOneBased(1);
+
+        String userInputInvalidProjectIndex = "0 " + PREFIX_REMOVE_TASK_INDEX + " "
+                + validTodoIndex;
+
+        // invalid project index
+        assertParseFailure(parser, userInputInvalidProjectIndex, MESSAGE_INVALID_INDEX);
+
+        String userInputInvalidTodoIndex = validProjectIndex + " " + PREFIX_REMOVE_TASK_INDEX + " 0";
+
+        // invalid remove todo index
+        assertParseFailure(parser, userInputInvalidTodoIndex,
+                MESSAGE_INVALID_INDEX);
+
+        String userInputInvalidProjectIndexExceeded = "10 " + PREFIX_REMOVE_TASK_INDEX + " "
+                + validTodoIndex;
+
+        // invalid project index
+        assertParseFailure(parser, userInputInvalidProjectIndexExceeded, MESSAGE_INVALID_INDEX);
+
+        String userInputInvalidTodoIndexExceeded = validProjectIndex + " " + PREFIX_REMOVE_TASK_INDEX + " 10";
+
+        // invalid remove todo index
+        assertParseFailure(parser, userInputInvalidTodoIndexExceeded,
+                MESSAGE_INVALID_INDEX);
+    }
+
+}
