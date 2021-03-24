30dc06632d176bb14a487747add32a6b44bc9d94
yutingzou <zouyuting0801@gmail.com>
2021-03-24 18:01:15 +0800

    Add Grade package and Grade class

---
+++ src/main/java/seedu/address/model/grade/Grade.java
@@ -0,0 +1,84 @@
+package seedu.address.model.grade;
+
+import seedu.address.model.subject.SubjectName;
+import seedu.address.model.tag.Filterable;
+
+import java.util.Objects;
+
+public class Grade implements Filterable {
+    public static final String MESSAGE_CONSTRAINTS =
+            "Each Grade must contain subject name, exam name and grade.";
+
+    /*
+     * The first character of the gradedItem must not be a whitespace,
+     * otherwise " " (a blank string) becomes a valid input.
+     */
+    public static final String VALIDATION_REGEX = "[\\p{Alnum}][\\p{Alnum} ]*";
+
+    /*
+     * The first character of the grade must not be a whitespace,
+     * otherwise " " (a blank string) becomes a valid input.
+     * The grade must be
+     */
+    public static final String VALIDATION_GRADE = "[\\p{Alnum}][-+]?";
+
+    private final SubjectName subject;
+    private final String gradedItem;
+    private final String grade;
+
+    public Grade(SubjectName subject, String gradedItem, String grade) {
+        this.subject = subject;
+        this.gradedItem = gradedItem;
+        this.grade = grade;
+    }
+
+    public SubjectName getSubject() {
+        return this.subject;
+    }
+
+    public String getGradedItem() {
+        return this.gradedItem;
+    }
+
+    public String getGrade() {
+        return this.grade;
+    }
+
+    public static boolean isValidGradedItem(String test) {
+        return test.matches(VALIDATION_REGEX);
+    }
+
+    public static boolean isValidGrade(String test) {
+        return test.matches(VALIDATION_GRADE);
+    }
+
+    @Override
+    public String toString() {
+        return subject.name + " - " + gradedItem + " - " + grade;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Grade that = (Grade) o;
+        return Objects.equals(subject, that.subject)
+                && Objects.equals(gradedItem, that.gradedItem)
+                && Objects.equals(grade, that.grade);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(subject, gradedItem, grade);
+    }
+
+    @Override
+    public boolean filter(String s) {
+        return subject.filter(s);
+    }
+
+}
