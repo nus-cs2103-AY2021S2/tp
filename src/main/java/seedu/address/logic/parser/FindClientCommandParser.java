01851a977424b98fd2b40d58d2ecfe4b36e79cff
yuchen <yuchenwang97@gmail.com>
2021-03-25 01:59:41 +0800

    Create FindClientCommandParser.

--- src/main/java/seedu/address/logic/parser/FindClientCommandParser.java
+++ src/main/java/seedu/address/logic/parser/FindClientCommandParser.java
@@ -1,8 +1,12 @@
 package seedu.address.logic.parser;
 
-import seedu.address.logic.commands.FindAppointmentCommand;
 import seedu.address.logic.commands.FindClientCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.appointment.AppointmentContainsKeywordsPredicate;
+import seedu.address.model.property.PropertyClientNamePredicate;
+
+import java.util.Arrays;
+import java.util.List;
 
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 
@@ -13,9 +17,14 @@ public class FindClientCommandParser implements Parser<FindClientCommand> {
         String trimmedArgs = args.trim();
         if (trimmedArgs.isEmpty()) {
             throw new ParseException(
-                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindAppointmentCommand.MESSAGE_USAGE));
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindClientCommand.MESSAGE_USAGE));
         }
 
-        return null;
+        String[] nameKeywords = trimmedArgs.split("\\s+");
+
+        List<String> keywordList = Arrays.asList(nameKeywords);
+
+        return new FindClientCommand(new PropertyClientNamePredicate(keywordList),
+            new AppointmentContainsKeywordsPredicate(keywordList));
     }
 }
