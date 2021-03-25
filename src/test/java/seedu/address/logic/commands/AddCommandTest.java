9add0fc0af1003d3b746cd6e98595741bd7ec800
yutingzou <zouyuting0801@gmail.com>
2021-03-25 22:12:53 +0800

    Fix checkstyle error for tests

--- src/test/java/seedu/address/logic/commands/AddCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddCommandTest.java
@@ -17,15 +17,15 @@ import javafx.collections.ObservableList;
 import seedu.address.commons.core.GuiSettings;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.AddressBook;
-import seedu.address.model.appointment.Appointment;
-import seedu.address.model.appointment.AppointmentDateTime;
-import seedu.address.model.grade.Grade;
 import seedu.address.model.Model;
-import seedu.address.model.person.Person;
 import seedu.address.model.ReadOnlyAddressBook;
 import seedu.address.model.ReadOnlyAppointmentBook;
 import seedu.address.model.ReadOnlyGradeBook;
 import seedu.address.model.ReadOnlyUserPrefs;
+import seedu.address.model.appointment.Appointment;
+import seedu.address.model.appointment.AppointmentDateTime;
+import seedu.address.model.grade.Grade;
+import seedu.address.model.person.Person;
 import seedu.address.testutil.PersonBuilder;
 
 public class AddCommandTest {
