c5a7b217be9a5f26102010921cc6d3b5c7cd06af
Mok Bing Wei Maurice <Maurice2n97@gmail.com>
2021-03-25 21:56:37 +0800

    Add Timetable model
    Allows booking of specific time ranges
    Allows freeing of time ranges
    Throws exception if booking is made twice
    Timetable model only has represents 7 days starting from a given input date.
    
    Lets
    - Implement some interfaces

--- src/main/java/seedu/address/model/scheduler/Timetable.java
+++ src/main/java/seedu/address/model/scheduler/Timetable.java
@@ -1,15 +1,20 @@
 package seedu.address.model.scheduler;
 
-import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.time.LocalTime;
+
+import static java.time.temporal.ChronoUnit.DAYS;
+import static java.util.Objects.requireNonNull;
 
 /**
  * A weekly timetable class, starting from Monday to Sunday, containing half hour slots that can be booked.
- * Internals should not be modified except for booking purposes.
+ * Internals should not be modified except for booking purposes. The startOfTheWeek date is the date of the first day
+ * of the week.
  *
  */
 
 public class Timetable {
+    public static final String MESSAGE_DAY_NOT_WITHIN_TIMETABLE = "the date is not within this week's timetable.";
     public static final int NUMBER_OF_DAYS = 7;
 
     private final DaySchedule[] weeklySchedule = new DaySchedule[NUMBER_OF_DAYS];
@@ -21,7 +26,6 @@ public class Timetable {
      */
 
     public Timetable(LocalDate startOfTheWeek) {
-        assert startOfTheWeek.getDayOfWeek().equals(DayOfWeek.MONDAY);
         this.startOfTheWeek = startOfTheWeek;
         for ( int i = 0; i < NUMBER_OF_DAYS; i++) {
             LocalDate currentDay = startOfTheWeek.plusDays(i);
@@ -40,16 +44,61 @@ public class Timetable {
     }
 
     /**
-     * Return a Day Schedule Object corresponding to the dayOfTheWeek.
-     * @param dayOfWeek
+     * get the Day schedule corresponding to a localDate;
+     * @param localDate
      * @return
+     * @throws TimetableAccessError if the date is not within the timetable.
      */
 
-    public DaySchedule getDaySchedule(DayOfWeek dayOfWeek) {
-        dayOfWeek.getValue();
-        return weeklySchedule[dayOfWeek.getValue()];
+    private DaySchedule getDaySchedule(LocalDate localDate) throws TimetableAccessError {
+        if (!isDateWithinTimetable(localDate)) {
+            throw new TimetableAccessError(MESSAGE_DAY_NOT_WITHIN_TIMETABLE);
+        }
+        int index = (int) DAYS.between(startOfTheWeek, localDate);
+        return weeklySchedule[index];
     }
 
+
+    /**
+     * checks if a given date is part of the timetable or not.
+     * @param localDate date within the timetable
+     * @return
+     */
+    public boolean isDateWithinTimetable(LocalDate localDate) {
+        requireNonNull(localDate);
+        return !(localDate.isBefore(startOfTheWeek) || localDate.isAfter(startOfTheWeek.plusDays(NUMBER_OF_DAYS)));
+    }
+
+    /**
+     * Books a slot in the timetable at a specified localDate, with interval range starTime and endTime;
+     * @param localDate
+     * @param startTime
+     * @param endTime
+     */
+    public void bookTimeRange(LocalDate localDate, LocalTime startTime, LocalTime endTime)
+        throws BookingException, TimetableAccessError {
+        getDaySchedule(localDate).bookTimeRange(startTime, endTime);
+    }
+
+    /**
+     * Frees the slots containing the time range in the timetable at a specified location, with interval range
+     * startTime and endTime.
+     */
+
+    public void freeTimeRange(LocalDate localDate, LocalTime startTime, LocalTime endTime) throws TimetableAccessError {
+        getDaySchedule(localDate).freeTimeRange(startTime, endTime);
+    }
+
+    public static void main(String args[]) {
+        Timetable t = new Timetable(LocalDate.now());
+        try {
+            t.bookTimeRange(LocalDate.now().plusDays(5),
+                    LocalTime.of(16, 0), LocalTime.of(18, 30));
+        } catch(Exception e) {
+            System.out.println("NO");
+        }
+        System.out.println(t);
+    }
 
 }
 
--- src/main/java/seedu/address/model/scheduler/TimetableAccessError.java
+++ src/main/java/seedu/address/model/scheduler/TimetableAccessError.java
@@ -1,4 +1,8 @@
 package seedu.address.model.scheduler;
 
-public class TimetableAccessError {
+public class TimetableAccessError extends Exception {
+    public TimetableAccessError(String message) {
+        super(message);
+    }
 }
+
