32fb9dc17b2e9444819674a04cc93e60bd0b8687
Onn Wei Sin <onnwards@gmail.com>
2021-03-26 05:17:00 +0800

    fix checkstyle

--- src/test/java/seedu/address/testutil/TypicalAppObjects.java
+++ src/test/java/seedu/address/testutil/TypicalAppObjects.java
@@ -1,7 +1,5 @@
 package seedu.address.testutil;
 
-import static seedu.address.logic.commands.CommandTestUtil.VALID_UUID_AMY;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_UUID_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_ADDRESS_AMY;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_ADDRESS_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_EMAIL_AMY;
@@ -12,6 +10,8 @@ import static seedu.address.logic.commands.CommandTestUtil.VALID_PHONE_AMY;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_PHONE_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_TAG_FRIEND;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_TAG_HUSBAND;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_UUID_AMY;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_UUID_BOB;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
