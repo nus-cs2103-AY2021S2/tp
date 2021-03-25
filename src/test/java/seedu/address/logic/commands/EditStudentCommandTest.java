3de57c6723e0abc99923190675979bd835912a11
Samuel Lee <samuel.lwy@live.com.sg>
2021-03-25 14:12:42 +0800

    Remove unnecessary imports

--- src/test/java/seedu/address/logic/commands/EditStudentCommandTest.java
+++ src/test/java/seedu/address/logic/commands/EditStudentCommandTest.java
@@ -18,7 +18,6 @@ import org.junit.jupiter.api.Test;
 import seedu.address.commons.core.Messages;
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.EditStudentCommand.EditStudentDescriptor;
-import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
