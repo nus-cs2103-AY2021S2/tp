7eab884072e7a7dae9125acecf8371b6cd44d7d8
BigDoot <glendonchua@gmail.com>
2021-03-25 11:55:34 +0800

    Update documentation for ListAppointmentsCommand

--- src/main/java/seedu/address/logic/commands/medical/ListAppointmentsCommand.java
+++ src/main/java/seedu/address/logic/commands/medical/ListAppointmentsCommand.java
@@ -1,58 +1,41 @@
 package seedu.address.logic.commands.medical;
 
 import static java.util.Objects.requireNonNull;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_DATE;
-import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
-import static seedu.address.model.medical.DateFormat.DATE_FORMAT_DISPLAY;
-
-import java.time.LocalDateTime;
 import java.util.*;
-import java.util.logging.Logger;
-
-import seedu.address.commons.core.LogsCenter;
-import seedu.address.commons.core.Messages;
-import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.Command;
 import seedu.address.logic.commands.CommandResult;
-import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.medical.Appointment;
-import seedu.address.model.person.Address;
-import seedu.address.model.person.Email;
-import seedu.address.model.person.Name;
 import seedu.address.model.person.Person;
-import seedu.address.model.person.Phone;
-import seedu.address.model.tag.Tag;
-import seedu.address.ui.UiManager;
 
+/**
+ * Lists all upcoming appointments to the user.
+ */
 public class ListAppointmentsCommand extends Command {
 
     public static final String COMMAND_WORD = "listappt";
-    private static final Logger logger = LogsCenter.getLogger(ListAppointmentsCommand.class);
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Lists all upcoming appointments \n"
-            + "Parameters: INDEX (must be a positive integer) "
-            + "[" + PREFIX_DATE + "DATE] \n" + Appointment.MESSAGE_CONSTRAINTS_DATE_FORMAT
-            + "\n Example: " + COMMAND_WORD + " 1 " + PREFIX_DATE + "24051800";
+            + ": Lists out all your upcoming appointments.\n"
+            + "Parameters: None\n"
+            + "Example: " + COMMAND_WORD;
 
-    public static final String MESSAGE_SUCCESS = "Here are your upcoming appointments: \n%s";
+    public static final String MESSAGE_SUCCESS = "Hey Doc, here are your upcoming appointments! \n%s";
 
     @Override
-    public CommandResult execute(Model model) throws CommandException {
+    public CommandResult execute(Model model) {
         requireNonNull(model);
         List<Person> lastShownList = model.getFilteredPersonList();
-        String allAppointments = "";
-        List<Appointment> allappt = new ArrayList<>();
+        List<Appointment> Appointments = new ArrayList<>();
         for (Person p: lastShownList) {
-            List<Appointment> allappts = p.getAppointments();
-            for (Appointment appt : allappts) {
-                Appointment updatedAppt = appt.setPerson(p);
-                allappt.add(updatedAppt);
-                //allAppointments += appt.getDate().format(DATE_FORMAT_DISPLAY) + " " + p.getName() + "\n";;
+            List<Appointment> AppointmentList = p.getAppointments();
+            for (Appointment appt : AppointmentList) {
+                Appointments.add(appt.setPerson(p));
             }
         }
-        Collections.sort(allappt);
-        for (Appointment appt : allappt) {
+        Collections.sort(Appointments);
+        String allAppointments = "";
+        for (Appointment appt : Appointments) {
             allAppointments += appt + "\n";
         }
         return new CommandResult(String.format(MESSAGE_SUCCESS, allAppointments));
