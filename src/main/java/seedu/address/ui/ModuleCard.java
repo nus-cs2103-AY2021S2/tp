52a929f4b8365df8e6e6829c4af1b1f8034de6f1
Hzxin <E0424941@u.nus.edu>
2021-03-25 22:56:50 +0800

    Add SchedulePanelPLaceHolder in CalendarWindow

--- src/main/java/seedu/address/ui/ModuleCard.java
+++ src/main/java/seedu/address/ui/ModuleCard.java
@@ -1,8 +1,9 @@
 package seedu.address.ui;
 
+import java.util.HashMap;
+
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
-import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Region;
 import seedu.address.model.module.Module;
@@ -11,16 +12,13 @@ import seedu.address.model.module.Module;
  * An UI component that displays information of a {@code Person}.
  */
 public class ModuleCard extends UiPart<Region> {
-
     private static final String FXML = "ModuleListCard.fxml";
-
+    private final Module module;
     /**
      * Note: Certain keywords such as "location" and "resources" are reserved keywords in JavaFX.
      * As a consequence, UI elements' variable names cannot be set to such keywords
      * or an exception will be thrown by JavaFX during runtime.
      */
-
-    public final Module module;
 
     @FXML
     private HBox cardPane;
@@ -31,7 +29,7 @@ public class ModuleCard extends UiPart<Region> {
     @FXML
     private Label exams;
     @FXML
-    private FlowPane tags;
+    private Label tag;
 
     /**
      * Creates a {@code ModuleCard} with the given {@code Module} and index to display.
@@ -45,6 +43,7 @@ public class ModuleCard extends UiPart<Region> {
         exams.setWrapText(true);
         exams.setText(module.getExams().toString());
     }
+
 
     @Override
     public boolean equals(Object other) {
