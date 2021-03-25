34cd1755931a2e3645564de07a4d2e508e7fd7a7
yutingzou <zouyuting0801@gmail.com>
2021-03-26 00:53:17 +0800

    Add AddGradeCommandParser

---
+++ src/main/java/seedu/address/logic/parser/gradeparser/AddGradeCommandParser.java
@@ -0,0 +1,46 @@
+package seedu.address.logic.parser.gradeparser;
+
+import seedu.address.logic.commands.gradecommands.AddGradeCommand;
+import seedu.address.logic.parser.ArgumentMultimap;
+import seedu.address.logic.parser.ArgumentTokenizer;
+import seedu.address.logic.parser.ParserUtil;
+import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.grade.Grade;
+import seedu.address.model.subject.SubjectName;
+
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.*;
+
+/**
+ * Parses input arguments and creates a new AddGradeCommand object
+ */
+public class AddGradeCommandParser {
+
+    /**
+     * Parses the given {@code String} of arguments in the context of the AddGradeCommand
+     * and returns an AddGradeCommand object for execution.
+     * @throws ParseException if the user input does not conform the expected format
+     */
+    public AddGradeCommand parse(String args) throws ParseException {
+        ArgumentMultimap argMultimap =
+                ArgumentTokenizer.tokenize(args, PREFIX_SUBJECT_NAME, PREFIX_GRADED_ITEM,
+                        PREFIX_GRADE);
+
+        if (!ArgumentTokenizer.arePrefixesPresent(argMultimap, PREFIX_SUBJECT_NAME,
+                PREFIX_GRADED_ITEM,
+                PREFIX_GRADE)
+                || !argMultimap.getPreamble().isEmpty()) {
+            throw new ParseException(String.format(
+                    MESSAGE_INVALID_COMMAND_FORMAT, AddGradeCommand.MESSAGE_USAGE));
+        }
+
+        SubjectName subjectName =
+                ParserUtil.parseSubjectName(argMultimap.getValue(PREFIX_SUBJECT_NAME).get());
+        String gradedItem = "";
+        String grade = "";
+
+        Grade gradeToAdd = new Grade(subjectName, gradedItem, grade);
+
+        return new AddGradeCommand(gradeToAdd);
+    }
+}
