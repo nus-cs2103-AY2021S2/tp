0efe9a9dcc220301d8ccd124493020bf3e6e9f15
nowknowing <s.sunaga00@gmail.com>
2021-03-25 13:54:13 +0800

    fix import order

--- src/test/java/seedu/address/model/session/RecurringSessionTest.java
+++ src/test/java/seedu/address/model/session/RecurringSessionTest.java
@@ -1,8 +1,8 @@
 package seedu.address.model.session;
 
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
 
 class RecurringSessionTest {
 
