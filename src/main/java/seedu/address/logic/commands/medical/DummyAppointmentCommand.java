65d32237148ca5d2c7ec02d14b229f199f46dcb1
BigDoot <glendonchua@gmail.com>
2021-03-25 14:25:35 +0800

    Fix checkstyle issue

---
+++ src/main/java/seedu/address/logic/commands/medical/DummyAppointmentCommand.java
@@ -0,0 +1,7 @@
+package seedu.address.logic.commands.medical;
+
+import seedu.address.logic.commands.Command;
+
+public class DummyAppointmentCommand {
+    public static final String COMMAND_WORD = "list";
+}
