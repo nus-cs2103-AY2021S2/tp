833ee49ba4c1f525ca18238dc160b04f919e19ed
Jay Chen <jaychenyj@gmail.com>
2021-03-26 13:44:22 +0800

    Abstract inner if-else in timeConversion method

--- src/main/java/seedu/storemando/logic/parser/ReminderCommandParser.java
+++ src/main/java/seedu/storemando/logic/parser/ReminderCommandParser.java
@@ -17,35 +17,9 @@ public class ReminderCommandParser implements Parser<ReminderCommand> {
     public static final String WEEKS_KEYWORD = "weeks";
 
     /**
-     * Convert the given number and the time unit to the number of days.
-     * @param parsedNum The number that is use to covert to days
-     * @param timeUnit  The time unit in terms of days and weeks
-     * @return The number of days
-     * @throws ParseException if the user input does not conform the expected keyword
-     */
-    private long timeConversion(Long parsedNum, String timeUnit) throws ParseException {
-        if (parsedNum == 1 || parsedNum == -1 || parsedNum == 0) {
-            if (timeUnit.equalsIgnoreCase(DAY_KEYWORD)) {
-                return parsedNum;
-            } else if (timeUnit.equalsIgnoreCase(WEEK_KEYWORD)) {
-                return parsedNum * 7;
-            } else {
-                throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, ReminderCommand.MESSAGE_USAGE));
-            }
-        } else {
-            if (timeUnit.equalsIgnoreCase(DAYS_KEYWORD)) {
-                return parsedNum;
-            } else if (timeUnit.equalsIgnoreCase(WEEKS_KEYWORD)) {
-                return parsedNum * 7;
-            } else {
-                throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, ReminderCommand.MESSAGE_USAGE));
-            }
-        }
-    }
-
-    /**
      * Parses the given {@code String} of arguments in the context of the ReminderCommand
      * and returns a ReminderCommand object for execution.
+     *
      * @throws ParseException if the user input does not conform the expected format
      */
     @Override
@@ -62,6 +36,52 @@ public class ReminderCommandParser implements Parser<ReminderCommand> {
             long numOfDaysFromToday = timeConversion(parsedNum, timeUnit);
             return new ReminderCommand(new ItemExpiringPredicate(numOfDaysFromToday));
         } catch (ArrayIndexOutOfBoundsException | NumberFormatException ex) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, ReminderCommand.MESSAGE_USAGE));
+        }
+    }
+
+    /**
+     * Convert the given number and the time unit to the number of days.
+     *
+     * @param parsedNum The number that is use to covert to days
+     * @param timeUnit  The time unit in terms of days and weeks
+     * @return The number of days
+     * @throws ParseException if the user input does not conform the expected keyword
+     */
+    private long timeConversion(Long parsedNum, String timeUnit) throws ParseException {
+        if (isSingular(parsedNum)) {
+            return singularTimeUnitConversion(parsedNum, timeUnit);
+        } else {
+            return pluralTimeUnitConversion(parsedNum, timeUnit);
+        }
+    }
+
+    /**
+     * Check if the given number is singular.
+     *
+     * @param number The number that is given
+     * @return A boolean that says if the number is singular
+     */
+    private boolean isSingular(Long number) {
+        return number >= -1 && number <= 1;
+    }
+
+    private long singularTimeUnitConversion(Long parsedNum, String timeUnit) throws ParseException {
+        if (timeUnit.equalsIgnoreCase(DAY_KEYWORD)) {
+            return parsedNum;
+        } else if (timeUnit.equalsIgnoreCase(WEEK_KEYWORD)) {
+            return parsedNum * 7;
+        } else {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, ReminderCommand.MESSAGE_USAGE));
+        }
+    }
+
+    private long pluralTimeUnitConversion(Long parsedNum, String timeUnit) throws ParseException {
+        if (timeUnit.equalsIgnoreCase(DAYS_KEYWORD)) {
+            return parsedNum;
+        } else if (timeUnit.equalsIgnoreCase(WEEKS_KEYWORD)) {
+            return parsedNum * 7;
+        } else {
             throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, ReminderCommand.MESSAGE_USAGE));
         }
     }
