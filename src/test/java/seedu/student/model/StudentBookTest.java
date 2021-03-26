fecadf2a88f1e4185de32f450c094b7cf6882d72
Dan N. Tran <picasdan99@gmail.com>
2021-03-27 05:14:25 +0800

    Fix checkstyleTest

--- src/test/java/seedu/student/model/StudentBookTest.java
+++ src/test/java/seedu/student/model/StudentBookTest.java
@@ -13,7 +13,6 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import org.junit.jupiter.api.Test;
 
