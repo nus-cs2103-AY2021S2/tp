63457ccaab0e4c3df69d4cb2266df7c272baf282
hvrca <wang.haochen.c@gmail.com>
2021-03-26 20:24:34 +0800

    Update help command to reflect our user guide

---
+++ src/test/java/seedu/address/logic/commands/ValidateCommandTest.java
@@ -0,0 +1,21 @@
+package seedu.address.logic.commands;
+
+import static java.util.Objects.requireNonNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.testutil.Assert.assertThrows;
+
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.function.Predicate;
+
+import org.junit.jupiter.api.Test;
+
+public class ValidateCommandTest {
+    @Test
+    public void constructor_nullPlan_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> new ValidateCommand());
+    }
+}
