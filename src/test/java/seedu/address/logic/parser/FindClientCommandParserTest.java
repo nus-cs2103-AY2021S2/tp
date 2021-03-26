7bdac0a031ed9db17493c3a684b0f22a159b4f37
yuchen <yuchenwang97@gmail.com>
2021-03-26 20:40:19 +0800

    Add FindClientCommandParsere tests.

---
+++ src/test/java/seedu/address/logic/parser/FindClientCommandParserTest.java
@@ -0,0 +1,35 @@
+package seedu.address.logic.parser;
+
+import org.junit.jupiter.api.Test;
+import seedu.address.logic.commands.find.FindClientCommand;
+import seedu.address.model.appointment.AppointmentContainsKeywordsPredicate;
+import seedu.address.model.property.PropertyClientNamePredicate;
+
+import java.util.Collections;
+
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
+
+public class FindClientCommandParserTest {
+
+    private FindClientCommandParser parser = new FindClientCommandParser();
+
+    @Test
+    public void parseEmptyTest() {
+        assertParseFailure(parser, "  ",
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindClientCommand.MESSAGE_USAGE));
+    }
+
+    @Test
+    public void parseValidKeywordsTest() {
+        PropertyClientNamePredicate propertyClientNamePredicate =
+                new PropertyClientNamePredicate(Collections.singletonList("bob"));
+        AppointmentContainsKeywordsPredicate appointmentContainsKeywordsPredicate =
+                new AppointmentContainsKeywordsPredicate(Collections.singletonList("bob"));
+        FindClientCommand expectedFindCommand =
+                new FindClientCommand(propertyClientNamePredicate, appointmentContainsKeywordsPredicate);
+
+        assertParseSuccess(parser, "bob", expectedFindCommand);
+    }
+}
