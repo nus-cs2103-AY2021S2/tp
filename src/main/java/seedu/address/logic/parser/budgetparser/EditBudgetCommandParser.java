23f48c946acb84029fe1bd9288097435d1898708
Jared Lim <jaredlim1998@hotmail.com>
2021-03-26 11:44:10 +0800

    Fixed small incorrect error message in EditBudgetCommandParser

--- src/main/java/seedu/address/logic/parser/budgetparser/EditBudgetCommandParser.java
+++ src/main/java/seedu/address/logic/parser/budgetparser/EditBudgetCommandParser.java
@@ -3,7 +3,6 @@ package seedu.address.logic.parser.budgetparser;
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_BUDGET;
 
-import seedu.address.logic.commands.budgetcommands.AddBudgetCommand;
 import seedu.address.logic.commands.budgetcommands.EditBudgetCommand;
 import seedu.address.logic.parser.ArgumentMultimap;
 import seedu.address.logic.parser.ArgumentTokenizer;
@@ -25,7 +24,7 @@ public class EditBudgetCommandParser implements Parser<EditBudgetCommand> {
         if (!ArgumentTokenizer.arePrefixesPresent(argMultimap, PREFIX_BUDGET)
                 || !argMultimap.getPreamble().isEmpty()) {
             throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
-                    AddBudgetCommand.MESSAGE_USAGE));
+                    EditBudgetCommand.MESSAGE_USAGE));
         }
 
         Budget budget =
