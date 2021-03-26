47d1350585ed29798af146202c0c586add9df0f5
Mok Bing Wei Maurice <Maurice2n97@gmail.com>
2021-03-26 12:17:51 +0800

    Refactor: Rename TimeTableError to TimeTableException

--- src/main/java/seedu/address/model/scheduler/Scheduler.java
+++ src/main/java/seedu/address/model/scheduler/Scheduler.java
@@ -4,17 +4,19 @@ package seedu.address.model.scheduler;
  * This is a class that will manage the scheduling of a set of Schedulable objects.
  */
 
-public interface Scheduler{
+public interface Scheduler {
 
     /**
      * adds a schedulable object to the schedule.
      * @param schedulable
      */
-    public void addToSchedule(Schedulable schedulable);
+    public void addToSchedule(Schedulable schedulable) throws BookingException;
 
     /**
      * deletes a schedulable object from the schedule.
      * @param schedulable
+     * @throws
      */
-    public void freeSchedule(Schedulable schedulable);
+    public void freeSchedule(Schedulable schedulable) throws TimetableAccessException;
+
 }
