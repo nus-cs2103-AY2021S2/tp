76af37ef420ae3c1a01c58a07cb8e8e2962859f3
w-yuchen <yuchenwang1997@gmail.com>
2021-03-26 21:57:38 +0800

    Create ListCommandTest and getTypicalPropertiesWithClient method.

--- src/test/java/seedu/address/logic/commands/ListCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ListCommandTest.java
@@ -2,31 +2,32 @@ package seedu.address.logic.commands;
 
 import org.junit.jupiter.api.Test;
 import seedu.address.logic.commands.clear.ClearPropertyCommand;
+import seedu.address.logic.commands.list.ListAppointmentCommand;
+import seedu.address.logic.commands.list.ListPropertyCommand;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.PropertyBook;
 import seedu.address.model.UserPrefs;
+import seedu.address.testutil.TypicalModelManager;
 
+import static seedu.address.commons.core.Messages.MESSAGE_PROPERTIES_LISTED_OVERVIEW;
+import static seedu.address.commons.core.Messages.MESSAGE_PROPERTIES_LISTED_OVERVIEW_SINGULAR;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.testutil.TypicalProperties.getTypicalProperties;
 import static seedu.address.testutil.TypicalProperties.getTypicalPropertyBook;
+import static seedu.address.testutil.TypicalProperties.getTypicalPropertyBookWithClient;
 
 class ListCommandTest {
-    @Test
-    public void execute_emptyPropertyBook_success() {
-        Model model = new ModelManager();
-        Model expectedModel = new ModelManager();
-
-        assertCommandSuccess(new ClearPropertyCommand(), model, ClearPropertyCommand.MESSAGE_SUCCESS,
-                expectedModel);
-    }
+    private Model model = TypicalModelManager.getTypicalModelManager();
+    private Model expectedModel = TypicalModelManager.getTypicalModelManager();
 
     @Test
-    public void execute_nonEmptyPropertyBook_success() {
-        Model model = new ModelManager(getTypicalPropertyBook(), new UserPrefs());
-        Model expectedModel = new ModelManager(getTypicalPropertyBook(), new UserPrefs());
-        expectedModel.setPropertyBook(new PropertyBook());
+    public void listPropertyTest() {
+        String expectedMessage = String.format(MESSAGE_PROPERTIES_LISTED_OVERVIEW, getTypicalProperties().size() + 1);
+        //clear property list
+        expectedModel.updateFilteredPropertyList(a -> false);
 
-        assertCommandSuccess(new ClearPropertyCommand(), model, ClearPropertyCommand.MESSAGE_SUCCESS,
+        assertCommandSuccess(new ListPropertyCommand(), model, ClearPropertyCommand.MESSAGE_SUCCESS,
                 expectedModel);
     }
 }
