fe4bd5fdf58ae2c0dc710cf143706ffed7ce618e
chanell <ngchanell287@gmail.com>
2021-03-27 20:48:31 +0800

    Adding ViewSessionCommandTest

---
+++ src/test/java/seedu/address/logic/commands/ViewSessionCommandTest.java
@@ -0,0 +1,34 @@
+package seedu.address.logic.commands;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.logic.commands.CommandTestUtil.showPersonAtIndex;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
+import static seedu.address.testutil.
+
+/**
+ * Contains integration tests (interaction with the Model) and unit tests for ViewSessionCommand.
+ */
+public class ViewSessionCommandTest {
+
+    private Model model;
+    private Model expectedModel;
+
+    @BeforeEach
+    public void setUp() {
+        model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+        expectedModel = new ModelManager(model.getAddressBook(), new UserPrefs());
+    }
+
+    @Test
+    public void execute_listIsNotFiltered_showsSameList() {
+        assertCommandSuccess(new ListCommand("persons"), model, ListCommand.MESSAGE_SUCCESS_PERSONS, expectedModel);
+    }
+    
+}
+
