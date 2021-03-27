1214f3b7fd5d55e672cdc4e4f0f83429c787b971
nicoleang09 <nicoleang09@gmail.com>
2021-03-27 22:17:26 +0800

    Fix checkstyle in tests

--- src/test/java/seedu/address/storage/JsonAppointmentBookStorageTest.java
+++ src/test/java/seedu/address/storage/JsonAppointmentBookStorageTest.java
@@ -1,12 +1,7 @@
 package seedu.address.storage;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static seedu.address.testutil.Assert.assertThrows;
-import static seedu.address.testutil.TypicalPersons.ALICE;
-import static seedu.address.testutil.TypicalPersons.HOON;
-import static seedu.address.testutil.TypicalPersons.IDA;
-//import static seedu.address.testutil.TypicalPersons.getTypicalAppointmentBook;
 
 import java.io.IOException;
 import java.nio.file.Path;
@@ -15,7 +10,6 @@ import java.nio.file.Paths;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
 
-import seedu.address.commons.exceptions.DataConversionException;
 import seedu.address.model.AppointmentBook;
 import seedu.address.model.ReadOnlyAppointmentBook;
 
@@ -31,7 +25,8 @@ public class JsonAppointmentBookStorageTest {
     }
 
     private java.util.Optional<ReadOnlyAppointmentBook> readAppointmentBook(String filePath) throws Exception {
-        return new JsonAppointmentBookStorage(Paths.get(filePath)).readAppointmentBook(addToTestDataPathIfNotNull(filePath));
+        return new JsonAppointmentBookStorage(Paths.get(filePath)).readAppointmentBook(
+                addToTestDataPathIfNotNull(filePath));
     }
 
     private Path addToTestDataPathIfNotNull(String prefsFileInTestDataFolder) {
@@ -45,46 +40,49 @@ public class JsonAppointmentBookStorageTest {
         assertFalse(readAppointmentBook("NonExistentFile.json").isPresent());
     }
 
-//    @Test
-//    public void read_notJsonFormat_exceptionThrown() {
-//        assertThrows(DataConversionException.class, () -> readAppointmentBook("notJsonFormatAppointmentBook.json"));
-//    }
+    //    @Test
+    //    public void read_notJsonFormat_exceptionThrown() {
+    //        assertThrows(DataConversionException.class, () -> readAppointmentBook(
+    //        "notJsonFormatAppointmentBook.json"));
+    //    }
 
-//    @Test
-//    public void readAppointmentBook_invalidPersonAppointmentBook_throwDataConversionException() {
-//        assertThrows(DataConversionException.class, () -> readAppointmentBook("invalidPersonAppointmentBook.json"));
-//    }
+    //    @Test
+    //    public void readAppointmentBook_invalidPersonAppointmentBook_throwDataConversionException() {
+    //        assertThrows(DataConversionException.class, () -> readAppointmentBook(
+    //        "invalidPersonAppointmentBook.json"));
+    //    }
 
-//    @Test
-//    public void readAppointmentBook_invalidAndValidPersonAppointmentBook_throwDataConversionException() {
-//        assertThrows(DataConversionException.class, () -> readAppointmentBook("invalidAndValidPersonAppointmentBook.json"));
-//    }
+    //    @Test
+    //    public void readAppointmentBook_invalidAndValidPersonAppointmentBook_throwDataConversionException() {
+    //        assertThrows(DataConversionException.class, () -> readAppointmentBook(
+    //        "invalidAndValidPersonAppointmentBook.json"));
+    //    }
 
-//    @Test
-//    public void readAndSaveAppointmentBook_allInOrder_success() throws Exception {
-//        Path filePath = testFolder.resolve("TempAppointmentBook.json");
-//        AppointmentBook original = getTypicalAppointmentBook();
-//        JsonAppointmentBookStorage jsonAppointmentBookStorage = new JsonAppointmentBookStorage(filePath);
-//
-//        // Save in new file and read back
-//        jsonAppointmentBookStorage.saveAppointmentBook(original, filePath);
-//        ReadOnlyAppointmentBook readBack = jsonAppointmentBookStorage.readAppointmentBook(filePath).get();
-//        assertEquals(original, new AppointmentBook(readBack));
-//
-//        // Modify data, overwrite exiting file, and read back
-//        original.addPerson(HOON);
-//        original.removePerson(ALICE);
-//        jsonAppointmentBookStorage.saveAppointmentBook(original, filePath);
-//        readBack = jsonAppointmentBookStorage.readAppointmentBook(filePath).get();
-//        assertEquals(original, new AppointmentBook(readBack));
-//
-//        // Save and read without specifying file path
-//        original.addPerson(IDA);
-//        jsonAppointmentBookStorage.saveAppointmentBook(original); // file path not specified
-//        readBack = jsonAppointmentBookStorage.readAppointmentBook().get(); // file path not specified
-//        assertEquals(original, new AppointmentBook(readBack));
-//
-//    }
+    //    @Test
+    //    public void readAndSaveAppointmentBook_allInOrder_success() throws Exception {
+    //        Path filePath = testFolder.resolve("TempAppointmentBook.json");
+    //        AppointmentBook original = getTypicalAppointmentBook();
+    //        JsonAppointmentBookStorage jsonAppointmentBookStorage = new JsonAppointmentBookStorage(filePath);
+    //
+    //        // Save in new file and read back
+    //        jsonAppointmentBookStorage.saveAppointmentBook(original, filePath);
+    //        ReadOnlyAppointmentBook readBack = jsonAppointmentBookStorage.readAppointmentBook(filePath).get();
+    //        assertEquals(original, new AppointmentBook(readBack));
+    //
+    //        // Modify data, overwrite exiting file, and read back
+    //        original.addPerson(HOON);
+    //        original.removePerson(ALICE);
+    //        jsonAppointmentBookStorage.saveAppointmentBook(original, filePath);
+    //        readBack = jsonAppointmentBookStorage.readAppointmentBook(filePath).get();
+    //        assertEquals(original, new AppointmentBook(readBack));
+    //
+    //        // Save and read without specifying file path
+    //        original.addPerson(IDA);
+    //        jsonAppointmentBookStorage.saveAppointmentBook(original); // file path not specified
+    //        readBack = jsonAppointmentBookStorage.readAppointmentBook().get(); // file path not specified
+    //        assertEquals(original, new AppointmentBook(readBack));
+    //
+    //    }
 
     @Test
     public void saveAppointmentBook_nullAppointmentBook_throwsNullPointerException() {
