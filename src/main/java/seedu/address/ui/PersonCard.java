18e8337518eaec2d4be5ea583e5ec6a1532b3108
vivegank <77456578+vivegank@users.noreply.github.com>
2021-03-27 16:57:39 +0800

    Modify ui to show height

--- src/main/java/seedu/address/ui/PersonCard.java
+++ src/main/java/seedu/address/ui/PersonCard.java
@@ -40,6 +40,8 @@ public class PersonCard extends UiPart<Region> {
     @FXML
     private Label email;
     @FXML
+    private Label height;
+    @FXML
     private Label weight;
     @FXML
     private FlowPane tags;
@@ -57,6 +59,7 @@ public class PersonCard extends UiPart<Region> {
         phone.setText(person.getPhone().value);
         address.setText(person.getAddress().value);
         email.setText(person.getEmail().value);
+        height.setText(person.getHeight().value);
         weight.setText(person.getWeight().value);
         person.getTags().stream()
                 .sorted(Comparator.comparing(tag -> tag.tagName))
--- src/main/java/seedu/address/ui/ViewPatientBox.java
+++ src/main/java/seedu/address/ui/ViewPatientBox.java
@@ -36,6 +36,8 @@ public class ViewPatientBox extends UiPart<Region> {
     @FXML
     private Label email;
     @FXML
+    private Label height;
+    @FXML
     private Label weight;
     @FXML
     private Label tagInfo;
@@ -56,6 +58,7 @@ public class ViewPatientBox extends UiPart<Region> {
         phone.setText("Phone: " + person.getPhone().value);
         address.setText("Address: " + person.getAddress().value);
         email.setText("Email: " + person.getEmail().value);
+        height.setText("Height: " + person.getHeight().value);
         weight.setText("Weight: " + person.getWeight().value);
         tagInfo.setText("Tags:");
         person.getTags().stream()
