7bcb9247e7fb3f7722e119e8da17fa778b608871
geraldfan <geraldfan@gmail.com>
2021-03-27 14:48:28 +0800

    Implement VersionedTaskTrackerClass

---
+++ src/main/java/seedu/address/model/VersionedTaskTracker.java
@@ -0,0 +1,38 @@
+package seedu.address.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class VersionedTaskTracker extends TaskTracker {
+    private List<ReadOnlyTaskTracker> taskTrackerStateList = new ArrayList<ReadOnlyTaskTracker>();
+    private int currentStatePointer;
+
+    public VersionedTaskTracker() {
+        this.currentStatePointer = 0;
+    }
+
+    public void commit(ReadOnlyTaskTracker taskToBeCommitted) {
+        taskTrackerStateList.add(taskToBeCommitted);
+        currentStatePointer = taskTrackerStateList.size() - 1;
+    }
+
+    public ReadOnlyTaskTracker undo() {
+        currentStatePointer -= 1;
+        return taskTrackerStateList.get(currentStatePointer);
+    }
+
+    public ReadOnlyTaskTracker redo() {
+        if (canRedoTaskTracker()) {
+            currentStatePointer += 1;
+            return taskTrackerStateList.get(currentStatePointer);
+        }
+    }
+
+    private boolean canRedoTaskTracker() {
+        if (currentStatePointer + 1 <= taskTrackerStateList.size() - 1) {
+            return true;
+        }
+
+        return false;
+    }
+}
