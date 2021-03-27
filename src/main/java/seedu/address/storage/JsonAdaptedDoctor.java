6b1dfd91807c0fdff5865e85f40064ffdc43fb4c
Zhang Peng <perfecttzp@gmail.com>
2021-03-27 16:15:51 +0800

    JsonAdaptedDoctor class

---
+++ src/main/java/seedu/address/storage/JsonAdaptedDoctor.java
@@ -0,0 +1,58 @@
+package seedu.address.storage;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import seedu.address.commons.exceptions.IllegalValueException;
+import seedu.address.model.person.Name;
+import seedu.address.model.person.Doctor;
+import seedu.address.model.tag.Tag;
+
+public class JsonAdaptedDoctor extends JsonAdaptedPerson {
+    public static final String MISSING_FIELD_MESSAGE_FORMAT = "Doctor's %s field is missing!";
+
+    /**
+     * Constructs a {@code JsonAdaptedDoctor} with the given person details.
+     */
+    @JsonCreator
+    public JsonAdaptedDoctor(@JsonProperty("name") String name,
+                             @JsonProperty("tagged") List<JsonAdaptedTag> tagged) {
+        super(name, tagged);
+    }
+
+    /**
+     * Converts a given {@code Doctor} into this class for Jackson use.
+     */
+    public JsonAdaptedDoctor(Doctor source) {
+        super(source);
+    }
+
+    /**
+     * Converts this Jackson-friendly adapted doctor object into the model's {@code Doctor} object.
+     *
+     * @throws IllegalValueException if there were any data constraints violated in the adapted doctor.
+     */
+    @Override
+    public Doctor toModelType() throws IllegalValueException {
+        final List<Tag> personTags = new ArrayList<>();
+        for (JsonAdaptedTag tag : tagged) {
+            personTags.add(tag.toModelType());
+        }
+
+        if (name == null) {
+            throw new IllegalValueException(String.format(MISSING_FIELD_MESSAGE_FORMAT, Name.class.getSimpleName()));
+        }
+        if (!Name.isValidName(name)) {
+            throw new IllegalValueException(Name.MESSAGE_CONSTRAINTS);
+        }
+        final Name modelName = new Name(name);
+
+        final Set<Tag> modelTags = new HashSet<>(personTags);
+        return new Doctor(modelName, modelTags);
+    }
+}
