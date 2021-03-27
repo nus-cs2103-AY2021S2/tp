450642770ca1bc2a0d57504b0d71f7fc2fbf570a
clarlzx <clarlzx@gmail.com>
2021-03-27 20:24:08 +0800

    Edit refactored files

--- src/main/java/seedu/address/model/ReadOnlyAddressBook.java
+++ src/main/java/seedu/address/model/ReadOnlyAddressBook.java
@@ -2,18 +2,16 @@ package seedu.address.model;
 
 import javafx.collections.ObservableList;
 import seedu.address.model.person.Person;
-import seedu.address.model.appointment.Appointment;
 
 /**
  * Unmodifiable view of an address book
  */
-public interface ReadOnlyHeliBook {
+public interface ReadOnlyAddressBook {
 
     /**
      * Returns an unmodifiable view of the persons list.
      * This list will not contain any duplicate persons.
      */
     ObservableList<Person> getPersonList();
-    ObservableList<Appointment> getAppointmentList();
 
 }
