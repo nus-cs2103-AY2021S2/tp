b982f44704cd9d1438881539d75381a761bf8065
SiTingST <33442908+SiTingST@users.noreply.github.com>
2021-03-29 02:33:25 +0800

    Fix bugs

--- src/main/java/seedu/student/logic/commands/ListCommand.java
+++ src/main/java/seedu/student/logic/commands/ListCommand.java
@@ -1,6 +1,7 @@
 package seedu.student.logic.commands;
 
 import static java.util.Objects.requireNonNull;
+import static seedu.student.model.Model.PREDICATE_SHOW_ALL_APPOINTMENTS;
 import static seedu.student.model.Model.PREDICATE_SHOW_ALL_STUDENTS;
 
 import seedu.student.model.Model;
@@ -12,13 +13,14 @@ public class ListCommand extends Command {
 
     public static final String COMMAND_WORD = "list";
 
-    public static final String MESSAGE_SUCCESS = "Listed all persons";
+    public static final String MESSAGE_SUCCESS = "Listed all students and appointments";
 
 
     @Override
     public CommandResult execute(Model model) {
         requireNonNull(model);
         model.updateFilteredStudentList(PREDICATE_SHOW_ALL_STUDENTS);
+        model.updateFilteredAppointmentList(PREDICATE_SHOW_ALL_APPOINTMENTS);
         return new CommandResult(MESSAGE_SUCCESS);
     }
 }
