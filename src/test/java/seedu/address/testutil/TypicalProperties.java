8eb25a8d2d9d4029827e37291e14bec11ec2612c
w2vgd <junwei.green@gmail.com>
2021-03-28 15:50:57 +0800

    Update typical test data
    * Update TypicalAppointments
    * Update TypicalProperties
    * Add TypicalClients and ClientBuilder

--- src/test/java/seedu/address/testutil/TypicalProperties.java
+++ src/test/java/seedu/address/testutil/TypicalProperties.java
@@ -1,5 +1,9 @@
 package seedu.address.testutil;
 
+import static seedu.address.testutil.TypicalClients.BOB;
+import static seedu.address.testutil.TypicalClients.CALEB;
+import static seedu.address.testutil.TypicalClients.DAVE;
+
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -14,22 +18,25 @@ import seedu.address.model.property.Property;
 public class TypicalProperties {
 
     public static final Property WOODLANDS_CRESCENT = new PropertyBuilder().withName("Woodlands Crescent")
-            .withType("Hdb").withAddress("Woodlands Crescent, #01-01")
+            .withType("Hdb").withAddress("Blk 784 Woodlands Crescent #01-01")
             .withPostal("731784").withDeadline(LocalDate.parse("2021-08-31"))
-            .withTags("2 bedrooms", "65 square metres").build();
+            .withTags("2 bedrooms", "65 square metres").withClient(CALEB)
+            .build();
     public static final Property MAYFAIR = new PropertyBuilder().withName("Mayfair")
             .withType("Condo").withAddress("1 Jurong East Street 32, #08-111")
             .withPostal("609477").withDeadline(LocalDate.parse("2021-12-31"))
-            .withTags("4 bedrooms", "No need for renovation").build();
+            .withTags("4 bedrooms", "No need for renovation")
+            .build();
     public static final Property BURGHLEY_DRIVE = new PropertyBuilder().withName("Burghley Drive")
             .withType("Landed").withAddress("12 Burghley Drive")
             .withPostal("123456").withDeadline(LocalDate.parse("2021-07-31"))
-            .withTags("99 year leasehold").build();
+            .withTags("99 year leasehold").withClient(BOB)
+            .build();
     public static final Property JURONG = new PropertyBuilder().withName("Jurong")
             .withType("Hdb").withAddress("Jurong Ave 1, #01-01")
             .withPostal("640111").withDeadline(LocalDate.parse("2021-08-31"))
-            .withTags("3 bedrooms", "100 square metres")
-            .withClient(TypicalClients.CLIENT_BOB).build(); //asking price 2000000
+            .withTags("3 bedrooms", "100 square metres").withClient(DAVE)
+            .build();
 
     private TypicalProperties() {} // prevents instantiation
 
@@ -55,6 +62,7 @@ public class TypicalProperties {
         propertyBook.addProperty(JURONG);
         return propertyBook;
     }
+    
 
     public static List<Property> getTypicalProperties() {
         return new ArrayList<>(Arrays.asList(WOODLANDS_CRESCENT, MAYFAIR, BURGHLEY_DRIVE));
