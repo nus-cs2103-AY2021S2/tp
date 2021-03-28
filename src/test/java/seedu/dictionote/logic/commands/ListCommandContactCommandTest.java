a2ee6c1486660abd74e25634c0922e4c4584e48d
TeoHoeKeat <TeoHoeKeat@gmail.com>
2021-03-29 00:35:24 +0800

    Update ListCommandCommand details test

--- src/test/java/seedu/dictionote/logic/commands/ListCommandContactCommandTest.java
+++ src/test/java/seedu/dictionote/logic/commands/ListCommandContactCommandTest.java
@@ -1,20 +1,20 @@
 package seedu.dictionote.logic.commands;
 
 import static seedu.dictionote.logic.commands.CommandTestUtil.assertCommandSuccess;
-import static seedu.dictionote.logic.commands.ListCommandCommand.SHOWING_LIST_COMMAND_MESSAGE;
+import static seedu.dictionote.logic.commands.ListCommandContactCommand.SHOWING_LIST_COMMAND_MESSAGE;
 
 import org.junit.jupiter.api.Test;
 
 import seedu.dictionote.model.Model;
 import seedu.dictionote.model.ModelManager;
 
-public class ListCommandNoteCommandTest {
+public class ListCommandContactCommandTest {
     private Model model = new ModelManager();
     private Model expectedModel = new ModelManager();
 
     @Test
     public void execute_help_success() {
         CommandResult expectedCommandResult = new CommandResult(SHOWING_LIST_COMMAND_MESSAGE);
-        assertCommandSuccess(new ListCommandCommand(), model, expectedCommandResult, expectedModel);
+        assertCommandSuccess(new ListCommandContactCommand(), model, expectedCommandResult, expectedModel);
     }
 }
--- src/test/java/seedu/dictionote/logic/commands/ListCommandUiCommandTest.java
+++ src/test/java/seedu/dictionote/logic/commands/ListCommandUiCommandTest.java
@@ -1,20 +1,20 @@
 package seedu.dictionote.logic.commands;
 
 import static seedu.dictionote.logic.commands.CommandTestUtil.assertCommandSuccess;
-import static seedu.dictionote.logic.commands.ListCommandCommand.SHOWING_LIST_COMMAND_MESSAGE;
+import static seedu.dictionote.logic.commands.ListCommandUiCommand.SHOWING_LIST_COMMAND_MESSAGE;
 
 import org.junit.jupiter.api.Test;
 
 import seedu.dictionote.model.Model;
 import seedu.dictionote.model.ModelManager;
 
-public class ListCommandCommandTest {
+public class ListCommandUiCommandTest {
     private Model model = new ModelManager();
     private Model expectedModel = new ModelManager();
 
     @Test
     public void execute_help_success() {
         CommandResult expectedCommandResult = new CommandResult(SHOWING_LIST_COMMAND_MESSAGE);
-        assertCommandSuccess(new ListCommandCommand(), model, expectedCommandResult, expectedModel);
+        assertCommandSuccess(new ListCommandUiCommand(), model, expectedCommandResult, expectedModel);
     }
 }
