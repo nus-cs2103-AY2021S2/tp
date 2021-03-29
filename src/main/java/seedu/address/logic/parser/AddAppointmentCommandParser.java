b231d5e67924d310ab51dab62afbd7487f1fdbad
nicoleang09 <nicoleang09@gmail.com>
2021-03-29 18:24:29 +0800

    Clean up code edited when implementing appointment logic

--- src/main/java/seedu/address/logic/parser/AddAppointmentCommandParser.java
+++ src/main/java/seedu/address/logic/parser/AddAppointmentCommandParser.java
@@ -30,18 +30,18 @@ public class AddAppointmentCommandParser implements Parser<AddAppointmentCommand
                 ArgumentTokenizer.tokenize(args, PREFIX_NAME, PREFIX_ADDRESS, PREFIX_DATE,
                         PREFIX_CONTACT);
 
-        //        if (!arePrefixesPresent(argMultimap, PREFIX_NAME)
         if (!arePrefixesPresent(argMultimap, PREFIX_NAME, PREFIX_ADDRESS, PREFIX_DATE)
                 || !argMultimap.getPreamble().isEmpty()) {
             throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
                     AddAppointmentCommand.MESSAGE_USAGE));
         }
+
         Name name = ParserUtil.parseName(argMultimap.getValue(PREFIX_NAME).get());
         Address address = ParserUtil.parseAddress(argMultimap.getValue(PREFIX_ADDRESS).get());
         DateTime date = ParserUtil.parseDate(argMultimap.getValue(PREFIX_DATE).get());
-        // LocalDateTime date = LocalDateTime.now(); // to edit
 
         Set<Person> contactList = ParserUtil.parseContacts(argMultimap.getAllValues(PREFIX_CONTACT));
+
         contactList.addAll(contactList);
 
         Appointment appointment = new Appointment(name, address, date, contactList);
