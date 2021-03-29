ff485e0aeca0739b6f55b82b321bf3dfc8397885
ampan98 <e0407566@u.nus.edu>
2021-03-30 00:42:04 +0800

    Fix checkstyle issues

--- src/test/java/seedu/address/logic/commands/CollectCommandTest.java
+++ src/test/java/seedu/address/logic/commands/CollectCommandTest.java
@@ -2,17 +2,15 @@ package seedu.address.logic.commands;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_ADDRESS;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
 import static seedu.address.testutil.TypicalPersons.ALICE;
 import static seedu.address.testutil.TypicalPersons.BENSON;
 import static seedu.address.testutil.TypicalPersons.CARL;
 
 import org.junit.jupiter.api.Test;
-
-import static seedu.address.logic.parser.CliSyntax.PREFIX_ADDRESS;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
 
 import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
