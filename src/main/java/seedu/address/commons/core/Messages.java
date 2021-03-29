b4aac64232ac40a7750ad0cd38ab18d020f1f8d4
LI <gabriellee0903@gmail.com>
2021-03-29 16:44:56 +0800

    Rephrase error message

--- src/main/java/seedu/address/commons/core/Messages.java
+++ src/main/java/seedu/address/commons/core/Messages.java
@@ -11,7 +11,7 @@ public class Messages {
     public static final String MESSAGE_INVALID_TASK_DISPLAYED_INDEX = "The task index provided is invalid";
     public static final String MESSAGE_EVENTS_LISTED_OVERVIEW = "%1$d events listed!";
     public static final String MESSAGE_PAST_EVENT_END_DATE_TIME = "The event end date and end time provided are past";
-    public static final String MESSAGE_PAST_DEADLINE = "The task deadline provided is past";
+    public static final String MESSAGE_PAST_DEADLINE = "The deadline provided should not be earlier than today";
     public static final String MESSAGE_TASKS_LISTED_OVERVIEW = "%1$d tasks listed!";
     public static final String MESSAGE_END_DATETIME_BEFORE_START_DATETIME = "The event end date and time "
             + "is before start date and time!";
