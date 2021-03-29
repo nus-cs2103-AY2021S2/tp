bc35a395855f7a9aca7430a7fb5d0fc89accc1cf
Stratostorm <h_weicong@outlook.com>
2021-03-29 15:46:48 +0800

    Allow adding of child tags to appointments via addAppt

--- src/main/java/seedu/address/logic/commands/TagAppointmentCommand.java
+++
@@ -1,6 +0,0 @@
-package seedu.address.logic.commands;
-
-public class TagAppointmentCommand extends Command {
-    public static final String COMMAND_WORD = "tagAppt";
-    public static final String MESSAGE_USAGE = C
-}
