a4f8596898c74d80b62534fe95d92b1ebf6d8668
En Hao <larloller@gmail.com>
2021-03-29 21:27:55 +0800

    Trying to resolve bug

--- src/main/java/seedu/address/model/ModelManager.java
+++ src/main/java/seedu/address/model/ModelManager.java
@@ -13,7 +13,9 @@ import javafx.collections.transformation.FilteredList;
 import seedu.address.commons.core.GuiSettings;
 import seedu.address.commons.core.LogsCenter;
 import seedu.address.commons.core.index.Index;
+import seedu.address.model.session.RecurringSession;
 import seedu.address.model.session.Session;
+import seedu.address.model.session.SessionDate;
 import seedu.address.model.student.Name;
 import seedu.address.model.student.Student;
 
@@ -201,13 +203,35 @@ public class ModelManager implements Model {
     public double getFeePerStudent(Student student, LocalDateTime startPeriod, LocalDateTime endPeriod) {
         double fee = 0;
         for (Session session : student.getListOfSessions()) {
-            LocalDateTime dateTime = session.getSessionDate().getDateTime();
-            if (dateTime.compareTo(startPeriod) >= 0 && dateTime.compareTo(endPeriod) < 0) {
-                // This session date is within the period
-                fee += session.getFee().getFee();
+            if (session instanceof RecurringSession) {
+                fee += getRecurringSessionFee((RecurringSession) session, startPeriod, endPeriod);
+            } else {
+                fee += getSingleSessionFee(session, startPeriod, endPeriod);
             }
         }
         return fee;
+    }
+
+    private double getRecurringSessionFee(RecurringSession recurringSession, LocalDateTime startPeriod, LocalDateTime endPeriod) {
+        SessionDate startDate = new SessionDate(startPeriod);
+        SessionDate endDate = new SessionDate(endPeriod);
+        int numOfSession = recurringSession.numOfSessionBetween(startDate, endDate);
+
+        // Ensures that there is more than 0 session, so we can get the fees accordingly
+        if (numOfSession > 0) {
+            return recurringSession.getFee().getFee() * numOfSession;
+        }
+        return 0;
+
+    }
+
+    private double getSingleSessionFee(Session session, LocalDateTime startPeriod, LocalDateTime endPeriod) {
+        LocalDateTime dateTime = session.getSessionDate().getDateTime();
+        if (dateTime.compareTo(startPeriod) >= 0 && dateTime.compareTo(endPeriod) < 0) {
+            // This session date is within the period
+            return session.getFee().getFee();
+        }
+        return 0;
     }
 
 }
