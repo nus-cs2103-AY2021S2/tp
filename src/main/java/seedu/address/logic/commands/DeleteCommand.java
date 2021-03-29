dbe58c404db7d70422bbe48d8c52949aa1035e71
mechastriker3 <33394879+mechastriker3@users.noreply.github.com>
2021-03-29 10:17:33 +0800

    fix index shifting issue with multiple delete

--- src/main/java/seedu/address/logic/commands/DeleteCommand.java
+++ src/main/java/seedu/address/logic/commands/DeleteCommand.java
@@ -2,6 +2,7 @@ package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import seedu.address.commons.core.Messages;
@@ -22,7 +23,8 @@ public class DeleteCommand extends Command {
             + "Parameters: INDEX (must be a positive integer)\n"
             + "Example: " + COMMAND_WORD + " 1";
 
-    public static final String MESSAGE_DELETE_PERSON_SUCCESS = "Deleted Person(s): %1$s";
+    public static final String MESSAGE_DELETE_PERSON_SUCCESS = "Deleted Person: %1$s";
+    public static final String MESSAGE_DELETE_PERSONS_SUCCESS = "Deleted People:\n - %1$s";
 
     private final List<Index> targetIndexList;
 
@@ -39,18 +41,27 @@ public class DeleteCommand extends Command {
     public CommandResult execute(Model model) throws CommandException {
         requireNonNull(model);
         List<Person> lastShownList = model.getFilteredPersonList();
-
         final StringBuilder outputString = new StringBuilder();
+        List<Person> personToDeleteList = new ArrayList<>();
+
         for (Index targetIndex : targetIndexList) {
             if (targetIndex.getZeroBased() >= lastShownList.size()) {
                 throw new CommandException(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
             }
-
             Person personToDelete = lastShownList.get(targetIndex.getZeroBased());
-            model.deletePerson(personToDelete);
-            outputString.append(personToDelete + "\n");
+            personToDeleteList.add(personToDelete);
         }
-        return new CommandResult(String.format(MESSAGE_DELETE_PERSON_SUCCESS, outputString));
+
+        for (Person personToDelete : personToDeleteList) {
+            model.deletePerson(personToDelete);
+            outputString.append(personToDelete);
+            outputString.append("\n");
+        }
+
+        return new CommandResult(String.format(
+                targetIndexList.size() == 1
+                ? MESSAGE_DELETE_PERSON_SUCCESS
+                : MESSAGE_DELETE_PERSONS_SUCCESS, outputString));
     }
 
     @Override
