ab1e17dac9f25e79ceb900615ccc53f95a8dac5d
hengyongming <e0425848@u.nus.edu>
2021-03-30 01:13:52 +0800

    Implement Test for HomeCommand

--- src/main/java/seedu/taskify/logic/commands/HomeCommand.java
+++ src/main/java/seedu/taskify/logic/commands/HomeCommand.java
@@ -3,12 +3,15 @@ package seedu.taskify.logic.commands;
 import seedu.taskify.logic.commands.exceptions.CommandException;
 import seedu.taskify.model.Model;
 
+import static seedu.taskify.model.Model.PREDICATE_SHOW_ALL_TASKS;
+
 public class HomeCommand extends Command {
     public static final String COMMAND_WORD = "home";
     public static final String MESSAGE_USAGE = "Switched to home tab";
 
     @Override
     public CommandResult execute(Model model) throws CommandException {
+        model.updateFilteredTaskList(PREDICATE_SHOW_ALL_TASKS);
         return CommandResult.switchToHome(MESSAGE_USAGE);
     }
 
