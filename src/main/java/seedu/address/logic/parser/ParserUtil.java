fe3bc167e31f3a0408679b83af85d22d1d0d05e2
lirc572 <admin@lirc572.com>
2021-03-30 03:46:20 +0800

    Update Role to support hyphens and underscores

--- src/main/java/seedu/address/logic/parser/ParserUtil.java
+++ src/main/java/seedu/address/logic/parser/ParserUtil.java
@@ -154,7 +154,7 @@ public class ParserUtil {
     public static Role parseRole(String role) throws ParseException {
         requireNonNull(role);
         String trimmedRole = role.trim();
-        if (!Tag.isValidTagName(trimmedRole)) {
+        if (!Role.isValidRoleName(trimmedRole)) {
             throw new ParseException(Role.MESSAGE_CONSTRAINTS);
         }
         return new Role(trimmedRole);
