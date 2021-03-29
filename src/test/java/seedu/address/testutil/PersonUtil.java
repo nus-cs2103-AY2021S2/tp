c8ad4b887caf5213a74876ae9aaa96ea3caa82b7
Winniehyx <e0425411@u.nus.edu>
2021-03-29 22:17:30 +0800

    Fixed test case error

--- src/test/java/seedu/address/testutil/PersonUtil.java
+++ src/test/java/seedu/address/testutil/PersonUtil.java
@@ -5,6 +5,7 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_EDUCATION_LEVEL;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_GENDER;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_NOTES;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_QUALIFICATION;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_RATE;
@@ -42,17 +43,18 @@ public class PersonUtil {
         sb.append(PREFIX_PHONE + person.getPhone().value + " ");
         sb.append(PREFIX_EMAIL + person.getEmail().value + " ");
         sb.append(PREFIX_ADDRESS + person.getAddress().value + " ");
+        sb.append(PREFIX_NOTES + person.getNotes().value + " ");
         person.getTags().stream().forEach(
-            s -> sb.append(PREFIX_TAG + s.tagName + " ")
+                s -> sb.append(PREFIX_TAG + s.tagName + " ")
         );
         person.getSubjectList().asUnmodifiableObservableList().stream().forEach(
-            s -> {
-                sb.append(PREFIX_SUBJECT_NAME + s.getName().name + " ");
-                sb.append(PREFIX_EDUCATION_LEVEL + s.getLevel().level + " ");
-                sb.append(PREFIX_RATE + s.getRate().rate.toString() + " ");
-                sb.append(PREFIX_YEAR + s.getExperience().experience.toString() + " ");
-                sb.append(PREFIX_QUALIFICATION + s.getQualification().qualification + " ");
-            });
+                s -> {
+                    sb.append(PREFIX_SUBJECT_NAME + s.getName().name + " ");
+                    sb.append(PREFIX_EDUCATION_LEVEL + s.getLevel().level + " ");
+                    sb.append(PREFIX_RATE + s.getRate().rate.toString() + " ");
+                    sb.append(PREFIX_YEAR + s.getExperience().experience.toString() + " ");
+                    sb.append(PREFIX_QUALIFICATION + s.getQualification().qualification + " ");
+                });
         return sb.toString();
     }
 
@@ -66,6 +68,7 @@ public class PersonUtil {
         descriptor.getPhone().ifPresent(phone -> sb.append(PREFIX_PHONE).append(phone.value).append(" "));
         descriptor.getEmail().ifPresent(email -> sb.append(PREFIX_EMAIL).append(email.value).append(" "));
         descriptor.getAddress().ifPresent(address -> sb.append(PREFIX_ADDRESS).append(address.value).append(" "));
+        descriptor.getNotes().ifPresent(notes -> sb.append(PREFIX_NOTES).append(notes.value).append(" "));
         if (descriptor.getTags().isPresent()) {
             Set<Tag> tags = descriptor.getTags().get();
             if (tags.isEmpty()) {
@@ -77,13 +80,13 @@ public class PersonUtil {
         if (descriptor.getSubjectList().isPresent()) {
             SubjectList subjects = descriptor.getSubjectList().get();
             subjects.asUnmodifiableObservableList().stream().forEach(
-                s -> {
-                    sb.append(PREFIX_SUBJECT_NAME + s.getName().name + " ");
-                    sb.append(PREFIX_EDUCATION_LEVEL + s.getLevel().level + " ");
-                    sb.append(PREFIX_RATE + s.getRate().rate.toString() + " ");
-                    sb.append(PREFIX_YEAR + s.getExperience().experience.toString() + " ");
-                    sb.append(PREFIX_QUALIFICATION + s.getQualification().qualification + " ");
-                });
+                    s -> {
+                        sb.append(PREFIX_SUBJECT_NAME + s.getName().name + " ");
+                        sb.append(PREFIX_EDUCATION_LEVEL + s.getLevel().level + " ");
+                        sb.append(PREFIX_RATE + s.getRate().rate.toString() + " ");
+                        sb.append(PREFIX_YEAR + s.getExperience().experience.toString() + " ");
+                        sb.append(PREFIX_QUALIFICATION + s.getQualification().qualification + " ");
+                    });
         }
 
         return sb.toString();
