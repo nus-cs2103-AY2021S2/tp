55e3bf0697df3351f97662212a376a8bc59db902
Vevek Selvam <vevekselvam@gmail.com>
2021-03-30 01:03:18 +0800

    Update JsonAdaptedEventTest

--- src/test/java/seedu/address/storage/JsonAdaptedEventTest.java
+++ src/test/java/seedu/address/storage/JsonAdaptedEventTest.java
@@ -4,27 +4,30 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static seedu.address.testutil.Assert.assertThrows;
 
 import java.time.LocalDate;
+import java.time.LocalTime;
 
 import org.junit.jupiter.api.Test;
 
 import seedu.address.commons.exceptions.IllegalValueException;
-import seedu.address.model.task.Interval;
 import seedu.address.model.task.repeatable.Event;
 import seedu.address.testutil.EventBuilder;
 
 public class JsonAdaptedEventTest {
     private static final String INVALID_DESCRIPTION = " ";
     private static final String INVALID_DATE_STRING = "2020-02-02";
+    private static final String INVALID_TIME_STRING = "17-30";
+    private static final Boolean INVALID_IS_WEEKLY = null;
 
     private static final String VALID_DESCRIPTION = EventBuilder.DEFAULT_DESCRIPTION;
-    private static final Interval VALID_INTERVAL = EventBuilder.DEFAULT_INTERVAL;
     private static final LocalDate VALID_DATE = EventBuilder.DEFAULT_DATE;
+    private static final LocalTime VALID_TIME = EventBuilder.DEFAULT_TIME;
+    private static final Boolean VALID_IS_WEEKLY = EventBuilder.DEFAULT_IS_WEEKLY;
     private static final String VALID_DATE_STRING = "02-02-2020";
-    private static final Boolean VALID_IS_DONE = EventBuilder.DEFAULT_IS_DONE;
+    private static final String VALID_TIME_STRING = "17:30";
 
     private static final Event TUTORIAL = new EventBuilder()
-            .withDescription(VALID_DESCRIPTION).withInterval(VALID_INTERVAL)
-            .withAtDate(VALID_DATE).withIsDone(VALID_IS_DONE).build();
+            .withDescription(VALID_DESCRIPTION).withDate(VALID_DATE).withTime(VALID_TIME).withIsWeekly(VALID_IS_WEEKLY)
+            .build();
 
     @Test
     public void toModelType_validEventDetails_returnsEvent() throws Exception {
@@ -35,7 +38,7 @@ public class JsonAdaptedEventTest {
     @Test
     public void toModelType_invalidDescription_throwsIllegalArgumentException() {
         JsonAdaptedEvent event =
-                new JsonAdaptedEvent(INVALID_DESCRIPTION, VALID_INTERVAL.toString(), VALID_DATE_STRING, VALID_IS_DONE);
+                new JsonAdaptedEvent(INVALID_DESCRIPTION, VALID_DATE_STRING, VALID_TIME_STRING, VALID_IS_WEEKLY);
         String expectedMessage = Event.MESSAGE_CONSTRAINTS_DESCRIPTION;
         assertThrows(IllegalArgumentException.class, expectedMessage, event::toModelType);
     }
@@ -43,14 +46,28 @@ public class JsonAdaptedEventTest {
     @Test
     public void toModelType_invalidDate_throwsIllegalValueException() {
         JsonAdaptedEvent event =
-                new JsonAdaptedEvent(VALID_DESCRIPTION, VALID_INTERVAL.toString(), INVALID_DATE_STRING, VALID_IS_DONE);
+                new JsonAdaptedEvent(VALID_DESCRIPTION, INVALID_DATE_STRING, VALID_TIME_STRING, VALID_IS_WEEKLY);
+        assertThrows(IllegalValueException.class, event::toModelType);
+    }
+
+    @Test
+    public void toModelType_invalidTime_throwsIllegalValueException() {
+        JsonAdaptedEvent event =
+                new JsonAdaptedEvent(VALID_DESCRIPTION, VALID_DATE_STRING, INVALID_TIME_STRING, VALID_IS_WEEKLY);
+        assertThrows(IllegalValueException.class, event::toModelType);
+    }
+
+    @Test
+    public void toModelType_invalidIsWeekly_throwsIllegalValueException() {
+        JsonAdaptedEvent event =
+                new JsonAdaptedEvent(VALID_DESCRIPTION, VALID_DATE_STRING, VALID_TIME_STRING, INVALID_IS_WEEKLY);
         assertThrows(IllegalValueException.class, event::toModelType);
     }
 
     @Test
     public void toModelType_nullDescription_throwsNullPointerException() {
-        JsonAdaptedEvent event = new JsonAdaptedEvent(null, VALID_INTERVAL.toString(),
-                VALID_DATE_STRING, VALID_IS_DONE);
+        JsonAdaptedEvent event = new JsonAdaptedEvent(null, VALID_DATE_STRING, VALID_TIME_STRING, VALID_IS_WEEKLY);
         assertThrows(NullPointerException.class, event::toModelType);
     }
+
 }
