853ee35ab9ebd498ea5fe29d5045002bb4aa69b3
jessen11 <51905834+jessen11@users.noreply.github.com>
2021-03-30 01:54:55 +0800

    Update FindNoteCommand.java

--- src/main/java/seedu/dictionote/logic/commands/FindNoteCommand.java
+++ src/main/java/seedu/dictionote/logic/commands/FindNoteCommand.java
@@ -36,13 +36,8 @@ public class FindNoteCommand extends Command {
     /**
      * Creates a new {@code FindContactCommand} with two predicates (i.e., conditions):
      * one applies to the contacts' names, and the other applies to their tags.
-<<<<<<< HEAD
      * @param notePredicate The predicate to be evaluated against the note's content.
      * @param tagsPredicate The predicate to be evaluated against the note's tags.
-=======
-     * @param notePredicate The predicate to be evaluated against the notes' names.
-     * @param tagsPredicate The predicate to be evaluated against the notes' tags.
->>>>>>> 9706ccb739fd7e5bcc25e1b090780fd374f34edf
      */
     public FindNoteCommand(NoteContainsKeywordsPredicate notePredicate,
                               TagNoteContainKeywordsPredicate tagsPredicate) {
