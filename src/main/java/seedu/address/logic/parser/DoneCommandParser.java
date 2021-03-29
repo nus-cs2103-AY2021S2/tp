08fe1844ae376d2c59952822b493d5272ff52f77
jrvslam <jarveis.lamzx@gmail.com>
2021-03-29 12:31:57 +0800

    Add Done Command that takes an Event and changes its EventStatus to Done

---
+++ src/main/java/seedu/address/logic/parser/DoneCommandParser.java
@@ -0,0 +1,23 @@
+package seedu.address.logic.parser;
+
+import seedu.address.commons.core.identifier.Identifier;
+import seedu.address.logic.commands.DoneCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+
+public class DoneCommandParser implements Parser<DoneCommand> {
+    @Override
+    public DoneCommand parse(String userInput) throws ParseException {
+        requireNonNull(userInput);
+
+        try {
+            Identifier identifier = ParserUtil.parseIdentifier(userInput);
+            return new DoneCommand(identifier);
+        } catch (ParseException pe) {
+            throw new ParseException(
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, DoneCommand.MESSAGE_USAGE), pe);
+        }
+    }
+}
