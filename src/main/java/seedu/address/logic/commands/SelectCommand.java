d5c41ae184ef986a87221af142dc9b76d915c70c
arihantjain97 <suhailarihant@gmail.com>
2021-03-29 19:02:42 +0800

    no message

--- src/main/java/seedu/address/logic/commands/SelectCommand.java
+++ src/main/java/seedu/address/logic/commands/SelectCommand.java
@@ -51,5 +51,11 @@ public class SelectCommand extends Command {
         return new CommandResult(String.format(MESSAGE_SELECT_GARMENT_SUCCESS, garmentChanged));
     }
 
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof SelectCommand // instanceof handles nulls
+                && targetIndex.equals(((SelectCommand) other).targetIndex)); // state check
+    }
 
 }
