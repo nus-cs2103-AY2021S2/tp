497a0984cbd1c86391ffc98ad6d86532b8800f44
JerardSoh <jerard.soh@gmail.com>
2021-03-30 15:17:50 +0800

    Remove AddressBook Storages

--- src/test/java/seedu/address/logic/LogicManagerTest.java
+++ src/test/java/seedu/address/logic/LogicManagerTest.java
@@ -27,7 +27,6 @@ import seedu.address.model.ReadOnlyAddressBook;
 import seedu.address.model.ReadOnlyEventBook;
 import seedu.address.model.UserPrefs;
 import seedu.address.model.person.Person;
-import seedu.address.storage.JsonAddressBookStorage;
 import seedu.address.storage.JsonEventBookStorage;
 import seedu.address.storage.JsonUserPrefsStorage;
 import seedu.address.storage.StorageManager;
