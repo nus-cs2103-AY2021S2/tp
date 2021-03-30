2fd20f7b4a2fa6e81262d53379aac8650776b181
Vinleon <kohvinleon@gmail.com>
2021-03-30 11:57:42 +0800

    Add Add, Delete, Edit and List Reminder Command

---
+++ src/main/java/seedu/address/logic/commands/remindercommands/ListReminderCommand.java
@@ -0,0 +1,26 @@
+package seedu.address.logic.commands.remindercommands;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.model.Model.PREDICATE_SHOW_ALL_REMINDER;
+
+import seedu.address.logic.commands.Command;
+import seedu.address.logic.commands.CommandResult;
+import seedu.address.model.Model;
+
+/**
+ * Lists all reminders in the reminder list to the user.
+ */
+public class ListReminderCommand extends Command {
+
+    public static final String COMMAND_WORD = "list_reminders";
+
+    public static final String MESSAGE_SUCCESS = "Listed all reminders";
+
+
+    @Override
+    public CommandResult execute(Model model) {
+        requireNonNull(model);
+        model.updateFilteredReminderList(PREDICATE_SHOW_ALL_REMINDER);
+        return new CommandResult(MESSAGE_SUCCESS);
+    }
+}
