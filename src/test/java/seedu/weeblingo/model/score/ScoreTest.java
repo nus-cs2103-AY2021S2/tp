f37b0d09d58cffb16b5c3dbef0d6a2c0405918c3
Cheng20010201 <E0493630@u.nus.edu>
2021-03-30 23:24:48 +0800

    Check style issues

--- src/test/java/seedu/weeblingo/model/score/ScoreTest.java
+++ src/test/java/seedu/weeblingo/model/score/ScoreTest.java
@@ -26,11 +26,16 @@ public class ScoreTest {
 
     @Test
     public void of_invalidValue_throwsRuntimeException() {
-        assertThrows(RuntimeException.class, () -> Score.of(0, 1, "0:03:04"));
-        assertThrows(RuntimeException.class, () -> Score.of(-1, 1, "0:03:04"));
-        assertThrows(RuntimeException.class, () -> Score.of(-100000, 1, "0:03:04"));
-        assertThrows(RuntimeException.class, () -> Score.of(1, -1, "0:03:04"));
-        assertThrows(RuntimeException.class, () -> Score.of(1, -10000, "0:03:04"));
+        assertThrows(RuntimeException.class, () -> Score
+                .of(0, 1, "0:03:04"));
+        assertThrows(RuntimeException.class, () -> Score
+                .of(-1, 1, "0:03:04"));
+        assertThrows(RuntimeException.class, () -> Score
+                .of(-100000, 1, "0:03:04"));
+        assertThrows(RuntimeException.class, () -> Score
+                .of(1, -1, "0:03:04"));
+        assertThrows(RuntimeException.class, () -> Score
+                .of(1, -10000, "0:03:04"));
     }
 
     @Test
@@ -59,7 +64,8 @@ public class ScoreTest {
 
     @Test
     public void compareTo_null_throwsNullPointerException() {
-        assertThrows(NullPointerException.class, () -> Score.of(1, 1, "0:03:04").compareTo(null));
+        assertThrows(NullPointerException.class, () -> Score.of(1, 1,
+                "0:03:04").compareTo(null));
     }
 
     @Test
