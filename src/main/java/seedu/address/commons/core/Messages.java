a20b27c721d055f9e41ffbf24c11d7a1172a77fb
goatygoatygoat <52666150+goatygoatygoat@users.noreply.github.com>
2021-03-30 23:10:50 +0800

    Fix messages

--- src/main/java/seedu/address/commons/core/Messages.java
+++ src/main/java/seedu/address/commons/core/Messages.java
@@ -13,7 +13,6 @@ public class Messages {
     public static final String MESSAGE_INVALID_DATE_RANGE = "The provided start date is after the provided end date!";
     public static final String MESSAGE_ENTRIES_LISTED_OVERVIEW = "%1$d entries listed";
     public static final String MESSAGE_PERSONS_LISTED_OVERVIEW = "%1$d persons listed!";
-    public static final String MESSAGE_ENTRIES_LISTED_OVERVIEW = "%1$d entries listed!";
     public static final String MESSAGE_SCHEDULES_LISTED_OVERVIEW = "%1$d schedules listed!";
     public static final String MESSAGE_TASKS_LISTED_OVERVIEW = "%1$d tasks listed!";
     public static final String MESSAGE_TASK_DOES_NOT_EXIST = "The task provided does not exist";
