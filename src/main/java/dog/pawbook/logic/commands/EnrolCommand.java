3717a3abe62b5133513946233d31dd2af5c31a31
Wei Yutong <weiyutongggg@gmail.com>
2021-03-30 14:56:24 +0800

    Improve enrol and drop command with more OOP

--- src/main/java/dog/pawbook/logic/commands/EnrolCommand.java
+++ src/main/java/dog/pawbook/logic/commands/EnrolCommand.java
@@ -1,25 +1,9 @@
 package dog.pawbook.logic.commands;
 
-import static dog.pawbook.commons.core.Messages.MESSAGE_NOT_DOG;
-import static dog.pawbook.commons.core.Messages.MESSAGE_NOT_DOG_AND_PROGRAM;
-import static dog.pawbook.commons.core.Messages.MESSAGE_NOT_PROGRAM;
-import static dog.pawbook.commons.core.Messages.MESSAGE_NO_SUCH_DOG_AND_PROGRAM_ID;
-import static dog.pawbook.commons.core.Messages.MESSAGE_NO_SUCH_DOG_ID;
-import static dog.pawbook.commons.core.Messages.MESSAGE_NO_SUCH_PROGRAM_ID;
 import static dog.pawbook.logic.parser.CliSyntax.PREFIX_DOGID;
 import static dog.pawbook.logic.parser.CliSyntax.PREFIX_PROGRAMID;
-import static java.util.Objects.requireNonNull;
 
-import java.util.HashSet;
-import java.util.Set;
-
-import dog.pawbook.logic.commands.exceptions.CommandException;
-import dog.pawbook.model.Model;
-import dog.pawbook.model.managedentity.dog.Dog;
-import dog.pawbook.model.managedentity.program.Program;
-
-public class EnrolCommand extends Command {
-
+public class EnrolCommand extends ProgramCommand {
     public static final String COMMAND_WORD = "enrol";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Enrols a dog into a program."
@@ -29,6 +13,8 @@ public class EnrolCommand extends Command {
             + "Example: " + COMMAND_WORD + " d/2 p/3";
 
     public static final String MESSAGE_SUCCESS_FORMAT = "Dog %s enrolled in program %s!";
+
+    public static final String MESSAGE_ALREADY_ENROLLED_FORMAT = "Dog %s has already been enrolled in program %s!";
 
     private final int dogId;
 
@@ -44,42 +30,26 @@ public class EnrolCommand extends Command {
         this.programId = programId;
     }
 
+    protected int getDogId() {
+        return this.dogId;
+    }
+
+    protected int getProgramId() {
+        return this.programId;
+    }
+
     @Override
-    public CommandResult execute(Model model) throws CommandException {
-        requireNonNull(model);
+    protected String getProgramCommand() {
+        return COMMAND_WORD;
+    }
 
-        boolean isValidDogId = model.hasEntity(dogId);
-        boolean isValidProgramId = model.hasEntity(programId);
+    @Override
+    protected String getSuccessMessage() {
+        return String.format(MESSAGE_SUCCESS_FORMAT, dogId, programId);
+    }
 
-        if (!isValidDogId && !isValidProgramId) {
-            throw new CommandException(MESSAGE_NO_SUCH_DOG_AND_PROGRAM_ID);
-        } else if (!isValidDogId && isValidProgramId) {
-            throw new CommandException(MESSAGE_NO_SUCH_DOG_ID);
-        } else if (isValidDogId && !isValidProgramId) {
-            throw new CommandException(MESSAGE_NO_SUCH_PROGRAM_ID);
-        }
-
-        boolean isDog = model.getEntity(dogId) instanceof Dog;
-        boolean isProgram = model.getEntity(programId) instanceof Program;
-
-        if (isDog && !isProgram) {
-            throw new CommandException(MESSAGE_NOT_PROGRAM);
-        } else if (!isDog && isProgram) {
-            throw new CommandException(MESSAGE_NOT_DOG);
-        } else if (!isDog && !isProgram) {
-            throw new CommandException(MESSAGE_NOT_DOG_AND_PROGRAM);
-        }
-
-        Program targetProgram = (Program) model.getEntity(programId);
-
-        Set<Integer> editedDogIdSet = new HashSet<>(targetProgram.getDogIdSet());
-        editedDogIdSet.add(dogId);
-
-        Program editedProgram = new Program(targetProgram.getName(), targetProgram.getSessions(),
-                targetProgram.getTags(), editedDogIdSet);
-
-        model.setEntity(programId, editedProgram);
-
-        return new CommandResult(String.format(MESSAGE_SUCCESS_FORMAT, dogId, programId));
+    @Override
+    protected String getDuplicateMessage() {
+        return String.format(MESSAGE_ALREADY_ENROLLED_FORMAT, dogId, programId);
     }
 }
