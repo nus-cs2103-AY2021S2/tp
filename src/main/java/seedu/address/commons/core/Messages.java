41cc7c6ac45006ca97e4b8232f078960bdebaf54
lrj689 <lrj689@hotmail.com>
2021-03-31 01:00:44 +0800

    Fix checkstyle

--- src/main/java/seedu/address/commons/core/Messages.java
+++ src/main/java/seedu/address/commons/core/Messages.java
@@ -17,6 +17,6 @@ public class Messages {
     public static final String MESSAGE_TASKS_LISTED_OVERVIEW = "%1$d tasks listed!";
     public static final String MESSAGE_TASK_DOES_NOT_EXIST = "The task provided does not exist";
     public static final String MESSAGE_SCHEDULE_DOES_NOT_EXIST = "The schedule provided does not exist!";
-    public static final String MESSAGE_OVERLAPPING_ENTRY = "This entry has dates that overlap" +
-                                                            " with another entry in the list.";
+    public static final String MESSAGE_OVERLAPPING_ENTRY = "This entry has dates that overlap"
+                                                            + " with another entry in the list.";
 }
