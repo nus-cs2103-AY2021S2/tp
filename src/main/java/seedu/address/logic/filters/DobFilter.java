f065d2695feb0eddfc2b53b72572a46395184ae6
rajobasu <rajobasu@gmail.com>
2021-03-31 04:34:47 +0800

    Add javadoc

--- src/main/java/seedu/address/logic/filters/DobFilter.java
+++ src/main/java/seedu/address/logic/filters/DobFilter.java
@@ -5,6 +5,10 @@ import seedu.address.model.customer.Customer;
 public class DobFilter extends AbstractFilter {
     private final String[] args;
 
+    /**
+     * Constructor for DoBFilter, creates a list of keywords in the date, to later match with the customer's DOB.
+     * @param filterString - the birthdate to match. No particular format needed.
+     */
     public DobFilter(String filterString) {
         super(filterString.trim());
         args = filterString.split(" ");
