b5631a802d16431453c7a9461983912331d10f3b
Nicholas <me@cheeze2000.net>
2021-03-30 22:10:27 +0800

    Handle invalid argument for edelete

--- src/main/java/seedu/address/logic/parser/DeleteEntryCommandParser.java
+++ src/main/java/seedu/address/logic/parser/DeleteEntryCommandParser.java
@@ -1,6 +1,9 @@
 package seedu.address.logic.parser;
 
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+
 import seedu.address.logic.commands.DeleteEntryCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.entry.EntryNameContainsKeywordsPredicate;
 
 /**
@@ -11,8 +14,13 @@ public class DeleteEntryCommandParser implements Parser<DeleteEntryCommand> {
     /**
      * Parses the given {@code String} of arguments in the context of the DeleteEntryCommand
      * and returns a DeleteEntryCommand object for execution.
+     * @throws ParseException if the user input does not conform the expected format
      */
-    public DeleteEntryCommand parse(String args) {
+    public DeleteEntryCommand parse(String args) throws ParseException {
+        if (args.length() == 0) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteEntryCommand.MESSAGE_USAGE));
+        }
+
         EntryNameContainsKeywordsPredicate predicate = new EntryNameContainsKeywordsPredicate(args);
         return new DeleteEntryCommand(predicate);
     }
