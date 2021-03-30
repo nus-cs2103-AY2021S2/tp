5011971234cfdcbb27882e8e3249ab35f5f7416f
rajobasu <rajobasu@gmail.com>
2021-03-31 04:42:34 +0800

    Update Argument Multimap

--- src/main/java/seedu/address/logic/filters/Filters.java
+++ src/main/java/seedu/address/logic/filters/Filters.java
@@ -1,17 +1,24 @@
 package seedu.address.logic.filters;
 
+import static seedu.address.logic.parser.CliSyntax.PREFIX_ADDRESS;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_CARSOWNED;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_CARSPREFERRED;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_DOB;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
+
+import java.util.List;
+
 import seedu.address.logic.parser.ArgumentMultimap;
 import seedu.address.logic.parser.ArgumentTokenizer;
 import seedu.address.model.customer.Customer;
 
-import java.util.List;
-
-import static seedu.address.logic.parser.CliSyntax.*;
-
 public class Filters {
     public static AbstractFilter getCorrespondingFilter(String info) {
         ArgumentMultimap argumentMultimap = ArgumentTokenizer.tokenize(info, PREFIX_NAME, PREFIX_EMAIL, PREFIX_PHONE,
-                PREFIX_ADDRESS, PREFIX_DOB, PREFIX_TAG, PREFIX_CARS);
+                PREFIX_ADDRESS, PREFIX_DOB, PREFIX_TAG, PREFIX_CARSOWNED, PREFIX_CARSPREFERRED);
 
         if(argumentMultimap.getTotalSize() != 1) {
             throw new NullPointerException("Number of filters between two logical operators should be exactly 1");
