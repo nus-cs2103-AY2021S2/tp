b4fc969f1dcb5086b86351a2c25b6246e1fdf763
garyljj <gary.lee@u.nus.edu>
2021-03-30 21:23:00 +0800

    Tag msg in List display None! if not tags

--- src/main/java/seedu/partyplanet/logic/commands/ListCommand.java
+++ src/main/java/seedu/partyplanet/logic/commands/ListCommand.java
@@ -75,8 +75,8 @@ public class ListCommand extends Command {
         requireNonNull(model);
         model.sortPersonList(comparator);
         model.updateFilteredPersonList(predicate);
-        String tagsRepresentation = displayTags(model.getFilteredPersonList())
-                .replace("[", "").replace("]", "");
+        String tagsRepresentation = displayTags(model.getFilteredPersonList());
+
         if (model.getPersonListCopy().size() == model.getFilteredPersonList().size()) {
             return new CommandResult(ListCommand.MESSAGE_SUCCESS // No person filtered out
                     + String.format(Messages.MESSAGE_PERSONS_LISTED_TAGS, tagsRepresentation));
@@ -97,7 +97,8 @@ public class ListCommand extends Command {
                 .sorted((x, y) -> x.getKey().tagName.compareTo(y.getKey().tagName))
                 .map(t -> String.format("%s (%d)", t.getKey(), t.getValue()))
                 .reduce((x, y) -> x + ", " + y)
-                .orElse("");
+                .orElse("None!")
+                .replace("[", "").replace("]", "");
 
         return output;
     }
