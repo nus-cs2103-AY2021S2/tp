3f0b87e68feeb1f19f28f587889e8b344c0a8fe2
yaowei-soc <e0407296@u.nus.edu>
2021-03-30 15:52:48 +0800

    feat: Implement DeleteCommand multiple index

--- src/main/java/seedu/address/logic/commands/DeleteCommand.java
+++ src/main/java/seedu/address/logic/commands/DeleteCommand.java
@@ -2,7 +2,10 @@ package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import seedu.address.commons.core.Messages;
 import seedu.address.commons.core.index.Index;
@@ -19,35 +22,103 @@ public class DeleteCommand extends Command {
 
     public static final String MESSAGE_USAGE = COMMAND_WORD
             + ": Deletes the person identified by the index number used in the displayed person list.\n"
-            + "Parameters: INDEX (must be a positive integer)\n"
+            + "Parameters: INDEX [INDEX…] (must be a valid positive integer)\n"
             + "Example: " + COMMAND_WORD + " 1";
 
-    public static final String MESSAGE_DELETE_PERSON_SUCCESS = "Deleted Person: %1$s";
+    public static final String MESSAGE_DELETE_PERSON_SUCCESS = "Deleted person: %1$s";
+    public static final String MESSAGE_DELETE_PERSONS_SUCCESS = "Deleted multiple person:\n%1$s";
 
-    private final Index targetIndex;
+    private final List<Index> targetIndexes;
+    private final boolean isSpecialIndex;
 
-    public DeleteCommand(Index targetIndex) {
-        this.targetIndex = targetIndex;
+    /**
+     * Initializes DeleteCommand that deletes all the shown items in list.
+     */
+    public DeleteCommand() {
+        targetIndexes = new ArrayList<>();
+        isSpecialIndex = true;
+    }
+
+    /**
+     * Initializes DeleteCommand with a list of parsed user input indexes.
+     *
+     * @param indexes parsed user input indexes
+     * @throws NullPointerException if {@code indexes} is null.
+     */
+    public DeleteCommand(List<Index> indexes) {
+        targetIndexes = requireNonNull(indexes);
+        isSpecialIndex = false;
     }
 
     @Override
     public CommandResult execute(Model model) throws CommandException {
         requireNonNull(model);
-        List<Person> lastShownList = model.getFilteredPersonList();
+        if (isSpecialIndex) {
+            return deleteAll(model);
+        }
+        return deleteOneOrMultiple(model);
+    }
 
-        if (targetIndex.getZeroBased() >= lastShownList.size()) {
-            throw new CommandException(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+    /**
+     * Deletes one or multiple person from model.
+     *
+     * @param model {@code Model} which the command should operate on.
+     * @return feedback message of the operation result for display
+     * @throws CommandException if index is invalid
+     */
+    private CommandResult deleteOneOrMultiple(Model model) throws CommandException {
+        List<Person> lastShownList = model.getFilteredPersonList();
+        // Validate indexes
+        for (Index targetIndex : targetIndexes) {
+            if (targetIndex.getZeroBased() >= lastShownList.size()) {
+                throw new CommandException(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+            }
         }
 
-        Person personToDelete = lastShownList.get(targetIndex.getZeroBased());
-        model.deletePerson(personToDelete);
-        return new CommandResult(String.format(MESSAGE_DELETE_PERSON_SUCCESS, personToDelete));
+        StringBuilder stringBuilder = new StringBuilder();
+        List<Person> personListToDelete = targetIndexes.stream()
+                .map(Index::getZeroBased)
+                .map(lastShownList::get)
+                .collect(Collectors.toList());
+
+        for (Person personToDelete : personListToDelete) {
+            model.deletePerson(personToDelete);
+            stringBuilder.append(personToDelete);
+            stringBuilder.append("\n");
+        }
+
+        if (targetIndexes.size() == 1) {
+            return new CommandResult(
+                    String.format(MESSAGE_DELETE_PERSON_SUCCESS, stringBuilder.toString().trim()));
+        }
+        return new CommandResult(
+                String.format(MESSAGE_DELETE_PERSONS_SUCCESS, stringBuilder.toString().trim()));
+    }
+
+    /**
+     * Deletes all the person in the shown person list.
+     *
+     * @param model {@code Model} which the command should operate on.
+     * @return feedback message of the operation result for display
+     */
+    private CommandResult deleteAll(Model model) {
+        List<Person> lastShownList = model.getFilteredPersonList();
+        StringBuilder stringBuilder = new StringBuilder();
+        Iterator<Person> iterator = lastShownList.iterator();
+
+        // Note: There is a need to clone the array list before deleting
+        for (Person personToDelete : new ArrayList<>(lastShownList)) {
+            model.deletePerson(personToDelete);
+            stringBuilder.append(personToDelete);
+            stringBuilder.append("\n");
+        }
+        return new CommandResult(String.format(MESSAGE_DELETE_PERSONS_SUCCESS, stringBuilder));
     }
 
     @Override
     public boolean equals(Object other) {
         return other == this // short circuit if same object
                 || (other instanceof DeleteCommand // instanceof handles nulls
-                && targetIndex.equals(((DeleteCommand) other).targetIndex)); // state check
+                && targetIndexes.containsAll(((DeleteCommand) other).targetIndexes)); // state check
     }
 }
--- src/main/java/seedu/address/logic/parser/DeleteCommandParser.java
+++ src/main/java/seedu/address/logic/parser/DeleteCommandParser.java
@@ -1,8 +1,8 @@
 package seedu.address.logic.parser;
 
+import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 
-import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.DeleteCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 
@@ -11,15 +11,23 @@ import seedu.address.logic.parser.exceptions.ParseException;
  */
 public class DeleteCommandParser implements Parser<DeleteCommand> {
 
+    public static final String SPECIAL_INDEX = "shown";
+
     /**
-     * Parses the given {@code String} of arguments in the context of the DeleteCommand
-     * and returns a DeleteCommand object for execution.
+     * Parses the given {@code String} of arguments in the context of the DeleteCommand and returns
+     * a DeleteCommand object for execution.
+     *
      * @throws ParseException if the user input does not conform the expected format
      */
     public DeleteCommand parse(String args) throws ParseException {
+        requireNonNull(args);
+
+        if (args.trim().equals(SPECIAL_INDEX)) {
+            return new DeleteCommand();
+        }
+
         try {
-            Index index = ParserUtil.parseIndex(args);
-            return new DeleteCommand(index);
+            return new DeleteCommand(ParserUtil.parseIndexes(args));
         } catch (ParseException pe) {
             throw new ParseException(
                     String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteCommand.MESSAGE_USAGE), pe);
@@ -27,8 +35,8 @@ public class DeleteCommandParser implements Parser<DeleteCommand> {
     }
 
     /**
-     * Parses the given {@code String} of arguments in the context of the DeleteCommand
-     * and returns true if arguments are valid to be aliased.
+     * Parses the given {@code String} of arguments in the context of the DeleteCommand and returns
+     * true if arguments are valid to be aliased.
      */
     @Override
     public boolean isValidCommandToAlias(String userInput) {
@@ -37,7 +45,7 @@ public class DeleteCommandParser implements Parser<DeleteCommand> {
         }
 
         try {
-            ParserUtil.parseIndex(userInput);
+            parse(userInput);
             return true;
         } catch (ParseException pe) {
             return false;
