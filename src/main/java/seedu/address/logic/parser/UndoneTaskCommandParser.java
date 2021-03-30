fb2aa029247fa353312d9f2cf6efcd0f97b97961
Jiaxiang <56266729+litone01@users.noreply.github.com>
2021-03-30 15:22:24 +0800

    Add undone_task
    Tasks marked as complete can now be marked as uncompleted.

--- src/main/java/seedu/address/logic/parser/UndoneTaskCommandParser.java
+++ src/main/java/seedu/address/logic/parser/UndoneTaskCommandParser.java
@@ -3,26 +3,25 @@ package seedu.address.logic.parser;
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 
 import seedu.address.commons.core.index.Index;
-import seedu.address.logic.commands.DoneTaskCommand;
+import seedu.address.logic.commands.UndoneTaskCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 
 /**
- * Parses input arguments and creates a new DoneTaskCommand object.
+ * Parses input arguments and creates a new UndoneTaskCommand object.
  */
-public class DoneTaskCommandParser implements Parser<DoneTaskCommand> {
-
+public class UndoneTaskCommandParser {
     /**
      * Parses the given {@code String} of arguments in the context of the DoneTaskCommand
      * and returns a DoneTaskCommand object for execution.
      * @throws ParseException if the user input does not conform the expected format
      */
-    public DoneTaskCommand parse(String args) throws ParseException {
+    public UndoneTaskCommand parse(String args) throws ParseException {
         try {
             Index index = SocheduleParserUtil.parseIndex(args);
-            return new DoneTaskCommand(index);
+            return new UndoneTaskCommand(index);
         } catch (ParseException pe) {
             throw new ParseException(
-                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, DoneTaskCommand.MESSAGE_USAGE), pe);
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, UndoneTaskCommand.MESSAGE_USAGE), pe);
         }
     }
 }
