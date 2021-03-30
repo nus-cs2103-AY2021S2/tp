4ebc4225a7ab170a1e8ff8864fa8d419a0d35138
nighoggDatatype <hosherman1965@gmail.com>
2021-03-30 20:48:56 +0800

    Add error checking based on the attribute classes of Customer in the model package

--- src/main/java/seedu/address/logic/filters/CoeExpiryFilter.java
+++ src/main/java/seedu/address/logic/filters/CoeExpiryFilter.java
@@ -1,5 +1,7 @@
 package seedu.address.logic.filters;
 
+import static seedu.address.commons.util.AppUtil.checkArgument;
+
 import java.time.LocalDate;
 import java.util.List;
 
@@ -7,6 +9,7 @@ import seedu.address.model.customer.CoeExpiry;
 import seedu.address.model.customer.Customer;
 
 public class CoeExpiryFilter extends AbstractFilter {
+    public static final String MESSAGE_CONSTRAINTS = "COE Expiry Filter can only accept 'exp' or a non negative number";
     private final LocalDate coeMinimumDeadline;
 
     /**
@@ -16,24 +19,36 @@ public class CoeExpiryFilter extends AbstractFilter {
      * A special keyword "exp" is accepted as an alias for COE
      * that expires in zero years or less (ie: Already expired).
      *
-     * TODO: Include how this will throw an exception upon getting negative years
+     * Note that {@code filterString} must either be "exp" or a non negative number.
      *
      * @param filterString The number of years that a COE can be valid for before
      *                     being skipped.
      */
     public CoeExpiryFilter(String filterString) {
         super(filterString);
+        checkArgument(isValidFilter(filterString), MESSAGE_CONSTRAINTS);
         LocalDate currentDate = LocalDate.now();
         boolean expiredOnly = filterString.equals("exp");
         int years = 0;
         if (!expiredOnly) {
             years = Integer.parseInt(filterString);
         }
-        if (years < 0) {
-            throw new Exception(); //TODO: Figure out the correct exception
-        }
         this.coeMinimumDeadline = currentDate.plusYears(years);
     }
+    /**
+     * Returns true if a given string is a valid filter.
+     */
+    public boolean isValidFilter(String filterString) {
+        if (filterString.equals("exp")) {
+            return true;
+        }
+        try {
+            return Integer.parseInt(filterString) >= 0;
+        } catch (NumberFormatException numberFormatException) {
+            return false;
+        }
+    }
+
     private boolean testCoeExpiry(CoeExpiry coeExpiry) {
         return !coeMinimumDeadline.isAfter(coeExpiry.toDate());
     }
