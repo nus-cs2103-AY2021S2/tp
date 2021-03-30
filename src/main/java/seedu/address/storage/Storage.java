1767d34a66f40a6c1a00560d2842d0ff331ba2ce
Vinleon <kohvinleon@gmail.com>
2021-03-31 04:19:25 +0800

    Create Storage Classes for Reminder

--- src/main/java/seedu/address/storage/Storage.java
+++ src/main/java/seedu/address/storage/Storage.java
@@ -15,7 +15,7 @@ import seedu.address.model.UserPrefs;
  * API of the Storage component
  */
 public interface Storage extends AddressBookStorage, AppointmentBookStorage, GradeBookStorage,
-        UserPrefsStorage, ScheduleTrackerStorage {
+        UserPrefsStorage, ScheduleTrackerStorage, ReminderTrackerStorage {
 
     @Override
     Optional<UserPrefs> readUserPrefs() throws DataConversionException, IOException;
