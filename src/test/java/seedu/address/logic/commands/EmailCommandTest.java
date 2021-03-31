6f11a6b3595c3b7490a9dbcad58a73ba73466f6d
yaowei-soc <e0407296@u.nus.edu>
2021-03-31 18:43:50 +0800

    test: Add test cases to validate parser output

--- src/test/java/seedu/address/logic/commands/EmailCommandTest.java
+++ src/test/java/seedu/address/logic/commands/EmailCommandTest.java
@@ -2,8 +2,15 @@ package seedu.address.logic.commands;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
+import static seedu.address.logic.commands.CommandTestUtil.showPersonAtIndex;
 import static seedu.address.testutil.Assert.assertThrows;
+import static seedu.address.testutil.TypicalAliases.getTypicalAliases;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
+import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
 import static seedu.address.testutil.TypicalIndexes.VALID_INDEXES;
+import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -11,13 +18,38 @@ import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
+import seedu.address.commons.core.Messages;
 import seedu.address.commons.core.index.Index;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
 
 public class EmailCommandTest {
+
+    private final Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs(),
+            getTypicalAliases());
 
     @Test
     public void initialize_nullIndexes_throwsException() {
         assertThrows(NullPointerException.class, () -> new EmailCommand(null));
+    }
+
+    @Test
+    public void execute_outOfBoundsIndex_throwsCommandException() {
+        Index outOfBoundIndex = Index.fromOneBased(model.getFilteredPersonList().size() + 1);
+        EmailCommand emailCommand = new EmailCommand(Collections.singletonList(outOfBoundIndex));
+        assertCommandFailure(emailCommand, model, Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+    }
+
+    @Test
+    public void execute_invalidIndexFilteredList_throwsCommandException() {
+        showPersonAtIndex(model, INDEX_FIRST_PERSON);
+
+        Index outOfBoundIndex = INDEX_SECOND_PERSON;
+        // ensures that outOfBoundIndex is still in bounds of address book list
+        assertTrue(outOfBoundIndex.getZeroBased() < model.getAddressBook().getPersonList().size());
+        EmailCommand emailCommand = new EmailCommand(Collections.singletonList(outOfBoundIndex));
+        assertCommandFailure(emailCommand, model, Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
     }
 
     @Test
--- src/test/java/seedu/address/logic/parser/EmailCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/EmailCommandParserTest.java
@@ -1,15 +1,20 @@
 package seedu.address.logic.parser;
 
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.commands.CommandTestUtil.PREAMBLE_WHITESPACE;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertValidCommandToAliasFailure;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertValidCommandToAliasSuccess;
+import static seedu.address.logic.parser.EmailCommandParser.SPECIAL_INDEX;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
 import static seedu.address.testutil.TypicalIndexes.INVALID_INDEX_STRING;
 import static seedu.address.testutil.TypicalIndexes.NEGATIVE_INDEX_STRING;
 import static seedu.address.testutil.TypicalIndexes.VALID_INDEXES;
 import static seedu.address.testutil.TypicalIndexes.VALID_INDEX_STRING;
 import static seedu.address.testutil.TypicalIndexes.ZERO_INDEX_STRING;
 
+import java.util.Collections;
 import java.util.stream.Collectors;
 
 import org.junit.jupiter.api.Test;
@@ -24,7 +29,20 @@ public class EmailCommandParserTest {
     @Test
     public void parse_emailShown_success() {
         EmailCommand emailCommand = new EmailCommand();
-        assertParseSuccess(parser, EmailCommandParser.SPECIAL_INDEX, emailCommand);
+        assertParseSuccess(parser, SPECIAL_INDEX, emailCommand);
+    }
+
+    @Test
+    public void parse_validIndex_success() {
+        EmailCommand emailCommand = new EmailCommand(Collections.singletonList(INDEX_FIRST_PERSON));
+        assertParseSuccess(parser, VALID_INDEX_STRING, emailCommand);
+    }
+
+    @Test
+    public void parse_validIndexWithWhitepace_success() {
+        EmailCommand emailCommand = new EmailCommand(Collections.singletonList(INDEX_FIRST_PERSON));
+        assertParseSuccess(parser, PREAMBLE_WHITESPACE + VALID_INDEX_STRING + PREAMBLE_WHITESPACE,
+                emailCommand);
     }
 
     @Test
@@ -37,13 +55,25 @@ public class EmailCommandParserTest {
     }
 
     @Test
-    public void parse_invalidIndexes_failure() {
-        assertParseFailure(parser, INVALID_INDEX_STRING, EmailCommand.MESSAGE_USAGE);
+    public void parse_validIndexesWithWhitespace_success() {
+        EmailCommand emailCommand = new EmailCommand(VALID_INDEXES);
+        String inputIndexes = VALID_INDEXES.stream()
+                .map(Index::getOneBased).map(String::valueOf)
+                .collect(Collectors.joining(PREAMBLE_WHITESPACE));
+        assertParseSuccess(parser, inputIndexes, emailCommand);
+    }
+
+    @Test
+    public void parse_invalidIndex_failure() {
+        assertParseFailure(parser, INVALID_INDEX_STRING,
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, EmailCommand.MESSAGE_USAGE));
     }
 
     @Test
     public void parse_emptyArgs_failure() {
-        assertParseFailure(parser, "", EmailCommand.MESSAGE_USAGE);
+        assertParseFailure(parser, "",
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, EmailCommand.MESSAGE_USAGE));
+
     }
 
     @Test
@@ -54,8 +84,18 @@ public class EmailCommandParserTest {
         // valid index
         assertValidCommandToAliasSuccess(parser, VALID_INDEX_STRING);
 
+        // valid index with whitespac
+        assertValidCommandToAliasSuccess(parser, PREAMBLE_WHITESPACE +
+                VALID_INDEX_STRING + PREAMBLE_WHITESPACE);
+
+        String inputIndexes = VALID_INDEXES.stream()
+                .map(Index::getOneBased).map(String::valueOf)
+                .collect(Collectors.joining(" "));
+        // valid indexes
+        assertValidCommandToAliasSuccess(parser, inputIndexes);
+
         // shown
-        assertValidCommandToAliasSuccess(parser, EmailCommandParser.SPECIAL_INDEX);
+        assertValidCommandToAliasSuccess(parser, SPECIAL_INDEX);
     }
 
     @Test
