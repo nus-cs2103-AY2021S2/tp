68b559757745dd044c687eade8f831ded7d6a8ff
Kou Yong Kang <skykinetic@stevenkou.xyz>
2021-04-01 00:33:18 +0800

    Fix #272

--- src/main/java/dog/pawbook/logic/commands/DeleteOwnerCommand.java
+++ src/main/java/dog/pawbook/logic/commands/DeleteOwnerCommand.java
@@ -4,6 +4,7 @@ import static dog.pawbook.commons.core.Messages.MESSAGE_INVALID_OWNER_ID;
 import static dog.pawbook.model.managedentity.owner.Owner.ENTITY_WORD;
 import static java.util.Objects.requireNonNull;
 
+import java.util.HashSet;
 import java.util.Set;
 
 import dog.pawbook.logic.commands.exceptions.CommandException;
@@ -11,6 +12,8 @@ import dog.pawbook.model.Model;
 import dog.pawbook.model.managedentity.Entity;
 import dog.pawbook.model.managedentity.dog.Dog;
 import dog.pawbook.model.managedentity.owner.Owner;
+import dog.pawbook.model.managedentity.program.Program;
+import javafx.util.Pair;
 
 /**
  * Deletes a owner identified using it's displayed index from the database.
@@ -46,6 +49,20 @@ public class DeleteOwnerCommand extends DeleteCommand {
         for (int dogId : dogsToDelete) {
             assert model.hasEntity(dogId) && model.getEntity(dogId) instanceof Dog : "Dog ID is invalid!";
             model.deleteEntity(dogId);
+        }
+
+        // remove all delete dogs from the programs
+        for (Pair<Integer, Entity> idEntityPair : model.getUnfilteredEntityList()) {
+            Entity entity = idEntityPair.getValue();
+            if (!(entity instanceof Program)) {
+                continue;
+            }
+
+            Program program = (Program) entity;
+            HashSet<Integer> updatedDogIdSet = new HashSet<>(program.getDogIdSet());
+            updatedDogIdSet.removeAll(dogsToDelete);
+            model.setEntity(idEntityPair.getKey(),
+                    new Program(program.getName(), program.getSessions(), program.getTags(), updatedDogIdSet));
         }
 
         // then actually delete the owner
