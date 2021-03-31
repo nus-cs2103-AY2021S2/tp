26b050f8f66bfc3ac047da9207195ee897cade74
Samuel Fang <samuelfangjw@gmail.com>
2021-04-01 01:59:39 +0800

    Update UndoCommand.java

--- src/main/java/seedu/address/logic/commands/UndoCommand.java
+++ src/main/java/seedu/address/logic/commands/UndoCommand.java
@@ -15,7 +15,7 @@ import seedu.address.model.colabfolderhistory.exceptions.NoUndoableStateExceptio
 public class UndoCommand extends Command {
 
     public static final String COMMAND_WORD = "undo";
-    public static final String MESSAGE_SUCCESS = "Undo success!\n%s";
+    public static final String MESSAGE_SUCCESS = "Undo successful!\n%s";
     public static final String MESSAGE_FAILURE = "No more commands to undo.";
 
     @Override
