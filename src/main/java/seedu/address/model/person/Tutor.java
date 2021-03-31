6616480d6703f44d0001aad3701af0a232c0f374
Yan Ling Kuek <yanlingkuek@gmail.com>
2021-03-31 18:41:58 +0800

    Change edit_person command to update list of session IDs

--- src/main/java/seedu/address/model/person/Tutor.java
+++ src/main/java/seedu/address/model/person/Tutor.java
@@ -1,12 +1,33 @@
 package seedu.address.model.person;
 
+import java.util.List;
 import java.util.Set;
 
+import seedu.address.model.session.SessionId;
 import seedu.address.model.tag.Tag;
 
 public class Tutor extends Person {
 
     private static int tutorCount = 0;
+
+    /**
+     * Every field must be present and not null.
+     *
+     * @param personId
+     * @param name
+     * @param phone
+     * @param email
+     * @param address
+     * @param tags
+     * @param sessions
+     */
+    public Tutor(PersonId personId, Name name, Phone phone, Email email, Address address, Set<Tag> tags,
+                 List<SessionId> sessions) {
+        super(name, phone, email, address, tags);
+        this.setPersonType(new PersonType("tutor"));
+        this.setPersonId(personId);
+        this.setSessions(sessions);
+    }
 
     /**
      * Every field must be present and not null.
