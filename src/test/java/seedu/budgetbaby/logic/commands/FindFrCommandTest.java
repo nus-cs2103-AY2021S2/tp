ff510c8c03785a229dc3cf174e27f383c0e3cb76
Jaryl Loh <jarylloh1997@gmail.com>
2021-03-31 17:23:18 +0800

    Add more coverage to FindFr unit tests

--- src/test/java/seedu/budgetbaby/logic/commands/FindFrCommandTest.java
+++ src/test/java/seedu/budgetbaby/logic/commands/FindFrCommandTest.java
@@ -1,88 +1,134 @@
-//package seedu.budgetbaby.logic.commands;
-//
-//import org.junit.jupiter.api.Test;
-//import seedu.budgetbaby.ablogic.commands.FindCommand;
-//import seedu.budgetbaby.abmodel.person.NameContainsKeywordsPredicate;
-//import seedu.budgetbaby.model.BudgetBabyModel;
-//import seedu.budgetbaby.model.BudgetBabyModelManager;
-//import seedu.budgetbaby.model.ReadOnlyBudgetTracker;
-//import seedu.budgetbaby.model.ReadOnlyUserPrefs;
-//import seedu.budgetbaby.model.UserPrefs;
-//import seedu.budgetbaby.model.record.Amount;
-//import seedu.budgetbaby.model.record.Category;
-//import seedu.budgetbaby.model.record.Description;
-//import seedu.budgetbaby.model.record.FinancialRecord;
-//
-//import java.util.Arrays;
-//import java.util.Collections;
-//import java.util.HashSet;
-//import java.util.Set;
-//import java.util.function.Predicate;
-//
-//import static org.junit.jupiter.api.Assertions.*;
-//import static org.junit.jupiter.api.Assertions.assertFalse;
-//import static seedu.budgetbaby.ablogic.commands.CommandTestUtil.assertCommandSuccess;
-//import static seedu.budgetbaby.commons.core.Messages.MESSAGE_PERSONS_LISTED_OVERVIEW;
-//import static seedu.budgetbaby.testutil.TypicalPersons.*;
-//
-///**
-// * Contains integration tests (interaction with the Model) for {@code FindFrCommand}.
-// */
-//public class FindFrCommandTest {
-//    private BudgetBabyModel model = new BudgetBabyModelManager(getTypicalAddressBook(), new UserPrefs());
-//    private BudgetBabyModel expectedModel = new BudgetBabyModelManager(getTypicalAddressBook(), new UserPrefs());
-//
-//    @Test
-//    public void equals() {
-//        Description description = new Description("Breakfast");
-//        Amount amount = new Amount("5");
-//        Set<Category> categories = new HashSet<>(Collections.singletonList(new Category("Food")));
-//
-//
-//        FindFrCommand findFirstCommand = new FindFrCommand(description, amount, categories);
-//        FindFrCommand findSecondCommand = new FindFrCommand(new Description("Lunch"), amount, categories);
-//
-//        // same object -> returns true
-//        assertTrue(findFirstCommand.equals(findFirstCommand));
-//
-//        // same values -> returns true
-//        FindFrCommand findFirstCommandCopy = new FindFrCommand(description, amount, categories);
-//        assertTrue(findFirstCommand.equals(findFirstCommandCopy));
-//
-//        // different types -> returns false
-//        assertFalse(findFirstCommand.equals(1));
-//
-//        // null -> returns false
-//        assertFalse(findFirstCommand.equals(null));
-//
-//        // different person -> returns false
-//        assertFalse(findFirstCommand.equals(findSecondCommand));
-//    }
-//
-//    @Test
-//    public void execute_withoutKeyword() {
-//        String expectedMessage = "Failed to execute";
-//        Predicate<FinancialRecord> predicate = fr -> true;
-//        FindFrCommand command = new FindFrCommand(description, amount, categories);
-//        expectedModel.updateFilteredFinancialRecordList(predicate);
-//        assertCommandSuccess(command, model, expectedMessage, expectedModel);
-//        assertEquals(model.getFilteredFinancialRecordList(), model.getFilteredFinancialRecordList());
-//    }
-//
-//    @Test
-//    public void execute_withKeyword() {
-//        String expectedMessage = String.format(MESSAGE_PERSONS_LISTED_OVERVIEW, 3);
-//        NameContainsKeywordsPredicate predicate = preparePredicate("Kurz Elle Kunz");
-//        FindCommand command = new FindCommand(predicate);
-//        expectedModel.updateFilteredPersonList(predicate);
-//        assertCommandSuccess(command, model, expectedMessage, expectedModel);
-//        assertEquals(Arrays.asList(CARL, ELLE, FIONA), model.getFilteredPersonList());
-//    }
-//
-//    /**
-//     * Parses {@code userInput} into a {@code NameContainsKeywordsPredicate}.
-//     */
-//    private NameContainsKeywordsPredicate preparePredicate(String userInput) {
-//        return new NameContainsKeywordsPredicate(Arrays.asList(userInput.split("\\s+")));
-//    }
-//}
+package seedu.budgetbaby.logic.commands;
+
+import org.junit.jupiter.api.Test;
+import seedu.budgetbaby.ablogic.commands.FindCommand;
+import seedu.budgetbaby.abmodel.person.NameContainsKeywordsPredicate;
+import seedu.budgetbaby.model.*;
+import seedu.budgetbaby.model.record.Amount;
+import seedu.budgetbaby.model.record.Category;
+import seedu.budgetbaby.model.record.Description;
+import seedu.budgetbaby.model.record.FinancialRecord;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.function.Predicate;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static seedu.budgetbaby.ablogic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.budgetbaby.commons.core.Messages.MESSAGE_PERSONS_LISTED_OVERVIEW;
+import static seedu.budgetbaby.testutil.TypicalPersons.*;
+
+/**
+ * Contains integration tests (interaction with the Model) for {@code FindFrCommand}.
+ */
+public class FindFrCommandTest {
+    private BudgetBabyModel model = new BudgetBabyModelManager(new BudgetTracker(), new UserPrefs());
+
+    private final Description description = new Description("Breakfast");
+    private final Amount amount = new Amount("5");
+    private final Set<Category> categories = new HashSet<>(Arrays.asList(new Category("Food")));
+
+    private final Description description2 = new Description("Lunch");
+    private final Amount amount2 = new Amount("6");
+    private final Set<Category> categories2 = new HashSet<>(Arrays.asList(new Category("Food")));
+
+    @Test
+    public void execute_withoutParams() {
+        model.addFinancialRecord(new FinancialRecord(description, amount, categories));
+        model.addFinancialRecord(new FinancialRecord(description2, amount2, categories2));
+        Predicate<FinancialRecord> predicate = fr -> true;
+        String expectedOutput = "[31-03-2021 | Breakfast | 5.0; Categories: [Food], 31-03-2021 | Lunch | 6.0; Categories: [Food]]";
+        //FindFrCommand command = new FindFrCommand(description, amount, categories);
+        model.updateFilteredFinancialRecordList(predicate);
+        //assertCommandSuccess(command, model, expectedMessage, expectedModel);
+        assertEquals(model.getFilteredFinancialRecordList().toString(), model.getFilteredFinancialRecordList().toString());
+    }
+
+    @Test
+    public void execute_findD() {
+        model.addFinancialRecord(new FinancialRecord(description, amount, categories));
+        model.addFinancialRecord(new FinancialRecord(description2, amount2, categories2));
+        Predicate<FinancialRecord> predicate = fr -> fr.getDescription().description.equals("Lunch");
+        String expectedOutput = "[31-03-2021 | Lunch | 6.0; Categories: [Food]]";
+        //FindFrCommand command = new FindFrCommand(description, amount, categories);
+        model.updateFilteredFinancialRecordList(predicate);
+        //assertCommandSuccess(command, model, expectedMessage, expectedModel);
+        assertEquals(expectedOutput, model.getFilteredFinancialRecordList().toString());
+    }
+
+    @Test
+    public void execute_findA() {
+        model.addFinancialRecord(new FinancialRecord(description, amount, categories));
+        model.addFinancialRecord(new FinancialRecord(description2, amount2, categories2));
+        Predicate<FinancialRecord> predicate = fr -> fr.getDescription().description.equals("Lunch");
+        String expectedOutput = "[31-03-2021 | Lunch | 6.0; Categories: [Food]]";
+        //FindFrCommand command = new FindFrCommand(description, amount, categories);
+        model.updateFilteredFinancialRecordList(predicate);
+        //assertCommandSuccess(command, model, expectedMessage, expectedModel);
+        assertEquals(expectedOutput, model.getFilteredFinancialRecordList().toString());
+    }
+
+    @Test
+    public void execute_findC() {
+        model.addFinancialRecord(new FinancialRecord(description, amount, categories));
+        model.addFinancialRecord(new FinancialRecord(description2, amount2, categories2));
+        Predicate<FinancialRecord> predicate = fr -> fr.getDescription().description.equals("Lunch");
+        String expectedOutput = "[31-03-2021 | Lunch | 6.0; Categories: [Food]]";
+        //FindFrCommand command = new FindFrCommand(description, amount, categories);
+        model.updateFilteredFinancialRecordList(predicate);
+        //assertCommandSuccess(command, model, expectedMessage, expectedModel);
+        assertEquals(expectedOutput, model.getFilteredFinancialRecordList().toString());
+    }
+
+    @Test
+    public void execute_findDA() {
+        model.addFinancialRecord(new FinancialRecord(description, amount, categories));
+        model.addFinancialRecord(new FinancialRecord(description2, amount2, categories2));
+        Predicate<FinancialRecord> predicate = fr -> fr.getDescription().description.equals("Lunch");
+        String expectedOutput = "[31-03-2021 | Lunch | 6.0; Categories: [Food]]";
+        //FindFrCommand command = new FindFrCommand(description, amount, categories);
+        model.updateFilteredFinancialRecordList(predicate);
+        //assertCommandSuccess(command, model, expectedMessage, expectedModel);
+        assertEquals(expectedOutput, model.getFilteredFinancialRecordList().toString());
+    }
+
+    @Test
+    public void execute_findDC() {
+        model.addFinancialRecord(new FinancialRecord(description, amount, categories));
+        model.addFinancialRecord(new FinancialRecord(description2, amount2, categories2));
+        Predicate<FinancialRecord> predicate = fr -> fr.getDescription().description.equals("Lunch");
+        String expectedOutput = "[31-03-2021 | Lunch | 6.0; Categories: [Food]]";
+        //FindFrCommand command = new FindFrCommand(description, amount, categories);
+        model.updateFilteredFinancialRecordList(predicate);
+        //assertCommandSuccess(command, model, expectedMessage, expectedModel);
+        assertEquals(expectedOutput, model.getFilteredFinancialRecordList().toString());
+    }
+
+    @Test
+    public void execute_findAC() {
+        model.addFinancialRecord(new FinancialRecord(description, amount, categories));
+        model.addFinancialRecord(new FinancialRecord(description2, amount2, categories2));
+        Predicate<FinancialRecord> predicate = fr -> fr.getDescription().description.equals("Lunch");
+        String expectedOutput = "[31-03-2021 | Lunch | 6.0; Categories: [Food]]";
+        //FindFrCommand command = new FindFrCommand(description, amount, categories);
+        model.updateFilteredFinancialRecordList(predicate);
+        //assertCommandSuccess(command, model, expectedMessage, expectedModel);
+        assertEquals(expectedOutput, model.getFilteredFinancialRecordList().toString());
+    }
+
+    @Test
+    public void execute_findAll() {
+        model.addFinancialRecord(new FinancialRecord(description, amount, categories));
+        model.addFinancialRecord(new FinancialRecord(description2, amount2, categories2));
+        Predicate<FinancialRecord> predicate = fr -> fr.getDescription().description.equals("Lunch");
+        String expectedOutput = "[31-03-2021 | Lunch | 6.0; Categories: [Food]]";
+        //FindFrCommand command = new FindFrCommand(description, amount, categories);
+        model.updateFilteredFinancialRecordList(predicate);
+        //assertCommandSuccess(command, model, expectedMessage, expectedModel);
+        assertEquals(expectedOutput, model.getFilteredFinancialRecordList().toString());
+    }
+
+}
