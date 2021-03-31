fd99996e8424729dcfd2e9044104fbc21f87af5e
david <dvdweien@gmail.com>
2021-03-31 08:59:44 +0800

    pass checkstyle

--- src/main/java/seedu/address/model/property/status/Offer.java
+++ src/main/java/seedu/address/model/property/status/Offer.java
@@ -1,10 +1,10 @@
 package seedu.address.model.property.status;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.util.AppUtil.checkArgument;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Represents a buyer's offer price for a property.
