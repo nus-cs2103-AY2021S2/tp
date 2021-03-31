5bdff4756f5f077eb96ba0e0cd331f6fbe697fe5
Daniel Ong <ongfishh@gmail.com>
2021-03-31 20:25:27 +0800

    Remove unused imports

--- src/main/java/seedu/address/ui/TaskCard.java
+++ src/main/java/seedu/address/ui/TaskCard.java
@@ -3,7 +3,6 @@ package seedu.address.ui;
 import java.util.Comparator;
 
 import javafx.beans.binding.Bindings;
-import javafx.beans.property.SimpleStringProperty;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.layout.FlowPane;
