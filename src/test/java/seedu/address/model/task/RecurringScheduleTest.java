3ea178d19332ed606c4e4b04a3d6962fc33cbf7d
kaixiangtay <taykaixiang97@gmail.com>
2021-03-31 12:35:07 +0800

    Remove RecurringDatesUtil after considering from OOP perspective it actually increase coupling and reduce cohesion as well as the ease of testability for RecurringSchedule
    Enhance code quality in Recurring Schedule by wrapping end Date as an
    Optional<LocalDate> so there will not be
    any instance of null initialisation
    
    Enhance code readability by using conditional expression
    in place of if else in Recurring Schedule
    
    Add check for unchecked and unsafe operations in build.gradle

--- src/test/java/seedu/address/model/task/RecurringScheduleTest.java
+++ src/test/java/seedu/address/model/task/RecurringScheduleTest.java
@@ -1,9 +1,15 @@
 package seedu.address.model.task;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.testutil.Assert.assertThrows;
 
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalAdjusters;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -11,19 +17,21 @@ import java.util.List;
 import org.junit.jupiter.api.Test;
 
 public class RecurringScheduleTest {
-
+    public static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd/MM/yyyy");
     @Test
     public void constructor_null_throwsNullPointerException() {
         assertThrows(NullPointerException.class, () -> new RecurringSchedule(null));
     }
 
-
     @Test
     public void constructor_invalidRecurringSchedule_throwsIllegalArgumentException() {
         String invalidRecurringSchedule = "recurring schedule";
         assertThrows(IllegalArgumentException.class, () -> new RecurringSchedule(invalidRecurringSchedule));
-    }
 
+
+        String blankRecurringSchedule = " ";
+        assertThrows(IllegalArgumentException.class, () -> new RecurringSchedule(blankRecurringSchedule));
+    }
 
     @Test
     public void equals() {
@@ -52,11 +60,9 @@ public class RecurringScheduleTest {
 
     @Test
     public void isEmptyRecurringScheduleInput() {
-        // reject blank space argument since there is input required
-        assertFalse(RecurringSchedule.isEmptyRecurringScheduleInput(" "));
-
+        RecurringSchedule emptyRecurringSchedule = new RecurringSchedule("");
         // accept no argument since it is optional for a task to be recurring schedule
-        assertTrue(RecurringSchedule.isEmptyRecurringScheduleInput(""));
+        assertTrue(emptyRecurringSchedule.isEmptyValue());
     }
 
     @Test
@@ -150,5 +156,84 @@ public class RecurringScheduleTest {
         String secondDateString = "31/05/2021";
         assertFalse(weekDates.stream().anyMatch(date -> date.equals(secondDateString)));
 
+    }
+
+    @Test
+    public void checkExpiryDate() {
+        LocalDate currentDate = LocalDate.now();
+        String endDateInput = "05/02/2019";
+        LocalDate endDate = LocalDate.parse(endDateInput, FORMATTER);
+
+        // end date is after current date
+        assertTrue(endDate.isBefore(currentDate));
+
+        endDateInput = "31/12/2021";
+        endDate = LocalDate.parse(endDateInput, FORMATTER);
+
+        // end date is ahead of current date
+        assertFalse(endDate.isBefore(currentDate));
+    }
+
+    @Test
+    public void formatRecurringScheduleInput() {
+        String recurringSchedule = "[30/06/2021][MON][Biweekly]";
+        String[] recurringScheduleData = recurringSchedule.replaceAll("\\]", "").split("\\[");
+
+        LocalDate endDate = LocalDate.parse(recurringScheduleData[1], FORMATTER);
+        String dayOfWeek = recurringScheduleData[2].toLowerCase();
+        String weekFreq = recurringScheduleData[3].toLowerCase();
+
+        // end of date is valid input
+        assertEquals(endDate, LocalDate.parse("30/06/2021", FORMATTER));
+
+        // day of week is valid input
+        assertEquals(dayOfWeek, "mon");
+
+        // week frequency is valid input
+        assertEquals(weekFreq, "biweekly");
+
+        String outputRecurringScheduleDetail = " every " + dayOfWeek + " " + weekFreq + " until "
+                + recurringScheduleData[1];
+
+        // valid output recurring detail
+        assertEquals(outputRecurringScheduleDetail, " every mon biweekly until 30/06/2021");
+    }
+
+    @Test
+    public void calculateNumWeeksBetweenDates() {
+        // current date to be 28/03/2021 (Sun)
+        LocalDate currentDate = LocalDate.parse("28/03/2021", FORMATTER);
+        // end date to be 02/04/2021 here (Fri of same week)
+        LocalDate firstEndDate = LocalDate.parse("02/04/2021", FORMATTER);
+
+        LocalDate startingDate = currentDate.with(TemporalAdjusters.previousOrSame(DayOfWeek.SUNDAY));
+        LocalDate endingDate = firstEndDate.with(TemporalAdjusters.previousOrSame(DayOfWeek.SATURDAY));
+
+        long daysBetweenDates = ChronoUnit.DAYS.between(startingDate, endingDate);
+        int numWeeks = (int) Math.ceil(daysBetweenDates / 7.0);
+
+        // number of weeks is considered 0 and not 1
+        // since it is within a single week range from the previous week Sun till the current week Sat
+        assertEquals(numWeeks, 0);
+
+        // end date to be 03/04/2021 here (Sat to be considered the last day of same week)
+        LocalDate secondEndDate = LocalDate.parse("03/04/2021", FORMATTER);
+        endingDate = secondEndDate.with(TemporalAdjusters.previousOrSame(DayOfWeek.SATURDAY));
+
+        daysBetweenDates = ChronoUnit.DAYS.between(startingDate, endingDate);
+        numWeeks = (int) Math.ceil(daysBetweenDates / 7.0);
+
+        // number of weeks is considered 1 since it has just completed a single week range (inclusive of Sat)
+        // from the previous week Sun till the current week Sat
+        assertEquals(numWeeks, 1);
+
+        LocalDate thirdEndDate = LocalDate.parse("18/04/2021", FORMATTER);
+        endingDate = thirdEndDate.with(TemporalAdjusters.previousOrSame(DayOfWeek.SATURDAY));
+
+        daysBetweenDates = ChronoUnit.DAYS.between(startingDate, endingDate);
+        numWeeks = (int) Math.ceil(daysBetweenDates / 7.0);
+
+        // number of weeks is considered 3 since it has completed 3 weeks range
+        assertEquals(numWeeks, 3);
     }
 }
