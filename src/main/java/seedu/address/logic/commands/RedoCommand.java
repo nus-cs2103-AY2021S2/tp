6c50881f5978b4abb2f63dda54aba3a92422b1a6
Samuel Fang <samuelfangjw@gmail.com>
2021-04-01 02:00:06 +0800

    Update RedoCommand.java

--- src/main/java/seedu/address/logic/commands/RedoCommand.java
+++ src/main/java/seedu/address/logic/commands/RedoCommand.java
@@ -14,7 +14,7 @@ import seedu.address.model.colabfolderhistory.exceptions.NoRedoableStateExceptio
 public class RedoCommand extends Command {
 
     public static final String COMMAND_WORD = "redo";
-    public static final String MESSAGE_SUCCESS = "Redo success!\n%s";
+    public static final String MESSAGE_SUCCESS = "Redo successful!\n%s";
     public static final String MESSAGE_FAILURE = "No more commands to redo.";
 
     @Override
