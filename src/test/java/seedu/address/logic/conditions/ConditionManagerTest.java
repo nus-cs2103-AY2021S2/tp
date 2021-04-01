8c700031f0e933d9aee06245d9c66dd6b2119c7a
maxxng <maxngkaishen@gmail.com>
2021-04-01 17:59:16 +0800

    add ConditionManagerTest

---
+++ src/test/java/seedu/address/logic/conditions/ConditionManagerTest.java
@@ -0,0 +1,31 @@
+package seedu.address.logic.conditions;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.task.Task;
+import seedu.address.testutil.TaskBuilder;
+
+import static seedu.address.logic.commands.CommandTestUtil.VALID_DATE_AMY;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_DURATION_AMY;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_RECURRINGSCHEDULE_AMY;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_TITLE_AMY;
+import static seedu.address.testutil.Assert.assertThrows;
+
+public class ConditionManagerTest {
+
+    @Test
+    public void check_durationNoDateOrRecurringSchedule_throwsCommandException() {
+        Task task = new TaskBuilder().withTitle(VALID_TITLE_AMY).withDuration(VALID_DURATION_AMY).build();
+
+        assertThrows(CommandException.class, () -> ConditionManager.enforceAttributeConstraints(task));
+    }
+
+    @Test
+    public void check_dateAndRecurringSchedule_throwsCommandException() {
+        Task task = new TaskBuilder().withTitle(VALID_TITLE_AMY).withDate(VALID_DATE_AMY)
+                .withRecurringSchedule(VALID_RECURRINGSCHEDULE_AMY).build();
+
+        assertThrows(CommandException.class, () -> ConditionManager.enforceAttributeConstraints(task));
+    }
+}
