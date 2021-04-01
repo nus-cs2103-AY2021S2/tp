5b32e1da2d035fc8626e311b4c81d63d33e53ebc
LimJunxue <junxue1@hotmail.com>
2021-04-01 22:22:13 +0800

    Fix checkstyle

--- src/test/java/seedu/address/logic/commands/ViewDayCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ViewDayCommandTest.java
@@ -2,7 +2,6 @@ package seedu.address.logic.commands;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
-import static seedu.address.testutil.TypicalTasks.DANIEL;
 import static seedu.address.testutil.TypicalTasks.GEORGE;
 import static seedu.address.testutil.TypicalTasks.getTypicalPlanner;
 
