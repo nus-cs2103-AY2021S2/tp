ca2621e6755d537343e117ffd2597eba6211b0ac
nicholasnge <59093518+nicholasnge@users.noreply.github.com>
2021-04-01 13:53:08 +0800

    changes to ModelStub since new methods were added to Model interface

--- src/test/java/seedu/address/logic/commands/AddCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddCommandTest.java
@@ -9,6 +9,7 @@ import static seedu.address.testutil.Assert.assertThrows;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.function.Predicate;
 
 import org.junit.jupiter.api.Test;
@@ -155,6 +156,21 @@ public class AddCommandTest {
 
         @Override
         public void updateFilteredPersonList(Predicate<Patient> predicate) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void sortFilteredPersonList(Comparator<Patient> comparator) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void selectPatient(Patient patient) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public Patient getSelectedPatient() {
             throw new AssertionError("This method should not be called.");
         }
     }
