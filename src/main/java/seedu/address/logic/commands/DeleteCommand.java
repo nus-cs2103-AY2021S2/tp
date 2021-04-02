db4a5edda843ee7da5431d2c796c237e5b0ee927
chesterhow <chesterhow@gmail.com>
2021-04-03 01:02:48 +0800

    Fix bug where PersonDetailsTab still shows person after deletion

--- src/main/java/seedu/address/logic/commands/DeleteCommand.java
+++ src/main/java/seedu/address/logic/commands/DeleteCommand.java
@@ -4,11 +4,13 @@ import static java.util.Objects.requireNonNull;
 
 import java.util.List;
 
+import seedu.address.commons.core.DetailsPanelTab;
 import seedu.address.commons.core.Messages;
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.person.Person;
+import seedu.address.ui.UpcomingEventsTab;
 
 /**
  * Deletes a person identified using it's displayed index from the address book.
@@ -41,6 +43,13 @@ public class DeleteCommand extends Command {
 
         Person personToDelete = lastShownList.get(targetIndex.getZeroBased());
         model.deletePerson(personToDelete);
+
+        // TODO: check current tab and only switch if user is on PersonDetailsTab
+        if (model.getDetailedPerson().contains(personToDelete)) {
+            return new CommandResult(String.format(MESSAGE_DELETE_PERSON_SUCCESS, personToDelete),
+                    DetailsPanelTab.UPCOMING_EVENTS);
+        }
+
         return new CommandResult(String.format(MESSAGE_DELETE_PERSON_SUCCESS, personToDelete));
     }
 
