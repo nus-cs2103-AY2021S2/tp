b1e001e9fa0ea0e9f92892ec15f28e150c531429
rajobasu <rajobasu@gmail.com>
2021-04-02 20:46:43 +0800

    Change DOB filter strategy

--- src/main/java/seedu/address/logic/filters/DobFilter.java
+++ src/main/java/seedu/address/logic/filters/DobFilter.java
@@ -3,25 +3,17 @@ package seedu.address.logic.filters;
 import seedu.address.model.customer.Customer;
 
 public class DobFilter extends AbstractFilter {
-    private final String[] args;
-
     /**
      * Constructor for DoBFilter, creates a list of keywords in the date, to later match with the customer's DOB.
      * @param filterString - the birthdate to match. No particular format needed.
      */
     public DobFilter(String filterString) {
         super(filterString.trim());
-        args = filterString.split(" ");
     }
 
     @Override
     public boolean test(Customer customer) {
         String dob = customer.getDateOfBirth().getBirthDate();
-        for (var e : args) {
-            if (dob.contains(e)) {
-                return true;
-            }
-        }
-        return false;
+        return dob.contains(filterString.trim());
     }
 }
