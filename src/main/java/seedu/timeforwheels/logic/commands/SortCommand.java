78f306c993c99d137a9ab19f5a78284b55fc5765
cheunggalen <galencheung97@gmail.com>
2021-04-03 10:57:14 +0800

    Edit checkstyle

--- src/main/java/seedu/timeforwheels/logic/commands/SortCommand.java
+++ src/main/java/seedu/timeforwheels/logic/commands/SortCommand.java
@@ -2,9 +2,9 @@ package seedu.timeforwheels.logic.commands;
 
 import static java.util.Objects.requireNonNull;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
-import java.time.LocalDate;
 import java.util.Set;
 
 import seedu.timeforwheels.model.DeliveryList;
