d5140aaf855fb232724157e70deeb2f7b3907434
Jellybeano <lanyuxuan98@gmail.com>
2021-04-05 00:29:59 +0800

    Fix #159

--- src/main/java/seedu/weeblingo/model/tag/Tag.java
+++ src/main/java/seedu/weeblingo/model/tag/Tag.java
@@ -11,7 +11,7 @@ import seedu.weeblingo.commons.util.RegexUtil;
  */
 public class Tag {
 
-    public static final String MESSAGE_CONSTRAINTS = "Tag names should be alphanumeric";
+    public static final String MESSAGE_CONSTRAINTS = "Tag names should be alphanumeric and not contain spaces.";
     public static final String VALIDATION_REGEX = RegexUtil.REGEX_AB3_TAG;
 
     public final String tagName;
