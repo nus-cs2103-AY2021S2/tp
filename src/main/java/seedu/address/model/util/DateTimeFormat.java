e7590d4740328a016bafe78136513ab7ba17ab82
w2vgd <junwei.green@gmail.com>
2021-04-05 01:30:45 +0800

    Enforce strict parsing of user input dates and times
    * For dates, it must be in the form DD-MM-YYYY
    * For times, it must be in 24-hour
    * Removed 2400 being a valid time input

--- src/main/java/seedu/address/model/util/DateTimeFormat.java
+++ src/main/java/seedu/address/model/util/DateTimeFormat.java
@@ -11,17 +11,18 @@ public class DateTimeFormat {
      * User input date format, e.g. 23-1-2021.
      */
     public static final DateTimeFormatter INPUT_DATE_FORMAT =
-            DateTimeFormatter.ofPattern("d-M-u").withResolverStyle(ResolverStyle.STRICT);
+            DateTimeFormatter.ofPattern("dd-MM-uuuu").withResolverStyle(ResolverStyle.STRICT);
 
     /**
      * User input time format in 24-hour clock, e.g. 1800.
      */
-    public static final DateTimeFormatter INPUT_TIME_FORMAT = DateTimeFormatter.ofPattern("HHmm");
+    public static final DateTimeFormatter INPUT_TIME_FORMAT =
+            DateTimeFormatter.ofPattern("HHmm").withResolverStyle(ResolverStyle.STRICT);
 
     /**
      * Output date format, e.g. Jan 23, 2021.
      */
-    public static final DateTimeFormatter OUTPUT_DATE_FORMAT = DateTimeFormatter.ofPattern("MMM d, u");
+    public static final DateTimeFormatter OUTPUT_DATE_FORMAT = DateTimeFormatter.ofPattern("MMM dd, uuuu");
 
     /**
      * Output time format, e.g. 7:33PM.
