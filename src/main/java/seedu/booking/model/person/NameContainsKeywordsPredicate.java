d87f2fa9acf57da6b8359c8705a231f93ba9db1b
vvan-essa <e0425352@u.nus.edu>
2021-04-04 20:27:03 +0800

    Fix failing tests

--- src/main/java/seedu/booking/model/person/NameContainsKeywordsPredicate.java
+++ src/main/java/seedu/booking/model/person/NameContainsKeywordsPredicate.java
@@ -19,6 +19,9 @@ public class NameContainsKeywordsPredicate implements Predicate<Person> {
 
     @Override
     public boolean test(Person person) {
+        if (keywords.isEmpty()) {
+            return false;
+        }
         return keywords.stream()
                 .allMatch(keyword -> StringUtil.containsWordIgnoreCase(person.getName().fullName, keyword));
     }
