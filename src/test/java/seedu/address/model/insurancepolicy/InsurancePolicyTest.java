b9807e948fb6c7b802f04cd7de4cd642105dfe97
jlxw48 <jlxw48@gmail.com>
2021-04-06 01:11:54 +0800

    Add tests for Insurance Policy for URL parsing

--- src/test/java/seedu/address/model/insurancepolicy/InsurancePolicyTest.java
+++ src/test/java/seedu/address/model/insurancepolicy/InsurancePolicyTest.java
@@ -11,23 +11,33 @@ import org.junit.jupiter.api.Test;
 public class InsurancePolicyTest {
     private static final String POLICY_ID_CONTAINS_ANGULAR_BRACKET = "Policy>1234>www.google.com";
     private static final String CORRECT_POLICY_WITH_URL = "Policy1234>www.google.com";
-    private static final String CORRECT_POLICY = "Policy1234";
+    private static final String CORRECT_POLICY_NO_URL = "Policy1234";
     private static final String DEFAULT_POLICY_ID = "P#123";
     private static final String DEFAULT_POLICY_URL = "www.aviva.com";
+    private static final String COMPLEX_POLICY_URL_WITH_URL_PARAMS = "https://www.google.com/page/?q=query&b=bye";
+    private static final String INVALID_URL = ")(%*!#)($@";
 
     @Test
-    public void execute_checkPolicyIdWithCarrotValidity_false() {
+    public void isValidPolicyId() {
+        // valid input of Policy ID without URL
+        assertTrue(InsurancePolicy.isValidPolicyId(CORRECT_POLICY_NO_URL));
+
+        // valid input of Policy ID with URL
+        assertTrue(InsurancePolicy.isValidPolicyId(CORRECT_POLICY_WITH_URL));
+
+        // invalid input of Policy ID with angular bracket in the ID
         assertFalse(InsurancePolicy.isValidPolicyId(POLICY_ID_CONTAINS_ANGULAR_BRACKET));
     }
 
-    @Test
-    public void execute_checkCorrectPolicyValid_true() {
-        assertTrue(InsurancePolicy.isValidPolicyId(CORRECT_POLICY));
-    }
+    public void isValidPolicyUrl() {
+        // valid input of policy URL
+        assertTrue(InsurancePolicy.isValidPolicyId(CORRECT_POLICY_NO_URL));
 
-    @Test
-    public void execute_checkCorrectPolicyWithUrl_true() {
-        assertTrue(InsurancePolicy.isValidPolicyId(CORRECT_POLICY_WITH_URL));
+        // valid input if a complex policy URL
+        assertTrue(InsurancePolicy.isValidPolicyId(COMPLEX_POLICY_URL_WITH_URL_PARAMS));
+
+        // invalid input of policy URL
+        assertFalse(InsurancePolicy.isValidPolicyId(INVALID_URL));
     }
 
     @Test
