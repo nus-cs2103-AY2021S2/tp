f07c9ae60de3f2c37e8a60bc0bf9c2b5a60060d9
Nicholas <me@cheeze2000.net>
2021-04-06 06:39:32 +0800

    Refactor Contact classes

--- src/main/java/seedu/address/model/contact/ContactEmail.java
+++ src/main/java/seedu/address/model/contact/ContactEmail.java
@@ -4,44 +4,50 @@ import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.util.AppUtil.checkArgument;
 
 /**
- * Represents a Contact's email in the address book.
+ * Represents a Contact's email in Teaching Assistant.
  * Guarantees: immutable; is valid as declared in {@link #isValidEmail(String)}
  */
-public class Email {
+public class ContactEmail {
 
-    private static final String SPECIAL_CHARACTERS = "!#$%&'*+/=?`{|}~^.-";
+    private static final String PRINTABLE_CHARACTERS = "!#$%&'*+-/=?^_`{|}~";
     public static final String MESSAGE_CONSTRAINTS = "Emails should be of the format local-part@domain "
             + "and adhere to the following constraints:\n"
             + "1. The local-part should only contain alphanumeric characters and these special characters, excluding "
-            + "the parentheses, (" + SPECIAL_CHARACTERS + ") .\n"
-            + "2. This is followed by a '@' and then a domain name. "
+            + "the parentheses, (" + PRINTABLE_CHARACTERS + ").\n"
+            + "2. The local-part may consist of dots (.) provided that it is not the first character.\n"
+            + "3. This is followed by a '@' and then a domain name. "
             + "The domain name must:\n"
             + "    - be at least 2 characters long\n"
             + "    - start and end with alphanumeric characters\n"
             + "    - consist of alphanumeric characters, a period or a hyphen for the characters in between, if any.";
-    // alphanumeric and special characters
-    private static final String LOCAL_PART_REGEX = "^[\\w" + SPECIAL_CHARACTERS + "]+";
-    private static final String DOMAIN_FIRST_CHARACTER_REGEX = "[^\\W_]"; // alphanumeric characters except underscore
-    private static final String DOMAIN_MIDDLE_REGEX = "[a-zA-Z0-9.-]*"; // alphanumeric, period and hyphen
-    private static final String DOMAIN_LAST_CHARACTER_REGEX = "[^\\W_]$";
+
+    // local-part validation
+    private static final String PARTIAL_LOCAL_PART_REGEX = "\\w" + PRINTABLE_CHARACTERS;
+    private static final String LOCAL_PART_REGEX = "^[" + PARTIAL_LOCAL_PART_REGEX + "]["
+            + PARTIAL_LOCAL_PART_REGEX + ".]*";
+
+    // domain validation
+    private static final String DOMAIN_FIRST_CHARACTER_REGEX = "\\w"; // alphanumeric characters except underscore
+    private static final String DOMAIN_MIDDLE_REGEX = "[\\w.-]*"; // alphanumeric, period and hyphen
+    private static final String DOMAIN_LAST_CHARACTER_REGEX = "\\w$";
     public static final String VALIDATION_REGEX = LOCAL_PART_REGEX + "@"
             + DOMAIN_FIRST_CHARACTER_REGEX + DOMAIN_MIDDLE_REGEX + DOMAIN_LAST_CHARACTER_REGEX;
 
     public final String value;
 
     /**
-     * Constructs an {@code Email}.
+     * Constructs a {@code ContactEmail}.
      *
      * @param email A valid email address.
      */
-    public Email(String email) {
+    public ContactEmail(String email) {
         requireNonNull(email);
         checkArgument(isValidEmail(email), MESSAGE_CONSTRAINTS);
         value = email;
     }
 
     /**
-     * Returns if a given string is a valid email.
+     * Returns true if a given string is a valid email.
      */
     public static boolean isValidEmail(String test) {
         return test.matches(VALIDATION_REGEX);
@@ -55,8 +61,8 @@ public class Email {
     @Override
     public boolean equals(Object other) {
         return other == this // short circuit if same object
-                || (other instanceof Email // instanceof handles nulls
-                && value.equals(((Email) other).value)); // state check
+                || (other instanceof ContactEmail // instanceof handles nulls
+                && value.equalsIgnoreCase(((ContactEmail) other).value)); // state check
     }
 
     @Override
--- src/main/java/seedu/address/model/contact/ContactPhone.java
+++ src/main/java/seedu/address/model/contact/ContactPhone.java
@@ -4,10 +4,10 @@ import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.util.AppUtil.checkArgument;
 
 /**
- * Represents a Contact's phone number in the address book.
+ * Represents a Contact's phone number in Teaching Assistant.
  * Guarantees: immutable; is valid as declared in {@link #isValidPhone(String)}
  */
-public class Phone {
+public class ContactPhone {
 
 
     public static final String MESSAGE_CONSTRAINTS =
@@ -16,11 +16,11 @@ public class Phone {
     public final String value;
 
     /**
-     * Constructs a {@code Phone}.
+     * Constructs a {@code ContactPhone}.
      *
      * @param phone A valid phone number.
      */
-    public Phone(String phone) {
+    public ContactPhone(String phone) {
         requireNonNull(phone);
         checkArgument(isValidPhone(phone), MESSAGE_CONSTRAINTS);
         value = phone;
@@ -41,8 +41,8 @@ public class Phone {
     @Override
     public boolean equals(Object other) {
         return other == this // short circuit if same object
-                || (other instanceof Phone // instanceof handles nulls
-                && value.equals(((Phone) other).value)); // state check
+                || (other instanceof ContactPhone // instanceof handles nulls
+                && value.equals(((ContactPhone) other).value)); // state check
     }
 
     @Override
