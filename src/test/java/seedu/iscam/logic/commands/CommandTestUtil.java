0380a1f7136dd0721565af71ad17e0b2ce8d045c
frisciliasultan <frisciliasultan@gmail.com>
2021-04-05 19:40:52 +0800

    Add valid and invalid fields for Meeting in CommandTestUtil

--- src/test/java/seedu/iscam/logic/commands/CommandTestUtil.java
+++ src/test/java/seedu/iscam/logic/commands/CommandTestUtil.java
@@ -2,12 +2,16 @@ package seedu.iscam.logic.commands;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.iscam.logic.parser.CliSyntax.PREFIX_CLIENT;
+import static seedu.iscam.logic.parser.CliSyntax.PREFIX_DESCRIPTION;
 import static seedu.iscam.logic.parser.CliSyntax.PREFIX_EMAIL;
 import static seedu.iscam.logic.parser.CliSyntax.PREFIX_IMAGE;
 import static seedu.iscam.logic.parser.CliSyntax.PREFIX_LOCATION;
 import static seedu.iscam.logic.parser.CliSyntax.PREFIX_NAME;
+import static seedu.iscam.logic.parser.CliSyntax.PREFIX_ON;
 import static seedu.iscam.logic.parser.CliSyntax.PREFIX_PHONE;
 import static seedu.iscam.logic.parser.CliSyntax.PREFIX_PLAN;
+import static seedu.iscam.logic.parser.CliSyntax.PREFIX_STATUS;
 import static seedu.iscam.logic.parser.CliSyntax.PREFIX_TAG;
 import static seedu.iscam.testutil.Assert.assertThrows;
 
@@ -20,7 +24,6 @@ import seedu.iscam.logic.commands.exceptions.CommandException;
 import seedu.iscam.model.Model;
 import seedu.iscam.model.client.Client;
 import seedu.iscam.model.commons.NameContainsKeywordsPredicate;
-import seedu.iscam.model.meeting.DateTime;
 import seedu.iscam.model.meeting.Meeting;
 import seedu.iscam.model.meeting.MeetingContainsKeywordsPredicate;
 import seedu.iscam.model.util.clientbook.ClientBook;
@@ -31,6 +34,7 @@ import seedu.iscam.testutil.EditClientDescriptorBuilder;
  */
 public class CommandTestUtil {
 
+    // Valid values for Client Amy and Client Bob.
     public static final String VALID_NAME_AMY = "Amy Bee";
     public static final String VALID_NAME_BOB = "Bob Choo";
     public static final String VALID_PHONE_AMY = "11111111";
@@ -45,6 +49,7 @@ public class CommandTestUtil {
     public static final String VALID_TAG_FRIEND = "friend";
     public static final String VALID_IMAGE = "default.png";
 
+    // String description of valid values for Client Amy and Client Bob.
     public static final String NAME_DESC_AMY = " " + PREFIX_NAME + VALID_NAME_AMY;
     public static final String NAME_DESC_BOB = " " + PREFIX_NAME + VALID_NAME_BOB;
     public static final String PHONE_DESC_AMY = " " + PREFIX_PHONE + VALID_PHONE_AMY;
@@ -59,6 +64,7 @@ public class CommandTestUtil {
     public static final String TAG_DESC_HUSBAND = " " + PREFIX_TAG + VALID_TAG_HUSBAND;
     public static final String IMAGE_DESC = " " + PREFIX_IMAGE + VALID_IMAGE;
 
+    // Invalid values for Client
     public static final String INVALID_NAME_DESC = " " + PREFIX_NAME + "James&"; // '&' not allowed in names
     public static final String INVALID_PHONE_DESC = " " + PREFIX_PHONE + "911a"; // 'a' not allowed in phones
     public static final String INVALID_EMAIL_DESC = " " + PREFIX_EMAIL + "bob!yahoo"; // missing '@' symbol
@@ -66,6 +72,41 @@ public class CommandTestUtil {
     public static final String INVALID_PLAN_DESC = " " + PREFIX_PLAN + "Plan $"; // '$" no allowed in insurance plans
     public static final String INVALID_TAG_DESC = " " + PREFIX_TAG + "hubby*"; // '*' not allowed in tags
     public static final String INVALID_IMAGE_DESC = " " + PREFIX_IMAGE + "morgan.jp"; // File extension not allowed
+
+
+    // Valid values for Meeting Cleo and Meeting Dan
+    public static final String VALID_CLIENT_NAME_CLEO = "Cleo Patra";
+    public static final String VALID_CLIENT_NAME_DAN = "Dan Ikris";
+    public static final String VALID_DATETIME_CLEO = "28-10-2099 10:00";
+    public static final String VALID_DATETIME_DAN = "29-2-2096 13:30";
+    public static final String VALID_LOCATION_CLEO = "Starbucks, Tampines Hub";
+    public static final String VALID_LOCATION_DAN = "Hon Sui Sen Memorial Library";
+    public static final String VALID_DESCRIPTION_CLEO = "Discuss the upgrading of Insurance Plan";
+    public static final String VALID_DESCRIPTION_DAN = "Discuss Insurance claim procedure";
+    public static final String VALID_TAG_PREMIUM = "premium";
+    public static final String VALID_TAG_URGENT = "urgent";
+    public static final String VALID_STATUS_CLEO = "complete";
+    public static final String VALID_STATUS_DAN = "incomplete";
+
+    // String description of valid values for Meeting Cleo and Dan
+    public static final String CLIENT_NAME_DESC_CLEO = " " + PREFIX_CLIENT + VALID_CLIENT_NAME_CLEO;
+    public static final String CLIENT_NAME_DESC_DAN = " " + PREFIX_CLIENT + VALID_CLIENT_NAME_DAN;
+    public static final String DATETIME_DESC_CLEO = " " + PREFIX_ON + VALID_DATETIME_CLEO;
+    public static final String DATETIME_DESC_DAN = " " + PREFIX_ON + VALID_DATETIME_DAN;
+    public static final String LOCATION_DESC_CLEO = " " + PREFIX_LOCATION + VALID_LOCATION_CLEO;
+    public static final String LOCATION_DESC_DAN = " " + PREFIX_LOCATION + VALID_LOCATION_DAN;
+    public static final String DESCRIPTION_DESC_CLEO = " " + PREFIX_DESCRIPTION + VALID_DESCRIPTION_CLEO;
+    public static final String DESCRIPTION_DESC_DAN = " " + PREFIX_DESCRIPTION + VALID_DESCRIPTION_DAN;
+    public static final String TAG_DESC_PREMIUM = " " + PREFIX_TAG + VALID_TAG_PREMIUM;
+    public static final String TAG_DESC_URGENT = " " + PREFIX_TAG + VALID_TAG_URGENT;
+    public static final String STATUS_DESC_CLEO = " " + PREFIX_STATUS + VALID_STATUS_CLEO;
+    public static final String STATUS_DESC_DAN = " " + PREFIX_STATUS + VALID_STATUS_DAN;
+
+    // Invalid values for Meeting
+    public static final String INVALID_DATETIME_DESC = " " + PREFIX_ON + "282099 10:00";
+    public static final String INVALID_DESCRIPTION_DESC = " " + PREFIX_DESCRIPTION
+            + "Describe @$(*!)@#$";
+    public static final String INVALID_STATUS_DESC = " " + PREFIX_STATUS + "done";
 
     public static final String PREAMBLE_WHITESPACE = "\t  \r  \n";
     public static final String PREAMBLE_NON_EMPTY = "NonEmptyPreamble";
