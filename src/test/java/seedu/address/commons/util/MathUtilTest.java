6ddbd83936ab38de09f16f23db8b1c36039e6f4a
Benedict Khoo <benedictkhoo.mw@u.nus.edu>
2021-04-06 17:35:17 +0800

    Test(MathUtil): Update test comments

--- src/test/java/seedu/address/commons/util/MathUtilTest.java
+++ src/test/java/seedu/address/commons/util/MathUtilTest.java
@@ -15,23 +15,23 @@ public class MathUtilTest {
         final int lower = -9812;
         final int value = 123;
 
-        // EP: Bounds and value equal -> returns value
-        assertEquals(upper, MathUtil.clamp(upper, upper, upper));
-
-        // EP: Value equals upper bound only -> returns value
-        assertEquals(upper, MathUtil.clamp(upper, lower, upper));
-
-        // EP: Value equals lower bound only -> returns value
-        assertEquals(lower, MathUtil.clamp(lower, lower, upper));
-
-        // EP: Value below lower bound -> returns lower bound
+        // EP: Value strictly below lower bound -> returns lower bound
         assertEquals(lower, MathUtil.clamp(lower - 213, lower, upper));
 
-        // EP: Value above upper bound -> returns upper bound
+        // EP: Value strictly above upper bound -> returns upper bound
         assertEquals(upper, MathUtil.clamp(upper + 91, lower, upper));
 
         // EP: Value strictly between bounds -> returns value
         assertEquals(value, MathUtil.clamp(value, lower, upper));
+
+        // BVA: Bounds and value equal -> returns value
+        assertEquals(upper, MathUtil.clamp(upper, upper, upper));
+
+        // BVA: Value equals upper bound only -> returns value
+        assertEquals(upper, MathUtil.clamp(upper, lower, upper));
+
+        // BVA: Value equals lower bound only -> returns value
+        assertEquals(lower, MathUtil.clamp(lower, lower, upper));
     }
 
     @Test
