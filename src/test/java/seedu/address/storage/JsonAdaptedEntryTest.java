0c614c919110cfdb4a3825aad12c05352076b69e
lrj689 <lrj689@hotmail.com>
2021-04-06 22:51:14 +0800

    Add JsonAdaptedEntryTest

---
+++ src/test/java/seedu/address/storage/JsonAdaptedEntryTest.java
@@ -0,0 +1,7 @@
+package seedu.address.storage;
+
+public class JsonAdaptedEntryTest {
+
+    public static final String INVALID_ENTRY_NAME = "consultation!!!!!!!!";
+    public static final String INVALID_START_DATE = "2021-02-31 18:00"
+}
