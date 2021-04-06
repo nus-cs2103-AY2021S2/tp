04c6dd26ad7b7ca5f60cdc76b93ce3f1dd45b6f0
vvan-essa <e0425352@u.nus.edu>
2021-04-06 10:40:19 +0800

    Add tests

--- src/test/java/seedu/booking/model/venue/CapacityMatchesKeywordPredicateTest.java
+++ src/test/java/seedu/booking/model/venue/CapacityMatchesKeywordPredicateTest.java
@@ -1,4 +1,50 @@
-import static org.junit.jupiter.api.Assertions.*;
+package seedu.booking.model.venue;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.booking.testutil.VenueBuilder;
+
 class CapacityMatchesKeywordPredicateTest {
-  
+
+    @Test
+    public void equals() {
+        Integer firstPredicateKeyword = 50;
+        Integer secondPredicateKeyword = 60;
+
+        CapacityMatchesKeywordPredicate firstPredicate = new CapacityMatchesKeywordPredicate(firstPredicateKeyword);
+        CapacityMatchesKeywordPredicate secondPredicate = new CapacityMatchesKeywordPredicate(secondPredicateKeyword);
+
+        // same object -> returns true
+        assertTrue(firstPredicate.equals(firstPredicate));
+
+        // same values -> returns true
+        CapacityMatchesKeywordPredicate firstPredicateCopy = new CapacityMatchesKeywordPredicate(firstPredicateKeyword);
+        assertTrue(firstPredicate.equals(firstPredicateCopy));
+
+        // different types -> returns false
+        assertFalse(firstPredicate.equals(1));
+
+        // null -> returns false
+        assertFalse(firstPredicate.equals(null));
+
+        // different venues -> returns false
+        assertFalse(firstPredicate.equals(secondPredicate));
+    }
+
+    @Test
+    public void test_capacityMatchesKeyword_returnsTrue() {
+        // One keyword
+        CapacityMatchesKeywordPredicate predicate = new CapacityMatchesKeywordPredicate(50);
+        assertTrue(predicate.test(new VenueBuilder().withCapacity(50).build()));
+    }
+
+    @Test
+    public void test_capacityDoesNotMatchKeyword_returnsFalse() {
+        // Non-matching keyword
+        CapacityMatchesKeywordPredicate predicate = new CapacityMatchesKeywordPredicate(50);
+        assertFalse(predicate.test(new VenueBuilder().withCapacity(40).build()));
+    }
 }
--- src/test/java/seedu/booking/model/venue/VenueNameContainsKeywordsPredicateTest.java
+++ src/test/java/seedu/booking/model/venue/VenueNameContainsKeywordsPredicateTest.java
@@ -1,4 +1,74 @@
-import static org.junit.jupiter.api.Assertions.*;
+package seedu.booking.model.venue;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.booking.testutil.VenueBuilder;
+
 class VenueNameContainsKeywordsPredicateTest {
-  
+
+    @Test
+    public void equals() {
+        List<String> firstPredicateKeywordList = Collections.singletonList("Victoria");
+        List<String> secondPredicateKeywordList = Arrays.asList("Victoria", "Hall");
+
+        VenueNameContainsKeywordsPredicate firstPredicate =
+                new VenueNameContainsKeywordsPredicate(firstPredicateKeywordList);
+        VenueNameContainsKeywordsPredicate secondPredicate =
+                new VenueNameContainsKeywordsPredicate(secondPredicateKeywordList);
+
+        // same object -> returns true
+        assertTrue(firstPredicate.equals(firstPredicate));
+
+        // same values -> returns true
+        VenueNameContainsKeywordsPredicate firstPredicateCopy =
+                new VenueNameContainsKeywordsPredicate(firstPredicateKeywordList);
+        assertTrue(firstPredicate.equals(firstPredicateCopy));
+
+        // different types -> returns false
+        assertFalse(firstPredicate.equals(1));
+
+        // null -> returns false
+        assertFalse(firstPredicate.equals(null));
+
+        // different venues -> returns false
+        assertFalse(firstPredicate.equals(secondPredicate));
+    }
+
+    @Test
+    public void test_venueNameContainsKeywords_returnsTrue() {
+        // One keyword
+        VenueNameContainsKeywordsPredicate predicate =
+                new VenueNameContainsKeywordsPredicate(Collections.singletonList("Victoria"));
+        assertTrue(predicate.test(new VenueBuilder().withName("Victoria Hall").build()));
+
+        // Multiple keywords
+        predicate = new VenueNameContainsKeywordsPredicate(Arrays.asList("Victoria", "Hall"));
+        assertTrue(predicate.test(new VenueBuilder().withName("Victoria Hall").build()));
+
+        // Only one matching keyword
+        predicate = new VenueNameContainsKeywordsPredicate(Arrays.asList("Hall", "Field"));
+        assertTrue(predicate.test(new VenueBuilder().withName("Victoria Hall").build()));
+
+        // Mixed-case keywords
+        predicate = new VenueNameContainsKeywordsPredicate(Arrays.asList("VicTOria", "hALL"));
+        assertTrue(predicate.test(new VenueBuilder().withName("Victoria Hall").build()));
+    }
+
+    @Test
+    public void test_venueNameDoesNotContainKeywords_returnsFalse() {
+        // Zero keywords
+        VenueNameContainsKeywordsPredicate predicate = new VenueNameContainsKeywordsPredicate(Collections.emptyList());
+        assertFalse(predicate.test(new VenueBuilder().withName("Victoria").build()));
+
+        // Non-matching keyword
+        predicate = new VenueNameContainsKeywordsPredicate(Arrays.asList("Field"));
+        assertFalse(predicate.test(new VenueBuilder().withName("Victoria hall").build()));
+    }
 }
