210945943871155923f43919ec419d28db693103
lue97 <llue97@hotmail.com>
2021-04-06 23:54:26 +0800

    Add magic number checking to images

--- src/main/java/seedu/address/model/person/Picture.java
+++ src/main/java/seedu/address/model/person/Picture.java
@@ -3,6 +3,8 @@ package seedu.address.model.person;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.HashSet;
+import java.util.Set;
 
 import seedu.address.commons.util.FileUtil;
 import seedu.address.logic.parser.ParserUtil;
@@ -11,6 +13,7 @@ import seedu.address.logic.parser.exceptions.ParseException;
 public class Picture {
 
     public static final String[] ALLOWED_FILE_EXTENSIONS = {".png", ".jpeg", ".jpg"};
+
     public static final String ALLOWED_FILE_EXTENSIONS_STRING = String.join(", ", ALLOWED_FILE_EXTENSIONS);
 
     public static final String MESSAGE_CONSTRAINTS = "An image file should exist at specified path. "
@@ -18,6 +21,22 @@ public class Picture {
 
     // 10 MB
     public static final long MAX_FILE_SIZE = 10 * 1024 * 1024;
+
+    public static final Set<Byte[]> IMAGE_MAGIC_NUMBERS = new HashSet<>();
+
+    // Initialize magic numbers of images. null represents any value for 2 bytes.
+    static {
+        // png
+        IMAGE_MAGIC_NUMBERS.add(FileUtil.intArrayToByteArray(new int[] {0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A,
+                0x0A}));
+        // jpg, jpeg
+        IMAGE_MAGIC_NUMBERS.add(FileUtil.intArrayToByteArray(new int[] {0xFF, 0xD8, 0xFF, 0xDB}));
+        IMAGE_MAGIC_NUMBERS.add(FileUtil.intArrayToByteArray(new int[] {0xFF, 0xD8, 0xFF, 0xE0, 0x00, 0x10, 0x4A, 0x46,
+                0x49, 0x46, 0x00, 0x01}));
+        IMAGE_MAGIC_NUMBERS.add(FileUtil.intArrayToByteArray(new int[] {0xFF, 0xD8, 0xFF, 0xEE}));
+        IMAGE_MAGIC_NUMBERS.add(FileUtil.intArrayToByteArray(new int[] {0xFF, 0xD8, 0xFF, 0xE1, -1, -1, 0x45, 0x78,
+                0x69, 0x66, 0x00, 0x00}));
+    }
 
     private final Path filePath;
 
@@ -42,6 +61,14 @@ public class Picture {
 
     public static boolean isValidFilePath(Path path) {
         return FileUtil.isFileExists(path) && FileUtil.hasExtension(path, ALLOWED_FILE_EXTENSIONS);
+    }
+
+    public static boolean isValidImage(Path path) {
+        try {
+            return FileUtil.hasMagicNumber(path, IMAGE_MAGIC_NUMBERS);
+        } catch (IOException ioException) {
+            return false;
+        }
     }
 
     public Path getFilePath() {
