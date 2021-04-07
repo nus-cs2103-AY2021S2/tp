afea3462e22d5f4a567a0c0ddef1cdc5a2c03882
kwmiw <pennypolarbear@gmail.com>
2021-04-07 21:55:19 +0800

    Fix PatientList Stream
    Ensure stream returns true if there are matches for `timeStartList` and `patientList`, and false only if there are no matches

--- src/main/java/seedu/address/model/appointment/AppointmentContainsKeywordsPredicate.java
+++ src/main/java/seedu/address/model/appointment/AppointmentContainsKeywordsPredicate.java
@@ -68,7 +68,7 @@ public class AppointmentContainsKeywordsPredicate implements Predicate<Appointme
 
         return patientList.stream().map(patientNameSplitMapper).anyMatch(isMatchPatientName)
                 || doctorList.stream().anyMatch(isMatchDoctor)
-                || timeStartList.stream().allMatch(isMatchTimeStart)
+                || (!timeStartList.isEmpty() && timeStartList.stream().allMatch(isMatchTimeStart))
                 || tagsList.stream().anyMatch(isMatchTags);
     }
 
