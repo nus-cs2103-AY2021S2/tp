35b1434e470912d777c1ee9ffd0253f828539e94
tsh22 <sihuitoh22@gmail.com>
2021-04-08 00:24:06 +0800

    Modify code to show error message if given start time is after end time

--- src/main/java/seedu/address/logic/parser/FreeCommandParser.java
+++ src/main/java/seedu/address/logic/parser/FreeCommandParser.java
@@ -1,6 +1,7 @@
 package seedu.address.logic.parser;
 
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_DATE_RANGE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_END_DATE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_START_DATE;
 
@@ -32,6 +33,10 @@ public class FreeCommandParser {
 
         EntryDate startDateTime = ParserUtil.parseEntryDate(argMultimap.getValue(PREFIX_START_DATE).get());
         EntryDate endDateTime = ParserUtil.parseEntryDate(argMultimap.getValue(PREFIX_END_DATE).get());
+
+        if (startDateTime.isAfter(endDateTime)) {
+            throw new ParseException(MESSAGE_INVALID_DATE_RANGE);
+        }
 
         return new FreeCommand(new ListOccupyingEntryPredicate(startDateTime, endDateTime));
     }
