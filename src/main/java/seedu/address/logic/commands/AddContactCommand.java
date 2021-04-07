cb011b0a056318262981ffc8a3ee84db0cb31d21
KevinLohJunYong <e0439232@u.nus.edu>
2021-04-07 10:48:18 +0800

    fixed checkstyle

--- src/main/java/seedu/address/logic/commands/AddContactCommand.java
+++ src/main/java/seedu/address/logic/commands/AddContactCommand.java
@@ -1,8 +1,6 @@
 package seedu.address.logic.commands;
-package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
-
 import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
