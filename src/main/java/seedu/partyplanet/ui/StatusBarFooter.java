f55cd66ffbd1901187f78c0662119e4f372bb822
Ellevy <46534252+Ellevy@users.noreply.github.com>
2021-04-07 17:17:39 +0800

    Added (Data Folder Directory: )

--- src/main/java/seedu/partyplanet/ui/StatusBarFooter.java
+++ src/main/java/seedu/partyplanet/ui/StatusBarFooter.java
@@ -22,7 +22,7 @@ public class StatusBarFooter extends UiPart<Region> {
      */
     public StatusBarFooter(Path saveLocation) {
         super(FXML);
-        saveLocationStatus.setText(Paths.get("").resolve(saveLocation).getParent().toAbsolutePath().toString());
+        saveLocationStatus.setText("Data Folder Directory: " + Paths.get("").resolve(saveLocation).getParent().toAbsolutePath().toString());
     }
 
 }
