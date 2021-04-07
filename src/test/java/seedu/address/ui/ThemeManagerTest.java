612e44e86c9e58609981aad0d20a93fd42a664d4
lue97 <llue97@hotmail.com>
2021-04-08 00:49:10 +0800

    Add @BeforeAll annotation

--- src/test/java/seedu/address/ui/ThemeManagerTest.java
+++ src/test/java/seedu/address/ui/ThemeManagerTest.java
@@ -1,12 +1,20 @@
 package seedu.address.ui;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 public class ThemeManagerTest {
 
-    @Test
-    public void themeManager_init_success() {
+    @BeforeAll
+    public static void initializeThemeManager() {
         ThemeManager.init();
+    }
+
+    @Test
+    public void themeManager_getDefaultTheme() {
+        assertEquals(ThemeFactory.getDefaultTheme(), ThemeManager.getTheme());
     }
 
 }
