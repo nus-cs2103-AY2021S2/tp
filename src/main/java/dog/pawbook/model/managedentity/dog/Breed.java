277e1d6116a097f85ad5c18bab56fe03d3164139
Lam Xuan Yi, Shaelyn <77260792+shaelynl@users.noreply.github.com>
2021-04-07 16:38:30 +0800

    Update Breed.java

--- src/main/java/dog/pawbook/model/managedentity/dog/Breed.java
+++ src/main/java/dog/pawbook/model/managedentity/dog/Breed.java
@@ -11,7 +11,7 @@ public class Breed {
 
     public static final String MESSAGE_CONSTRAINTS = "Breeds should only contain alphabet characters and spaces,"
             + " and it should not be blank.";
-    public static final String VALIDATION_REGEX = "[a-zA-Z][a-zA-Z ]+[a-zA-Z]$";
+    public static final String VALIDATION_REGEX = "[a-zA-Z][a-zA-Z ]+[a-zA-Z]";
 
     public final String value;
 
