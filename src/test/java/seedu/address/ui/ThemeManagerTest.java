1295edd85b31c2be58966040e884264cd05c70a9
lue97 <llue97@hotmail.com>
2021-04-08 00:42:21 +0800

    Add tests for `ThemeCommandParser`

---
+++ src/test/java/seedu/address/ui/ThemeManagerTest.java
@@ -0,0 +1,12 @@
+package seedu.address.ui;
+
+import org.junit.jupiter.api.Test;
+
+public class ThemeManagerTest {
+
+    @Test
+    public void themeManager_init_success() {
+        ThemeManager.init();
+    }
+
+}
