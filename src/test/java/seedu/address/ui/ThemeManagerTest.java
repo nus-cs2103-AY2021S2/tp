febd279eab1926bb00edb8aaa8995df6921a1780
lue97 <llue97@hotmail.com>
2021-04-08 00:55:40 +0800

    Add setTheme test

--- src/test/java/seedu/address/ui/ThemeManagerTest.java
+++ src/test/java/seedu/address/ui/ThemeManagerTest.java
@@ -2,18 +2,13 @@ package seedu.address.ui;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 public class ThemeManagerTest {
 
-    @BeforeAll
-    public static void initializeThemeManager() {
-        ThemeManager.init();
-    }
-
     @Test
-    public void themeManager_getDefaultTheme() {
+    public void themeManager_setTheme() {
+        ThemeManager.setTheme(ThemeFactory.getDefaultTheme(), null);
         assertEquals(ThemeFactory.getDefaultTheme(), ThemeManager.getTheme());
     }
 
