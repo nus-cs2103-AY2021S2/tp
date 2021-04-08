1c97009c5db9a3b23475c85a9bac61cc7ce6bd56
xinweit <tengxinwei98@gmail.com>
2021-04-08 19:07:59 +0800

    Edit import ordering

--- src/test/java/seedu/weeblingo/logic/commands/StartCommandTest.java
+++ src/test/java/seedu/weeblingo/logic/commands/StartCommandTest.java
@@ -1,30 +1,30 @@
 package seedu.weeblingo.logic.commands;
 
-import static seedu.weeblingo.logic.commands.CommandTestUtil.*;
-import static seedu.weeblingo.logic.commands.StartCommand.*;
+import static seedu.weeblingo.logic.commands.CommandTestUtil.VALID_START_INTEGER_GENERIC;
+import static seedu.weeblingo.logic.commands.CommandTestUtil.VALID_START_TAGS_SET_GENERIC;
+import static seedu.weeblingo.logic.commands.CommandTestUtil.assertCommandFailure;
+//import static seedu.weeblingo.logic.commands.StartCommand.MESSAGE_SUCCESS;
+import static seedu.weeblingo.logic.commands.StartCommand.MESSAGE_IN_QUIZ_SESSION;
+import static seedu.weeblingo.logic.commands.StartCommand.MESSAGE_NOT_IN_QUIZ_MODE;
+
+import org.junit.jupiter.api.Test;
 
 import seedu.weeblingo.logic.commands.exceptions.CommandException;
 import seedu.weeblingo.model.Model;
 import seedu.weeblingo.model.ModelManager;
-import seedu.weeblingo.model.tag.Tag;
-
-import org.junit.jupiter.api.Test;
-
-
-import java.util.Set;
 
 public class StartCommandTest {
     private Model model = new ModelManager();
     private Model expectedModel = new ModelManager();
 
-//    @Test
-//    public void execute_startInQuiz_success() throws CommandException {
-//        model.switchModeQuiz();
-//        CommandResult expectedCommandResult = new CommandResult(
-//                MESSAGE_SUCCESS, false, false);
-//        assertCommandSuccess(new StartCommand(VALID_START_INTEGER_GENERIC, VALID_START_TAGS_SET_GENERIC),
-//                model, expectedCommandResult, expectedModel);
-//    }
+    //@Test
+    //public void execute_startInQuiz_success() throws CommandException {
+    //    model.switchModeQuiz();
+    //    CommandResult expectedCommandResult = new CommandResult(
+    //            MESSAGE_SUCCESS, false, false);
+    //    assertCommandSuccess(new StartCommand(VALID_START_INTEGER_GENERIC, VALID_START_TAGS_SET_GENERIC),
+    //            model, expectedCommandResult, expectedModel);
+    //}
 
     @Test
     public void execute_startInMenu_failure() {
@@ -61,13 +61,13 @@ public class StartCommandTest {
                 model, MESSAGE_NOT_IN_QUIZ_MODE);
     }
 
-//    @Test
-//    public void execute_startInQuizSessionEnded_success() throws CommandException {
-//        model.getMode().switchModeQuizSessionEnded();
-//        CommandResult expectedCommandResult = new CommandResult(
-//                MESSAGE_SUCCESS, false, false);
-//        assertCommandSuccess(new StartCommand(VALID_START_INTEGER_GENERIC, VALID_START_TAGS_SET_GENERIC),
-//                model, expectedCommandResult, expectedModel);
-//    }
+    //@Test
+    //public void execute_startInQuizSessionEnded_success() throws CommandException {
+    //    model.getMode().switchModeQuizSessionEnded();
+    //    CommandResult expectedCommandResult = new CommandResult(
+    //            MESSAGE_SUCCESS, false, false);
+    //    assertCommandSuccess(new StartCommand(VALID_START_INTEGER_GENERIC, VALID_START_TAGS_SET_GENERIC),
+    //            model, expectedCommandResult, expectedModel);
+    //}
 
 }
