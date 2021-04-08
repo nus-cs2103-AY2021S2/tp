7bd0f219153f761e4d4d40a54fd72df7545dcebd
ALZAHRANI-PC\riyad <riyadh-h@outlook.sa>
2021-04-08 21:33:26 +0800

    Fix code styling issues

--- src/main/java/seedu/dictionote/logic/commands/EditContactCommand.java
+++ src/main/java/seedu/dictionote/logic/commands/EditContactCommand.java
@@ -13,7 +13,6 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
-import java.util.function.Predicate;
 
 import seedu.dictionote.commons.core.Messages;
 import seedu.dictionote.commons.core.index.Index;
