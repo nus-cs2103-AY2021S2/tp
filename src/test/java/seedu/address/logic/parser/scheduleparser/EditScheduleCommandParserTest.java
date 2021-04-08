3e80162ef288a29e20ce6d76778d2b7cf54ff91c
Vinleon <kohvinleon@gmail.com>
2021-04-08 14:53:04 +0800

    Add Schedule CommandParser tests

---
+++ src/test/java/seedu/address/logic/parser/scheduleparser/EditScheduleCommandParserTest.java
@@ -0,0 +1,25 @@
+package seedu.address.logic.parser.scheduleparser;
+
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
+
+import org.junit.jupiter.api.Test;
+import seedu.address.logic.commands.schedulecommands.EditScheduleCommand;
+
+public class EditScheduleCommandParserTest {
+
+    private EditScheduleCommandParser parser =
+            new EditScheduleCommandParser();
+
+    @Test
+    public void parse_emptyArg_throwsParseException() {
+        assertParseFailure(parser, "   ", String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                EditScheduleCommand.MESSAGE_USAGE));
+    }
+
+    @Test
+    public void parse_invalidArg_throwsParseException() {
+        assertParseFailure(parser, "2 Chloelim", String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                EditScheduleCommand.MESSAGE_USAGE));
+    }
+}
