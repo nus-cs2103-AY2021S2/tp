d84ed1b68a7ec30d912607f5d526900eb369ab21
yhtMinceraft1010X <tayyihsuen@gmail.com>
2021-04-08 08:14:50 +0800

    Fix checkstyle

--- src/test/java/seedu/module/model/tag/TagTest.java
+++ src/test/java/seedu/module/model/tag/TagTest.java
@@ -2,7 +2,6 @@ package seedu.module.model.tag;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.module.testutil.Assert.assertThrows;
 
