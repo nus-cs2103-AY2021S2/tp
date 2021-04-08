54c9b9fbe002207d53a629c8d7766a85eea5eae6
Nicholas <me@cheeze2000.net>
2021-04-08 20:45:32 +0800

    Appease gradle

--- src/test/java/seedu/address/logic/commands/AddEntryCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddEntryCommandTest.java
@@ -1,10 +1,7 @@
 package seedu.address.logic.commands;
 
-import static java.util.Objects.requireNonNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.testutil.Assert.assertThrows;
 import static seedu.address.testutil.TypicalEntries.CLASS_MEETING;
 import static seedu.address.testutil.TypicalEntries.CONSULTATION;
