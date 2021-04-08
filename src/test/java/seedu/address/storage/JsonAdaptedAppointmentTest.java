7f60e1901f14afd20bfb6fe0ac3b68e36de755eb
candyhy <candiceyhy@gmail.com>
2021-04-09 02:24:15 +0800

    Create JsonAdaptedAppointmentTest

---
+++ src/test/java/seedu/address/storage/JsonAdaptedAppointmentTest.java
@@ -0,0 +1,7 @@
+package seedu.address.storage;
+
+public class JsonAdaptedAppointmentTest {
+    private static final String INVALID_NAME = "R@chel";
+    private static final String INVALID_REMARK = "#";
+    private static final String INVALID_DATE =
+}
