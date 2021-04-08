1b2ef1f1743e7f12949a602183476d5352c595ed
hengyongming <e0425848@u.nus.edu>
2021-04-08 17:26:37 +0800

    Add more description to list command

--- src/main/java/seedu/taskify/commons/core/Messages.java
+++ src/main/java/seedu/taskify/commons/core/Messages.java
@@ -9,4 +9,7 @@ public class Messages {
     public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Invalid command format! \n%1$s";
     public static final String MESSAGE_INVALID_TASK_DISPLAYED_INDEX = "The task index provided is invalid";
     public static final String MESSAGE_TASKS_LISTED_OVERVIEW = "%1$d tasks listed!";
+    public static final String MESSAGE_TASKS_LISTED_EXPIRED = "%1$d expired tasks listed!";
+    public static final String MESSAGE_TASKS_LISTED_COMPLETED = "%1$d completed tasks listed!";
+    public static final String MESSAGE_TASKS_LISTED_UNCOMPLETED = "%1$d uncompleted tasks listed!";
 }
