3631e0af82d60919761a81a94e8573191642f448
umer <umergta@gmail.com>
2021-04-08 20:08:07 +0800

    add recurrence related tasks for Task.java

--- src/test/java/seedu/module/model/task/TaskTest.java
+++ src/test/java/seedu/module/model/task/TaskTest.java
@@ -1,5 +1,6 @@
 package seedu.module.model.task;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.module.logic.commands.CommandTestUtil.VALID_DEADLINE_LAB;
@@ -7,17 +8,22 @@ import static seedu.module.logic.commands.CommandTestUtil.VALID_DEADLINE_PRACTIC
 import static seedu.module.logic.commands.CommandTestUtil.VALID_DESCRIPTION_PRACTICAL;
 import static seedu.module.logic.commands.CommandTestUtil.VALID_MODULE_LAB;
 import static seedu.module.logic.commands.CommandTestUtil.VALID_MODULE_PRACTICAL;
+import static seedu.module.logic.commands.CommandTestUtil.VALID_RECURRENCE_LAB;
 import static seedu.module.logic.commands.CommandTestUtil.VALID_START_TIME_PRACTICAL;
 import static seedu.module.logic.commands.CommandTestUtil.VALID_TAG_PRIORITY_HIGH;
 import static seedu.module.logic.commands.CommandTestUtil.VALID_TASK_NAME_LAB;
 import static seedu.module.logic.commands.CommandTestUtil.VALID_TASK_NAME_PRACTICAL;
 import static seedu.module.logic.commands.CommandTestUtil.VALID_WORKLOAD_2;
 import static seedu.module.testutil.Assert.assertThrows;
+import static seedu.module.testutil.TypicalTasks.LAB;
 import static seedu.module.testutil.TypicalTasks.QUIZ;
 import static seedu.module.testutil.TypicalTasks.REVIEW;
 
+import java.time.LocalDateTime;
+
 import org.junit.jupiter.api.Test;
 
+import seedu.module.commons.core.optionalfield.OptionalField;
 import seedu.module.testutil.TaskBuilder;
 import seedu.module.testutil.TypicalTasks;
 
@@ -155,5 +161,75 @@ public class TaskTest {
         // different isDeadline -> returns false
         editedQuiz = new TaskBuilder(QUIZ).deactivateStartTime().build();
         assertFalse(QUIZ.equals(editedQuiz));
+    }
+
+    @Test
+    public void makeNextRecurringTaskTest() {
+        //EP: recurrence is null
+        OptionalField<Recurrence> emptyRecurrence = new OptionalField<>(null);
+        assertTrue(Task.makeNextRecurringTask(LAB, emptyRecurrence).equals(LAB));
+
+        //EP: recurrence is valid
+        OptionalField<Recurrence> validOptionalRecurrence = new OptionalField<>(new Recurrence(VALID_RECURRENCE_LAB));
+        Time oldTime = LAB.getDeadline();
+        Task lab = new TaskBuilder(LAB).withRecurrence(VALID_RECURRENCE_LAB).build();
+        Time newTime = Task.getRecurringTime(lab, oldTime);
+        Task recurredLab = new TaskBuilder(lab).withDeadline(newTime.value).build();
+        // check if recurrence is successfully added and isSameTask property still holds
+        Task newTask = Task.makeNextRecurringTask(lab, validOptionalRecurrence);
+        assertEquals(newTask.getRecurrence(), recurredLab.getRecurrence());
+        assertTrue(newTask.isSameTask(recurredLab));
+
+        //EP: recurrence is invalid
+        String invalidRecurrenceString = "hourly";
+        assertThrows(IllegalArgumentException.class, () -> Task.makeNextRecurringTask(lab,
+                new OptionalField<Recurrence>(new Recurrence(invalidRecurrenceString))));
+
+        //EP: Task is null
+        assertThrows(NullPointerException.class, () -> Task.makeNextRecurringTask(null,
+                validOptionalRecurrence));
+    }
+
+    @Test
+    public void updateRecurrenceTaskTest() {
+        //EP: task is null
+        assertThrows(NullPointerException.class, () -> Task.updateRecurrenceTask(null));
+
+        // Recurring task. deadline updates successfully
+        Task lab = new TaskBuilder(LAB).withRecurrence(VALID_RECURRENCE_LAB).build();
+        Task taskToCheckWith = new TaskBuilder(lab)
+                .withDeadline(Task.getRecurringTime(lab, lab.getDeadline()).value)
+                .build();
+
+        assertTrue(taskToCheckWith.equals(Task.updateRecurrenceTask(lab)));
+
+        //EP: Non-recurring task. assertion error thrown as task needs to be recurring
+        assertThrows(AssertionError.class, () -> LAB.equals(Task.updateRecurrenceTask(LAB)));
+
+    }
+
+    @Test
+    public void getRecurringTimeTest() {
+        //EP: task is null
+        assertThrows(NullPointerException.class, () -> Task.getRecurringTime(null, LAB.getDeadline()));
+
+        //EP: oldTime is null
+        assertThrows(AssertionError.class, () -> Task.getRecurringTime(LAB, null));
+
+        //EP: task is not recurring
+        assertThrows(AssertionError.class, () -> Task.getRecurringTime(LAB, LAB.getDeadline()));
+
+        Task recurringLab = new TaskBuilder(LAB).withRecurrence(VALID_RECURRENCE_LAB).build();
+        //EP: deadline is not expired
+        Time nonExpiredDeadlineTime = Time.makeTimeObject(LocalDateTime.now());
+        Task nonExpiredRecurringLab = new TaskBuilder(recurringLab).withDeadline(nonExpiredDeadlineTime.value).build();
+
+        assertTrue(nonExpiredRecurringLab.getDeadline().equals(nonExpiredDeadlineTime));
+
+        //EP: deadline is expired
+        Time expiredDeadlineTime = Time.makeTimeObject(LocalDateTime.now().minusDays(1));
+        Task expiredRecurringLab = new TaskBuilder(recurringLab).withDeadline(expiredDeadlineTime.value).build();
+
+        assertTrue(Task.getRecurringTime(expiredRecurringLab, expiredDeadlineTime).equals(nonExpiredDeadlineTime));
     }
 }
