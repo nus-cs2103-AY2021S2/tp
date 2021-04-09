2fda18f428c0c3bbdcd76878cc4e7a5195087291
goatygoatygoat <52666150+goatygoatygoat@users.noreply.github.com>
2021-04-10 05:11:49 +0800

    Resolve a merge conflict

--- src/main/java/seedu/address/commons/core/Messages.java
+++ src/main/java/seedu/address/commons/core/Messages.java
@@ -14,6 +14,7 @@ public class Messages {
             + " date time!";
     public static final String MESSAGE_INVALID_DATE_RANGE = "The start date/time must be strictly before"
             + " the end date/time!";
+    public static final String MESSAGE_CONTACTS_LISTED_OVERVIEW = "%1$d contacts listed!";
     public static final String MESSAGE_ENTRIES_LISTED_OVERVIEW = "%1$d entries listed!";
     public static final String MESSAGE_PERSONS_LISTED_OVERVIEW = "%1$d persons listed!";
     public static final String MESSAGE_SCHEDULES_LISTED_OVERVIEW = "%1$d schedules listed!";
