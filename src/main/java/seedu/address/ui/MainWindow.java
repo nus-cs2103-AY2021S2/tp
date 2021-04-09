e4de383d78b70533c6e99f152dda4935ac9580ef
rjeez <77440906+rjeez@users.noreply.github.com>
2021-04-09 22:54:19 +0800

    Fix Checkstyles

--- src/main/java/seedu/address/ui/MainWindow.java
+++ src/main/java/seedu/address/ui/MainWindow.java
@@ -221,7 +221,7 @@ public class MainWindow extends UiPart<Stage> {
             }
 
             if (commandResult.isLight()) {
-                if(currentCss.equals("MainWindowLight.fxml")) {
+                if (currentCss.equals("MainWindowLight.fxml")) {
                     resultDisplay.setFeedbackToUser("Already in light mode");
                 }
                 else {
@@ -231,7 +231,7 @@ public class MainWindow extends UiPart<Stage> {
             }
 
             if (commandResult.isDark()) {
-                if(currentCss.equals("MainWindow.fxml")) {
+                if (currentCss.equals("MainWindow.fxml")) {
                     resultDisplay.setFeedbackToUser("Already in dark mode");
                 }
                 else {
