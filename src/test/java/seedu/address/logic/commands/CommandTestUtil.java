12716dff04dd5819a2cb685b5fa37cb4e2ac2a3c
Assyarul <muhdassyarul@gmail.com>
2021-04-10 00:12:43 +0800

    Minor style errors and typo fixes

--- src/test/java/seedu/address/logic/commands/CommandTestUtil.java
+++ src/test/java/seedu/address/logic/commands/CommandTestUtil.java
@@ -131,6 +131,7 @@ public class CommandTestUtil {
         assertEquals(expectedAddressBook, actualModel.getAddressBook());
         assertEquals(expectedFilteredList, actualModel.getFilteredPersonList());
     }
+
     /**
      * Updates {@code model}'s filtered list to show only the person at the given {@code targetIndex} in the
      * {@code model}'s address book.
@@ -144,8 +145,9 @@ public class CommandTestUtil {
 
         assertEquals(1, model.getFilteredPersonList().size());
     }
+
     /**
-     * Updates Updates {@code model}'s filtered list to show only the persons in the given {@code group} in the
+     * Updates {@code model}'s filtered list to show only the persons in the given {@code group} in the
      * {@code model}'s address book.
      */
     public static void showPersonsWithGroup(Model model, Group group) {
