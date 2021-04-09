f6392f234e57cdd844767aa3c3a7a59e8a3b9ed1
andrea-twl <77443618+andrea-twl@users.noreply.github.com>
2021-04-09 11:56:09 +0800

    checkstyle

--- src/main/java/seedu/address/model/person/level/Level.java
+++ src/main/java/seedu/address/model/person/level/Level.java
@@ -76,16 +76,16 @@ public class Level implements Comparable<Level> {
             return "Secondary 1";
 
         case "sec2":
-             return "Secondary 2";
+            return "Secondary 2";
 
         case "sec3":
-             return "Secondary 3";
+            return "Secondary 3";
 
         case "sec4":
-             return "Secondary 4";
+            return "Secondary 4";
 
         case "sec5":
-             return "Secondary 5";
+            return "Secondary 5";
 
         case "jc1":
             return "Junior College 1";
