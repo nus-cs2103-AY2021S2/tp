3b15689771a661435caabd83d8353ad2f5a6f66d
Jay Chen <jaychenyj@gmail.com>
2021-04-09 22:40:35 +0800

    Rearranged imports

--- src/test/java/seedu/storemando/commons/core/ConfigTest.java
+++ src/test/java/seedu/storemando/commons/core/ConfigTest.java
@@ -1,9 +1,9 @@
 package seedu.storemando.commons.core;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 
 import org.junit.jupiter.api.Test;
 
