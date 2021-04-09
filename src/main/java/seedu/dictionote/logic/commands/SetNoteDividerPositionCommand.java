ac9722b8b6ea54ddfc56c131466e39782c6c8981
TeoHoeKeat <TeoHoeKeat@gmail.com>
2021-04-09 20:19:02 +0800

    Change error format for setdivider

--- src/main/java/seedu/dictionote/logic/commands/SetNoteDividerPositionCommand.java
+++ src/main/java/seedu/dictionote/logic/commands/SetNoteDividerPositionCommand.java
@@ -1,10 +1,12 @@
 package seedu.dictionote.logic.commands;
 
 import static java.util.Objects.requireNonNull;
+import static seedu.dictionote.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 
 import seedu.dictionote.logic.commands.enums.UiAction;
 import seedu.dictionote.logic.commands.enums.UiActionOption;
 import seedu.dictionote.logic.commands.exceptions.CommandException;
+import seedu.dictionote.logic.parser.exceptions.ParseException;
 import seedu.dictionote.model.Model;
 
 /**
@@ -17,7 +19,7 @@ public class SetNoteDividerPositionCommand extends SetDividerPositionCommand {
     public static final String MESSAGE_USAGE = COMMAND_WORD
             + ": Set the position of note divider.\n"
             + "Parameters: Position (must be a integer between 1 to 9 inclusively)\n"
-            + "Example: " + COMMAND_WORD + " 1";
+            + "Example: " + COMMAND_WORD + " 3";
 
     public static final String MESSAGE_SET_DIVIDER_SUCCESS = "Note Divider Position Set to ";
 
@@ -31,6 +33,11 @@ public class SetNoteDividerPositionCommand extends SetDividerPositionCommand {
     @Override
     public CommandResult execute(Model model) throws CommandException {
         requireNonNull(model);
+
+        if (position > 9 || position < 1) {
+            throw new CommandException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, MESSAGE_USAGE));
+        }
+
         model.getGuiSettings().setNoteSplitRatio(position / NORMALIZE_RATIO);
         return new CommandResult(String.format(MESSAGE_SET_DIVIDER_SUCCESS + position),
                 UiAction.OPEN, UiActionOption.NOTE);
