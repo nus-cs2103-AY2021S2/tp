9fe70b737da584280d949096ae9438726e3c2cec
SoulUseless <jkyap99@gmail.com>
2021-04-09 11:16:40 +0800

    Standardise error message for PinTask, UnpinTask

--- src/main/java/seedu/address/logic/parser/PinTaskCommandParser.java
+++ src/main/java/seedu/address/logic/parser/PinTaskCommandParser.java
@@ -1,5 +1,7 @@
 package seedu.address.logic.parser;
 
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.PinTaskCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
@@ -16,8 +18,7 @@ public class PinTaskCommandParser implements Parser<PinTaskCommand> {
             return new PinTaskCommand(index);
         } catch (ParseException pe) {
             throw new ParseException(
-                    String.format(pe.getMessage()
-                            + "%1$s", PinTaskCommand.MESSAGE_USAGE), pe);
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, PinTaskCommand.MESSAGE_USAGE), pe);
         }
     }
 }
