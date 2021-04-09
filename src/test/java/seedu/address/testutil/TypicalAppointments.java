0ec6b5d34b658d8bde3cdb1cae43510ae78866bd
clarlzx <clarlzx@gmail.com>
2021-04-10 00:09:30 +0800

    Edit DateTime class to detect more wrong dates

--- src/test/java/seedu/address/testutil/TypicalAppointments.java
+++ src/test/java/seedu/address/testutil/TypicalAppointments.java
@@ -9,13 +9,13 @@ import seedu.address.model.appointment.Appointment;
 
 public class TypicalAppointments {
     public static final Appointment PTM = new AppointmentBuilder().withName("PTM")
-            .withAddress("Child 1's school").withDateTime("09/01/2021 00:00")
+            .withAddress("Child 1's school").withDateTime("09/07/2021 00:00")
             .withContacts("2").build();
     public static final Appointment PLAY_DATE = new AppointmentBuilder().withName("Play date with xyz")
-            .withAddress("xyz's house").withDateTime("30/03/2021 10:00")
+            .withAddress("xyz's house").withDateTime("30/07/2021 10:00")
             .withContacts("1", "2").build();
     public static final Appointment PSG_MEETING = new AppointmentBuilder().withName("PSG meeting")
-            .withAddress("child 3's school").withDateTime("15/04/2021 14:00")
+            .withAddress("child 3's school").withDateTime("15/08/2021 14:00")
             .withContacts("3", "4", "1").build();
     public static final Appointment BALLET_RECITAL = new AppointmentBuilder().withName("ballet recital")
             .withAddress("child 1's school").withDateTime("23/12/2021 18:00").build();
