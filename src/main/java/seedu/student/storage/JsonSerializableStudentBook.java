3f7bef87e7a06d7ef9e17e4e1b46b33e1024ecd1
Si Ting <33442908+SiTingST@users.noreply.github.com>
2021-04-10 01:55:44 +0800

    Update grammar

--- src/main/java/seedu/student/storage/JsonSerializableStudentBook.java
+++ src/main/java/seedu/student/storage/JsonSerializableStudentBook.java
@@ -22,7 +22,7 @@ class JsonSerializableStudentBook {
 
     public static final String MESSAGE_DUPLICATE_STUDENT = "Student list contains duplicate student(s).";
     public static final String MESSAGE_CLASHING_APPOINTMENT = "Appointment list contains clashing appointment(s),";
-    public static final String MESSAGE_DUPLICATE_APPOINTMENT = "Appointment list contains duplicated appointment(s),";
+    public static final String MESSAGE_DUPLICATE_APPOINTMENT = "Appointment list contains duplicate appointment(s),";
     public static final String MESSAGE_MISSING_STUDENT = "The student does not exist in the records.";
     public static final String MESSAGE_INVALID_START_TIME = "Time should be a valid time of the form HH:00 or HH:30";
 
