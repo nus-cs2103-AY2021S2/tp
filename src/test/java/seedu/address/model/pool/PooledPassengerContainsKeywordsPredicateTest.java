bfd93cbde21b4543a016a285063a639f47732e42
Mark Wong <markmcwong@yahoo.com>
2021-04-10 03:14:30 +0800

    Remove magic literals from Predicate test

--- src/test/java/seedu/address/model/pool/PooledPassengerContainsKeywordsPredicateTest.java
+++ src/test/java/seedu/address/model/pool/PooledPassengerContainsKeywordsPredicateTest.java
@@ -2,6 +2,10 @@ package seedu.address.model.pool;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.testutil.TypicalPassengers.ALICE;
+import static seedu.address.testutil.TypicalPassengers.CARL;
+import static seedu.address.testutil.TypicalPassengers.DANIEL;
+import static seedu.address.testutil.TypicalPassengers.GEORGE;
 import static seedu.address.testutil.TypicalPools.OFFICEPOOL;
 import static seedu.address.testutil.TypicalPools.WORKPOOL;
 
@@ -45,15 +49,20 @@ public class PooledPassengerContainsKeywordsPredicateTest {
     public void test_nameContainsKeywords_returnsTrue() {
         // One keyword
         PooledPassengerContainsKeywordsPredicate predicate =
-                new PooledPassengerContainsKeywordsPredicate(Collections.singletonList("Alice"));
+                new PooledPassengerContainsKeywordsPredicate(
+                        Arrays.asList(ALICE.getName().toString().split("\\s")[0]));
         assertTrue(predicate.test(WORKPOOL));
 
         // Multiple keywords
-        predicate = new PooledPassengerContainsKeywordsPredicate(Arrays.asList("Alice", "George"));
+        predicate = new PooledPassengerContainsKeywordsPredicate(Arrays.asList(
+                ALICE.getName().toString().split("\\s")[0],
+                GEORGE.getName().toString().split("\\s")[0]));
         assertTrue(predicate.test(WORKPOOL));
 
         // Only one matching keyword
-        predicate = new PooledPassengerContainsKeywordsPredicate(Arrays.asList("Alice", "Bob"));
+        predicate = new PooledPassengerContainsKeywordsPredicate(Arrays.asList(
+                ALICE.getName().toString().split("\\s")[0],
+                DANIEL.getName().toString().split("\\s")[0]));
         assertTrue(predicate.test(WORKPOOL));
 
         // Mixed-case keywords
@@ -69,11 +78,13 @@ public class PooledPassengerContainsKeywordsPredicateTest {
         assertFalse(predicate.test(OFFICEPOOL));
 
         // Non-matching keyword
-        predicate = new PooledPassengerContainsKeywordsPredicate(Arrays.asList("Carol"));
+        predicate = new PooledPassengerContainsKeywordsPredicate(Arrays.asList(CARL.getName().toString().split("\\s")));
         assertFalse(predicate.test(OFFICEPOOL));
 
-        // Keywords match Driver's name, tag and TripDay, but does not match any passenger's name
-        predicate = new PooledPassengerContainsKeywordsPredicate(Arrays.asList("Alice", "friends", "Monday"));
+        // Keywords match Driver's name and TripDay, but does not match any passenger's name
+        predicate = new PooledPassengerContainsKeywordsPredicate(Arrays.asList(
+                OFFICEPOOL.getDriver().getName().toString().split("\\s")[0],
+                OFFICEPOOL.getTripDayAsStr()));
         assertFalse(predicate.test(OFFICEPOOL));
     }
 }
