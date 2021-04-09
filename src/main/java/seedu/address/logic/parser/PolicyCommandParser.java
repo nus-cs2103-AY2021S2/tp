704222fb31f3dedfc1562f9cfb399f6bafcf5bda
jlxw48 <jlxw48@gmail.com>
2021-04-09 23:54:38 +0800

    Improve abstraction of code

--- src/main/java/seedu/address/logic/parser/PolicyCommandParser.java
+++ src/main/java/seedu/address/logic/parser/PolicyCommandParser.java
@@ -7,8 +7,8 @@ import seedu.address.logic.parser.exceptions.ParseException;
 public class PolicyCommandParser implements Parser<PolicyCommand> {
 
     /**
-     * Parses the given {@code String} of arguments in the context of the DeleteCommand
-     * and returns a DeleteCommand object for execution.
+     * Parses the given {@code String} of arguments in the context of the PolicyCommand
+     * and returns a PolicyCommand object for execution.
      *
      * @throws ParseException if the user input does not conform the expected format
      */
