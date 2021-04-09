bbb7ded2e7912b8585e6da1700a916dcda86b00a
goatygoatygoat <52666150+goatygoatygoat@users.noreply.github.com>
2021-04-10 04:22:50 +0800

    Find contact command tests

---
+++ src/test/java/seedu/address/logic/parser/FindContactCommandParserTest.java
@@ -0,0 +1,33 @@
+package seedu.address.logic.parser;
+
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
+
+import java.util.Arrays;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.logic.commands.FindContactCommand;
+import seedu.address.model.contact.ContactNameContainsKeywordsPredicate;
+
+public class FindContactCommandParserTest {
+
+    private FindContactCommandParser parser = new FindContactCommandParser();
+
+    @Test
+    public void parse_emptyArg_throwsParseException() {
+        assertParseFailure(parser, "     ", String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindContactCommand.MESSAGE_USAGE));
+    }
+
+    @Test
+    public void parse_validArgs_returnsFindCommand() {
+        // no leading and trailing whitespaces
+        FindContactCommand expectedFindCommand =
+                new FindContactCommand(new ContactNameContainsKeywordsPredicate(Arrays.asList("Alice", "Bob")));
+        assertParseSuccess(parser, "Alice Bob", expectedFindCommand);
+
+        // multiple whitespaces between keywords
+        assertParseSuccess(parser, " \n Alice \n \t Bob  \t", expectedFindCommand);
+    }
+}
