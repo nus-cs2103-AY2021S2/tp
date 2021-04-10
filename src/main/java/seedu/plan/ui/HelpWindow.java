5140cefa72785c0801658d44c633cf4ac2b5ffa7
seaniy <28254509+seaniy@users.noreply.github.com>
2021-04-10 14:21:45 +0800

    Update UG link in help window

--- src/main/java/seedu/plan/ui/HelpWindow.java
+++ src/main/java/seedu/plan/ui/HelpWindow.java
@@ -15,7 +15,7 @@ import seedu.plan.commons.core.LogsCenter;
  */
 public class HelpWindow extends UiPart<Stage> {
 
-    public static final String USERGUIDE_URL = "https://nus-cs2103-ay2021s2.github.io/tp/UserGuide.html";
+    public static final String USERGUIDE_URL = "https://ay2021s2-cs2103-w17-1.github.io/tp/UserGuide.html";
     public static final String HELP_MESSAGE = "Refer to the user guide: " + USERGUIDE_URL;
 
     private static final Logger logger = LogsCenter.getLogger(HelpWindow.class);
