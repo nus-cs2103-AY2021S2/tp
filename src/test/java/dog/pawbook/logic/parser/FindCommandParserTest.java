1d19f1804405389941ee99473d2fd89110b5387c
ZhangAnli <zhanganli1998@hotmail.com>
2021-04-10 11:05:46 +0800

    Complete FindCommandParserTest

--- src/test/java/dog/pawbook/logic/parser/FindCommandParserTest.java
+++ src/test/java/dog/pawbook/logic/parser/FindCommandParserTest.java
@@ -1,3 +1,4 @@
+//@@author Zhang Anli
 package dog.pawbook.logic.parser;
 
 import static dog.pawbook.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
@@ -16,19 +17,26 @@ public class FindCommandParserTest {
     private FindCommandParser parser = new FindCommandParser();
 
     @Test
-    public void parse_emptyArg_throwsParseException() {
-        assertParseFailure(parser, "     ", String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
+    public void parse_emptyArg_failure() {
+        // empty arg parse failure
+        assertParseFailure(parser, "", String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
+
+        // white space parse failure
+        assertParseFailure(parser, "   ", String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
     }
 
     @Test
-    public void parse_validArgs_returnsFindCommand() {
-        // no leading and trailing whitespaces
+    public void parse_validArgs_success() {
+
         FindCommand expectedFindCommand =
                 new FindCommand(new NameContainsKeywordsPredicate(Arrays.asList("Alice", "Bob")));
+
+        // no leading and trailing whitespaces
         assertParseSuccess(parser, "Alice Bob", expectedFindCommand);
 
         // multiple whitespaces between keywords
         assertParseSuccess(parser, " \n Alice \n \t Bob  \t", expectedFindCommand);
+
     }
 
 }
--- src/test/java/dog/pawbook/logic/parser/ViewCommandParserTest.java
+++ src/test/java/dog/pawbook/logic/parser/ViewCommandParserTest.java
@@ -1,5 +1,4 @@
 //@@ ZhangAnli
-
 package dog.pawbook.logic.parser;
 
 import static dog.pawbook.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
@@ -31,26 +30,23 @@ public class ViewCommandParserTest {
     }
 
     @Test
-    public void parse_emptyArguments_parseException() {
+    public void parse_emptyArguments_failure() {
         // Test that empty arguments ParseException is thrown
         assertParseFailure(parser, INVALID_EMPTY_STRING, String.format(MESSAGE_INVALID_COMMAND_FORMAT,
                 ViewCommand.MESSAGE_USAGE));
     }
 
     @Test
-    public void parse_negativeId_parseException() {
+    public void parse_invalidId_failure() {
         // Test that negative ID ParseException is thrown
         assertParseFailure(parser, INVALID_NEGATIVE_ID_STRING, MESSAGE_INVALID_ID_GENERAL);
-    }
 
-    @Test
-    public void parse_invalidId_parseException() {
-        // Test that invalid ID parseException is thrown
+        // Test that invalid ID ParseException is thrown
         assertParseFailure(parser, INVALID_UNKNOWN_ID_STRING, MESSAGE_INVALID_ID_GENERAL);
     }
 
     @Test
-    public void parse_validId_parseException() {
+    public void parse_validId_success() {
         // Test that no parseException thrown
         assertParseSuccess(parser, VALID_ENTITY_ID, new ViewCommand(Integer.parseInt(VALID_ENTITY_ID)));
     }
