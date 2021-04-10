0a95155e9396f30ceb1ccaf3cad73aca1e607b15
Onn Wei Sin <onnwards@gmail.com>
2021-04-11 06:14:31 +0800

    rename message for find related commands

--- src/main/java/seedu/address/commons/core/Messages.java
+++ src/main/java/seedu/address/commons/core/Messages.java
@@ -9,7 +9,7 @@ public class Messages {
     public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Invalid command format! \n%1$s";
 
     //=================================Patient related messages===============================================
-    public static final String MESSAGE_PATIENTS_LISTED_OVERVIEW = "%1$d patients listed!";
+    public static final String MESSAGE_PATIENTS_FOUND_OVERVIEW = "%1$d patients found!";
     public static final String MESSAGE_INVALID_PATIENT_DISPLAYED_INDEX = "The patient index provided is invalid";
     public static final String MESSAGE_FORCE_DELETE_PATIENT_REQUIRED =
             "The patient has existing appointments in appointment schedule."
@@ -21,7 +21,7 @@ public class Messages {
     public static final String MESSAGE_DELETE_PATIENT_SUCCESS = "Deleted Patient: %1$s";
 
     //=================================Doctor related messages================================================
-    public static final String MESSAGE_DOCTORS_LISTED_OVERVIEW = "%1$d doctors listed!";
+    public static final String MESSAGE_DOCTORS_FOUND_OVERVIEW = "%1$d doctors found!";
     public static final String MESSAGE_INVALID_DOCTOR_DISPLAYED_INDEX = "The doctor index provided is invalid";
     public static final String MESSAGE_FORCE_DELETE_DOCTOR_REQUIRED =
             "The doctor has existing appointments in appointment schedule."
@@ -35,5 +35,5 @@ public class Messages {
     //=================================Appointment related messages===========================================
     public static final String MESSAGE_INVALID_APPOINTMENT_DISPLAYED_INDEX =
             "The appointment index provided is invalid";
-    public static final String MESSAGE_APPOINTMENTS_LISTED_OVERVIEW = "%1$d appointments listed!";
+    public static final String MESSAGE_APPOINTMENTS_FOUND_OVERVIEW = "%1$d appointments found!";
 }
