c280bf07b27dcc80f3d1b019987f38324ac410c5
goatygoatygoat <52666150+goatygoatygoat@users.noreply.github.com>
2021-04-10 20:43:15 +0800

    Made requested changes

--- src/main/java/seedu/address/logic/commands/FindContactCommand.java
+++ src/main/java/seedu/address/logic/commands/FindContactCommand.java
@@ -7,7 +7,7 @@ import seedu.address.model.Model;
 import seedu.address.model.contact.ContactNameContainsKeywordsPredicate;
 
 /**
- * Finds and lists all contacts in Teaching Assistant whose name contains any of the argument keywords.
+ * Finds and lists all contacts in Teaching Assistant whose name contains all of the argument keywords.
  * Keyword matching is case insensitive.
  */
 public class FindContactCommand extends Command {
--- src/main/java/seedu/address/model/contact/ContactTagsContainKeywordsPredicate.java
+++ src/main/java/seedu/address/model/contact/ContactTagsContainKeywordsPredicate.java
@@ -6,7 +6,7 @@ import java.util.function.Predicate;
 import seedu.address.commons.util.StringUtil;
 
 /**
- * Tests that a {@code Contact}'s {@code Tags} matches any of the keywords given.
+ * Tests that a {@code Contact}'s {@code Tags} matches all of the keywords given.
  */
 public class ContactTagsContainKeywordsPredicate implements Predicate<Contact> {
     private final List<String> keywords;
