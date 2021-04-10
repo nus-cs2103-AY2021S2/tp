5101bcaf246af541d8b0ceb7f7fac2498cd12f77
Mark Wong <markmcwong@yahoo.com>
2021-04-10 14:21:28 +0800

    Fix checkstyleTest error

--- src/test/java/seedu/address/model/person/passenger/PriceIsGreaterThanAmountPredicateTest.java
+++ src/test/java/seedu/address/model/person/passenger/PriceIsGreaterThanAmountPredicateTest.java
@@ -16,14 +16,15 @@ public class PriceIsGreaterThanAmountPredicateTest {
         Double firstPredicatePrice = VALID_PRICE_AMY;
         Double secondPredicatePrice = VALID_PRICE_BOB;
 
-        PriceContainsKeywordsPredicate firstPredicate = new PriceContainsKeywordsPredicate(firstPredicatePrice);
-        PriceContainsKeywordsPredicate secondPredicate = new PriceContainsKeywordsPredicate(secondPredicatePrice);
+        PriceIsGreaterThanAmountPredicate firstPredicate = new PriceIsGreaterThanAmountPredicate(firstPredicatePrice);
+        PriceIsGreaterThanAmountPredicate secondPredicate = new PriceIsGreaterThanAmountPredicate(secondPredicatePrice);
 
         // same object -> returns true
         assertTrue(firstPredicate.equals(firstPredicate));
 
         // same values -> returns true
-        PriceContainsKeywordsPredicate firstPredicateCopy = new PriceContainsKeywordsPredicate(firstPredicatePrice);
+        PriceIsGreaterThanAmountPredicate firstPredicateCopy =
+                new PriceIsGreaterThanAmountPredicate(firstPredicatePrice);
         assertTrue(firstPredicate.equals(firstPredicateCopy));
 
         // different types -> returns false
@@ -39,21 +40,21 @@ public class PriceIsGreaterThanAmountPredicateTest {
     @Test
     public void test_passengerHasPrice() {
         // Passenger price equals to Predicate
-        PriceContainsKeywordsPredicate predicate = new PriceContainsKeywordsPredicate(VALID_PRICE_AMY);
+        PriceIsGreaterThanAmountPredicate predicate = new PriceIsGreaterThanAmountPredicate(VALID_PRICE_AMY);
         assertTrue(predicate.test(new PassengerBuilder().withPrice(VALID_PRICE_AMY).build()));
 
         // Passenger price bigger than Predicate
-        predicate = new PriceContainsKeywordsPredicate(VALID_PRICE_AMY);
+        predicate = new PriceIsGreaterThanAmountPredicate(VALID_PRICE_AMY);
         assertTrue(predicate.test(new PassengerBuilder().withPrice(VALID_PRICE_BOB).build()));
 
         // Passenger price smaller than Predicate
-        predicate = new PriceContainsKeywordsPredicate(VALID_PRICE_BOB);
+        predicate = new PriceIsGreaterThanAmountPredicate(VALID_PRICE_BOB);
         assertFalse(predicate.test(new PassengerBuilder().withPrice(VALID_PRICE_AMY).build()));
     }
 
     @Test
     public void test_passengerWithoutPrice_returnsFalse() {
-        PriceContainsKeywordsPredicate predicate = new PriceContainsKeywordsPredicate(VALID_PRICE_AMY);
+        PriceIsGreaterThanAmountPredicate predicate = new PriceIsGreaterThanAmountPredicate(VALID_PRICE_AMY);
         assertFalse(predicate.test(new PassengerBuilder().build()));
     }
 }
