33ec792d48a78f690872a3163f78f0d79a128236
Huang Po-Wei <71061276+georgepwhuang@users.noreply.github.com>
2021-04-10 16:17:30 +0800

    Updated Ui of NotifWindow and NotesWindow

--- src/main/java/seedu/address/ui/NotesWindow.java
+++ src/main/java/seedu/address/ui/NotesWindow.java
@@ -1,6 +1,11 @@
 package seedu.address.ui;
 
+import java.io.IOException;
+
 import javafx.scene.control.Alert;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextArea;
 import javafx.stage.Stage;
 import seedu.address.model.person.Person;
 
@@ -24,6 +29,15 @@ public class NotesWindow extends Alert {
      */
     public void setMessage(Person personWithNotes) {
         setHeaderText(String.format("Notes for %s:", personWithNotes.getName()));
-        setContentText(personWithNotes.getNotesString());
+        getDialogPane().setMaxWidth(1000);
+        ScrollPane scroll = new ScrollPane();
+        Label label = new Label(personWithNotes.getNotesString());
+        label.setMaxWidth(1000);
+        label.setWrapText(true);
+        scroll.setContent(label);
+        scroll.setPrefViewportWidth(1000);
+        scroll.setFitToWidth(true);
+        scroll.setPrefViewportHeight(400);
+        getDialogPane().setContent(scroll);
     }
 }
