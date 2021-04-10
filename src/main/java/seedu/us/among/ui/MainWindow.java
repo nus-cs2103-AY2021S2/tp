706f27a4c607cf266b218fd15f4f20c0c3fd6ffe
ong6 <junxiongong2@gmail.com>
2021-04-10 15:01:00 +0800

    Refactor MainWindow

--- src/main/java/seedu/us/among/ui/MainWindow.java
+++ src/main/java/seedu/us/among/ui/MainWindow.java
@@ -207,40 +207,57 @@ public class MainWindow extends UiPart<Stage> {
     }
 
     /**
+     * Outputs the endpoint response for send and run commands to the
+     * result display and handles endpoint focus.
+     *
+     * @param commandResult the command entered
+     */
+    public void handelApiResponse(CommandResult commandResult) {
+        Endpoint e = commandResult.getEndpoint();
+        resultDisplay.setApiFeedbackToUser(commandResult.getFeedbackToUser(), e);
+        endpointListPanel.focusSelectedEndpoint(e);
+    }
+
+    /**
+     * Outputs the endpoint response for add, edit, remove, and show commands
+     * to the result display and handles endpoint focus.
+     *
+     * @param commandResult the command entered
+     */
+    public void handelEndpointResponse(CommandResult commandResult) {
+        Endpoint e = commandResult.getEndpoint();
+        resultDisplay.setResponseMetaFeedbackHelper(commandResult.getFeedbackToUser(), e);
+        endpointListPanel.focusSelectedEndpoint(e);
+    }
+
+    /**
+     * Cleans the result display.
+     */
+    public void refreshResultDisplay() {
+        resultDisplay.setFeedbackToUser("");
+        resultDisplay.getLoadingSpinnerPlaceholder().setVisible(true);
+        resultDisplay.getEmptyListPlaceholder().setVisible(false);
+    }
+
+    /**
      * Executes the command and returns the result.
      *
      * @see Logic#execute(String)
      */
     private CommandResult executeCommand(String commandText) throws CommandException, ParseException,
             RequestException, AbortRequestException {
-        resultDisplay.setFeedbackToUser("");
-        resultDisplay.getLoadingSpinnerPlaceholder().setVisible(true);
-        resultDisplay.getEmptyListPlaceholder().setVisible(false);
+        refreshResultDisplay();
         try {
             CommandResult commandResult = logic.execute(commandText);
             resultDisplay.getLoadingSpinnerPlaceholder().setVisible(false);
             logger.info("Result: " + commandResult.getFeedbackToUser());
+
+            endpointListPanel.unfocusEndpointList();
+
             if (commandResult.isApiResponse()) {
-                Endpoint e = commandResult.getEndpoint();
-                resultDisplay.setApiFeedbackToUser(commandResult.getFeedbackToUser(), e);
-                if (endpointListPanel.contains(e)) {
-                    //if command is send command
-                    endpointListPanel.focusSelectedEndpoint(e);
-                } else {
-                    //if command is run command
-                    endpointListPanel.unfocusEndpointList();
-                }
-                endpointListPanel.focusSelectedEndpoint(e);
+                handelApiResponse(commandResult);
             } else if (commandResult.getEndpoint() != null) {
-                Endpoint e = commandResult.getEndpoint();
-                resultDisplay.setResponseMetaFeedbackHelper(commandResult.getFeedbackToUser(), e);
-                if (commandResult.hasEndpointToShow() == true) {
-                    //if command is add, edit, show
-                    endpointListPanel.focusSelectedEndpoint(e);
-                } else {
-                    //if command is remove
-                    endpointListPanel.unfocusEndpointList();
-                }
+                handelEndpointResponse(commandResult);
             } else {
                 resultDisplay.setFeedbackToUser(commandResult.getFeedbackToUser());
             }
@@ -263,24 +280,27 @@ public class MainWindow extends UiPart<Stage> {
 
             return commandResult;
         } catch (AbortRequestException e) {
-            //stop loading spinner (if any)
-            resultDisplay.getLoadingSpinnerPlaceholder().setVisible(false);
-            logger.info("Execution failed: " + commandText);
-            resultDisplay.setFeedbackToUser(e.getMessage());
+            executeErrorHandling(e, commandText);
             throw e;
         } catch (CommandException | ParseException | RequestException e) {
-            //stop loading spinner (if any)
-            resultDisplay.getLoadingSpinnerPlaceholder().setVisible(false);
-
             //play error message
             resultDisplay.getErrorGifTimeline().play();
-
-            logger.info("Execution failed: " + commandText);
-            resultDisplay.setFeedbackToUser(e.getMessage());
+            executeErrorHandling(e, commandText);
             throw e;
         } finally {
             logger.info("Current Number of Threads: " + Thread.activeCount());
         }
+    }
+
+    /**
+     * Logs the exception message and displays it in result display.
+     * @param e the exception
+     * @param commandText the user input to be handled
+     */
+    public void executeErrorHandling(Exception e, String commandText) {
+        resultDisplay.getLoadingSpinnerPlaceholder().setVisible(false);
+        logger.info("Execution failed: " + commandText);
+        resultDisplay.setFeedbackToUser(e.getMessage());
     }
 
     /**
