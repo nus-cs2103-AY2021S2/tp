8ad537caf912903a971f681a461bf913a5e516c6
nowknowing <s.sunaga00@gmail.com>
2021-04-10 09:33:59 +0800

    parser test

---
+++ src/test/java/seedu/address/logic/parser/AddRecurringSessionCommandParserTest.java
@@ -0,0 +1,12 @@
+package seedu.address.logic.parser;
+
+import org.junit.jupiter.api.Test;
+
+public class AddRecurringSessionCommandParserTest {
+    private AddRecurringSessionCommandParser parser = new AddRecurringSessionCommandParser();
+
+    @Test
+    public void parse_allFieldsPresent_success() {
+
+    }
+}
