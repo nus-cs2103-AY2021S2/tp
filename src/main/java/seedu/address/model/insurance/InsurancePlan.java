967288ef2b5c9457fac404fecd86f6fe348275d8
HuaiZe <kohhuaize@hotmail.com>
2021-04-11 16:27:28 +0800

    Allow users to input premium amount with up to two decimal places

--- src/main/java/seedu/address/model/insurance/InsurancePlan.java
+++ src/main/java/seedu/address/model/insurance/InsurancePlan.java
@@ -12,16 +12,17 @@ public class InsurancePlan {
             + "e.g. plan 3 i/Protecc $4000 OR plan 2 c/3\n"
             + "The plan name should only contain alphanumeric characters and spaces,"
             + " and it should not be blank.\n"
-            + "The yearly premium amount should be a positive integer.";
+            + "The yearly premium should be a positive amount with up to 2 decimal places.";
     public static final String PREMIUM_CONSTRAINTS =
-            "The yearly premium amount should be a positive integer.";
+            "The yearly premium should be a positive amount with up to 2 decimal places.";
 
     /*
      * The first character of the plan name must not be a whitespace,
      * otherwise " " (a blank string) becomes a valid input.
      */
     public static final String NAME_VALIDATION_REGEX = "[\\p{Alnum}][\\p{Alnum} ]*";
-    public static final String PREMIUM_VALIDATION_REGEX = "[0]*[123456789][\\p{Digit}]*";
+    public static final String REGEX_POSITIVE_INTEGER = "[0]*[123456789][\\p{Digit}]*";
+    public static final String REGEX_ALL_ZEROES = "0*";
 
     public final String original;
     public final String name;
@@ -39,8 +40,25 @@ public class InsurancePlan {
         checkArgument(isValidAmount(fragments[1]), PREMIUM_CONSTRAINTS);
         this.original = plan;
         this.name = fragments[0].trim();
+
         // Remove leading zeroes from the string
-        this.premium = fragments[1].replaceAll("^0+(?!\\$)", "");
+        StringBuilder temp = new StringBuilder(fragments[1].replaceAll("^0+(?!\\$)", ""));
+        // Ensure there is at least 1 digit in front of the decimal point
+        if (temp.charAt(0) == '.') {
+            temp.insert(0, "0");
+        }
+        // Pad zeroes behind the decimal point up to 2 decimal places
+        if (temp.toString().contains(".")) {
+            String[] tempArr = temp.toString().split("\\.");
+            int numOfDecPlaces;
+            if (tempArr.length == 1) {
+                numOfDecPlaces = 0;
+            } else {
+                numOfDecPlaces = tempArr[1].length();
+            }
+            temp.append("0".repeat(2 - numOfDecPlaces));
+        }
+        this.premium = temp.toString();
     }
 
     /**
@@ -54,9 +72,20 @@ public class InsurancePlan {
      * Returns true if a given premium amount is a valid amount.
      */
     public static boolean isValidAmount(String premium) {
-        return premium.matches(PREMIUM_VALIDATION_REGEX);
+        String[] fragments = premium.split("\\.");
+        if (fragments.length == 1) {
+            return fragments[0].matches(REGEX_POSITIVE_INTEGER);
+        } else if (fragments.length > 2) {
+            return false;
+        } else if (fragments[0].matches(REGEX_ALL_ZEROES)) {
+            return fragments[1].length() <= 2 && fragments[1].matches(REGEX_POSITIVE_INTEGER);
+        } else if (fragments[0].matches(REGEX_POSITIVE_INTEGER)) {
+            return fragments[1].length() <= 2
+                    && (fragments[1].matches(REGEX_ALL_ZEROES) || fragments[1].matches(REGEX_POSITIVE_INTEGER));
+        } else {
+            return false;
+        }
     }
-
 
     @Override
     public String toString() {
