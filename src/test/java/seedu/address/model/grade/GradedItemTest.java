415f93a50ec4c1ad846e99d5f9a2e366b0f4e7c3
yutingzou <zouyuting0801@gmail.com>
2021-04-12 02:01:53 +0800

    Improve GradeTest and GradedItemTest

--- src/test/java/seedu/address/model/grade/GradedItemTest.java
+++ src/test/java/seedu/address/model/grade/GradedItemTest.java
@@ -29,12 +29,15 @@ public class GradedItemTest {
         assertFalse(GradedItem.isValidGradedItem(" ")); // spaces only
         assertFalse(GradedItem.isValidGradedItem("^")); // only non-alphanumeric characters
         assertFalse(GradedItem.isValidGradedItem("final*")); // contains non-alphanumeric characters
+        // max number of characters is 29 but 30 here
+        assertFalse(GradedItem.isValidGradedItem("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"));
 
         // valid graded item
         assertTrue(GradedItem.isValidGradedItem("final exam")); // alphabets only
         assertTrue(GradedItem.isValidGradedItem("12345")); // numbers only
         assertTrue(GradedItem.isValidGradedItem("2nd lab")); // alphanumeric characters
         assertTrue(GradedItem.isValidGradedItem("Capital Final")); // with capital letters
-        assertTrue(GradedItem.isValidGradedItem("GCE O Level Maths Paper 1 and 2")); // long names
+        assertTrue(GradedItem.isValidGradedItem("GCE O Level Maths Paper 1")); // long names
+        assertTrue(GradedItem.isValidGradedItem("aaaaaaaaaaaaaaaaaaaaaaaaaaaaa")); // contains 29 characters
     }
 }
