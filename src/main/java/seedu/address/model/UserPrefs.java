6e5955d5b165130d8c89644d0837140cfb95b268
Justin Gnoh <justingnoh123@gmail.com>
2021-04-11 23:11:16 +0800

    refactor: Rename a-bashbook.json to abashbook.json

--- src/main/java/seedu/address/model/UserPrefs.java
+++ src/main/java/seedu/address/model/UserPrefs.java
@@ -14,7 +14,7 @@ import seedu.address.commons.core.GuiSettings;
 public class UserPrefs implements ReadOnlyUserPrefs {
 
     private GuiSettings guiSettings = new GuiSettings();
-    private Path addressBookFilePath = Paths.get("data" , "a-bashbook.json");
+    private Path addressBookFilePath = Paths.get("data" , "abashbook.json");
     private Path aliasesFilePath = Paths.get("data" , "aliases.json");
 
     /**
