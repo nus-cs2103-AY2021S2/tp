770cea0b6fd635db9bc0fc755b5d1fed0ae08682
lrj689 <lrj689@hotmail.com>
2021-04-11 13:53:13 +0800

    Update import statements

--- src/test/java/seedu/address/model/entry/ListEntryFormatPredicateTest.java
+++ src/test/java/seedu/address/model/entry/ListEntryFormatPredicateTest.java
@@ -1,13 +1,14 @@
 package seedu.address.model.entry;
 
-import org.junit.jupiter.api.Test;
-import seedu.address.testutil.EntryBuilder;
-
-import java.time.LocalDateTime;
-
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.model.entry.EntryDate.DEFAULT_FORMATTER;
+
+import java.time.LocalDateTime;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.testutil.EntryBuilder;
 
 public class ListEntryFormatPredicateTest {
 
