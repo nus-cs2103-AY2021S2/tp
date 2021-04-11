7daf7624456d05cb0a4a47020e1d954e8a936762
Kou Yong Kang <skykinetic@stevenkou.xyz>
2021-04-11 12:35:18 +0800

    Test: Improve edit owner command test
    Restored the test that edits an owner to become an duplicate of another.
    
    Some other minor coding standard improvements.

--- src/test/java/dog/pawbook/logic/commands/EditOwnerCommandTest.java
+++ src/test/java/dog/pawbook/logic/commands/EditOwnerCommandTest.java
@@ -1,5 +1,7 @@
 package dog.pawbook.logic.commands;
 
+import static dog.pawbook.commons.core.Messages.MESSAGE_DUPLICATE_OWNER;
+import static dog.pawbook.commons.core.Messages.MESSAGE_INVALID_OWNER_ID;
 import static dog.pawbook.logic.commands.CommandTestUtil.DESC_AMY;
 import static dog.pawbook.logic.commands.CommandTestUtil.DESC_BOB;
 import static dog.pawbook.logic.commands.CommandTestUtil.VALID_NAME_BOB;
@@ -12,18 +14,16 @@ import static dog.pawbook.testutil.TypicalId.ID_ONE;
 import static dog.pawbook.testutil.TypicalId.ID_THREE;
 import static dog.pawbook.testutil.TypicalId.ID_TWO;
 import static java.util.stream.Collectors.toList;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 import org.junit.jupiter.api.Test;
 
-import dog.pawbook.commons.core.Messages;
 import dog.pawbook.logic.commands.EditOwnerCommand.EditOwnerDescriptor;
 import dog.pawbook.model.Database;
 import dog.pawbook.model.Model;
 import dog.pawbook.model.ModelManager;
 import dog.pawbook.model.UserPrefs;
-import dog.pawbook.model.managedentity.Entity;
 import dog.pawbook.model.managedentity.IdMatchPredicate;
 import dog.pawbook.model.managedentity.owner.Owner;
 import dog.pawbook.testutil.EditOwnerDescriptorBuilder;
@@ -35,21 +35,20 @@ import javafx.util.Pair;
  */
 public class EditOwnerCommandTest {
 
-    private Model model = new ModelManager(getTypicalDatabase(), new UserPrefs());
+    private final Model model = new ModelManager(getTypicalDatabase(), new UserPrefs());
+    private Model expectedModel;
 
     @Test
     public void execute_allFieldsSpecified_success() {
-        Pair<Integer, Entity> firstIdEntity = model.getDatabase().getEntityList().get(0);
-        Owner editedOwner = new OwnerBuilder((Owner) firstIdEntity.getValue()).build();
-        EditOwnerDescriptor descriptor = new EditOwnerDescriptorBuilder(editedOwner).build();
-        EditOwnerCommand editOwnerCommand = new EditOwnerCommand(firstIdEntity.getKey(), descriptor);
+        Owner owner = (Owner) model.getEntity(ID_ONE);
+        EditOwnerDescriptor descriptor = new EditOwnerDescriptorBuilder(owner).build();
+        EditOwnerCommand editOwnerCommand = new EditOwnerCommand(ID_ONE, descriptor);
 
-        String expectedMessage = String.format(EditOwnerCommand.MESSAGE_EDIT_OWNER_SUCCESS, editedOwner);
+        String expectedMessage = String.format(EditOwnerCommand.MESSAGE_EDIT_OWNER_SUCCESS, owner);
 
-        Model expectedModel = new ModelManager(new Database(model.getDatabase()), new UserPrefs());
+        expectedModel = new ModelManager(new Database(model.getDatabase()), new UserPrefs());
 
-        expectedModel.setEntity(firstIdEntity.getKey(), editedOwner);
-        expectedModel.updateFilteredEntityList(new IdMatchPredicate(firstIdEntity.getKey()));
+        expectedModel.updateFilteredEntityList(new IdMatchPredicate(ID_ONE));
         assertCommandSuccess(editOwnerCommand, model, expectedMessage, expectedModel);
     }
 
@@ -63,15 +62,15 @@ public class EditOwnerCommandTest {
 
         EditOwnerDescriptor descriptor = new EditOwnerDescriptorBuilder().withName(VALID_NAME_BOB)
                 .withPhone(VALID_PHONE_BOB).withTags(VALID_TAG_HUSBAND).build();
-        EditOwnerCommand editEntityCommand = new EditOwnerCommand(ID_THREE, descriptor);
+        EditOwnerCommand editOwnerCommand = new EditOwnerCommand(ID_THREE, descriptor);
 
         String expectedMessage = String.format(EditOwnerCommand.MESSAGE_EDIT_OWNER_SUCCESS, editedOwner);
 
-        Model expectedModel = new ModelManager(new Database(model.getDatabase()), new UserPrefs());
+        expectedModel = new ModelManager(new Database(model.getDatabase()), new UserPrefs());
         expectedModel.setEntity(ID_THREE, editedOwner);
         expectedModel.updateFilteredEntityList(new IdMatchPredicate(ID_THREE));
 
-        assertCommandSuccess(editEntityCommand, model, expectedMessage, expectedModel);
+        assertCommandSuccess(editOwnerCommand, model, expectedMessage, expectedModel);
     }
 
     @Test
@@ -81,22 +80,37 @@ public class EditOwnerCommandTest {
 
         String expectedMessage = String.format(EditOwnerCommand.MESSAGE_EDIT_OWNER_SUCCESS, editedOwner);
 
-        Model expectedModel = new ModelManager(new Database(model.getDatabase()), new UserPrefs());
+        expectedModel = new ModelManager(new Database(model.getDatabase()), new UserPrefs());
         expectedModel.updateFilteredEntityList(new IdMatchPredicate(ID_ONE));
 
         assertCommandSuccess(editEntityCommand, model, expectedMessage, expectedModel);
     }
 
-    // todo: restore execute_invalidOwnerIdUnfilteredList_failure after identity crisis is solved
+    @Test
+    public void execute_duplicateOwner_failure() {
+        Owner firstOwner = (Owner) model.getEntity(ID_ONE);
+        EditOwnerDescriptor descriptor = new EditOwnerDescriptorBuilder(firstOwner).build();
+        EditOwnerCommand editOwnerCommand = new EditOwnerCommand(ID_THREE, descriptor);
+
+        assertCommandFailure(editOwnerCommand, model, MESSAGE_DUPLICATE_OWNER);
+    }
 
     @Test
-    public void execute_invalidOwnerId_failure() {
+    public void execute_outOfBoundOwnerId_failure() {
         Integer outOfBoundId = model.getUnfilteredEntityList().stream()
                 .map(Pair::getKey).sorted().collect(toList()).get(model.getUnfilteredEntityList().size() - 1) + 1;
         EditOwnerDescriptor descriptor = new EditOwnerDescriptorBuilder().withName(VALID_NAME_BOB).build();
-        EditOwnerCommand editEntityCommand = new EditOwnerCommand(outOfBoundId, descriptor);
+        EditOwnerCommand editOwnerCommand = new EditOwnerCommand(outOfBoundId, descriptor);
 
-        assertCommandFailure(editEntityCommand, model, Messages.MESSAGE_INVALID_OWNER_ID);
+        assertCommandFailure(editOwnerCommand, model, MESSAGE_INVALID_OWNER_ID);
+    }
+
+    @Test
+    public void execute_validIdDifferentEntityType_failure() {
+        EditOwnerDescriptor descriptor = new EditOwnerDescriptorBuilder().withName(VALID_NAME_BOB).build();
+        EditOwnerCommand editOwnerCommand = new EditOwnerCommand(ID_TWO, descriptor);
+
+        assertCommandFailure(editOwnerCommand, model, MESSAGE_INVALID_OWNER_ID);
     }
 
     @Test
@@ -106,22 +120,22 @@ public class EditOwnerCommandTest {
         // same values -> returns true
         EditOwnerDescriptor copyDescriptor = new EditOwnerDescriptor(DESC_AMY);
         EditOwnerCommand commandWithSameValues = new EditOwnerCommand(ID_ONE, copyDescriptor);
-        assertTrue(standardCommand.equals(commandWithSameValues));
+        assertEquals(commandWithSameValues, standardCommand);
 
         // same object -> returns true
-        assertTrue(standardCommand.equals(standardCommand));
+        assertEquals(standardCommand, standardCommand);
 
         // null -> returns false
-        assertFalse(standardCommand.equals(null));
+        assertNotEquals(standardCommand, null);
 
         // different types -> returns false
-        assertFalse(standardCommand.equals(new HelpCommand()));
+        assertNotEquals(new HelpCommand(), standardCommand);
 
         // different index -> returns false
-        assertFalse(standardCommand.equals(new EditOwnerCommand(ID_TWO, DESC_AMY)));
+        assertNotEquals(new EditOwnerCommand(ID_TWO, DESC_AMY), standardCommand);
 
         // different descriptor -> returns false
-        assertFalse(standardCommand.equals(new EditOwnerCommand(ID_ONE, DESC_BOB)));
+        assertNotEquals(new EditOwnerCommand(ID_ONE, DESC_BOB), standardCommand);
     }
 
 }
