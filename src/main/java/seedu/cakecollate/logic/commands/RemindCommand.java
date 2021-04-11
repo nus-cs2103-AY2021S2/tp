70ed24d23af7a4c0bec445b67b10412c08681c8e
RuiXiong2211 <ruixiong8@hotmail.com>
2021-04-11 20:28:10 +0800

    no message

--- src/main/java/seedu/cakecollate/logic/commands/RemindCommand.java
+++ src/main/java/seedu/cakecollate/logic/commands/RemindCommand.java
@@ -15,11 +15,11 @@ public class RemindCommand extends Command {
 
     public static final String COMMAND_WORD = "remind";
     public static final String MESSAGE_EMPTY = "Days cannot be left blank!";
-    public static final String MESSAGE_MULTIPLE_INPUTS = "Only one input for remind command is accepted! Did you leave" 
+    public static final String MESSAGE_MULTIPLE_INPUTS = "Only one input for remind command is accepted! Did you leave"
             + "a space between 2 digits?";
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds all orders whose delivery dates "
             + "are within X days from the current date, as a list with index numbers.\n"
-            + "Parameters: DAYS (Integer more than equal to 0 but less than or equal to 2147483647) \n"
+            + "Parameters: DAYS (Integer more than or equal to 0 but less than or equal to 2147483647) \n"
             + "Example: " + COMMAND_WORD + " 3";
 
     private final ReminderDatePredicate predicate;
