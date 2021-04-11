3f28fd733bb420824e26d7d022c2d59b4eaeec5c
HuaiZe <kohhuaize@hotmail.com>
2021-04-12 03:23:26 +0800

    Standardise error messages displayed when user enters invalid client or plan index for plan command

--- src/main/java/seedu/address/commons/core/Messages.java
+++ src/main/java/seedu/address/commons/core/Messages.java
@@ -5,7 +5,7 @@ package seedu.address.commons.core;
  */
 public class Messages {
 
-    public static final String MESSAGE_UNKNOWN_COMMAND = "Unknown command";
+    public static final String MESSAGE_UNKNOWN_COMMAND = "Unknown command!";
     public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Invalid command format! \n%1$s";
     public static final String MESSAGE_INVALID_PREFIX =
             "Invalid prefix! Please refer to the list of prefixes! \n%1$s";
@@ -16,12 +16,12 @@ public class Messages {
     public static final String MESSAGE_INVALID_GENDER_INPUT =
             "Invalid gender input! Please refer to Tip 2! \n%1$s";
     public static final String MESSAGE_EMPTY_ARGUMENT =
-            "Missing keyword! Please refer to the instructions below \n%1$s";
+            "Missing keyword! Please refer to the instructions below: \n%1$s";
     public static final String MESSAGE_MULTIPLE_WORD_PER_ATTRIBUTE = "You can only input one keyword per attribute!";
-    public static final String MESSAGE_INVALID_PERSON_DISPLAYED_INDEX = "The client index provided is invalid";
-    public static final String MESSAGE_INVALID_MEETING_DISPLAYED_INDEX = "The meeting index provided is invalid";
+    public static final String MESSAGE_INVALID_PERSON_DISPLAYED_INDEX = "The client index provided is invalid.";
+    public static final String MESSAGE_INVALID_MEETING_DISPLAYED_INDEX = "The meeting index provided is invalid.";
     public static final String MESSAGE_PERSONS_LISTED_OVERVIEW = "%1$d clients listed!";
-    public static final String MESSAGE_INVALID_PLAN_INDEX = "The plan index provided is invalid";
+    public static final String MESSAGE_INVALID_PLAN_INDEX = "The plan index provided is invalid.";
     public static final String messagePersonFilteredOverview(String s) {
         return "%1$d clients listed! \nYour filter keyword(s): " + s;
     }
