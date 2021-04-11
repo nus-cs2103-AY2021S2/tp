1d5db1c36035ae58b01bc74af76ae09767d950d3
yaowei-soc <e0407296@u.nus.edu>
2021-04-11 20:08:36 +0800

    test: Update FilterCommand test cases

--- src/test/java/seedu/address/logic/parser/FilterCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/FilterCommandParserTest.java
@@ -1,15 +1,22 @@
 package seedu.address.logic.parser;
 
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_PREFIX;
 import static seedu.address.logic.commands.CommandTestUtil.PREAMBLE_WHITESPACE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_ADDRESS;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_COMPANY;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_JOB_TITLE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_REMARK;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertValidCommandToAliasFailure;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertValidCommandToAliasSuccess;
+import static seedu.address.logic.parser.FilterCommandParser.FORCE_PREAMBLE;
+
+import java.util.Arrays;
 
 import org.junit.jupiter.api.Test;
 
@@ -19,6 +26,7 @@ import seedu.address.model.DisplayFilterPredicate;
 class FilterCommandParserTest {
 
     private final FilterCommandParser parser = new FilterCommandParser();
+    final String INVALID_PREFIX = "-dfdf";
 
     @Test
     public void parse_emptyArgs_success() {
@@ -28,33 +36,71 @@ class FilterCommandParserTest {
 
     @Test
     public void parse_allFields_success() {
-        String arguments = PREFIX_PHONE + " " + PREFIX_EMAIL + " " + PREFIX_COMPANY + " " + PREFIX_JOB_TITLE + " "
-                + PREFIX_ADDRESS + " " + PREFIX_TAG + " " + PREFIX_REMARK;
-        ArgumentMultimap argumentMultimap = ArgumentTokenizer.tokenize(arguments, PREFIX_PHONE, PREFIX_EMAIL,
-                PREFIX_COMPANY, PREFIX_JOB_TITLE, PREFIX_ADDRESS, PREFIX_TAG, PREFIX_REMARK);
+        String arguments = FORCE_PREAMBLE + PREFIX_PHONE + " " + PREFIX_EMAIL + " " + PREFIX_COMPANY
+                + " " + PREFIX_JOB_TITLE + " " + PREFIX_ADDRESS + " " + PREFIX_TAG + " "
+                + PREFIX_REMARK;
+        ArgumentMultimap argumentMultimap = ArgumentTokenizer
+                .tokenize(arguments, PREFIX_NAME, PREFIX_PHONE, PREFIX_EMAIL, PREFIX_ADDRESS,
+                        PREFIX_COMPANY, PREFIX_JOB_TITLE, PREFIX_TAG, PREFIX_REMARK);
         FilterCommand expectedFilterCommand = new FilterCommand(
-                new DisplayFilterPredicate(argumentMultimap));
+                new DisplayFilterPredicate(argumentMultimap),
+                Arrays.asList(PREFIX_PHONE.getPrefix(), PREFIX_EMAIL.getPrefix(),
+                        PREFIX_COMPANY.getPrefix(), PREFIX_JOB_TITLE.getPrefix(),
+                        PREFIX_ADDRESS.getPrefix(), PREFIX_TAG.getPrefix(),
+                        PREFIX_REMARK.getPrefix())
+        );
         assertParseSuccess(parser, arguments, expectedFilterCommand);
     }
 
     @Test
     public void parse_someFields_success() {
-        String arguments = PREFIX_ADDRESS + " " + PREFIX_TAG + " " + PREFIX_REMARK;
-        ArgumentMultimap argumentMultimap = ArgumentTokenizer.tokenize(arguments, PREFIX_PHONE, PREFIX_EMAIL,
-                PREFIX_COMPANY, PREFIX_JOB_TITLE, PREFIX_ADDRESS, PREFIX_TAG, PREFIX_REMARK);
+        String arguments = FORCE_PREAMBLE + PREFIX_ADDRESS + " " + PREFIX_TAG + " " + PREFIX_REMARK;
+        ArgumentMultimap argumentMultimap = ArgumentTokenizer
+                .tokenize(" " + arguments, PREFIX_NAME, PREFIX_PHONE, PREFIX_EMAIL, PREFIX_ADDRESS,
+                        PREFIX_COMPANY, PREFIX_JOB_TITLE, PREFIX_TAG, PREFIX_REMARK);
         FilterCommand expectedFilterCommand = new FilterCommand(
-                new DisplayFilterPredicate(argumentMultimap));
+                new DisplayFilterPredicate(argumentMultimap),
+                Arrays.asList(PREFIX_ADDRESS.getPrefix(), PREFIX_TAG.getPrefix(),
+                        PREFIX_REMARK.getPrefix()));
         assertParseSuccess(parser, arguments, expectedFilterCommand);
+    }
+
+    @Test
+    public void parse_invalidPrefix_failure() {
+        // invalid prefix
+        assertParseFailure(parser, FORCE_PREAMBLE + INVALID_PREFIX,
+                String.format(MESSAGE_INVALID_PREFIX, INVALID_PREFIX) + "\n"
+                        + FilterCommand.MESSAGE_USAGE);
+
+        // invalid prefix
+        assertParseFailure(parser, FORCE_PREAMBLE + INVALID_PREFIX + " " + INVALID_PREFIX,
+                String.format(MESSAGE_INVALID_PREFIX, INVALID_PREFIX + " " + INVALID_PREFIX) + "\n"
+                        + FilterCommand.MESSAGE_USAGE);
+
+        // invalid prefix followed by valid
+        assertParseFailure(parser, FORCE_PREAMBLE + INVALID_PREFIX + " " + PREFIX_PHONE,
+                String.format(MESSAGE_INVALID_PREFIX, INVALID_PREFIX) + "\n"
+                        + FilterCommand.MESSAGE_USAGE);
+
+        // valid followed by invalid prefix
+        assertParseFailure(parser, FORCE_PREAMBLE + PREFIX_PHONE + " " + INVALID_PREFIX,
+                String.format(MESSAGE_INVALID_PREFIX, INVALID_PREFIX) + "\n"
+                        + FilterCommand.MESSAGE_USAGE);
     }
 
     @Test
     public void parse_validAddCommandAlias_returnsTrue() {
         // there are no filter commands that cannot be aliased - parser always return true
         assertValidCommandToAliasSuccess(parser, PREAMBLE_WHITESPACE);
+
+        // valid prefix to alias
+        assertValidCommandToAliasSuccess(parser, PREFIX_ADDRESS + " " + PREFIX_TAG);
     }
 
     @Test
     public void parse_invalidAddCommandAlias_returnsFalse() {
-        // there are no filter commands that cannot be aliased - parser always return true
+        assertValidCommandToAliasFailure(parser, INVALID_PREFIX);
+
+        assertValidCommandToAliasFailure(parser, INVALID_PREFIX + " " + INVALID_PREFIX);
     }
 }
