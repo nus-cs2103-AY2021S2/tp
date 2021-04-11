751cc856a1f970d654a00e61c6ef0148e7414642
Kingsley Kuan <kingsley@kingsleykuan.com>
2021-04-12 04:45:12 +0800

    Add tests for delete filter parser

---
+++ src/test/java/seedu/address/logic/parser/filterparser/DeleteTutorFilterCommandParserTest.java
@@ -0,0 +1,49 @@
+package seedu.address.logic.parser.filterparser;
+
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.logic.commands.filtercommands.DeletePersonFilterCommand;
+import seedu.address.model.filter.TutorFilter;
+import seedu.address.testutil.TypicalFilters;
+
+/**
+ * Integration tests for DeleteTutorFilterCommandParser.
+ */
+public class DeleteTutorFilterCommandParserTest {
+    private DeletePersonFilterCommandParser parser = new DeletePersonFilterCommandParser();
+
+    @Test
+    public void parse() {
+        // EP 1: No subjects
+        TutorFilter tutorFilter = TypicalFilters.getBensonNoSubjectsFilter();
+        DeletePersonFilterCommand deletePersonFilterCommand = new DeletePersonFilterCommand(tutorFilter);
+
+        assertParseSuccess(
+                parser,
+                " n/Benson Meier g/Male p/98765432 e/johnd@example.com a/311, Clementi Ave 2, #02-25",
+                deletePersonFilterCommand);
+
+        // EP 2: One subject
+        tutorFilter = TypicalFilters.getBensonOneSubjectFilter();
+        deletePersonFilterCommand = new DeletePersonFilterCommand(tutorFilter);
+
+        assertParseSuccess(
+                parser,
+                " n/Benson Meier g/Male p/98765432 e/johnd@example.com a/311, Clementi Ave 2, #02-25"
+                + " s/English l/Secondary 4 r/60 y/4 q/A-Level",
+                deletePersonFilterCommand);
+
+        // EP 3: Multiple Subjects
+        tutorFilter = TypicalFilters.getBensonTwoSubjectsFilter();
+        deletePersonFilterCommand = new DeletePersonFilterCommand(tutorFilter);
+
+        assertParseSuccess(
+                parser,
+                " n/Benson Meier g/Male p/98765432 e/johnd@example.com a/311, Clementi Ave 2, #02-25"
+                + " s/English l/Secondary 4 r/60 y/4 q/A-Level"
+                + " s/Geography l/Secondary 4 r/60 y/4 q/A-Level",
+                deletePersonFilterCommand);
+    }
+}
