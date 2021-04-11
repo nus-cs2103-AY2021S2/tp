b45b983c80831990bdaeea1b3d46d199eb006a7a
deyixtan <deyixtan@gmail.com>
2021-04-12 01:59:34 +0800

    Add test data for CommandTestUtil

--- src/test/java/seedu/budgetbaby/logic/commands/CommandTestUtil.java
+++ src/test/java/seedu/budgetbaby/logic/commands/CommandTestUtil.java
@@ -2,72 +2,52 @@ package seedu.budgetbaby.logic.commands;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static seedu.budgetbaby.logic.parser.CliSyntax.PREFIX_ADDRESS;
+import static seedu.budgetbaby.logic.parser.CliSyntax.PREFIX_AMOUNT;
 import static seedu.budgetbaby.logic.parser.CliSyntax.PREFIX_CATEGORY;
-import static seedu.budgetbaby.logic.parser.CliSyntax.PREFIX_EMAIL;
-import static seedu.budgetbaby.logic.parser.CliSyntax.PREFIX_NAME;
-import static seedu.budgetbaby.logic.parser.CliSyntax.PREFIX_PHONE;
+import static seedu.budgetbaby.logic.parser.CliSyntax.PREFIX_DESCRIPTION;
+import static seedu.budgetbaby.logic.parser.CliSyntax.PREFIX_TIME;
 import static seedu.budgetbaby.testutil.Assert.assertThrows;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import seedu.budgetbaby.ablogic.commands.EditCommand;
 import seedu.budgetbaby.commons.core.index.Index;
 import seedu.budgetbaby.logic.commands.exceptions.CommandException;
 import seedu.budgetbaby.model.BudgetBabyModel;
 import seedu.budgetbaby.model.BudgetTracker;
 import seedu.budgetbaby.model.record.Description;
 import seedu.budgetbaby.model.record.FinancialRecord;
-import seedu.budgetbaby.testutil.EditPersonDescriptorBuilder;
 
 /**
  * Contains helper methods for testing commands.
  */
 public class CommandTestUtil {
 
-    public static final String VALID_NAME_AMY = "Amy Bee";
-    public static final String VALID_NAME_BOB = "Bob Choo";
-    public static final String VALID_PHONE_AMY = "11111111";
-    public static final String VALID_PHONE_BOB = "22222222";
-    public static final String VALID_EMAIL_AMY = "amy@example.com";
-    public static final String VALID_EMAIL_BOB = "bob@example.com";
-    public static final String VALID_ADDRESS_AMY = "Block 312, Amy Street 1";
-    public static final String VALID_ADDRESS_BOB = "Block 123, Bobby Street 3";
-    public static final String VALID_TAG_HUSBAND = "husband";
-    public static final String VALID_TAG_FRIEND = "friend";
+    public static final String VALID_NAME_BREAKFAST = "Carrot Cake";
+    public static final String VALID_NAME_LUNCH = "Chicken Chop";
+    public static final String VALID_NAME_DINNER = "Curry Chicken Noodles";
+    public static final String VALID_AMOUNT_BREAKFAST = "4.0";
+    public static final String VALID_AMOUNT_LUNCH = "5.0";
+    public static final String VALID_AMOUNT_DINNER = "6.5";
+    public static final String VALID_TIME_BREAKFAST = "2021-04-01";
+    public static final String VALID_TIME_LUNCH = "2021-04-02";
+    public static final String VALID_TIME_DINNER = "2021-04-03";
+    public static final String VALID_CATEGORY_BREAKFAST = "breakfast";
+    public static final String VALID_CATEGORY_LUNCH = "lunch";
+    public static final String VALID_CATEGORY_DINNER = "dinner";
 
-    public static final String NAME_DESC_AMY = " " + PREFIX_NAME + VALID_NAME_AMY;
-    public static final String NAME_DESC_BOB = " " + PREFIX_NAME + VALID_NAME_BOB;
-    public static final String PHONE_DESC_AMY = " " + PREFIX_PHONE + VALID_PHONE_AMY;
-    public static final String PHONE_DESC_BOB = " " + PREFIX_PHONE + VALID_PHONE_BOB;
-    public static final String EMAIL_DESC_AMY = " " + PREFIX_EMAIL + VALID_EMAIL_AMY;
-    public static final String EMAIL_DESC_BOB = " " + PREFIX_EMAIL + VALID_EMAIL_BOB;
-    public static final String ADDRESS_DESC_AMY = " " + PREFIX_ADDRESS + VALID_ADDRESS_AMY;
-    public static final String ADDRESS_DESC_BOB = " " + PREFIX_ADDRESS + VALID_ADDRESS_BOB;
-    public static final String TAG_DESC_FRIEND = " " + PREFIX_CATEGORY + VALID_TAG_FRIEND;
-    public static final String TAG_DESC_HUSBAND = " " + PREFIX_CATEGORY + VALID_TAG_HUSBAND;
-
-    public static final String INVALID_NAME_DESC = " " + PREFIX_NAME + "James&"; // '&' not allowed in names
-    public static final String INVALID_PHONE_DESC = " " + PREFIX_PHONE + "911a"; // 'a' not allowed in phones
-    public static final String INVALID_EMAIL_DESC = " " + PREFIX_EMAIL + "bob!yahoo"; // missing '@' symbol
-    public static final String INVALID_ADDRESS_DESC = " " + PREFIX_ADDRESS; // empty string not allowed for addresses
-    public static final String INVALID_TAG_DESC = " " + PREFIX_CATEGORY + "hubby*"; // '*' not allowed in tags
-
-    public static final String PREAMBLE_WHITESPACE = "\t  \r  \n";
-    public static final String PREAMBLE_NON_EMPTY = "NonEmptyPreamble";
-
-    public static final EditCommand.EditPersonDescriptor DESC_AMY;
-    public static final EditCommand.EditPersonDescriptor DESC_BOB;
-
-    static {
-        DESC_AMY = new EditPersonDescriptorBuilder().withName(VALID_NAME_AMY)
-                .withPhone(VALID_PHONE_AMY).withEmail(VALID_EMAIL_AMY).withAddress(VALID_ADDRESS_AMY)
-                .withTags(VALID_TAG_FRIEND).build();
-        DESC_BOB = new EditPersonDescriptorBuilder().withName(VALID_NAME_BOB)
-                .withPhone(VALID_PHONE_BOB).withEmail(VALID_EMAIL_BOB).withAddress(VALID_ADDRESS_BOB)
-                .withTags(VALID_TAG_HUSBAND, VALID_TAG_FRIEND).build();
-    }
+    public static final String NAME_DESC_BREAKFAST = " " + PREFIX_DESCRIPTION + VALID_NAME_BREAKFAST;
+    public static final String NAME_DESC_LUNCH = " " + PREFIX_DESCRIPTION + VALID_NAME_LUNCH;
+    public static final String NAME_DESC_DINNER = " " + PREFIX_DESCRIPTION + VALID_NAME_DINNER;
+    public static final String AMOUNT_DESC_BREAKFAST = " " + PREFIX_AMOUNT + VALID_AMOUNT_BREAKFAST;
+    public static final String AMOUNT_DESC_LUNCH = " " + PREFIX_AMOUNT + VALID_AMOUNT_LUNCH;
+    public static final String AMOUNT_DESC_BOB = " " + PREFIX_AMOUNT + VALID_AMOUNT_DINNER;
+    public static final String TIME_DESC_BREAKFAST = " " + PREFIX_TIME + VALID_TIME_BREAKFAST;
+    public static final String TIME_DESC_LUNCH = " " + PREFIX_TIME + VALID_TIME_LUNCH;
+    public static final String TIME_DESC_DINNER = " " + PREFIX_TIME + VALID_TIME_DINNER;
+    public static final String CATEGORY_DESC_BREAKFAST = " " + PREFIX_CATEGORY + VALID_CATEGORY_BREAKFAST;
+    public static final String CATEGORY_DESC_LUNCH = " " + PREFIX_CATEGORY + VALID_CATEGORY_LUNCH;
+    public static final String CATEGORY_DESC_DINNER = " " + PREFIX_CATEGORY + VALID_CATEGORY_DINNER;
 
     /**
      * Executes the given {@code command}, confirms that <br>
