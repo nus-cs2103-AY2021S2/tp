1011d9635e6d4f17a96e50d2ee4c4d1b53087ce6
Mok Bing Wei Maurice <Maurice2n97@gmail.com>
2021-04-11 22:38:54 +0800

    Change Status Bar Footer
    Change the status bar footer to update the actual locations of storage files for
    person, meeting, connections respectively.

--- src/main/java/seedu/address/ui/MainWindow.java
+++ src/main/java/seedu/address/ui/MainWindow.java
@@ -143,7 +143,10 @@ public class MainWindow extends UiPart<Stage> {
         resultDisplay = new ResultDisplay();
         resultDisplayPlaceholder.getChildren().add(resultDisplay.getRoot());
 
-        StatusBarFooter statusBarFooter = new StatusBarFooter(logic.getAddressBookFilePath());
+        StatusBarFooter statusBarFooter = new StatusBarFooter(
+                logic.getAddressBookFilePath(),
+                logic.getMeetingBookFilePath(),
+                logic.getConnectionsFilePath());
         statusbarPlaceholder.getChildren().add(statusBarFooter.getRoot());
 
         CommandBox commandBox = new CommandBox(this::executeCommand);
