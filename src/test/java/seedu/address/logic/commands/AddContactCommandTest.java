6e8bbed2515a4e3a74187d47df447eaa2ad42915
KevinLohJunYong <e0439232@u.nus.edu>
2021-04-12 00:04:54 +0800

    updated files

--- src/test/java/seedu/address/logic/commands/AddContactCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddContactCommandTest.java
@@ -5,18 +5,18 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.commons.core.Messages.MESSAGE_DUPLICATE_CONTACT;
 import static seedu.address.testutil.Assert.assertThrows;
-import static seedu.address.testutil.TypicalEntries.getTypicalEntriesList;
 
 import org.junit.jupiter.api.Test;
 
 import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.AddressBook;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
 import seedu.address.model.contact.Contact;
 import seedu.address.testutil.ContactBuilder;
 
 public class AddContactCommandTest {
-    private ModelManager model = new ModelManager(getTypicalEntriesList(), new UserPrefs());
+    private ModelManager model = new ModelManager(new AddressBook(), new UserPrefs());
     @Test
     public void constructor_nullcontact_throwsNullPointerException() {
         assertThrows(NullPointerException.class, () -> new AddContactCommand(null));
