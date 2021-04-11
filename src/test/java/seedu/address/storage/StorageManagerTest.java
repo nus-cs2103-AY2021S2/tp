0e1fcd0329c5c51a34cde5e5e9e0860b9e79f715
Zhang Peng <perfecttzp@gmail.com>
2021-04-11 20:20:44 +0800

    fix checkstyle

--- src/test/java/seedu/address/storage/StorageManagerTest.java
+++ src/test/java/seedu/address/storage/StorageManagerTest.java
@@ -5,7 +5,6 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static seedu.address.testutil.TypicalAppObjects.getTypicalDoctorRecords;
 import static seedu.address.testutil.TypicalAppObjects.getTypicalPatientRecords;
 
-
 import java.nio.file.Path;
 
 import org.junit.jupiter.api.BeforeEach;
