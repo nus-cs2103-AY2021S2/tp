abc0407e89fd6aaa16dfc1d0783bcbf9e28c6d88
eksinyue <xinyuehehxy@gmail.com>
2021-04-11 23:53:13 +0800

    Add AddFrCommandParser test

---
+++ src/test/java/seedu/budgetbaby/logic/parser/AddFrCommandParserTest.java
@@ -0,0 +1,30 @@
+package seedu.budgetbaby.logic.parser;
+
+import static seedu.budgetbaby.logic.parser.CommandParserTestUtil.assertParseSuccess;
+import static seedu.budgetbaby.testutil.TypicalFinancialRecord.CAIFAN;
+import static seedu.budgetbaby.testutil.TypicalFinancialRecord.LUNCH;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.budgetbaby.logic.commands.AddFrCommand;
+
+public class AddCommandParserTest {
+
+    private AddFrCommandParser parser = new AddFrCommandParser();
+
+    @Test
+    public void parse_validArgs_returnsAddFrCommand() {
+        assertParseSuccess(parser, " d/Lunch a/10.0 t/01-01-2021", new AddFrCommand(LUNCH));
+    }
+
+    @Test
+    public void parse_trailiingSpaces_returnsAddFrCommand() {
+        assertParseSuccess(parser, " d/ Lunch a/ 10.0 t/ 01-01-2021", new AddFrCommand(LUNCH));
+    }
+
+    @Test
+    public void parse_withTags_returnsAddFrCommand() {
+        assertParseSuccess(parser, " d/Caifan a/4.50 t/11-02-2021 c/food", new AddFrCommand(CAIFAN));
+    }
+
+}
---
+++ src/test/java/seedu/budgetbaby/logic/parser/CommandParserTestUtil.java
@@ -0,0 +1,39 @@
+package seedu.budgetbaby.logic.parser;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import seedu.budgetbaby.logic.commands.BudgetBabyCommand;
+import seedu.budgetbaby.logic.parser.exceptions.ParseException;
+
+/**
+ * Contains helper methods for testing command parsers.
+ */
+public class CommandParserTestUtil {
+
+    /**
+     * Asserts that the parsing of {@code userInput} by {@code parser} is successful and the command created
+     * equals to {@code expectedCommand}.
+     */
+    public static void assertParseSuccess(BudgetBabyCommandParser parser,
+                                          String userInput, BudgetBabyCommand expectedCommand) {
+        try {
+            BudgetBabyCommand command = parser.parse(userInput);
+            assertEquals(expectedCommand, command);
+        } catch (ParseException pe) {
+            throw new IllegalArgumentException("Invalid userInput.", pe);
+        }
+    }
+
+    /**
+     * Asserts that the parsing of {@code userInput} by {@code parser} is unsuccessful and the error message
+     * equals to {@code expectedMessage}.
+     */
+    public static void assertParseFailure(BudgetBabyCommandParser parser, String userInput, String expectedMessage) {
+        try {
+            parser.parse(userInput);
+            throw new AssertionError("The expected ParseException was not thrown.");
+        } catch (ParseException pe) {
+            assertEquals(expectedMessage, pe.getMessage());
+        }
+    }
+}
