2ecb95c49b2e2f9c75cc93aee9e272d97ee757d4
TeoHoeKeat <TeoHoeKeat@gmail.com>
2021-04-11 21:48:57 +0800

    Add testcases to VersionTest

--- src/test/java/seedu/dictionote/commons/core/VersionTest.java
+++ src/test/java/seedu/dictionote/commons/core/VersionTest.java
@@ -94,6 +94,11 @@ public class VersionTest {
         another = new Version(2, 15, 0, false);
         assertTrue(one.compareTo(another) < 0);
 
+        // Tests not early access vs early access on same version number
+        one = new Version(2, 15, 0, false);
+        another = new Version(2, 15, 0, true);
+        assertTrue(one.compareTo(another) > 0);
+
         // Tests early access lower version vs not early access higher version compare by version number first
         one = new Version(2, 15, 0, true);
         another = new Version(2, 15, 5, false);
@@ -126,6 +131,15 @@ public class VersionTest {
         one = new Version(100, 191, 275, true);
         another = new Version(100, 191, 275, true);
         assertTrue(one.equals(another));
+    }
+
+    @Test
+    public void versionComparable_validVersion_equalIsNotCorrect() {
+        Version one;
+
+        one = new Version(0, 0, 0, false);
+        assertTrue(!(one.equals(null)));
+        assertTrue(!(one.equals("test")));
     }
 
     private void verifyVersionParsedCorrectly(String versionString,
