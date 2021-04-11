52df39bc995c09fca1d76f8bd8a7b7405854c362
github-amanda <57126871+github-amanda@users.noreply.github.com>
2021-04-12 00:02:27 +0800

    Add test for empty list in ClearLocationCommandTest

--- src/test/java/seedu/storemando/logic/commands/ClearLocationCommandTest.java
+++ src/test/java/seedu/storemando/logic/commands/ClearLocationCommandTest.java
@@ -2,12 +2,14 @@ package seedu.storemando.logic.commands;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.storemando.logic.commands.CommandTestUtil.assertCommandFailure;
 import static seedu.storemando.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.storemando.model.Model.PREDICATE_SHOW_ALL_ITEMS;
 import static seedu.storemando.testutil.TypicalItems.getTypicalStoreMando;
 
 import org.junit.jupiter.api.Test;
 
+import seedu.storemando.commons.core.Messages;
 import seedu.storemando.model.Model;
 import seedu.storemando.model.ModelManager;
 import seedu.storemando.model.UserPrefs;
@@ -15,6 +17,13 @@ import seedu.storemando.model.item.LocationContainsPredicate;
 
 public class ClearLocationCommandTest {
 
+    @Test
+    public void execute_emptyStoreMando_failure() {
+        Model model = new ModelManager();
+
+        assertCommandFailure(new ClearAllCommand(), model, Messages.MESSAGE_NO_ITEM_IN_LIST);
+    }
+    
     @Test
     public void execute_nonEmptyStoreMando_clearSpecificLocationSuccess() {
         Model model = new ModelManager(getTypicalStoreMando(), new UserPrefs());
