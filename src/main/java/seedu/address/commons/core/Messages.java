c6d931bee6a488970973f183edfb665d46ef9aad
chewterence <chewterence@hotmail.com>
2021-04-11 12:39:26 +0800

    Update message shown when invalid index for pool is given

--- src/main/java/seedu/address/commons/core/Messages.java
+++ src/main/java/seedu/address/commons/core/Messages.java
@@ -9,8 +9,7 @@ public class Messages {
     public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Invalid command format! \n%1$s";
     public static final String MESSAGE_INVALID_PASSENGER_DISPLAYED_INDEX = "One of the passenger index"
             + " provided is invalid";
-    public static final String MESSAGE_INVALID_POOL_DISPLAYED_INDEX = "One of the pool index"
-            + " provided is invalid";
+    public static final String MESSAGE_INVALID_POOL_DISPLAYED_INDEX = "The pool index provided is invalid";
     public static final String MESSAGE_PASSENGER_LISTED_OVERVIEW = "%1$d passengers listed!";
     public static final String MESSAGE_POOLS_LISTED_OVERVIEW = "%1$d pools listed!";
 
