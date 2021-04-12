22c9e7b4acb2326e762b8a125d95c76b330dd89e
yaowei-soc <e0407296@u.nus.edu>
2021-04-12 16:12:46 +0800

    docs: Parenthetical plural for command results

--- src/main/java/seedu/address/logic/commands/EditCommand.java
+++ src/main/java/seedu/address/logic/commands/EditCommand.java
@@ -58,10 +58,10 @@ public class EditCommand extends Command {
             + PREFIX_PHONE + " 91234567 "
             + PREFIX_EMAIL + " johndoe@example.com";
 
-    public static final String MESSAGE_EDIT_PERSON_SUCCESS = "Edited Person: %1$s";
-    public static final String MESSAGE_EDIT_PERSONS_SUCCESS = "Edited %1$d person";
+    public static final String MESSAGE_EDIT_PERSON_SUCCESS = "Edited person: %1$s";
+    public static final String MESSAGE_EDIT_PERSONS_SUCCESS = "Edited %1$d person(s)";
     public static final String MESSAGE_NOT_EDITED = "At least one field to edit must be provided";
-    public static final String MESSAGE_NO_SELECTED = "No selected person to edit";
+    public static final String MESSAGE_NO_SELECTED = "No selected person(s) to edit";
     public static final String MESSAGE_NO_PERSON = "No person(s) to edit";
     public static final String MESSAGE_DUPLICATE_PERSON = "This person already exists in the address book";
     public static final String MESSAGE_DUPLICATE_PERSON_BULK =
