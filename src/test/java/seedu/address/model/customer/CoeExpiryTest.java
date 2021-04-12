f2aa4fc09b6066947964c9862773e904b801c5c8
Oh Jun Ming <e0398574@u.nus.edu>
2021-04-12 19:45:12 +0800

    correct import statment order

--- src/test/java/seedu/address/model/customer/CoeExpiryTest.java
+++ src/test/java/seedu/address/model/customer/CoeExpiryTest.java
@@ -1,5 +1,6 @@
 package seedu.address.model.customer;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.testutil.Assert.assertThrows;
