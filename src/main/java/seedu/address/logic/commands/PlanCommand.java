16eb38f283722f84a9aeefca7b769ef3227ebb28
HuaiZe <kohhuaize@hotmail.com>
2021-04-12 18:52:53 +0800

    Make plan command's usage message more compact

--- src/main/java/seedu/address/logic/commands/PlanCommand.java
+++ src/main/java/seedu/address/logic/commands/PlanCommand.java
@@ -16,15 +16,13 @@ public abstract class PlanCommand extends Command {
 
     public static final String MESSAGE_USAGE = COMMAND_WORD
             + ": Adds/remove an insurance plan of the client identified by the "
-            + "index number used in the displayed client list.\n\n"
-            + "Add plan:\n"
-            + "Parameters: INDEX (must be a positive integer) "
-            + PREFIX_INSURANCE + "PLAN_NAME $PREMIUM (must be a positive integer)\n"
-            + "Example: " + COMMAND_WORD + " 3 " + PREFIX_INSURANCE + "Protecc $4000\n\n"
-            + "Remove plan:\n"
-            + "Parameters: INDEX (must be a positive integer) "
+            + "index number used in the displayed client list.\n"
+            + "Parameters for adding a plan: plan INDEX (must be a positive integer) "
+            + PREFIX_INSURANCE + "PLAN_NAME $PREMIUM (must be a positive amount with up to 2 decimal places)\n"
+            + "    Example: " + COMMAND_WORD + " 3 " + PREFIX_INSURANCE + "Protecc $4000.50\n"
+            + "Parameters for removing a plan: plan INDEX (must be a positive integer) "
             + PREFIX_CLEAR + "PLAN_INDEX (must be a positive integer)\n"
-            + "Example: " + COMMAND_WORD + " 2 " + PREFIX_CLEAR + "3";
+            + "    Example: " + COMMAND_WORD + " 2 " + PREFIX_CLEAR + "3";
 
     private final Index targetIndex;
 
