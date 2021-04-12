9e83fc0af6ea9737d97961548340167a3a31dc41
SoonKeatNeo <neo@soonkeat.sg>
2021-04-12 10:47:01 +0800

    Update tests for user class

--- src/test/java/seedu/address/model/user/UserTest.java
+++ src/test/java/seedu/address/model/user/UserTest.java
@@ -1,8 +1,18 @@
 package seedu.address.model.user;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.testutil.Assert.assertThrows;
 
 import org.junit.jupiter.api.Test;
+
+import seedu.address.model.food.Food;
+import seedu.address.model.food.FoodIntakeList;
+
+
 
 public class UserTest {
     public static final double INIT_USER_WEIGHT = 70;
@@ -20,4 +30,20 @@ public class UserTest {
         assertThrows(NullPointerException.class, ()
             -> new User(bmi, null, null, age, gender, idealWeight));
     }
+
+    @Test
+    public void equals_checkEquality() {
+        Bmi bmi = new Bmi(INIT_USER_WEIGHT, INIT_USER_HEIGHT);
+        Gender gender = new Gender(INIT_USER_GENDER);
+        Age age = new Age(INIT_USER_AGE);
+        IdealWeight idealWeight = new IdealWeight(INIT_USER_IDEAL_WEIGHT);
+        FoodIntakeList foodIntakeList = new FoodIntakeList();
+        List<Food> foodList = new ArrayList<Food>();
+        User user = new User(bmi, foodList, foodIntakeList, age, gender,idealWeight);
+        assertTrue(user.equals(new User(bmi, foodList, foodIntakeList, age, gender, idealWeight)));
+
+        Age newAge = new Age(15);
+        assertFalse(user.equals(new User(bmi, foodList, foodIntakeList, newAge, gender, idealWeight)));
+    }
+
 }
