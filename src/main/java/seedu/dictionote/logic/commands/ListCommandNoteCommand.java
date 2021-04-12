6ee82889cdd9503bbe3a1316467f517b21667056
TeoHoeKeat <TeoHoeKeat@gmail.com>
2021-04-12 20:06:10 +0800

    Update ListCommand and CommandDetails to fit UG

--- src/main/java/seedu/dictionote/logic/commands/ListCommandNoteCommand.java
+++ src/main/java/seedu/dictionote/logic/commands/ListCommandNoteCommand.java
@@ -12,21 +12,22 @@ public class ListCommandNoteCommand extends Command {
     public static final String SHOWING_LIST_COMMAND_MESSAGE =
         "Note Command Details (enter in the command for usage details)\n"
         + AddNoteCommand.COMMAND_WORD + " : Add a new note \n"
+        + ListNoteCommand.COMMAND_WORD + " : List every existing note \n"
+        + ShowNoteCommand.COMMAND_WORD + " : Show a specific note \n"
         + EditNoteCommand.COMMAND_WORD + " : Edit an existing note \n"
         + EditModeCommand.COMMAND_WORD + " : Enter Edit Mode to edit the shown note\n"
-        + ShowNoteCommand.COMMAND_WORD + " : Show a specific note \n"
-        + DeleteNoteCommand.COMMAND_WORD + " : Deletes an existing note  \n"
-        + ListNoteCommand.COMMAND_WORD + " : Lists every existing note \n"
-        + SortNoteCommand.COMMAND_WORD + " : Sorts every note alphabetically  \n"
-        + SortNoteByTimeCommand.COMMAND_WORD + " : Sorts every note by the last edit time \n"
-        + MarkAsDoneNoteCommand.COMMAND_WORD + " : Marks a note as done \n"
-        + MarkAsUndoneNoteCommand.COMMAND_WORD + " : Marks a note as undone \n"
-        + MarkAllAsUndoneNoteCommand.COMMAND_WORD + " : Marks all note as undone \n"
-        + ConvertTxtNoteCommand.COMMAND_WORD + " : Converts a note into a text file \n"
-        + EditModeQuitCommand.COMMAND_WORD + " : Exits Edit Mode \n"
-        + EditModeSaveCommand.COMMAND_WORD + " : Saves the changes made in Edit Mode and also quits the Edit Mode \n"
+        + EditModeQuitCommand.COMMAND_WORD + " : Exit Edit Mode \n"
+        + EditModeSaveCommand.COMMAND_WORD + " : Save the changes made in Edit Mode and also quits the Edit Mode \n"
         + FindNoteCommand.COMMAND_WORD + " : Find notes matching to a query \n"
-        + MergeNoteCommand.COMMAND_WORD + " : Merge two notes into one note \n";
+        + DeleteNoteCommand.COMMAND_WORD + " : Delete an existing note  \n"
+        + SortNoteCommand.COMMAND_WORD + " : Sort every note alphabetically  \n"
+        + SortNoteByTimeCommand.COMMAND_WORD + " : Sort every note by the last edit time \n"
+        + MarkAsDoneNoteCommand.COMMAND_WORD + " : Mark a note as done \n"
+        + MarkAsUndoneNoteCommand.COMMAND_WORD + " : Mark a note as undone \n"
+        + MarkAllAsUndoneNoteCommand.COMMAND_WORD + " : Mark all note as undone \n"
+        + MergeNoteCommand.COMMAND_WORD + " : Merge two notes into one note \n"
+        + ConvertTxtNoteCommand.COMMAND_WORD + " : Convert a note into a text file";
+
 
     @Override
     public CommandResult execute(Model model) {
