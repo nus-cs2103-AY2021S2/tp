024c64ad1bc6a2d82f240be7f2995fb7adb86537
Jason <jasontay9645@gmail.com>
2021-04-12 23:53:54 +0800

    Update EditCommandTest.java

--- src/test/java/seedu/address/logic/commands/EditCommandTest.java
+++ src/test/java/seedu/address/logic/commands/EditCommandTest.java
@@ -74,28 +74,6 @@ public class EditCommandTest {
     }
 
     @Test
-    public void execute_someFieldsSpecifiedWithPolicy_success() {
-        Index indexLastPerson = Index.fromOneBased(model.getFilteredPersonList().size());
-        Person lastPerson = model.getFilteredPersonList().get(indexLastPerson.getZeroBased());
-
-        PersonBuilder personInList = new PersonBuilder(lastPerson);
-        Person editedPerson = personInList.withName(VALID_NAME_BOB).withPhone(VALID_PHONE_BOB)
-                .withTags(VALID_TAG_HUSBAND).withPolicies(VALID_POLICY_ID).build();
-
-        EditPersonDescriptor descriptor = new EditPersonDescriptorBuilder().withName(VALID_NAME_BOB)
-                .withPhone(VALID_PHONE_BOB).withTags(VALID_TAG_HUSBAND).withPolicies(VALID_POLICY_ID).build();
-        EditCommand editCommand = new EditCommand(indexLastPerson, descriptor, EditCommand.EditPolicyMode.REPLACE);
-
-        String expectedMessage = String.format(EditCommand.MESSAGE_EDIT_PERSON_SUCCESS, editedPerson);
-
-        Model expectedModel = new ModelManager(new AddressBook(model.getAddressBook()), new UserPrefs(),
-                new ShortcutLibrary());
-        expectedModel.setPerson(lastPerson, editedPerson);
-
-        assertCommandSuccess(editCommand, model, expectedMessage, expectedModel);
-    }
-
-    @Test
     public void execute_noFieldSpecifiedUnfilteredList_success() {
         EditCommand editCommand = new EditCommand(INDEX_FIRST_PERSON, new EditPersonDescriptor(),
                 EditCommand.EditPolicyMode.REPLACE);
