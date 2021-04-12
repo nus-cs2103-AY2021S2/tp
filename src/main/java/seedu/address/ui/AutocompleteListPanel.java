35b6453a36e670e33bedfa5d43090f88cf72fd16
Boon Ji <tanboonji@hotmail.com>
2021-04-12 18:44:50 +0800

    fix: Allow auto complete list to be clickable and scrollable

--- src/main/java/seedu/address/ui/AutocompleteListPanel.java
+++ src/main/java/seedu/address/ui/AutocompleteListPanel.java
@@ -30,7 +30,6 @@ public class AutocompleteListPanel extends UiPart<Region> {
         super(FXML);
         autocompleteListView.setItems(new ImmutableObservableList<>());
         autocompleteListView.setCellFactory(listView -> new AutocompleteListViewCell());
-        autocompleteListView.setMouseTransparent(true);
     }
 
     public void updateList(ObservableList<String> list) {
