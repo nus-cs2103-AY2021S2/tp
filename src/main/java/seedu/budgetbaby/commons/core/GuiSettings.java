62d7e9e1b223532907384150f126c9c544306334
deyixtan <deyixtan@gmail.com>
2021-04-12 10:01:15 +0800

    Fix default screen resolution

--- src/main/java/seedu/budgetbaby/commons/core/GuiSettings.java
+++ src/main/java/seedu/budgetbaby/commons/core/GuiSettings.java
@@ -10,8 +10,8 @@ import java.util.Objects;
  */
 public class GuiSettings implements Serializable {
 
-    private static final double DEFAULT_HEIGHT = 750;
-    private static final double DEFAULT_WIDTH = 650;
+    private static final double DEFAULT_HEIGHT = 650;
+    private static final double DEFAULT_WIDTH = 750;
     private static final boolean DEFAULT_CLI_VISIBILITY = true;
 
     private final double windowWidth;
