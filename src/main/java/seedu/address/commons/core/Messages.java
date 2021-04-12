c4baaa5224f95afce85c177ef4b748eca8ea0209
goatygoatygoat <52666150+goatygoatygoat@users.noreply.github.com>
2021-04-12 20:03:30 +0800

    Make requested changes

--- src/main/java/seedu/address/commons/core/Messages.java
+++ src/main/java/seedu/address/commons/core/Messages.java
@@ -7,7 +7,6 @@ public class Messages {
     public static final String MESSAGE_UNKNOWN_COMMAND = "Unknown command";
     public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Invalid command format! \n%1$s";
     public static final String MESSAGE_INVALID_ENTRY_INDEX = "The entry index provided is invalid";
-    public static final String MESSAGE_DELETE_ENTRY_SUCCESS = "Deleted Entry: %1$s";
     public static final String MESSAGE_INVALID_CONTACT_DISPLAYED_INDEX = "The contact index provided is invalid";
     public static final String MESSAGE_ENTRY_START_DATE_IN_PAST = "Time travel is not allowed! Please provide a valid"
             + " date time!";
@@ -17,13 +16,6 @@ public class Messages {
     public static final String MESSAGE_ENTRIES_LISTED_OVERVIEW = "%1$d entries listed!";
     public static final String MESSAGE_OVERLAPPING_ENTRY = "This entry has dates that overlap with other"
             + " existing entries!";
-    public static final String MESSAGE_EDIT_CONTACT_SUCCESS = "Edited Contact: %1$s";
     public static final String MESSAGE_DUPLICATE_CONTACT = "The contact provided already exists in the address book.";
-    public static final String MESSAGE_EDIT_ENTRY_SUCCESS = "Edited entry: %1$s";
     public static final String MESSAGE_NOT_EDITED = "At least one field to edit must be provided.";
-
-    public static final String MESSAGE_FREE = "You're free!";
-    public static final String MESSAGE_NOT_FREE = "Sorry, you're not free. Entries occupying that time interval "
-            + "listed below!";
-
 }
--- src/main/java/seedu/address/model/contact/UniqueContactList.java
+++ src/main/java/seedu/address/model/contact/UniqueContactList.java
@@ -16,12 +16,9 @@ import seedu.address.model.contact.exceptions.DuplicateContactException;
  * A list of contacts that enforces uniqueness between its elements and does not allow nulls.
  * A contact is considered unique by comparing using {@code Contact#isSameContact(Contact)}.
  *
- * As such, adding and updating of contacts uses Contact#isSameContact(Contact) for equality
+ * As such, adding, deleting and updating of contacts uses Contact#isSameContact(Contact) for equality
  * so as to ensure that the contact being added or updated is unique in terms of identity
  * in the UniqueContactList.
- *
- * However, the removal of a contact uses Contact#equals(Object) so as to
- * ensure that the contact with exactly the same fields will be removed.
  *
  * Supports a minimal set of list operations.
  *
