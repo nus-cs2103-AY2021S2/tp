e5ac92f52f632f40f037994c07718dfc9a020d73
kumsssss <>
2021-04-12 20:41:04 +0800

    edit message constraints for item attributes

--- src/main/java/seedu/storemando/model/item/Location.java
+++ src/main/java/seedu/storemando/model/item/Location.java
@@ -9,7 +9,7 @@ import static seedu.storemando.commons.util.AppUtil.checkArgument;
  */
 public class Location {
 
-    public static final String MESSAGE_CONSTRAINTS = "Locations can take any values, and it should not be blank";
+    public static final String MESSAGE_CONSTRAINTS = "Locations can take any values, and it should not be blank.";
 
     /*
      * The first character of the location must not be a whitespace,
--- src/main/java/seedu/storemando/model/tag/Tag.java
+++ src/main/java/seedu/storemando/model/tag/Tag.java
@@ -9,7 +9,7 @@ import static seedu.storemando.commons.util.AppUtil.checkArgument;
  */
 public class Tag {
 
-    public static final String MESSAGE_CONSTRAINTS = "Tags names should be alphanumeric and contain only a single word";
+    public static final String MESSAGE_CONSTRAINTS = "Tags names should be alphanumeric and contain only a single word.";
     public static final String VALIDATION_REGEX = "\\p{Alnum}+";
 
     public final String tagName;
