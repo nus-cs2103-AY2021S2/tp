1ee73d5a979a70581b9d73fcca2a6d402fc077ba
rajobasu <rajobasu@gmail.com>
2021-04-12 21:06:05 +0800

    Fix find command

--- src/main/java/seedu/address/logic/commands/FindCommand.java
+++ src/main/java/seedu/address/logic/commands/FindCommand.java
@@ -15,12 +15,12 @@ public class FindCommand extends Command {
     public static final String COMMAND_WORD = "find";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds all customers whose names contain any of "
-        + "the specified keywords (case-insensitive) and displays them as a list with index numbers.\n"
+        + "the specified keywords (case-sensitive) and displays them as a list with index numbers.\n"
         + "Parameters: KEYWORD [MORE_KEYWORDS]...\n"
-        + "Example: " + COMMAND_WORD + " \' find n/Raj /AND [ p/9876 /OR /NOT b/1998 ] \'";
+        + "Example: " + COMMAND_WORD + " \' find n/Raj /and [ p/9876 /or /not b/1998 ] \'";
 
     public static final String MESSAGE_SYNTAX_ERROR = "There is a syntax error in the given find command : syntax "
-        + "should be a well bracketed sequence like \' find n/Raj /AND [ p/9876 /OR /NOT b/1998 ] \'";
+        + "should be a well bracketed sequence like \' find n/Raj /and [ p/9876 /or /not b/1998 ] \'";
 
     private final FilterCombinator predicate;
 
@@ -36,6 +36,7 @@ public class FindCommand extends Command {
         }
 
         model.updateFilteredCustomerList(predicate);
+
         if (model.getFilteredCustomerList().size() == 1) {
             return new CommandResult(String.format(Messages.MESSAGE_SINGULAR_CUSTOMER_LISTED_OVERVIEW, 1));
         } else {
